c5c8626cc11ef16cd7a552bfa7e5ab41fed9f5ba
John Maheswaran <maheswaran@google.fake>
2014-06-13 16:57:03 -0600

    removed duplicate files for package restructuring

--- java/src/KeyVersion.java
+++
@@ -1,9 +0,0 @@
-/**
- * This class represents a KeyVersion in K2. It is abstract and extended by specific key
- * implementations such as SymmetricKey which is extended by AESKey
- *
- * @author John Maheswaran (maheswaran@google.com)
- */
-public abstract class KeyVersion {
-
-}
--- java/src/SymmetricKey.java
+++
@@ -1,9 +0,0 @@
-/**
- * This class represents a SymmetricKey in K2. It is abstract and extended by specific symmetric key
- * implementations such as AESKey
- *
- * @author John Maheswaran (maheswaran@google.com)
- */
-public abstract class SymmetricKey {
-
-}
