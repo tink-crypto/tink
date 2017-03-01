aeb818d93c5697e74fad38ea5a8a8cb3384bde66
Daniel Bleichenbacher <bleichen@google.com>
2017-03-01 14:23:43 +0000

    Add AesGcm.
    Change-Id: I4d2fd2d0ff50aa8da379c572514d09d96bbd8d86
    ORIGINAL_AUTHOR=Daniel Bleichenbacher <bleichen@google.com>
    
    GitOrigin-RevId: cfc05673e60962bd71305d437640e339ba35cd9b

---
+++ java/src/main/java/com/google/cloud/crypto/tink/subtle/AesGcmJce.java
@@ -0,0 +1,102 @@
+// Copyright 2017 Google Inc.
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //      http://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
+// //
+// ////////////////////////////////////////////////////////////////////////////////
+
+package com.google.cloud.crypto.tink.subtle;
+
+import com.google.cloud.crypto.tink.Aead;
+import com.google.cloud.crypto.tink.Random;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+public final class AesGcmJce implements Aead {
+
+  // All instances of this class use a 12 byte IV and a 16 byte tag.
+  private static int IV_SIZE_IN_BYTES = 12;
+  private static int TAG_SIZE_IN_BYTES = 16;
+
+  private final SecretKeySpec keySpec;
+
+  AesGcmJce(final byte[] key) {
+    keySpec = new SecretKeySpec(key, "AES");
+  }
+
+  private Cipher instance() throws NoSuchAlgorithmException, NoSuchPaddingException {
+    return Cipher.getInstance("AES/GCM/NoPadding");
+  }
+
+  @Override
+  public byte[] encrypt(final byte[] plaintext, final byte[] aad)
+      throws GeneralSecurityException {
+    // Check that ciphertext is not longer than the max. size of a Java array.
+    if (plaintext.length > Integer.MAX_VALUE - IV_SIZE_IN_BYTES - TAG_SIZE_IN_BYTES) {
+      throw new GeneralSecurityException("Plaintext too long");
+    }
+    byte[] ciphertext = new byte[IV_SIZE_IN_BYTES + plaintext.length + TAG_SIZE_IN_BYTES];
+    byte[] iv = Random.randBytes(IV_SIZE_IN_BYTES);
+    System.arraycopy(iv, 0, ciphertext, 0, IV_SIZE_IN_BYTES);
+
+    Cipher cipher = instance();
+    GCMParameterSpec params = new GCMParameterSpec(8 * TAG_SIZE_IN_BYTES, iv);
+    cipher.init(Cipher.ENCRYPT_MODE, keySpec, params);
+    cipher.updateAAD(aad);
+    int written = cipher.doFinal(plaintext, 0, plaintext.length, ciphertext, IV_SIZE_IN_BYTES);
+    return ciphertext;
+  }
+
+  @Override
+  public byte[] decrypt(final byte[] ciphertext, final byte[] aad)
+      throws GeneralSecurityException {
+    if (ciphertext.length < IV_SIZE_IN_BYTES + TAG_SIZE_IN_BYTES) {
+      throw new GeneralSecurityException("ciphertext too short");
+    }
+    GCMParameterSpec params =
+        new GCMParameterSpec(8 * TAG_SIZE_IN_BYTES, ciphertext, 0, IV_SIZE_IN_BYTES);
+    Cipher cipher = instance();
+    cipher.init(Cipher.DECRYPT_MODE, keySpec, params);
+    cipher.updateAAD(aad);
+    return cipher.doFinal(
+        ciphertext, IV_SIZE_IN_BYTES, ciphertext.length - IV_SIZE_IN_BYTES);
+  }
+
+  /**
+   * TODO(bleichen): Would it be possible to implement this in AEAD or some generic sub-class
+   *   of this? Is there a simpler implementation using lambda expressions?
+   *   Shouldn't the caller be responsible to decide how these tasks are scheduled,
+   *   i.e. shouldn't the caller be able to add the task to a threadpool?
+   * TODO(bleichen): Do we have to clone the inputs or can we assume that the inputs remain
+   *   unchanged until the asynchronous encryption terminates?
+   */
+  @Override
+  public Future<byte[]> asyncEncrypt(final byte[] plaintext, final byte[] aad)
+      throws GeneralSecurityException {
+    return Executors.newSingleThreadExecutor().submit(() -> encrypt(plaintext, aad));
+  }
+
+  @Override
+  public Future<byte[]> asyncDecrypt(final byte[] ciphertext, final byte[] aad)
+      throws GeneralSecurityException {
+    return Executors.newSingleThreadExecutor().submit(() -> decrypt(ciphertext, aad));
+  }
+};
---
+++ java/src/test/java/com/google/cloud/crypto/tink/subtle/AesGcmJceTest.java
@@ -0,0 +1,122 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.cloud.crypto.tink.subtle;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
+
+import com.google.cloud.crypto.tink.Random;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import org.junit.Test;
+
+/**
+ * Unit tests for AesGcm
+ * TODO(bleichen): Add more tests.
+ *   - test vectors for compatibility.
+ *   - maybe add NIST style verification.
+ *   - more tests for asynchronous encryption.
+ *   - tests with long ciphertexts (e.g. BC had a bug with messages of size 8k or longer)
+ *   - check that IVs are distinct.
+ *   - more sizes for the AAD
+ *   - modify the AAD.
+ */
+public class AesGcmJceTest {
+
+  @Test
+  public void testEncryptDecrypt() throws Exception {
+    final int KEY_SIZE = 16;
+    byte aad[] = new byte[] {1, 2, 3};
+    byte key[] = Random.randBytes(KEY_SIZE);
+    AesGcmJce gcm = new AesGcmJce(key);
+    for (int messageSize = 0; messageSize < 75; messageSize++) {
+      byte[] message = Random.randBytes(messageSize);
+      byte[] ciphertext = gcm.encrypt(message, aad);
+      byte[] decrypted = gcm.decrypt(ciphertext, aad);
+      assertArrayEquals(message, decrypted);
+    }
+  }
+
+  @Test
+  public void testModifyCiphertext() throws Exception {
+    final int KEY_SIZE = 16;
+    byte aad[] = new byte[] {1, 2, 3};
+    byte key[] = Random.randBytes(KEY_SIZE);
+    byte message[] = Random.randBytes(32);
+    AesGcmJce gcm = new AesGcmJce(key);
+    byte[] ciphertext = gcm.encrypt(message, aad);
+
+    // Flipping bits
+    for (int b = 0; b < ciphertext.length; b++) {
+      for (int bit = 0; bit < 8; bit++) {
+        byte[] modified = Arrays.copyOf(ciphertext, ciphertext.length);
+        modified[b] ^= (byte)(1 << bit);
+        try {
+          byte[] unused = gcm.decrypt(modified, aad);
+          fail("Decrypting modified ciphertext should fail");
+        } catch (GeneralSecurityException ex) {
+          // This is expected.
+          // AeadBadTagException is the typical case here,
+          // but we only guarantee GeneralSecurityException for modified
+          // ciphertexts in an AEAD.
+        }
+      }
+    }
+
+    // Truncate the message.
+    for (int length = 0; length < ciphertext.length; length++) {
+      byte[] modified = Arrays.copyOf(ciphertext, length);
+      try {
+        byte[] unused = gcm.decrypt(modified, aad);
+        fail("Decrypting modified ciphertext should fail");
+      } catch (GeneralSecurityException ex) {
+        // This is expected.
+        // This could be a AeadBadTagException when the tag verification
+        // fails or some not yet specified Exception when the ciphertext is too short.
+        // In all cases a GeneralSecurityException or a subclass of it must be thrown.
+      }
+    }
+  }
+
+  @Test
+  /**
+   * A very basic test for asynchronous encryption.
+   */
+  public void testAsync() throws Exception {
+    final int KEY_SIZE = 16;
+    byte aad[] = new byte[] {1, 2, 3};
+    byte key[] = Random.randBytes(KEY_SIZE);
+    AesGcmJce gcm = new AesGcmJce(key);
+    byte[] plaintext = Random.randBytes(20);
+
+    byte[] ciphertext = gcm.asyncEncrypt(plaintext, aad).get();
+    byte[] decrypted = gcm.asyncDecrypt(ciphertext, aad).get();
+    assertArrayEquals(plaintext, decrypted);
+    for (int length = 0; length < ciphertext.length; length++) {
+      byte[] truncated = Arrays.copyOf(ciphertext, length);
+      try {
+        byte[] unused = gcm.asyncDecrypt(truncated, aad).get();
+        fail("Decrypting a truncated ciphertext should fail");
+      } catch (ExecutionException ex) {
+        // The decryption should fail because the ciphertext has been truncated.
+        assertTrue(ex.getCause() instanceof GeneralSecurityException);
+      }
+    }
+  }
+}
