ab3b1b87b5dfd36255d33ac2d364c21a12c4043b
Thai Duong <thaidn@users.noreply.github.com>
2017-05-12 18:55:32 -0700

    Add Kokoro configuration. (#48)

---
+++ kokoro/README.md
@@ -0,0 +1,18 @@
+# Tink presubmit and continous test
+
+![presubmit status](https://storage.googleapis.com/tink-kokoro-build-badges/tink.png)
+
+Tink is testing continously with [Kokoro]
+(https://www.cloudbees.com/sites/default/files/2016-jenkins-world-jenkins_inside_google.pdf)
+an internal deployment of Jenkins at Google.
+
+Kokoro will poll from this repo every 5 minutes and trigger a continuous build
+if there are new commits.
+
+A presubmit build will be triggered for the pull request if one of the
+following conditions is met:
+
+  * Pull request is created by a Googler.
+  * Pull request is attached with a kokoro:run label.
+
+Continuous and presubmit builds are done on Ubuntu.
---
+++ kokoro/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+####################################################################################
+
+# Fail on any error.
+set -e
+
+# Display commands being run.
+set -x
+
+# Build everything.
+# bazel sandbox currently doesn't work with Kokoro's MacOS image.
+bazel build --strategy=CppCompile=standalone --strategy=Turbine=standalone --strategy=ProtoCompile=standalone //...
+
+# Run all tests
+bazel test --strategy=CppCompile=standalone --strategy=Turbine=standalone --strategy=ProtoCompile=standalone --test_output=all //...
+
+exit 0
