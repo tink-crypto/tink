679d23f9e8ecbc45f6184f7d090c83734524ae2d
Haris Andrianakis <candrian@google.com>
2017-06-15 15:31:39 -0700

    Update presubmit.cfg and presubmit.sh for macos_external to use the bazel-0.5.1 from x20.
    Also update the command line flags for obj-c code.
    
    Change-Id: Iea2b24a628edc1bcda7941dd4ad30148244b877f
    ORIGINAL_AUTHOR=Haris Andrianakis <candrian@google.com>
    GitOrigin-RevId: fd950e1cda49d5410c9ac756ab405579c56bed62

--- kokoro/macos_external/presubmit.sh
+++ kokoro/macos_external/presubmit.sh
@@ -25,21 +25,30 @@ set -x
 : "${IOS_SDK_VERSION:=10.2}"
 : "${XCODE_VERSION:=8.2.1}"
 
+BAZEL_BIN="${KOKORO_GFILE_DIR}/bazel-0.5.1-darwin-x86_64"
+
 DISABLE_SANDBOX="--strategy=CppCompile=standalone \
 --strategy=Turbine=standalone --strategy=ProtoCompile=standalone \
 --strategy=GenProto=standalone --strategy=GenProtoDescriptorSet=standalone \
 --strategy=ObjcCompile=standalone"
 
+export DEVELOPER_DIR="/Applications/Xcode_${XCODE_VERSION}.app/Contents/Developer"
+
+# See https://groups.google.com/a/google.com/d/msg/kokoro-users/ddpTkIzLprs/UOiFYVheFwAJ
+chmod +x "${BAZEL_BIN}"
+
 cd github/tink/
 
-# build the iOS targets
-bazel build \
+echo "using bazel binary: ${BAZEL_BIN}"
+${BAZEL_BIN} version
+
+# Build the iOS targets.
+${BAZEL_BIN} build \
   $DISABLE_SANDBOX \
   --compilation_mode=dbg \
   --dynamic_mode=off \
-  --cpu=ios_armv7 \
-  --ios_multi_cpus=armv7,arm64 \
-  --watchos_cpus=armv7k \
+  --cpu=ios_x86_64 \
+  --ios_cpu=x86_64 \
   --experimental_enable_objc_cc_deps \
   --ios_sdk_version="${IOS_SDK_VERSION}" \
   --xcode_version="${XCODE_VERSION}" \
@@ -49,10 +58,11 @@ bazel build \
 
 echo "bazel obj-c passed"
 
+# Build all targets except iOS.
 # bazel sandbox doesn't work with Kokoro's MacOS image, see b/38040081.
-bazel build $DISABLE_SANDBOX -- //... -//objc/... || ( ls -l ; df -h / ; exit 1 )
+${BAZEL_BIN} build $DISABLE_SANDBOX -- //... -//objc/... || ( ls -l ; df -h / ; exit 1 )
 
 echo "bazel c++ / java passed"
 
-# Run all tests.
-bazel test --strategy=TestRunner=standalone --test_output=all -- //... -//objc/...
+# Run all non-iOS tests.
+${BAZEL_BIN} test --strategy=TestRunner=standalone --test_output=all -- //... -//objc/...
