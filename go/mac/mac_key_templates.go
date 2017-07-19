0705c7ba99a376dd9160495ea99248c264e7e570
Thanh Bui <thanhb@google.com>
2017-07-18 18:47:31 -0700

    Add MAC's key templates.
    Change-Id: I2e7a14094657a24594f7e2f17bb10fe4ddb6de58
    ORIGINAL_AUTHOR=Thanh Bui <thanhb@google.com>
    GitOrigin-RevId: 2699ce6cfeb42869db63777108423d13965eab3d

---
+++ go/mac/mac_key_templates.go
@@ -0,0 +1,62 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+package mac
+
+import (
+  "github.com/golang/protobuf/proto"
+  hmacpb "github.com/google/tink/proto/hmac_go_proto"
+  tinkpb "github.com/google/tink/proto/tink_go_proto"
+  commonpb "github.com/google/tink/proto/common_go_proto"
+)
+
+// This file contains pre-generated KeyTemplate for MAC.
+
+// HmacSha256Tag128KeyTemplate is a KeyTemplate for HmacKey with the following
+// parameters:
+//   - Key size: 32 bytes
+//   - Tag size: 16 bytes
+//   - Hash function: SHA256
+func HmacSha256Tag128KeyTemplate() *tinkpb.KeyTemplate {
+  return createHmacKeyTemplate(32, 16, commonpb.HashType_SHA256)
+}
+
+// HmacSha256Tag256KeyTemplate is a KeyTemplate for HmacKey with the following
+// parameters:
+//   - Key size: 32 bytes
+//   - Tag size: 32 bytes
+//   - Hash function: SHA256
+func HmacSha256Tag256KeyTemplate() *tinkpb.KeyTemplate {
+  return createHmacKeyTemplate(32, 32, commonpb.HashType_SHA256)
+}
+
+// createHmacKeyTemplate creates a new KeyTemplate for Hmac using the given parameters.
+func createHmacKeyTemplate(keySize uint32,
+                          tagSize uint32,
+                          hashType commonpb.HashType) *tinkpb.KeyTemplate {
+  params := hmacpb.HmacParams{
+    Hash: hashType,
+    TagSize: tagSize,
+  }
+  format := hmacpb.HmacKeyFormat{
+    Params: &params,
+    KeySize: keySize,
+  }
+  serializedFormat, _ := proto.Marshal(&format)
+  return &tinkpb.KeyTemplate{
+    TypeUrl: HMAC_TYPE_URL,
+    Value: serializedFormat,
+  }
+}
---
+++ go/mac/mac_key_templates_test.go
@@ -0,0 +1,56 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+package mac_test
+
+import (
+  "fmt"
+  "testing"
+  "github.com/google/tink/go/mac/mac"
+  "github.com/golang/protobuf/proto"
+  hmacpb "github.com/google/tink/proto/hmac_go_proto"
+  tinkpb "github.com/google/tink/proto/tink_go_proto"
+  commonpb "github.com/google/tink/proto/common_go_proto"
+)
+
+func TestTemplates(t *testing.T) {
+  template := mac.HmacSha256Tag128KeyTemplate()
+  if err := checkTemplate(template, 32, 16, commonpb.HashType_SHA256); err != nil {
+    t.Errorf("incorrect HmacSha256Tag128KeyTemplate: %s", err)
+  }
+  template = mac.HmacSha256Tag256KeyTemplate()
+  if err := checkTemplate(template, 32, 32, commonpb.HashType_SHA256); err != nil {
+    t.Errorf("incorrect HmacSha256Tag256KeyTemplate: %s", err)
+  }
+}
+
+func checkTemplate(template *tinkpb.KeyTemplate,
+                  keySize uint32,
+                  tagSize uint32,
+                  hashType commonpb.HashType) error {
+  if template.TypeUrl != mac.HMAC_TYPE_URL {
+    return fmt.Errorf("TypeUrl is incorrect")
+  }
+  format := new(hmacpb.HmacKeyFormat)
+  if err := proto.Unmarshal(template.Value, format); err != nil {
+    return fmt.Errorf("unable to unmarshal serialized key format")
+  }
+  if format.KeySize != keySize ||
+      format.Params.Hash != hashType ||
+      format.Params.TagSize != tagSize {
+    return fmt.Errorf("KeyFormat is incorrect")
+  }
+  return nil
+}
