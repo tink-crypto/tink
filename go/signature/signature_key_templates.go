f1cb9cbc99c6ed110c80d799e15aa8046c4ed1df
Thanh Bui <thanhb@google.com>
2017-07-27 19:52:12 -0700

    Add key templates for ECDSA.
    Change-Id: Iea30a0408951bcb4afba6cab91cb27f8d7738436
    ORIGINAL_AUTHOR=Thanh Bui <thanhb@google.com>
    GitOrigin-RevId: 28e475a8d0cdf7ba0e68e4e48b999062f538c92d

---
+++ go/signature/signature_key_templates.go
@@ -0,0 +1,72 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+package signature
+
+import (
+  "github.com/google/tink/go/util/util"
+  "github.com/golang/protobuf/proto"
+  ecdsapb "github.com/google/tink/proto/ecdsa_go_proto"
+  tinkpb "github.com/google/tink/proto/tink_go_proto"
+  commonpb "github.com/google/tink/proto/common_go_proto"
+)
+
+// This file contains pre-generated KeyTemplate for PublicKeySign and PublicKeyVerify.
+// One can use these templates to generate new Keyset, using utility functions
+// in either cleartext_keyset_handle or encrypted_keyset_handle.
+
+// EcdsaP256KeyTemplate is a KeyTemplate of EcdsaPrivateKey with the following parameters:
+//   - Hash function: SHA256
+//   - Curve: NIST P-256
+//   - Signature encoding: DER
+func EcdsaP256KeyTemplate() *tinkpb.KeyTemplate {
+  return createEcdsaKeyTemplate(commonpb.HashType_SHA256,
+                                commonpb.EllipticCurveType_NIST_P256,
+                                ecdsapb.EcdsaSignatureEncoding_DER)
+}
+
+// EcdsaP384KeyTemplate is a KeyTemplate of EcdsaPrivateKey with the following parameters:
+//   - Hash function: SHA512
+//   - Curve: NIST P-384
+//   - Signature encoding: DER
+func EcdsaP384KeyTemplate() *tinkpb.KeyTemplate {
+  return createEcdsaKeyTemplate(commonpb.HashType_SHA512,
+                                commonpb.EllipticCurveType_NIST_P384,
+                                ecdsapb.EcdsaSignatureEncoding_DER)
+}
+
+// EcdsaP521KeyTemplate is a KeyTemplate of EcdsaPrivateKey with the following parameters:
+//   - Hash function: SHA512
+//   - Curve: NIST P-521
+//   - Signature encoding: DER
+func EcdsaP521KeyTemplate() *tinkpb.KeyTemplate {
+  return createEcdsaKeyTemplate(commonpb.HashType_SHA512,
+                                commonpb.EllipticCurveType_NIST_P521,
+                                ecdsapb.EcdsaSignatureEncoding_DER)
+}
+
+// createEcdsaKeyTemplate creates a KeyTemplate containing a EcdasKeyFormat
+// with the given parameters.
+func createEcdsaKeyTemplate(hashType commonpb.HashType,
+                            curve commonpb.EllipticCurveType,
+                            encoding ecdsapb.EcdsaSignatureEncoding) *tinkpb.KeyTemplate {
+  params := util.NewEcdsaParams(hashType, curve, encoding)
+  format := util.NewEcdsaKeyFormat(params)
+  serializedFormat, _ := proto.Marshal(format)
+  return &tinkpb.KeyTemplate{
+    TypeUrl: ECDSA_SIGN_TYPE_URL,
+    Value: serializedFormat,
+  }
+}
---
+++ go/signature/signature_key_templates_test.go
@@ -0,0 +1,82 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+package signature_test
+
+import (
+  "fmt"
+  "testing"
+  "github.com/google/tink/go/signature/signature"
+  "github.com/golang/protobuf/proto"
+  ecdsapb "github.com/google/tink/proto/ecdsa_go_proto"
+  tinkpb "github.com/google/tink/proto/tink_go_proto"
+  commonpb "github.com/google/tink/proto/common_go_proto"
+)
+
+func TestEcdsaKeyTemplates(t *testing.T) {
+  var template *tinkpb.KeyTemplate
+  var err error
+  // ECDSA P-256
+  template = signature.EcdsaP256KeyTemplate()
+  err = checkEcdsaKeyTemplate(template,
+                              commonpb.HashType_SHA256,
+                              commonpb.EllipticCurveType_NIST_P256,
+                              ecdsapb.EcdsaSignatureEncoding_DER)
+  if err != nil {
+    t.Errorf("invalid ECDSA P-256 key template: %s", err)
+  }
+  // ECDSA P-384
+  template = signature.EcdsaP384KeyTemplate()
+  err = checkEcdsaKeyTemplate(template,
+                              commonpb.HashType_SHA512,
+                              commonpb.EllipticCurveType_NIST_P384,
+                              ecdsapb.EcdsaSignatureEncoding_DER)
+  if err != nil {
+    t.Errorf("invalid ECDSA P-384 key template: %s", err)
+  }
+  // ECDSA P-521
+  template = signature.EcdsaP521KeyTemplate()
+  err = checkEcdsaKeyTemplate(template,
+                              commonpb.HashType_SHA512,
+                              commonpb.EllipticCurveType_NIST_P521,
+                              ecdsapb.EcdsaSignatureEncoding_DER)
+  if err != nil {
+    t.Errorf("invalid ECDSA P-521 key template: %s", err)
+  }
+}
+
+func checkEcdsaKeyTemplate(template *tinkpb.KeyTemplate,
+                            hashType commonpb.HashType,
+                            curve commonpb.EllipticCurveType,
+                            encoding ecdsapb.EcdsaSignatureEncoding) error {
+  if template.TypeUrl != signature.ECDSA_SIGN_TYPE_URL {
+    return fmt.Errorf("incorrect typeurl: expect %s, got %s", signature.ECDSA_SIGN_TYPE_URL, template.TypeUrl)
+  }
+  format := new(ecdsapb.EcdsaKeyFormat)
+  if err := proto.Unmarshal(template.Value, format); err != nil {
+    return fmt.Errorf("cannot unmarshak key format: %s", err)
+  }
+  params := format.Params
+  if params.HashType != hashType {
+    return fmt.Errorf("incorrect hash type: expect %d, got %d", hashType, params.HashType)
+  }
+  if params.Curve != curve {
+    return fmt.Errorf("incorrect curve: expect %d, got %d", curve, params.Curve)
+  }
+  if params.Encoding != encoding {
+    return fmt.Errorf("incorrect encoding: expect %d, got %d", encoding, params.Encoding)
+  }
+  return nil
+}
