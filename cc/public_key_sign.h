db42f81a18c57214968d77d8f9c485144e198a5b
Thai Duong <thaidn@google.com>
2017-10-22 09:22:28 -0700

    Adding PublicKeySign and PublicKeyVerify interface in C++.
    This just adds the interface, actual impls will be added
    after 1.1.0 release.
    
    Change-Id: I9ddb607220b5f1115a9d3b51b13b3cab98e8b470
    ORIGINAL_AUTHOR=Thai Duong <thaidn@google.com>
    GitOrigin-RevId: d1c2f2b46220a6a6392aa05e66d21cda909840c7

---
+++ cc/public_key_sign.h
@@ -0,0 +1,46 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef PUBLIC_KEY_SIGN_H_
+#define PUBLIC_KEY_SIGN_H_
+
+#include "absl/strings/string_view.h"
+#include "cc/util/statusor.h"
+
+namespace crypto {
+namespace tink {
+
+///////////////////////////////////////////////////////////////////////////////
+// Interface for public key signing.
+// Digital Signatures provide functionality of signing data and verification of
+// the signatures. They are represented by a pair of primitives (interfaces)
+// 'PublicKeySign' for signing of data, and 'PublicKeyVerify' for verification
+// of signatures. Implementations of these interfaces are secure against
+// adaptive chosen-message attacks. Signing data ensures the authenticity and
+// the integrity of that data, but not its secrecy.
+class PublicKeySign {
+ public:
+  // Computes the signature for 'data'.
+  virtual crypto::tink::util::StatusOr<std::string> Sign(
+      absl::string_view data) const = 0;
+
+  virtual ~PublicKeySign() {}
+};
+
+}  // namespace tink
+}  // namespace crypto
+
+#endif  // PUBLIC_KEY_SIGN_H_
