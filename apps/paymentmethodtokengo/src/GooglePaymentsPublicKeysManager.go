53c1344d5f7a8adcfd64e2b1d436133a3b00a82c
Mohammed Saqib <mohammed.saqib@patientco.com>
2018-02-24 15:53:55 -0500

    initial commit, put in the constants

---
+++ apps/paymentmethodtokengo/src/GooglePaymentsPublicKeysManager.go
@@ -0,0 +1,4 @@
+package paymentmethodtoken
+
+const KEYS_URL_PRODUCTION = "https://payments.developers.google.com/paymentmethodtoken/keys.json"
+const KEYS_URL_TEST = "https://payments.developers.google.com/paymentmethodtoken/test/keys.json"
