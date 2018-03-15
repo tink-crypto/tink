2c4e6617b015a18e384ff038279cdddcb11d6a2c
Mohammed Saqib <mohammed.saqib@patientco.com>
2018-03-15 11:20:36 -0400

    shallow copy around golang/x/crypto/hkdf

---
+++ go/subtle/kdf/hkdf.go
@@ -0,0 +1,31 @@
+package kdf
+
+//Package kdf is just a wrapper arround the golang kdf method
+
+import (
+	"hash"
+
+	"github.com/google/tink/go/subtle"
+	"golang.org/x/crypto/hkdf"
+)
+
+//HKDF is shallow wrapper around x/crypto/HKDF
+type HKDF struct {
+}
+
+// GenerateKeyWithHash is just a shallow wrapper around x/crypto/hkdf, takes an actual hash function
+func (h *HKDF) GenerateKeyWithHash(hash func() hash.Hash, secret, salt, info []byte, keySize int) ([]byte, error) {
+	keyReader := hkdf.New(hash, secret, salt, info)
+	key := make([]byte, keySize)
+	_, err := keyReader.Read(key)
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+// GenerateKey is just a shallow wrapper around x/crypto/hkdf, takes the name of a hash function
+func (h *HKDF) GenerateKey(hashName string, secret, salt, info []byte, keySize int) ([]byte, error) {
+	hash := subtle.GetHashFunc(hashName)
+	return h.GenerateKeyWithHash(hash, secret, salt, info, keySize)
+}
