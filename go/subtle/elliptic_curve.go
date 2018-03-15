9243cc8e64c3123b539f5903e1697e9aaab7c9f6
Mohammed Saqib <mohammed.saqib@patientco.com>
2018-03-15 15:03:11 -0400

    added some utility methods to import and export to ecdsa types already in golang crypto library

--- go/subtle/elliptic_curve.go
+++ go/subtle/elliptic_curve.go
@@ -4,27 +4,57 @@
 package subtle
 
 import (
+	"crypto/ecdsa"
 	"crypto/elliptic"
 	"errors"
 	"io"
 	"math/big"
 )
 
+//EllipticPublicKey define curve and point
 type EllipticPublicKey struct {
-	X, Y  *big.Int
 	Curve elliptic.Curve
+	X, Y  *big.Int
 }
 
+//EllipticPrivateKey contains corresponding public key and generator D
 type EllipticPrivateKey struct {
-	D *big.Int
 	EllipticPublicKey
+	D *big.Int
 }
 
-// The default curve for this package is the NIST P256 curve, which
+// ExportECDSA an ECIES public key as an ECDSA public key.
+func (pub *EllipticPublicKey) ExportECDSA() *ecdsa.PublicKey {
+	return &ecdsa.PublicKey{pub.Curve, pub.X, pub.Y}
+}
+
+// ImportECDSAPublic an ECDSA public key as an ECIES public key.
+func ImportECDSAPublic(pub *ecdsa.PublicKey) *EllipticPublicKey {
+	return &EllipticPublicKey{
+		X:     pub.X,
+		Y:     pub.Y,
+		Curve: pub.Curve,
+	}
+}
+
+// ExportECDSA an ECIES private key as an ECDSA private key.
+func (prv *EllipticPrivateKey) ExportECDSA() *ecdsa.PrivateKey {
+	pub := &prv.EllipticPublicKey
+	pubECDSA := pub.ExportECDSA()
+	return &ecdsa.PrivateKey{*pubECDSA, prv.D}
+}
+
+// ImportECDSA an ECDSA private key as an ECIES private key.
+func ImportECDSA(prv *ecdsa.PrivateKey) *EllipticPrivateKey {
+	pub := ImportECDSAPublic(&prv.PublicKey)
+	return &EllipticPrivateKey{*pub, prv.D}
+}
+
+// DefaultCurve for this package is the NIST P256 curve, which
 // provides security equivalent to AES-128.
 var DefaultCurve = elliptic.P256()
 
-// Generate an elliptic curve public / private keypair. If params is nil,
+// GenerateKey returns an elliptic curve public / private keypair. If params is nil,
 // the recommended default paramters for the key will be chosen.
 func GenerateKey(rand io.Reader, curve elliptic.Curve) (prv *EllipticPrivateKey, err error) {
 	pb, x, y, err := elliptic.GenerateKey(curve, rand)
@@ -44,7 +74,7 @@ var (
 	ErrSharedKeyTooBig = errors.New("Requested invalid key size (too big)")
 )
 
-// ECDH key agreement method used to establish secret keys for encryption.
+// GenerateShared is part of ECDH key agreement method used to establish secret keys for encryption.
 func (prv *EllipticPrivateKey) GenerateShared(pub *EllipticPublicKey, keyLength int) (sk []byte, err error) {
 	if prv.EllipticPublicKey.Curve != pub.Curve {
 		return nil, ErrBadSharedKeys
