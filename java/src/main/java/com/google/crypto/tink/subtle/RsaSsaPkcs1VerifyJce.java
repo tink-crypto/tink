4828a81a144b1c12f38919bdb6a6d4bf6443fefc
Quan Nguyen <quannguyen@google.com>
2018-08-20 13:50:40 -0700

    Implement RsaSsaPkcs1 padding from scratch. Add RsaSsaPkcs1VerifyJceTest.java back to Android versions 19 and 21.
    PiperOrigin-RevId: 209477297
    GitOrigin-RevId: 74bbd938d571ac02260787eef76ff866a9269b9f

--- java/src/main/java/com/google/crypto/tink/subtle/RsaSsaPkcs1VerifyJce.java
+++ java/src/main/java/com/google/crypto/tink/subtle/RsaSsaPkcs1VerifyJce.java
@@ -18,37 +18,136 @@ package com.google.crypto.tink.subtle;
 
 import com.google.crypto.tink.PublicKeyVerify;
 import com.google.crypto.tink.subtle.Enums.HashType;
+import java.math.BigInteger;
 import java.security.GeneralSecurityException;
-import java.security.Signature;
+import java.security.MessageDigest;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Arrays;
 
 /**
  * RsaSsaPkcs1 (i.e. RSA Signature Schemes with Appendix (SSA) using PKCS1-v1_5 encoding) verifying
  * with JCE.
  */
 public final class RsaSsaPkcs1VerifyJce implements PublicKeyVerify {
+  private static final String ASN_PREFIX_SHA256 = "3031300d060960864801650304020105000420";
+  private static final String ASN_PREFIX_SHA512 = "3051300d060960864801650304020305000440";
+
   private final RSAPublicKey publicKey;
-  private final String signatureAlgorithm;
+  private final HashType hash;
 
   public RsaSsaPkcs1VerifyJce(final RSAPublicKey pubKey, HashType hash)
       throws GeneralSecurityException {
     this.publicKey = pubKey;
-    this.signatureAlgorithm = SubtleUtil.toRsaSsaPkcs1Algo(hash);
+    Validators.validateSignatureHash(hash);
+    this.hash = hash;
   }
 
   @Override
   public void verify(final byte[] signature, final byte[] data) throws GeneralSecurityException {
-    Signature verifier = EngineFactory.SIGNATURE.getInstance(signatureAlgorithm);
-    verifier.initVerify(publicKey);
-    verifier.update(data);
-    boolean verified = false;
-    try {
-      verified = verifier.verify(signature);
-    } catch (java.lang.RuntimeException ex) {
-      verified = false;
+    // The algorithm is described at (https://tools.ietf.org/html/rfc8017#section-8.2). As signature
+    // verification is a public operation,  throwing different exception messages doesn't give
+    // attacker any useful information.
+    BigInteger e = publicKey.getPublicExponent();
+    BigInteger n = publicKey.getModulus();
+    int nLengthInBytes = (n.bitLength() + 7) / 8;
+
+    // Step 1. Length checking.
+    if (nLengthInBytes != signature.length) {
+      throw new GeneralSecurityException("invalid signature's length");
     }
-    if (!verified) {
-      throw new GeneralSecurityException("Invalid signature");
+
+    // Step 2. RSA verification.
+    BigInteger s = bytes2Integer(signature);
+    if (s.compareTo(n) >= 0) {
+      throw new GeneralSecurityException("signature out of range");
+    }
+    BigInteger m = s.modPow(e, n);
+    byte[] em = integer2Bytes(m, nLengthInBytes);
+
+    // Step 3. PKCS1 encoding.
+    byte[] expectedEm = emsaPkcs1(data, nLengthInBytes, hash);
+
+    // Step 4. Compare the results.
+    if (!Bytes.equal(em, expectedEm)) {
+      throw new GeneralSecurityException("invalid signature");
+    }
+  }
+
+  // https://tools.ietf.org/html/rfc8017#section-9.2.
+  private byte[] emsaPkcs1(byte[] m, int emLen, HashType hash) throws GeneralSecurityException {
+    MessageDigest digest = EngineFactory.MESSAGE_DIGEST.getInstance(toDigestAlgo(this.hash));
+    digest.update(m);
+    byte[] h = digest.digest();
+    byte[] asnPrefix = toAsnPrefix(hash);
+    int tLen = asnPrefix.length + h.length;
+    if (emLen < tLen + 11) {
+      throw new GeneralSecurityException("intended encoded message length too short");
+    }
+    byte[] em = new byte[emLen];
+    int offset = 0;
+    em[offset++] = 0x00;
+    em[offset++] = 0x01;
+    for (int i = 0; i < emLen - tLen - 3; i++) {
+      em[offset++] = (byte) 0xff;
+    }
+    em[offset++] = 0x00;
+    System.arraycopy(asnPrefix, 0, em, offset, asnPrefix.length);
+    System.arraycopy(h, 0, em, offset + asnPrefix.length, h.length);
+    return em;
+  }
+
+  /**
+   * Converts an byte array to a nonnegative integer
+   * (https://tools.ietf.org/html/rfc8017#section-4.1).
+   */
+  private BigInteger bytes2Integer(byte[] bs) {
+    return new BigInteger(1, bs);
+  }
+
+  /**
+   * Converts a nonnegative integer to a byte array of a specified length
+   * (https://tools.ietf.org/html/rfc8017#section-4.2).
+   */
+  private byte[] integer2Bytes(BigInteger num, int intendedLength) throws GeneralSecurityException {
+    byte[] b = num.toByteArray();
+    if (b.length == intendedLength) {
+      return b;
+    }
+    if (b.length > intendedLength + 1 /* potential leading zero */) {
+      throw new GeneralSecurityException("integer too large");
+    }
+    if (b.length == intendedLength + 1) {
+      if (b[0] == 0 /* leading zero */) {
+        return Arrays.copyOfRange(b, 1, b.length);
+      } else {
+        throw new GeneralSecurityException("integer too large");
+      }
+    }
+    // Left zero pad b.
+    byte[] res = new byte[intendedLength];
+    System.arraycopy(b, 0, res, intendedLength - b.length, b.length);
+    return res;
+  }
+
+  private byte[] toAsnPrefix(HashType hash) throws GeneralSecurityException {
+    switch (hash) {
+      case SHA256:
+        return Hex.decode(ASN_PREFIX_SHA256);
+      case SHA512:
+        return Hex.decode(ASN_PREFIX_SHA512);
+      default:
+        throw new GeneralSecurityException("Unsupported hash " + hash);
+    }
+  }
+
+  private String toDigestAlgo(HashType hash) throws GeneralSecurityException {
+    switch (hash) {
+      case SHA256:
+        return "SHA-256";
+      case SHA512:
+        return "SHA-512";
+      default:
+        throw new GeneralSecurityException("Unsupported hash " + hash);
     }
   }
 }
