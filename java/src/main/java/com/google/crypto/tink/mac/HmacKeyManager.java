73162f79f4b0ae0605218a7ba473a813c40662e1
tholenst <tholenst@google.com>
2018-10-19 01:38:00 -0700

    Add a KeyManagerBase class in java, similar to the C++ class. Then, use it to implement the HmacKeyManager.
    This gets rid of some of the boilerplate in each keymanager. However, we do add two more methods to parse key and keymanager in each instance. Nevertheless, it reduces much more code than it adds overall, and I think it's a clear simplification.
    
    I marked the KeyManagerBase as alpha while it's in development.
    
    PiperOrigin-RevId: 217838368
    GitOrigin-RevId: fb8cbaa9ebf6663ecba5a862f197c952ded3758e

--- java/src/main/java/com/google/crypto/tink/mac/HmacKeyManager.java
+++ java/src/main/java/com/google/crypto/tink/mac/HmacKeyManager.java
@@ -16,26 +16,28 @@
 
 package com.google.crypto.tink.mac;
 
-import com.google.crypto.tink.KeyManager;
+import com.google.crypto.tink.KeyManagerBase;
 import com.google.crypto.tink.Mac;
 import com.google.crypto.tink.proto.HashType;
 import com.google.crypto.tink.proto.HmacKey;
 import com.google.crypto.tink.proto.HmacKeyFormat;
 import com.google.crypto.tink.proto.HmacParams;
-import com.google.crypto.tink.proto.KeyData;
+import com.google.crypto.tink.proto.KeyData.KeyMaterialType;
 import com.google.crypto.tink.subtle.MacJce;
 import com.google.crypto.tink.subtle.Random;
 import com.google.crypto.tink.subtle.Validators;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.MessageLite;
 import java.security.GeneralSecurityException;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
  * This key manager generates new {@code HmacKey} keys and produces new instances of {@code MacJce}.
  */
-class HmacKeyManager implements KeyManager<Mac> {
+class HmacKeyManager extends KeyManagerBase<Mac, HmacKey, HmacKeyFormat> {
+  public HmacKeyManager() {
+    super(HmacKey.class, HmacKeyFormat.class, TYPE_URL);
+  }
   /** Type url that this manager does support. */
   public static final String TYPE_URL = "type.googleapis.com/google.crypto.tink.HmacKey";
   /** Current version of this key manager. Keys with version equal or smaller are supported. */
@@ -49,22 +51,7 @@ class HmacKeyManager implements KeyManager<Mac> {
 
   /** @param serializedKey serialized {@code HmacKey} proto */
   @Override
-  public Mac getPrimitive(ByteString serializedKey) throws GeneralSecurityException {
-    try {
-      HmacKey keyProto = HmacKey.parseFrom(serializedKey);
-      return getPrimitive(keyProto);
-    } catch (InvalidProtocolBufferException e) {
-      throw new GeneralSecurityException("expected serialized HmacKey proto", e);
-    }
-  }
-
-  /** @param key {@code HmacKey} proto */
-  @Override
-  public Mac getPrimitive(MessageLite key) throws GeneralSecurityException {
-    if (!(key instanceof HmacKey)) {
-      throw new GeneralSecurityException("expected HmacKey proto");
-    }
-    HmacKey keyProto = (HmacKey) key;
+  public Mac getPrimitiveFromKey(HmacKey keyProto) throws GeneralSecurityException {
     validate(keyProto);
     HashType hash = keyProto.getParams().getHash();
     byte[] keyValue = keyProto.getKeyValue().toByteArray();
@@ -87,25 +74,7 @@ class HmacKeyManager implements KeyManager<Mac> {
    * @return new {@code HmacKey} proto
    */
   @Override
-  public MessageLite newKey(ByteString serializedKeyFormat) throws GeneralSecurityException {
-    try {
-      HmacKeyFormat format = HmacKeyFormat.parseFrom(serializedKeyFormat);
-      return newKey(format);
-    } catch (InvalidProtocolBufferException e) {
-      throw new GeneralSecurityException("expected serialized HmacKeyFormat proto", e);
-    }
-  }
-
-  /**
-   * @param keyFormat {@code HmacKeyFormat} proto
-   * @return new {@code HmacKey} proto
-   */
-  @Override
-  public MessageLite newKey(MessageLite keyFormat) throws GeneralSecurityException {
-    if (!(keyFormat instanceof HmacKeyFormat)) {
-      throw new GeneralSecurityException("expected HmacKeyFormat proto");
-    }
-    HmacKeyFormat format = (HmacKeyFormat) keyFormat;
+  public HmacKey newKeyFromFormat(HmacKeyFormat format) throws GeneralSecurityException {
     validate(format);
     return HmacKey.newBuilder()
         .setVersion(VERSION)
@@ -114,33 +83,26 @@ class HmacKeyManager implements KeyManager<Mac> {
         .build();
   }
 
-  /**
-   * @param serializedKeyFormat serialized {@code HmacKeyFormat} proto
-   * @return {@code KeyData} with a new {@code HmacKey} proto
-   */
-  @Override
-  public KeyData newKeyData(ByteString serializedKeyFormat) throws GeneralSecurityException {
-    HmacKey key = (HmacKey) newKey(serializedKeyFormat);
-    return KeyData.newBuilder()
-        .setTypeUrl(TYPE_URL)
-        .setValue(key.toByteString())
-        .setKeyMaterialType(KeyData.KeyMaterialType.SYMMETRIC)
-        .build();
-  }
-
-  @Override
-  public boolean doesSupport(String typeUrl) {
-    return typeUrl.equals(TYPE_URL);
-  }
-
-  @Override
-  public String getKeyType() {
-    return TYPE_URL;
-  }
-
   @Override
   public int getVersion() {
     return VERSION;
+  }
+
+  @Override
+  protected KeyMaterialType keyMaterialType() {
+    return KeyMaterialType.SYMMETRIC;
+  }
+
+  @Override
+  protected HmacKey parseKeyProto(ByteString byteString)
+      throws InvalidProtocolBufferException {
+    return HmacKey.parseFrom(byteString);
+  }
+
+  @Override
+  protected HmacKeyFormat parseKeyFormatProto(ByteString byteString)
+      throws InvalidProtocolBufferException {
+    return HmacKeyFormat.parseFrom(byteString);
   }
 
   private void validate(HmacKey key) throws GeneralSecurityException {
