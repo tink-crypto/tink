c0223fbc53afe720390cb55be39679806cc4b9a5
thaidn <thaidn@google.com>
2018-11-14 09:45:17 -0800

    Switch back to latest Bazel.
    PiperOrigin-RevId: 221460179
    GitOrigin-RevId: 0b64b34572e19a62458b323642e624dae6e53fff

--- kokoro/run_tests.sh
+++ kokoro/run_tests.sh
@@ -52,9 +52,7 @@ readonly DISABLE_SANDBOX_ARGS
 if ! [ -z "${KOKORO_ROOT}" ]; then
   rm -f ~/.bazelrc
   # Install the latest version of Bazel.
-  # TODO(candrian): Temporarily set to 0.15.2 because 0.16.x with JDK 9 throws
-  # errors. Once these are fixed we can revert to latest.
-  use_bazel.sh 0.15.2
+  use_bazel.sh latest
   if [[ "$PLATFORM" == 'darwin' ]]; then
     export DEVELOPER_DIR="/Applications/Xcode_${XCODE_VERSION}.app/Contents/Developer"
     export ANDROID_HOME="/Users/kbuilder/Library/Android/sdk"
