d57c5d3607a2847d367ff4b63ce9f053dc25e8f0
LE Manh Cuong <cuong.manhle.vn@gmail.com>
2019-01-10 22:08:48 +0700

    go/tink: refine KeyId validation
    KeyId is an uint32, it can not be negative,
    so just check it equals zero.

--- go/tink/util.go
+++ go/tink/util.go
@@ -111,8 +111,8 @@ func ValidateKey(key *tinkpb.Keyset_Key) error {
 	if key == nil {
 		return fmt.Errorf("ValidateKey() called with nil")
 	}
-	if key.KeyId <= 0 {
-		return fmt.Errorf("key has non-positive key id: %d", key.KeyId)
+	if key.KeyId == 0 {
+		return fmt.Errorf("key has zero key id: %d", key.KeyId)
 	}
 	if key.KeyData == nil {
 		return fmt.Errorf("key %d has no key data", key.KeyId)
