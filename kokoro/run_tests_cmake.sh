0c4048dc320371adbd53dead8df9a769df644a81
tholenst <tholenst@google.com>
2019-06-23 23:58:15 -0700

    Create a new kokoro config which runs the tests only under CMake; remove the tests from the other config.
    PiperOrigin-RevId: 254697776

---
+++ kokoro/run_tests_cmake.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+echo "========================================================= Running cmake"
+cmake --version
+cmake . -DTINK_BUILD_TESTS=ON
+echo "==================================================== Building with make"
+make -j8 all
+echo "===================================================== Testing with make"
+CTEST_OUTPUT_ON_FAILURE=1 make test
+echo "================================================ Done testing with make"
+
