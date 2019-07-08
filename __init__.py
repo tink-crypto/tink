924ea8bb72e8567ebc556c9a015185d4ec92e182
epuig <epuig@google.com>
2019-07-08 08:34:20 -0700

    Create core package.
    PiperOrigin-RevId: 256984303

--- __init__.py
+++ __init__.py
@@ -5,12 +5,12 @@ from __future__ import google_type_annotations
 from __future__ import print_function
 
 from google3.third_party.tink.python import aead
+from google3.third_party.tink.python import core
 from google3.third_party.tink.python import daead
 from google3.third_party.tink.python import hybrid
-from google3.third_party.tink.python import key_manager
 from google3.third_party.tink.python import mac
 from google3.third_party.tink.python import signature
-from google3.third_party.tink.python import tink_config
+from google3.third_party.tink.python.core import tink_config
 
 
 Aead = aead.Aead
@@ -21,7 +21,24 @@ Mac = mac.Mac
 PublicKeySign = signature.PublicKeySign
 PublicKeyVerify = signature.PublicKeyVerify
 
-KeyManager = key_manager.KeyManager
-PrivateKeyManager = key_manager.PrivateKeyManager
+KeyManager = core.KeyManager
+PrivateKeyManager = core.PrivateKeyManager
 
-del key_manager
+Registry = core.Registry
+
+new_keyset_handle = core.new_keyset_handle
+read_keyset_handle = core.read
+KeysetHandle = core.KeysetHandle
+
+
+KeysetReader = core.KeysetReader
+JsonKeysetReader = core.JsonKeysetReader
+BinaryKeysetReader = core.BinaryKeysetReader
+
+KeysetWriter = core.KeysetWriter
+JsonKeysetWriter = core.JsonKeysetWriter
+BinaryKeysetWriter = core.BinaryKeysetWriter
+
+new_primitive_set = core.new_primitive_set
+
+TinkError = core.TinkError
