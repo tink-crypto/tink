6a9157a7f4d72857d2f9337bb94dd5247f895509
epuig <epuig@google.com>
2019-07-15 09:55:09 -0700

    Add python Tink signature.
    PiperOrigin-RevId: 258183343

---
+++ python/signature/public_key_sign.py
@@ -0,0 +1,47 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Interface for PublicKeySign."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import google_type_annotations
+from __future__ import print_function
+
+import abc
+
+
+class PublicKeySign(object):
+  """Interface for public key signing.
+
+  Digital Signatures provide functionality of signing data and verification of
+  the signatures. They are represented by a pair of primitives (interfaces)
+  'PublicKeySign' for signing of data, and 'PublicKeyVerify' for verification
+  of signatures. Implementations of these interfaces are secure against
+  adaptive chosen-message attacks. Signing data ensures the authenticity and
+  the integrity of that data, but not its secrecy.
+  """
+
+  __metaclass__ = abc.ABCMeta
+
+  @abc.abstractmethod
+  def sign(self, data: bytes) -> bytes:
+    """Computes the signature for data.
+
+    Args:
+      data: bytes, the input data.
+    Returns:
+      The signature as bytes.
+    """
+    raise NotImplementedError()
---
+++ python/signature/public_key_verify_key_manager.py
@@ -0,0 +1,47 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Python wrapper of the CLIF-wrapped C++ Public Key Verify key manager."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import google_type_annotations
+from __future__ import print_function
+
+from typing import Text
+
+from tink.cc.python import public_key_verify as cc_public_key_verify
+from tink.python.cc.clif import cc_key_manager
+from tink.python.core import key_manager
+from tink.python.core import tink_error
+from tink.python.signature import public_key_verify
+
+
+class _PublicKeyVerifyCcToPyWrapper(public_key_verify.PublicKeyVerify):
+  """Transforms cliffed C++ PublicKeyVerify into a Python primitive."""
+
+  def __init__(self, cc_primitive: cc_public_key_verify.PublicKeyVerify):
+    self._public_key_verify = cc_primitive
+
+  @tink_error.use_tink_errors
+  def verify(self, signature: bytes, data: bytes) -> None:
+    self._public_key_verify.verify(signature, data)
+
+
+def from_cc_registry(
+    type_url: Text
+) -> key_manager.KeyManager[public_key_verify.PublicKeyVerify]:
+  return key_manager.KeyManagerCcToPyWrapper(
+      cc_key_manager.PublicKeyVerifyKeyManager.from_cc_registry(type_url),
+      public_key_verify.PublicKeyVerify, _PublicKeyVerifyCcToPyWrapper)
