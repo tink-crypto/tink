db77ee5d54637659ef757c299ea40baf0af687e6
tanujdhir <tanujdhir@google.com>
2019-07-22 04:45:52 -0700

    Fix some docstrings.
    PiperOrigin-RevId: 259302187

--- python/core/keyset_reader.py
+++ python/core/keyset_reader.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Reads KeySets from File."""
+"""Reads Keysets from file."""
 
 from __future__ import absolute_import
 from __future__ import division
--- python/core/keyset_writer_test.py
+++ python/core/keyset_writer_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for tink.python.core."""
+"""Tests for tink.python.core.keyset_writer."""
 
 from __future__ import absolute_import
 from __future__ import division
