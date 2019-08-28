70fad1e22b787732c403a4f3994bb49d9ad73d53
tanujdhir <tanujdhir@google.com>
2019-08-28 06:20:28 -0700

    Delete stray single quote.
    PiperOrigin-RevId: 265895143

--- python/aead/aead.py
+++ python/aead/aead.py
@@ -52,7 +52,7 @@ class Aead(object):
 
   @abc.abstractmethod
   def decrypt(self, ciphertext: bytes, associated_data: bytes) -> bytes:
-    """Decrypts ciphertext' with associated_data.
+    """Decrypts ciphertext with associated_data.
 
     The decryption verifies the authenticity and integrity of the associated
     data, but there are no guarantees with respect to secrecy of that data.
