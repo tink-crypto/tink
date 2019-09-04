7037853987305e8aa8b6c401d90aa38cbcb638a7
tanujdhir <tanujdhir@google.com>
2019-09-04 08:18:19 -0700

    Fix a typo.
    PiperOrigin-RevId: 267156127

--- python/daead/deterministic_aead_key_manager_test.py
+++ python/daead/deterministic_aead_key_manager_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for tink.python.aead_key_manager."""
+"""Tests for tink.python.deterministic_aead_key_manager."""
 
 from __future__ import absolute_import
 from __future__ import division
