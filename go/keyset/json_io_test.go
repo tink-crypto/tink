175f95ea27263e47ece914a762eb24a90417556d
tholenst <tholenst@google.com>
2020-01-21 07:24:59 -0800

    Add tests which document current behavior of the json_io for IDs larger than 2^31 in go.
    Such ids are created in C++. As it turns out, Java can read them, however, if Java reads it, then writes it as Json, they are written as negative numbers, and go will not be able to read them.
    
    PiperOrigin-RevId: 290738497

--- go/keyset/json_io_test.go
+++ go/keyset/json_io_test.go
@@ -16,6 +16,8 @@ package keyset_test
 
 import (
 	"bytes"
+	"encoding/base64"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -24,6 +26,7 @@ import (
 	"github.com/google/tink/go/testkeyset"
 	"github.com/google/tink/go/testutil"
 
+	commonpb "github.com/google/tink/proto/common_go_proto"
 	tinkpb "github.com/google/tink/proto/tink_go_proto"
 )
 
@@ -53,6 +56,177 @@ func TestJSONIOUnencrypted(t *testing.T) {
 	}
 }
 
+func TestJSONReader(t *testing.T) {
+	gcmkey := []byte(testutil.NewAESGCMKey(0, 16).String())
+	eaxkey := []byte(testutil.NewHMACKey(commonpb.HashType_SHA512, 32).String())
+	jsonKeyset := fmt.Sprintf(`{
+         "primaryKeyId":42,
+         "key":[
+            {
+               "keyData":{
+                  "typeUrl":"type.googleapis.com/google.crypto.tink.AesGcmKey",
+                  "keyMaterialType":"SYMMETRIC",
+                  "value": %q
+               },
+               "outputPrefixType":"TINK",
+               "keyId":42,
+               "status":"ENABLED"
+            },
+            {
+               "keyData":{
+                  "typeUrl":"type.googleapis.com/google.crypto.tink.AesEaxKey",
+                  "keyMaterialType":"SYMMETRIC",
+                  "value": %q
+               },
+               "outputPrefixType":"RAW",
+               "keyId":711,
+               "status":"ENABLED"
+            }
+         ]
+      }`, base64.StdEncoding.EncodeToString([]byte(gcmkey)), base64.StdEncoding.EncodeToString([]byte(eaxkey)))
+	r := keyset.NewJSONReader(bytes.NewBufferString(jsonKeyset))
+
+	got, err := r.Read()
+	if err != nil {
+		t.Fatalf("cannot read keyset: %v", err)
+	}
+
+	want := &tinkpb.Keyset{
+		PrimaryKeyId: 42,
+		Key: []*tinkpb.Keyset_Key{
+			{
+				KeyData: &tinkpb.KeyData{
+					TypeUrl:         "type.googleapis.com/google.crypto.tink.AesGcmKey",
+					KeyMaterialType: tinkpb.KeyData_SYMMETRIC,
+					Value:           gcmkey,
+				},
+				OutputPrefixType: tinkpb.OutputPrefixType_TINK,
+				KeyId:            42,
+				Status:           tinkpb.KeyStatusType_ENABLED,
+			},
+			{
+				KeyData: &tinkpb.KeyData{
+					TypeUrl:         "type.googleapis.com/google.crypto.tink.AesEaxKey",
+					KeyMaterialType: tinkpb.KeyData_SYMMETRIC,
+					Value:           eaxkey,
+				},
+				OutputPrefixType: tinkpb.OutputPrefixType_RAW,
+				KeyId:            711,
+				Status:           tinkpb.KeyStatusType_ENABLED,
+			},
+		},
+	}
+
+	if !proto.Equal(got, want) {
+		t.Errorf("written keyset %q doesn't match expected keyset %q", got, want)
+	}
+}
+
+func TestJSONReaderLargeIds(t *testing.T) {
+	gcmkey := []byte(testutil.NewAESGCMKey(0, 16).String())
+	jsonKeyset := fmt.Sprintf(`{
+         "primaryKeyId":4294967275,
+         "key":[
+            {
+               "keyData":{
+                  "typeUrl":"type.googleapis.com/google.crypto.tink.AesGcmKey",
+                  "keyMaterialType":"SYMMETRIC",
+                  "value": %q
+               },
+               "outputPrefixType":"TINK",
+               "keyId":4294967275,
+               "status":"ENABLED"
+            }
+         ]
+      }`, base64.StdEncoding.EncodeToString([]byte(gcmkey)))
+	r := keyset.NewJSONReader(bytes.NewBufferString(jsonKeyset))
+
+	got, err := r.Read()
+	if err != nil {
+		t.Fatalf("cannot read keyset: %v", err)
+	}
+
+	want := &tinkpb.Keyset{
+		PrimaryKeyId: 4294967275,
+		Key: []*tinkpb.Keyset_Key{
+			{
+				KeyData: &tinkpb.KeyData{
+					TypeUrl:         "type.googleapis.com/google.crypto.tink.AesGcmKey",
+					KeyMaterialType: tinkpb.KeyData_SYMMETRIC,
+					Value:           gcmkey,
+				},
+				OutputPrefixType: tinkpb.OutputPrefixType_TINK,
+				KeyId:            4294967275,
+				Status:           tinkpb.KeyStatusType_ENABLED,
+			},
+		},
+	}
+
+	if !proto.Equal(got, want) {
+		t.Errorf("written keyset %q doesn't match expected keyset %q", got, want)
+	}
+}
+
+func TestJSONReaderNegativeIds(t *testing.T) {
+	gcmkey := []byte(testutil.NewAESGCMKey(0, 16).String())
+	jsonKeyset := fmt.Sprintf(`{
+         "primaryKeyId": -10,
+         "key":[
+            {
+               "keyData":{
+                  "typeUrl":"type.googleapis.com/google.crypto.tink.AesGcmKey",
+                  "keyMaterialType":"SYMMETRIC",
+                  "value": %q
+               },
+               "outputPrefixType":"TINK",
+               "keyId": -10,
+               "status":"ENABLED"
+            }
+         ]
+      }`, base64.StdEncoding.EncodeToString(gcmkey))
+	r := keyset.NewJSONReader(bytes.NewBufferString(jsonKeyset))
+
+	_, err := r.Read()
+	// TODO(b/144348283): We should allow negative key ids, as Java can produce them.
+	if err == nil {
+		t.Fatalf("Expected failure due to negative key id")
+	}
+}
+
+// Tests that large IDs (>2^31) are written correctly.
+func TestJSONWriterLargeId(t *testing.T) {
+	eaxkey := []byte(testutil.NewHMACKey(commonpb.HashType_SHA512, 32).String())
+
+	ks := tinkpb.Keyset{
+		PrimaryKeyId: 4294967275,
+		Key: []*tinkpb.Keyset_Key{
+			{
+				KeyData: &tinkpb.KeyData{
+					TypeUrl:         "type.googleapis.com/google.crypto.tink.AesEaxKey",
+					KeyMaterialType: tinkpb.KeyData_SYMMETRIC,
+					Value:           eaxkey,
+				},
+				OutputPrefixType: tinkpb.OutputPrefixType_RAW,
+				KeyId:            4294967275,
+				Status:           tinkpb.KeyStatusType_ENABLED,
+			},
+		},
+	}
+
+	buf := new(bytes.Buffer)
+	w := keyset.NewJSONWriter(buf)
+	if err := w.Write(&ks); err != nil {
+		t.Fatalf("cannot write keyset: %v", err)
+	}
+
+	if !strings.Contains(buf.String(), `"keyId":4294967275`) {
+		t.Errorf("written keyset %q does not contain a key with keyId 4294967275", buf.Bytes())
+	}
+	if !strings.Contains(buf.String(), "\"primaryKeyId\":4294967275") {
+		t.Errorf("written keyset %q does not contain have primaryKeyId 4294967275", buf.Bytes())
+	}
+}
+
 func TestJSONIOEncrypted(t *testing.T) {
 	buf := new(bytes.Buffer)
 	w := keyset.NewJSONWriter(buf)
@@ -70,6 +244,6 @@ func TestJSONIOEncrypted(t *testing.T) {
 	}
 
 	if !proto.Equal(kse1, kse2) {
-		t.Errorf("written encryped keyset (%s) doesn't match read encryped keyset (%s)", kse1, kse2)
+		t.Errorf("written encryped keyset %q doesn't match read encryped keyset %q", kse1, kse2)
 	}
 }
