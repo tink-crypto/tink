8d1d359360dafeebedf5803508e1e4053700ba96
Tink Team <tink-dev@google.com>
2020-01-28 09:10:19 -0800

    aead: Introduce a mock class and target for testing code that uses an aead
    This allows users to write tests that make specific assertions about the inputs
    to the Encrypt and Decrypt functionality.
    
    PiperOrigin-RevId: 291945832

---
+++ cc/aead/mock_aead.h
@@ -0,0 +1,38 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef TINK_MOCK_AEAD_H_
+#define TINK_MOCK_AEAD_H_
+
+#include "gmock/gmock.h"
+#include "tink/aead.h"
+
+namespace crypto {
+namespace tink {
+
+class MockAead : public Aead {
+ public:
+  MOCK_METHOD(util::StatusOr<std::string>, Encrypt,
+              (absl::string_view plaintext, absl::string_view associated_data),
+              (const, override));
+
+  MOCK_METHOD(util::StatusOr<std::string>, Decrypt,
+              (absl::string_view ciphertext, absl::string_view associated_data),
+              (const, override));
+};
+
+}  // namespace tink
+}  // namespace crypto
+
+#endif  // TINK_MOCK_AEAD_H_
