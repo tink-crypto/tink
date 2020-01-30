02bdc3e37818d2367ee75e7835f0eb8129f857d5
kste <kste@google.com>
2020-01-30 12:56:54 -0800

    Encrypt only DEK bytes for envelope encryption.
    This changes the envelope encryption in C++ to solely encrypt the key
    bytes of the DEK instead of the full KeyData proto. This reduces the
    overhead in the envelope encryption and fixes an issue with incompatible
    format between C++ and the envelope encryption in Java and Go.
    
    PiperOrigin-RevId: 292406414

--- cc/aead/kms_envelope_aead.cc
+++ cc/aead/kms_envelope_aead.cc
@@ -16,6 +16,7 @@
 
 #include <string>
 
+#include "absl/base/internal/endian.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "tink/aead.h"
@@ -25,7 +26,6 @@
 #include "tink/util/statusor.h"
 #include "proto/tink.pb.h"
 
-
 namespace crypto {
 namespace tink {
 
@@ -33,20 +33,6 @@ namespace {
 
 const int kEncryptedDekPrefixSize = 4;
 const char* kEmptyAssociatedData = "";
-
-void BigEndianStore32(uint8_t dst[4], uint32_t val) {
-  dst[0] = (val >> 24) & 0xff;
-  dst[1] = (val >> 16) & 0xff;
-  dst[2] = (val >> 8) & 0xff;
-  dst[3] = val & 0xff;
-}
-
-int32_t BigEndianLoad32(const uint8_t src[4]) {
-  return static_cast<int32_t>(src[3])
-      | (static_cast<int32_t>(src[2]) << 8)
-      | (static_cast<int32_t>(src[1]) << 16)
-      | (static_cast<int32_t>(src[0]) << 24);
-}
 
 // Constructs a ciphertext of KMS envelope encryption.
 // The format of the ciphertext is the following:
@@ -56,7 +42,7 @@ int32_t BigEndianLoad32(const uint8_t src[4]) {
 std::string GetEnvelopeCiphertext(absl::string_view encrypted_dek,
                                   absl::string_view encrypted_plaintext) {
   uint8_t enc_dek_size[kEncryptedDekPrefixSize];
-  BigEndianStore32(enc_dek_size, encrypted_dek.size());
+  absl::big_endian::Store32(enc_dek_size, encrypted_dek.size());
   return absl::StrCat(std::string(reinterpret_cast<const char*>(enc_dek_size),
                                   kEncryptedDekPrefixSize),
                       encrypted_dek, encrypted_plaintext);
@@ -64,11 +50,10 @@ std::string GetEnvelopeCiphertext(absl::string_view encrypted_dek,
 
 }  // namespace
 
-
 // static
 util::StatusOr<std::unique_ptr<Aead>> KmsEnvelopeAead::New(
-      const google::crypto::tink::KeyTemplate& dek_template,
-      std::unique_ptr<Aead> remote_aead) {
+    const google::crypto::tink::KeyTemplate& dek_template,
+    std::unique_ptr<Aead> remote_aead) {
   if (remote_aead == nullptr) {
     return util::Status(util::error::INVALID_ARGUMENT,
                         "remote_aead must be non-null");
@@ -87,9 +72,9 @@ util::StatusOr<std::string> KmsEnvelopeAead::Encrypt(
   if (!dek_result.ok()) return dek_result.status();
   auto dek = std::move(dek_result.ValueOrDie());
 
-  // Wrap DEK with remote.
-  auto dek_encrypt_result = remote_aead_->Encrypt(
-      dek->SerializeAsString(), kEmptyAssociatedData);
+  // Wrap DEK key values with remote.
+  auto dek_encrypt_result =
+      remote_aead_->Encrypt(dek->value(), kEmptyAssociatedData);
   if (!dek_encrypt_result.ok()) return dek_encrypt_result.status();
 
   // Encrypt plaintext using DEK.
@@ -108,15 +93,13 @@ util::StatusOr<std::string> KmsEnvelopeAead::Decrypt(
     absl::string_view ciphertext, absl::string_view associated_data) const {
   // Parse the ciphertext.
   if (ciphertext.size() < kEncryptedDekPrefixSize) {
-    return util::Status(util::error::INVALID_ARGUMENT,
-                        "ciphertext too short");
+    return util::Status(util::error::INVALID_ARGUMENT, "ciphertext too short");
   }
-  auto enc_dek_size = BigEndianLoad32(
+  auto enc_dek_size = absl::big_endian::Load32(
       reinterpret_cast<const uint8_t*>(ciphertext.data()));
   if (enc_dek_size > ciphertext.size() - kEncryptedDekPrefixSize ||
       enc_dek_size < 0) {
-    return util::Status(util::error::INVALID_ARGUMENT,
-                        "invalid ciphertext");
+    return util::Status(util::error::INVALID_ARGUMENT, "invalid ciphertext");
   }
   // Decrypt the DEK with remote.
   auto dek_decrypt_result = remote_aead_->Decrypt(
@@ -131,10 +114,9 @@ util::StatusOr<std::string> KmsEnvelopeAead::Decrypt(
 
   // Create AEAD from DEK.
   google::crypto::tink::KeyData dek;
-  if (!dek.ParseFromString(dek_decrypt_result.ValueOrDie())) {
-    return util::Status(util::error::INVALID_ARGUMENT,
-                        "invalid ciphertext");
-  }
+  dek.set_type_url(dek_template_.type_url());
+  dek.set_value(dek_decrypt_result.ValueOrDie());
+  dek.set_key_material_type(google::crypto::tink::KeyData::SYMMETRIC);
 
   // Encrypt plaintext using DEK.
   auto aead_result = Registry::GetPrimitive<Aead>(dek);
