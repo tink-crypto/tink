6bb05b6ef8e2d302ba1ea3bf9dfaff436872d157
Thai Duong <thaidn@users.noreply.github.com>
2020-03-20 14:56:17 -0700

    Revert "feat: Go: Add signature key templates WithoutPrefix (#326)" (#329)
    This reverts commit 0e86c4e2ffcff39ddd5655bfb932a930091479a5.

--- go/signature/signature_key_templates.go
+++ go/signature/signature_key_templates.go
@@ -28,81 +28,35 @@ import (
 //   - Hash function: SHA256
 //   - Curve: NIST P-256
 //   - Signature encoding: DER
-//   - Output prefix type: TINK
 func ECDSAP256KeyTemplate() *tinkpb.KeyTemplate {
 	return createECDSAKeyTemplate(commonpb.HashType_SHA256,
 		commonpb.EllipticCurveType_NIST_P256,
-		ecdsapb.EcdsaSignatureEncoding_DER,
-		tinkpb.OutputPrefixType_TINK)
-}
-
-// ECDSAP256KeyWithoutPrefixTemplate is a KeyTemplate that generates a new ECDSA private key with the following
-// parameters:
-//   - Hash function: SHA256
-//   - Curve: NIST P-256
-//   - Signature encoding: DER
-//   - Output prefix type: RAW
-func ECDSAP256KeyWithoutPrefixTemplate() *tinkpb.KeyTemplate {
-	return createECDSAKeyTemplate(commonpb.HashType_SHA256,
-		commonpb.EllipticCurveType_NIST_P256,
-		ecdsapb.EcdsaSignatureEncoding_DER,
-		tinkpb.OutputPrefixType_RAW)
+		ecdsapb.EcdsaSignatureEncoding_DER)
 }
 
 // ECDSAP384KeyTemplate is a KeyTemplate that generates a new ECDSA private key with the following parameters:
 //   - Hash function: SHA512
 //   - Curve: NIST P-384
 //   - Signature encoding: DER
-//   - Output prefix type: TINK
 func ECDSAP384KeyTemplate() *tinkpb.KeyTemplate {
 	return createECDSAKeyTemplate(commonpb.HashType_SHA512,
 		commonpb.EllipticCurveType_NIST_P384,
-		ecdsapb.EcdsaSignatureEncoding_DER,
-		tinkpb.OutputPrefixType_TINK)
-}
-
-// ECDSAP384KeyWithoutPrefixTemplate is a KeyTemplate that generates a new ECDSA private key with the following
-// parameters:
-//   - Hash function: SHA512
-//   - Curve: NIST P-384
-//   - Signature encoding: DER
-//   - Output prefix type: RAW
-func ECDSAP384KeyWithoutPrefixTemplate() *tinkpb.KeyTemplate {
-	return createECDSAKeyTemplate(commonpb.HashType_SHA512,
-		commonpb.EllipticCurveType_NIST_P384,
-		ecdsapb.EcdsaSignatureEncoding_DER,
-		tinkpb.OutputPrefixType_RAW)
+		ecdsapb.EcdsaSignatureEncoding_DER)
 }
 
 // ECDSAP521KeyTemplate is a KeyTemplate that generates a new ECDSA private key with the following parameters:
 //   - Hash function: SHA512
 //   - Curve: NIST P-521
 //   - Signature encoding: DER
-//   - Output prefix type: TINK
 func ECDSAP521KeyTemplate() *tinkpb.KeyTemplate {
 	return createECDSAKeyTemplate(commonpb.HashType_SHA512,
 		commonpb.EllipticCurveType_NIST_P521,
-		ecdsapb.EcdsaSignatureEncoding_DER,
-		tinkpb.OutputPrefixType_TINK)
-}
-
-// ECDSAP521KeyWithoutPrefixTemplate is a KeyTemplate that generates a new ECDSA private key with the following
-// parameters:
-//   - Hash function: SHA512
-//   - Curve: NIST P-521
-//   - Signature encoding: DER
-//   - Output prefix type: TINK
-func ECDSAP521KeyWithoutPrefixTemplate() *tinkpb.KeyTemplate {
-	return createECDSAKeyTemplate(commonpb.HashType_SHA512,
-		commonpb.EllipticCurveType_NIST_P521,
-		ecdsapb.EcdsaSignatureEncoding_DER,
-		tinkpb.OutputPrefixType_RAW)
+		ecdsapb.EcdsaSignatureEncoding_DER)
 }
 
 // createECDSAKeyTemplate creates a KeyTemplate containing a EcdasKeyFormat
 // with the given parameters.
-func createECDSAKeyTemplate(hashType commonpb.HashType, curve commonpb.EllipticCurveType,
-	encoding ecdsapb.EcdsaSignatureEncoding, prefixType tinkpb.OutputPrefixType) *tinkpb.KeyTemplate {
+func createECDSAKeyTemplate(hashType commonpb.HashType, curve commonpb.EllipticCurveType, encoding ecdsapb.EcdsaSignatureEncoding) *tinkpb.KeyTemplate {
 	params := &ecdsapb.EcdsaParams{
 		HashType: hashType,
 		Curve:    curve,
@@ -111,24 +65,14 @@ func createECDSAKeyTemplate(hashType commonpb.HashType, curve commonpb.EllipticC
 	format := &ecdsapb.EcdsaKeyFormat{Params: params}
 	serializedFormat, _ := proto.Marshal(format)
 	return &tinkpb.KeyTemplate{
-		TypeUrl:          ecdsaSignerTypeURL,
-		Value:            serializedFormat,
-		OutputPrefixType: prefixType,
+		TypeUrl: ecdsaSignerTypeURL,
+		Value:   serializedFormat,
 	}
 }
 
 // ED25519KeyTemplate is a KeyTemplate that generates a new ED25519 private key.
 func ED25519KeyTemplate() *tinkpb.KeyTemplate {
 	return &tinkpb.KeyTemplate{
-		TypeUrl:          ed25519SignerTypeURL,
-		OutputPrefixType: tinkpb.OutputPrefixType_TINK,
-	}
-}
-
-// ED25519KeyWithoutPrefixTemplate is a KeyTemplate that generates a new ED25519 private key.
-func ED25519KeyWithoutPrefixTemplate() *tinkpb.KeyTemplate {
-	return &tinkpb.KeyTemplate{
-		TypeUrl:          ed25519SignerTypeURL,
-		OutputPrefixType: tinkpb.OutputPrefixType_RAW,
+		TypeUrl: ed25519SignerTypeURL,
 	}
 }
