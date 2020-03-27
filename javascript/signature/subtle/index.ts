95e97642971ee4b6c5189c41958599f23dcfc175
taymon <taymon@google.com>
2020-03-26 17:11:13 -0700

    Create new TypeScript import paths for signature/subtle
    This is part of the ongoing TypeScript migration efforts. It was omitted from the first round due to open questions about how to handle it.
    
    This change just addresses the import paths; there may be more changes later.
    
    PiperOrigin-RevId: 303225203

---
+++ javascript/signature/subtle/index.ts
@@ -0,0 +1,2 @@
+export {default as EcdsaSign} from 'goog:tink.subtle.EcdsaSign';  // from //third_party/tink/javascript/subtle:signature
+export {EcdsaSignatureEncodingType, exportCryptoKey, generateKeyPair, importPrivateKey, importPublicKey} from 'goog:tink.subtle.EllipticCurves';  // from //third_party/tink/javascript/subtle
