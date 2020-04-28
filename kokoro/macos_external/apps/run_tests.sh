ddffa5cdf3c42c76b3f1da4bfd9556cd7725cc61
tholenst <tholenst@google.com>
2020-04-28 02:00:43 -0700

    Add environment variables for Android tests on MacOS.
    PiperOrigin-RevId: 308784794

--- kokoro/macos_external/apps/run_tests.sh
+++ kokoro/macos_external/apps/run_tests.sh
@@ -3,6 +3,10 @@
 set -euo pipefail
 cd ${KOKORO_ARTIFACTS_DIR}/git/tink
 
+export XCODE_VERSION=11.3
+export DEVELOPER_DIR="/Applications/Xcode_${XCODE_VERSION}.app/Contents/Developer"
+export ANDROID_HOME="/Users/kbuilder/Library/Android/sdk"
+
 cd apps
 use_bazel.sh $(cat .bazelversion)
 time bazel build -- ...
--- kokoro/macos_external/java_src/run_tests.sh
+++ kokoro/macos_external/java_src/run_tests.sh
@@ -3,6 +3,10 @@
 set -euo pipefail
 cd ${KOKORO_ARTIFACTS_DIR}/git/tink
 
+export XCODE_VERSION=11.3
+export DEVELOPER_DIR="/Applications/Xcode_${XCODE_VERSION}.app/Contents/Developer"
+export ANDROID_HOME="/Users/kbuilder/Library/Android/sdk"
+
 cd java_src
 use_bazel.sh $(cat .bazelversion)
 bazel build ...
