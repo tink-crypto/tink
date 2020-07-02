908314e56853561c91de4a9a521007f726cedc52
thaidn <thaidn@google.com>
2020-07-01 19:36:48 -0700

    Don't generate fresh keys if existing keys are corrupted.
    In other words, generate fresh keys if and only if keys were not found.
    
    PiperOrigin-RevId: 319339272

--- java_src/src/main/java/com/google/crypto/tink/integration/android/AndroidKeysetManager.java
+++ java_src/src/main/java/com/google/crypto/tink/integration/android/AndroidKeysetManager.java
@@ -27,12 +27,14 @@ import com.google.crypto.tink.KeysetManager;
 import com.google.crypto.tink.KeysetReader;
 import com.google.crypto.tink.KeysetWriter;
 import com.google.crypto.tink.proto.OutputPrefixType;
-import com.google.crypto.tink.subtle.Hex;
-import com.google.crypto.tink.subtle.Random;
 import com.google.protobuf.InvalidProtocolBufferException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.security.GeneralSecurityException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.ProviderException;
 import javax.annotation.concurrent.GuardedBy;
 
 /**
@@ -46,11 +48,9 @@ import javax.annotation.concurrent.GuardedBy;
  * <h3>Usage</h3>
  *
  * <pre>{@code
- * String masterKeyUri = "android-keystore://my_master_key_id";
  * AndroidKeysetManager manager = AndroidKeysetManager.Builder()
  *    .withSharedPref(getApplicationContext(), "my_keyset_name", "my_pref_file_name")
  *    .withKeyTemplate(AesGcmHkfStreamingKeyManager.aes128GcmHkdf4KBTemplate())
- *    .withMasterKeyUri(masterKeyUri)
  *    .build();
  * StreamingAead streamingAead = manager.getKeysetHandle().getPrimitive(StreamingAead.class);
  * }</pre>
@@ -59,27 +59,12 @@ import javax.annotation.concurrent.GuardedBy;
  * my_pref_file_name} preferences file. If the preference file name is null, it uses the default
  * preferences file.
  *
- * <p>If the keyset is not found or invalid, and a valid {@link KeyTemplate} is set with {@link
- * AndroidKeysetManager.Builder#withKeyTemplate}, a fresh keyset is generated and is written to the
- * {@code my_keyset_name} preference of the {@code my_pref_file_name} shared preferences file.
+ * <p>If an invalid keyset is found, an {@link InvalidKeyException} is thrown.
  *
- * <p>On Android M or newer and if a master key URI is set with {@link
- * AndroidKeysetManager.Builder#withMasterKeyUri}, the keyset is encrypted with a master key
- * generated and stored in <a
- * href="https://developer.android.com/training/articles/keystore.html">Android Keystore</a>. When
- * Tink cannot decrypt the keyset it would assume that it is not encrypted.
- *
- * <p>The master key URI must start with {@code android-keystore://}. If the master key doesn't
- * exist, a fresh one is generated.
- *
- * <p>Usage of Android Keystore can be disabled with {@link
- * AndroidKeysetManager.Builder#doNotUseKeystore}. Android Keystore on certain devices is broken.
- * Tink runs a self-test to detect such problems and disable Android Keystore accordingly. Users can
- * check whether Android Keystore is in use with {@link #isUsingKeystore}.
- *
- * <p>On Android L or older, or when either Android Keystore is disabled or the master key URI is
- * not set, the keyset will be stored in cleartext in private preferences which, thanks to the
- * security of the Android framework, no other apps can read or write.
+ * <p>If a keyset is not found, and a {@link KeyTemplate} is set with {@link
+ * AndroidKeysetManager.Builder#withKeyTemplate(com.google.crypto.tink.KeyTemplate)}, a fresh keyset
+ * is generated and is written to the {@code my_keyset_name} preference of the {@code
+ * my_pref_file_name} shared preferences file.
  *
  * <p>The resulting manager supports all operations supported by {@link KeysetManager}. For example
  * to rotate the keyset, one can do:
@@ -91,13 +76,41 @@ import javax.annotation.concurrent.GuardedBy;
  * <p>All operations that manipulate the keyset would automatically persist the new keyset to
  * permanent storage.
  *
+ * <h3>Opportunistic keyset encryption with Android Keystore</h3>
+ *
+ * <b>Warning:</b> because Android Keystore is unreliable, we strongly recommend disabling it by not
+ * setting any master key URI.
+ *
+ * <p>If a master key URI is set with {@link AndroidKeysetManager.Builder#withMasterKeyUri}, the
+ * keyset may be encrypted with a key generated and stored in <a
+ * href="https://developer.android.com/training/articles/keystore.html">Android Keystore</a>.
+ *
+ * <p>Android Keystore is only available on Android M or newer. Since it has been found that Android
+ * Keystore is unreliable on certain devices. Tink runs a self-test to detect such problems and
+ * disables Android Keystore accordingly, even if a master key URI is set. Users can check whether
+ * Android Keystore is in use with {@link #isUsingKeystore}.
+ *
+ * <p>When Android Keystore is disabled or otherwise unavailable, keysets will be stored in
+ * cleartext. This is not as bad as it sounds because keysets remain inaccessible to any other apps
+ * running on the same device. Moreover, as of July 2020, most active Android devices support either
+ * full-disk encryption or file-based encryption, which provide strong security protection against
+ * key theft even from attackers with physical access to the device. Android Keystore is only useful
+ * when you want to <a
+ * href="https://developer.android.com/training/articles/keystore#UserAuthentication">require user
+ * authentication for key use</a>, which should be done if and only if you're absolutely sure that
+ * Android Keystore is working properly on your target devices.
+ *
+ * <p>The master key URI must start with {@code android-keystore://}. The remaining of the URI is
+ * used as a key ID when calling Android Keystore. If the master key doesn't exist, a fresh one is
+ * generated. If the master key already exists but is unusable, a {@link KeyStoreException} is
+ * thrown.
+ *
  * @since 1.0.0
  */
 public final class AndroidKeysetManager {
   private static final String TAG = AndroidKeysetManager.class.getSimpleName();
   private final KeysetReader reader;
   private final KeysetWriter writer;
-  private final boolean useKeystore;
   private final Aead masterKey;
   private final KeyTemplate keyTemplate;
 
@@ -106,29 +119,8 @@ public final class AndroidKeysetManager {
 
   private AndroidKeysetManager(Builder builder) throws GeneralSecurityException, IOException {
     reader = builder.reader;
-    if (reader == null) {
-      throw new IllegalArgumentException(
-          "need to specify where to read the keyset from with Builder#withSharedPref");
-    }
-
     writer = builder.writer;
-    if (writer == null) {
-      throw new IllegalArgumentException(
-          "need to specify where to write the keyset to with Builder#withSharedPref");
-    }
-
-    if (builder.useKeystore && builder.masterKeyUri == null) {
-      throw new IllegalArgumentException(
-          "need a master key URI, please set it with Builder#masterKeyUri");
-    }
-    useKeystore = builder.useKeystore && verifyAndroidKeystore();
-
-    if (shouldUseKeystore()) {
-      masterKey = AndroidKeystoreKmsClient.getOrGenerateNewAeadKey(builder.masterKeyUri);
-    } else {
-      masterKey = null;
-    }
-
+    masterKey = builder.masterKey;
     keyTemplate = builder.keyTemplate;
     keysetManager = readOrGenerateNewKeyset();
   }
@@ -138,8 +130,10 @@ public final class AndroidKeysetManager {
     private KeysetReader reader = null;
     private KeysetWriter writer = null;
     private String masterKeyUri = null;
+    private Aead masterKey = null;
     private boolean useKeystore = true;
     private KeyTemplate keyTemplate = null;
+    private KeyStore keyStore = null;
 
     public Builder() {}
 
@@ -168,7 +162,11 @@ public final class AndroidKeysetManager {
         throw new IllegalArgumentException(
             "key URI must start with " + AndroidKeystoreKmsClient.PREFIX);
       }
-      masterKeyUri = val;
+      if (!useKeystore) {
+        throw new IllegalArgumentException(
+            "cannot call withMasterKeyUri() after calling doNotUseKeystore()");
+      }
+      this.masterKeyUri = val;
       return this;
     }
 
@@ -197,14 +195,33 @@ public final class AndroidKeysetManager {
      *
      * <p><b>Warning:</b> When Android Keystore is disabled, keys are stored in cleartext. This
      * should be safe because they are stored in private preferences.
+     *
+     * @deprecated Android Keystore can be disabled by not setting a master key URI.
      */
+    @Deprecated
     public Builder doNotUseKeystore() {
+      masterKeyUri = null;
       useKeystore = false;
       return this;
     }
 
-    /** @return a {@link KeysetHandle} with the specified options. */
+    /** This is for testing only */
+    Builder withKeyStore(KeyStore val) {
+      this.keyStore = val;
+      return this;
+    }
+
+    /**
+     * Builds and returns a new {@link AndroidKeysetManager} with the specified options.
+     *
+     * @throws IOException If a keyset is found but unusable.
+     * @throws KeystoreException If a master key is found but unusable.
+     * @throws GeneralSecurityException If cannot read an existing keyset or generate a new one.
+     */
     public AndroidKeysetManager build() throws GeneralSecurityException, IOException {
+      if (masterKeyUri != null) {
+        masterKey = readOrGenerateNewMasterKey(masterKeyUri, keyStore);
+      }
       return new AndroidKeysetManager(this);
     }
   }
@@ -329,7 +346,7 @@ public final class AndroidKeysetManager {
     return this;
   }
 
-  /** Returns whether this keyset manager is wrapping keys with Android Keystore. */
+  /** Returns whether Android Keystore is being used to wrap Tink keysets. */
   public synchronized boolean isUsingKeystore() {
     return shouldUseKeystore();
   }
@@ -337,9 +354,9 @@ public final class AndroidKeysetManager {
   private KeysetManager readOrGenerateNewKeyset() throws GeneralSecurityException, IOException {
     try {
       return read();
-    } catch (IOException e) {
+    } catch (FileNotFoundException ex) {
       // Not found, handle below.
-      Log.w(TAG, "cannot read keyset: " + e);
+      Log.w(TAG, "keyset not found, will generate a new one", ex);
     }
 
     // Not found.
@@ -350,15 +367,16 @@ public final class AndroidKeysetManager {
       write(manager);
       return manager;
     }
-    throw new GeneralSecurityException("cannot obtain keyset handle");
+    throw new GeneralSecurityException("cannot read or generate keyset");
   }
 
   private KeysetManager read() throws GeneralSecurityException, IOException {
     if (shouldUseKeystore()) {
       try {
         return KeysetManager.withKeysetHandle(KeysetHandle.read(reader, masterKey));
-      } catch (InvalidProtocolBufferException | GeneralSecurityException e) {
-        // This edge case happens when
+      } catch (InvalidProtocolBufferException | GeneralSecurityException ex) {
+        // Swallow the exception and attempt to read the keyset in cleartext.
+        // This edge case may happen when either
         //   - the keyset was generated on a pre M phone which is then upgraded to M or newer, or
         //   - the keyset was generated with Keystore being disabled, then Keystore is enabled.
         // By ignoring the security failure here, an adversary with write access to private
@@ -367,16 +385,11 @@ public final class AndroidKeysetManager {
         // overwrite the encrypted keyset in private preferences of an app, said adversaries must
         // have the same privilege as the app, thus they can call Android Keystore to read or write
         // the encrypted keyset in the first place.
-        // So it's okay to ignore the failure and try to read the keyset in cleartext.
-        Log.w(TAG, "cannot decrypt keyset: " + e);
+        Log.w(TAG, "cannot decrypt keyset: ", ex);
       }
     }
-    KeysetHandle handle = CleartextKeysetHandle.read(reader);
-    if (shouldUseKeystore()) {
-      // Opportunistically encrypt the keyset to avoid further fallback to cleartext.
-      handle.write(writer, masterKey);
-    }
-    return KeysetManager.withKeysetHandle(handle);
+
+    return KeysetManager.withKeysetHandle(CleartextKeysetHandle.read(reader));
   }
 
   private void write(KeysetManager manager) throws GeneralSecurityException {
@@ -392,7 +405,7 @@ public final class AndroidKeysetManager {
   }
 
   private boolean shouldUseKeystore() {
-    return useKeystore && isAtLeastM();
+    return masterKey != null && isAtLeastM();
   }
 
   private static KeyTemplate.OutputPrefixType fromProto(OutputPrefixType outputPrefixType) {
@@ -414,70 +427,46 @@ public final class AndroidKeysetManager {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
   }
 
-  /**
-   * Does a self-test to verify whether we can rely on Android Keystore, which is broken in many
-   * devices.
-   */
-  private static boolean verifyAndroidKeystore() {
+  private static Aead readOrGenerateNewMasterKey(String keyId, KeyStore keyStore)
+      throws GeneralSecurityException {
     if (!isAtLeastM()) {
-      return false;
+      Log.w(TAG, "Android Keystore requires at least Android M");
+      return null;
+    }
+
+    AndroidKeystoreKmsClient client;
+    if (keyStore != null) {
+      client = new AndroidKeystoreKmsClient.Builder().setKeyStore(keyStore).build();
+    } else {
+      client = new AndroidKeystoreKmsClient();
+    }
+
+    boolean existed = client.hasKey(keyId);
+    if (!existed) {
+      try {
+        AndroidKeystoreKmsClient.generateNewAeadKey(keyId);
+      } catch (GeneralSecurityException ex) {
+        Log.w(TAG, "cannot use Android Keystore, it'll be disabled", ex);
+        return null;
+      }
     }
 
     try {
-      String randomKeyId =
-          AndroidKeystoreKmsClient.PREFIX
-              + new String(Random.randBytes(16), Charset.forName("UTF-8"));
-      Aead aead = AndroidKeystoreKmsClient.getOrGenerateNewAeadKey(randomKeyId);
-
-      // Empty message.
-      // Empty aad.
-      byte[] message = new byte[0];
-      byte[] aad = new byte[0];
-      byte[] ciphertext = aead.encrypt(message, aad);
-      byte[] decrypted = aead.decrypt(ciphertext, aad);
-      if (decrypted.length != 0) {
-        Log.w(
-            TAG,
-            "cannot use Android Keystore: encryption/decryption of empty message and empty aad"
-                + " returns incorrect results");
-        return false;
+      return client.getAead(keyId);
+    } catch (GeneralSecurityException | ProviderException ex) {
+      // Throw the exception if the key exists but is unusable. We can't recover by generating a new
+      // key because there might be existing encrypted data under the unusable key.
+      // Users can provide a master key that is stored in StrongBox, which may throw a
+      // ProviderException if there's any problem with it.
+      if (existed) {
+        throw new KeyStoreException(
+            String.format("the master key %s exists but is unusable", keyId), ex);
       }
-
-      // Non-empty message.
-      // Empty aad.
-      message = Random.randBytes(10);
-      aad = new byte[0];
-      ciphertext = aead.encrypt(message, aad);
-      decrypted = aead.decrypt(ciphertext, aad);
-      if (!Hex.encode(decrypted).equals(Hex.encode(message))) {
-        Log.w(
-            TAG,
-            "cannot use Android Keystore: encryption/decryption of non-empty message and empty"
-                + " aad returns incorrect results");
-        return false;
-      }
-
-      // Non-empty message.
-      // Non-empty aad.
-      message = Random.randBytes(10);
-      aad = Random.randBytes(10);
-      ciphertext = aead.encrypt(message, aad);
-      decrypted = aead.decrypt(ciphertext, aad);
-      if (!Hex.encode(decrypted).equals(Hex.encode(message))) {
-        Log.w(
-            TAG,
-            "cannot use Android Keystore: encryption/decryption of non-empty message and"
-                + " non-empty aad returns incorrect results");
-        return false;
-      }
-
-      new AndroidKeystoreKmsClient().deleteKey(randomKeyId);
-
-      return true;
-    } catch (Exception ex) {
-      Log.w(TAG, "cannot use Android Keystore: " + ex);
+      // Otherwise swallow the exception if the key doesn't exist yet. We can recover by disabling
+      // Keystore.
+      Log.w(TAG, "cannot use Android Keystore, it'll be disabled", ex);
     }
 
-    return false;
+    return null;
   }
 }
