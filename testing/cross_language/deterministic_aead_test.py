0b2a658c3b5317df98f6218348e5f43382dfcc8d
juerg <juerg@google.com>
2020-07-10 05:13:37 -0700

    Add key rotation cross-language tests for MAC and DAEAD.
    PiperOrigin-RevId: 320585437

--- testing/cross_language/deterministic_aead_test.py
+++ testing/cross_language/deterministic_aead_test.py
@@ -16,10 +16,23 @@ from absl.testing import parameterized
 
 import tink
 from tink import daead
+from tink.proto import tink_pb2
+from util import keyset_builder
 from util import supported_key_types
 from util import testing_servers
 
 SUPPORTED_LANGUAGES = testing_servers.SUPPORTED_LANGUAGES_BY_PRIMITIVE['daead']
+
+
+def key_rotation_test_cases():
+  for enc_lang in SUPPORTED_LANGUAGES:
+    for dec_lang in SUPPORTED_LANGUAGES:
+      for prefix in [tink_pb2.RAW, tink_pb2.TINK]:
+        daead_templates = daead.deterministic_aead_key_templates
+        old_key_tmpl = daead_templates.create_aes_siv_key_template(64)
+        old_key_tmpl.output_prefix_type = prefix
+        new_key_tmpl = daead.deterministic_aead_key_templates.AES256_SIV
+        yield (enc_lang, dec_lang, old_key_tmpl, new_key_tmpl)
 
 
 def setUpModule():
@@ -72,6 +85,73 @@ class DeterministicAeadTest(parameterized.TestCase):
     for p in unsupported_daeads:
       with self.assertRaises(tink.TinkError):
         p.encrypt_deterministically(b'plaintext', b'associated_data')
+
+  @parameterized.parameters(key_rotation_test_cases())
+  def test_key_rotation(self, enc_lang, dec_lang, old_key_tmpl, new_key_tmpl):
+    # Do a key rotation from an old key generated from old_key_tmpl to a new
+    # key generated from new_key_tmpl. Encryption and decryption are done
+    # in languages enc_lang and dec_lang.
+    builder = keyset_builder.new_keyset_builder()
+    older_key_id = builder.add_new_key(old_key_tmpl)
+    builder.set_primary_key(older_key_id)
+    enc_daead1 = testing_servers.deterministic_aead(enc_lang, builder.keyset())
+    dec_daead1 = testing_servers.deterministic_aead(dec_lang, builder.keyset())
+    newer_key_id = builder.add_new_key(new_key_tmpl)
+    enc_daead2 = testing_servers.deterministic_aead(enc_lang, builder.keyset())
+    dec_daead2 = testing_servers.deterministic_aead(dec_lang, builder.keyset())
+
+    builder.set_primary_key(newer_key_id)
+    enc_daead3 = testing_servers.deterministic_aead(enc_lang, builder.keyset())
+    dec_daead3 = testing_servers.deterministic_aead(dec_lang, builder.keyset())
+
+    builder.disable_key(older_key_id)
+    enc_daead4 = testing_servers.deterministic_aead(enc_lang, builder.keyset())
+    dec_daead4 = testing_servers.deterministic_aead(dec_lang, builder.keyset())
+
+    self.assertNotEqual(older_key_id, newer_key_id)
+    # 1 encrypts with the older key. So 1, 2 and 3 can decrypt it, but not 4.
+    ciphertext1 = enc_daead1.encrypt_deterministically(b'plaintext', b'ad')
+    self.assertEqual(dec_daead1.decrypt_deterministically(ciphertext1, b'ad'),
+                     b'plaintext')
+    self.assertEqual(dec_daead2.decrypt_deterministically(ciphertext1, b'ad'),
+                     b'plaintext')
+    self.assertEqual(dec_daead3.decrypt_deterministically(ciphertext1, b'ad'),
+                     b'plaintext')
+    with self.assertRaises(tink.TinkError):
+      _ = dec_daead4.decrypt_deterministically(ciphertext1, b'ad')
+
+    # 2 encrypts with the older key. So 1, 2 and 3 can decrypt it, but not 4.
+    ciphertext2 = enc_daead2.encrypt_deterministically(b'plaintext', b'ad')
+    self.assertEqual(dec_daead1.decrypt_deterministically(ciphertext2, b'ad'),
+                     b'plaintext')
+    self.assertEqual(dec_daead2.decrypt_deterministically(ciphertext2, b'ad'),
+                     b'plaintext')
+    self.assertEqual(dec_daead3.decrypt_deterministically(ciphertext2, b'ad'),
+                     b'plaintext')
+    with self.assertRaises(tink.TinkError):
+      _ = dec_daead4.decrypt_deterministically(ciphertext2, b'ad')
+
+    # 3 encrypts with the newer key. So 2, 3 and 4 can decrypt it, but not 1.
+    ciphertext3 = enc_daead3.encrypt_deterministically(b'plaintext', b'ad')
+    with self.assertRaises(tink.TinkError):
+      _ = dec_daead1.decrypt_deterministically(ciphertext3, b'ad')
+    self.assertEqual(dec_daead2.decrypt_deterministically(ciphertext3, b'ad'),
+                     b'plaintext')
+    self.assertEqual(dec_daead3.decrypt_deterministically(ciphertext3, b'ad'),
+                     b'plaintext')
+    self.assertEqual(dec_daead4.decrypt_deterministically(ciphertext3, b'ad'),
+                     b'plaintext')
+
+    # 4 encrypts with the newer key. So 2, 3 and 4 can decrypt it, but not 1.
+    ciphertext4 = enc_daead4.encrypt_deterministically(b'plaintext', b'ad')
+    with self.assertRaises(tink.TinkError):
+      _ = dec_daead1.decrypt_deterministically(ciphertext4, b'ad')
+    self.assertEqual(dec_daead2.decrypt_deterministically(ciphertext4, b'ad'),
+                     b'plaintext')
+    self.assertEqual(dec_daead3.decrypt_deterministically(ciphertext4, b'ad'),
+                     b'plaintext')
+    self.assertEqual(dec_daead4.decrypt_deterministically(ciphertext4, b'ad'),
+                     b'plaintext')
 
 if __name__ == '__main__':
   absltest.main()
--- testing/cross_language/mac_test.py
+++ testing/cross_language/mac_test.py
@@ -17,10 +17,24 @@ from absl.testing import parameterized
 import tink
 from tink import mac
 
+from tink.proto import common_pb2
+from tink.proto import tink_pb2
+from util import keyset_builder
 from util import supported_key_types
 from util import testing_servers
 
 SUPPORTED_LANGUAGES = testing_servers.SUPPORTED_LANGUAGES_BY_PRIMITIVE['mac']
+
+
+def key_rotation_test_cases():
+  for compute_lang in SUPPORTED_LANGUAGES:
+    for verify_lang in SUPPORTED_LANGUAGES:
+      for prefix in [tink_pb2.RAW, tink_pb2.TINK]:
+        old_key_tmpl = mac.mac_key_templates.create_hmac_key_template(
+            key_size=32, tag_size=16, hash_type=common_pb2.SHA256)
+        old_key_tmpl.output_prefix_type = prefix
+        new_key_tmpl = mac.mac_key_templates.HMAC_SHA512_512BITTAG
+        yield (compute_lang, verify_lang, old_key_tmpl, new_key_tmpl)
 
 
 def setUpModule():
@@ -64,6 +78,61 @@ class MacTest(parameterized.TestCase):
       with self.assertRaises(tink.TinkError):
         p.compute_mac(data)
 
+  @parameterized.parameters(key_rotation_test_cases())
+  def test_key_rotation(
+      self, compute_lang, verify_lang, old_key_tmpl, new_key_tmpl):
+    # Do a key rotation from an old key generated from old_key_tmpl to a new
+    # key generated from new_key_tmpl. MAC computation and verification are done
+    # in languages compute_lang and verify_lang.
+    builder = keyset_builder.new_keyset_builder()
+    older_key_id = builder.add_new_key(old_key_tmpl)
+    builder.set_primary_key(older_key_id)
+    compute_mac1 = testing_servers.mac(compute_lang, builder.keyset())
+    verify_mac1 = testing_servers.mac(verify_lang, builder.keyset())
+    newer_key_id = builder.add_new_key(new_key_tmpl)
+    compute_mac2 = testing_servers.mac(compute_lang, builder.keyset())
+    verify_mac2 = testing_servers.mac(verify_lang, builder.keyset())
+
+    builder.set_primary_key(newer_key_id)
+    compute_mac3 = testing_servers.mac(compute_lang, builder.keyset())
+    verify_mac3 = testing_servers.mac(verify_lang, builder.keyset())
+
+    builder.disable_key(older_key_id)
+    compute_mac4 = testing_servers.mac(compute_lang, builder.keyset())
+    verify_mac4 = testing_servers.mac(verify_lang, builder.keyset())
+
+    self.assertNotEqual(older_key_id, newer_key_id)
+    # 1 uses the older key. So 1, 2 and 3 can verify the mac, but not 4.
+    mac_value1 = compute_mac1.compute_mac(b'plaintext')
+    verify_mac1.verify_mac(mac_value1, b'plaintext')
+    verify_mac2.verify_mac(mac_value1, b'plaintext')
+    verify_mac3.verify_mac(mac_value1, b'plaintext')
+    with self.assertRaises(tink.TinkError):
+      verify_mac4.verify_mac(mac_value1, b'plaintext')
+
+    # 2 uses the older key. So 1, 2 and 3 can verify the mac, but not 4.
+    mac_value2 = compute_mac2.compute_mac(b'plaintext')
+    verify_mac1.verify_mac(mac_value2, b'plaintext')
+    verify_mac2.verify_mac(mac_value2, b'plaintext')
+    verify_mac3.verify_mac(mac_value2, b'plaintext')
+    with self.assertRaises(tink.TinkError):
+      verify_mac4.verify_mac(mac_value2, b'plaintext')
+
+    # 3 uses the newer key. So 2, 3 and 4 can verify the mac, but not 1.
+    mac_value3 = compute_mac3.compute_mac(b'plaintext')
+    with self.assertRaises(tink.TinkError):
+      verify_mac1.verify_mac(mac_value3, b'plaintext')
+    verify_mac2.verify_mac(mac_value3, b'plaintext')
+    verify_mac3.verify_mac(mac_value3, b'plaintext')
+    verify_mac4.verify_mac(mac_value3, b'plaintext')
+
+    # 4 uses the newer key. So 2, 3 and 4 can verify the mac, but not 1.
+    mac_value4 = compute_mac4.compute_mac(b'plaintext')
+    with self.assertRaises(tink.TinkError):
+      verify_mac1.verify_mac(mac_value4, b'plaintext')
+    verify_mac2.verify_mac(mac_value4, b'plaintext')
+    verify_mac3.verify_mac(mac_value4, b'plaintext')
+    verify_mac4.verify_mac(mac_value4, b'plaintext')
 
 if __name__ == '__main__':
   absltest.main()
