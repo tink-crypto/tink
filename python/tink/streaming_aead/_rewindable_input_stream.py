5ae506e41c57a23e5ad4b6cac567f49bb6264292
juerg <juerg@google.com>
2020-08-18 00:22:27 -0700

    Add rewindable input stream in Python.
    PiperOrigin-RevId: 327177855

---
+++ python/tink/streaming_aead/_rewindable_input_stream.py
@@ -0,0 +1,90 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""A Raw Input stream wrapper that supports rewinding."""
+
+from __future__ import absolute_import
+from __future__ import division
+# Placeholder for import for type annotations
+from __future__ import print_function
+
+import io
+from typing import Optional, BinaryIO
+
+
+class RewindableInputStream(io.RawIOBase):
+  """Implements a readable io.RawIOBase wrapper that supports rewinding.
+
+  The wrapped input_stream can either be a io.RawIOBase or io.BufferedIOBase.
+  """
+
+  def __init__(self, input_stream: BinaryIO):
+    super(RewindableInputStream, self).__init__()
+    if not input_stream.readable():
+      raise ValueError('input_stream must be readable')
+    self._input_stream = input_stream
+    self._buffer = bytearray()
+    self._pos = 0
+    self._rewindable = True
+
+  def read(self, size: int = -1) -> Optional[bytes]:
+    """Read and return up to size bytes when size >= 0.
+
+    Args:
+      size: Maximum number of bytes to be returned, if >= 0. If size is smaller
+        than 0 or None, return the whole content of the file.
+    Returns:
+      bytes read. b'' is returned on EOF, and None if there is currently
+      no data available, but EOF is not reached yet.
+    """
+    if size is None or size < 0:
+      return self.readall()  # implemented in io.RawIOBase
+    if self._pos < len(self._buffer):
+      # buffer has some data left. Return up to 'size' bytes from the buffer
+      new_pos = min(len(self._buffer), self._pos + size)
+      b = self._buffer[self._pos:new_pos]
+      self._pos = new_pos
+      return bytes(b)
+    # no data left in buffer
+    if not self._rewindable and self._buffer:
+      # buffer is not needed anymore
+      self._buffer = bytearray()
+      self._pos = 0
+    try:
+      data = self._input_stream.read(size)
+    except BlockingIOError:
+      # self._input_stream is a BufferedIOBase and has currently no data
+      return None
+    if data is None:
+      # self._input_stream is a RawIOBase and has currently no data
+      return None
+    if self._rewindable:
+      self._buffer += data
+      self._pos += len(data)
+    return data
+
+  def rewind(self):
+    if not self._rewindable:
+      raise ValueError('rewind is disabled')
+    self._pos = 0
+
+  def disable_rewind(self):
+    self._rewindable = False
+
+  def readable(self):
+    return True
+
+  def close(self) -> None:
+    """Close the stream and the wrapped input_stream."""
+    if self.closed:  # pylint:disable=using-constant-test
+      return
+    self._input_stream.close()
+    super(RewindableInputStream, self).close()
---
+++ python/tink/streaming_aead/_rewindable_input_stream_test.py
@@ -0,0 +1,140 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for tink.python.tink.util.bytes_io."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import io
+import tempfile
+from typing import BinaryIO, cast
+from absl.testing import absltest
+
+from tink.streaming_aead import _rewindable_input_stream
+from tink.testing import bytes_io
+
+
+def _rewindable(data) -> _rewindable_input_stream.RewindableInputStream:
+  return _rewindable_input_stream.RewindableInputStream(
+      cast(BinaryIO, io.BytesIO(data)))
+
+
+class RewindableInputStreamTest(absltest.TestCase):
+
+  def test_read(self):
+    with _rewindable(b'The quick brown fox') as f:
+      self.assertEqual(b'The q', f.read(5))
+      self.assertEqual(b'uick ', f.read(5))
+      self.assertEqual(b'brown', f.read(5))
+      self.assertEqual(b' fox', f.read(5))
+      self.assertEqual(b'', f.read(5))
+      self.assertEqual(b'', f.read(5))
+
+  def test_read_no_argument(self):
+    with _rewindable(b'The quick brown fox') as f:
+      self.assertEqual(b'The quick brown fox', f.read())
+
+  def test_read_minus_one(self):
+    with _rewindable(b'The quick brown fox') as f:
+      self.assertEqual(b'The quick brown fox', f.read(-1))
+
+  def test_readall(self):
+    with _rewindable(b'The quick brown fox') as f:
+      self.assertEqual(b'The quick brown fox', f.readall())
+
+  def test_rewind_read(self):
+    with _rewindable(b'The quick brown fox') as f:
+      self.assertEqual(b'The quick', f.read(9))
+      f.rewind()
+      self.assertEqual(b'The ', f.read(4))
+      # this only reads the rest of current buffer content.
+      self.assertEqual(b'quick', f.read(100))
+      self.assertEqual(b' brown fox', f.read())
+
+  def test_rewind_readall(self):
+    with _rewindable(b'The quick brown fox') as f:
+      self.assertEqual(b'The q', f.read(5))
+      f.rewind()
+      # this must read the whole file.
+      self.assertEqual(b'The quick brown fox', f.read())
+
+  def test_rewind_twice(self):
+    with _rewindable(b'The quick brown fox') as f:
+      self.assertEqual(b'The q', f.read(5))
+      f.rewind()
+      self.assertEqual(b'The q', f.read(5))
+      self.assertEqual(b'uick ', f.read(5))
+      f.rewind()
+      self.assertEqual(b'The quick brown fox', f.read())
+
+  def test_disable_rewind(self):
+    with _rewindable(b'The quick brown fox') as f:
+      self.assertEqual(b'The q', f.read(5))
+      f.rewind()
+      f.disable_rewind()
+      # this only reads the current buffer content.
+      self.assertEqual(b'The q', f.read(100))
+      self.assertEqual(b'u', f.read(1))
+      self.assertEmpty(f._buffer)
+      self.assertEqual(b'ick brown fox', f.read())
+
+  def test_disable_rewind_readall(self):
+    with _rewindable(b'The quick brown fox') as f:
+      self.assertEqual(b'The q', f.read(5))
+      f.rewind()
+      f.disable_rewind()
+      self.assertEqual(b'The quick brown fox', f.read())
+
+  def test_read_slow(self):
+    input_stream = bytes_io.SlowBytesIO(b'The quick brown fox')
+    with _rewindable_input_stream.RewindableInputStream(
+        cast(BinaryIO, input_stream)) as f:
+      self.assertIsNone(f.read(10))
+      self.assertEqual(b'The q', f.read(10))
+      self.assertEqual(b'uick ', f.read(10))
+      self.assertIsNone(f.read(10))
+      self.assertEqual(b'brown', f.read(10))
+      self.assertEqual(b' fox', f.read(10))
+      self.assertIsNone(f.read(10))
+      self.assertEqual(b'', f.read(10))
+
+  def test_read_slow_raw(self):
+    input_stream = bytes_io.SlowReadableRawBytes(b'The quick brown fox')
+    with _rewindable_input_stream.RewindableInputStream(
+        cast(BinaryIO, input_stream)) as f:
+      self.assertIsNone(f.read(10))
+      self.assertEqual(b'The q', f.read(10))
+      self.assertEqual(b'uick ', f.read(10))
+      self.assertIsNone(f.read(10))
+      self.assertEqual(b'brown', f.read(10))
+      self.assertEqual(b' fox', f.read(10))
+      self.assertIsNone(f.read(10))
+      self.assertEqual(b'', f.read(10))
+
+  def test_read_slow_raw_readall(self):
+    input_stream = bytes_io.SlowReadableRawBytes(b'The quick brown fox')
+    with _rewindable_input_stream.RewindableInputStream(
+        cast(BinaryIO, input_stream)) as f:
+      self.assertIsNone(f.readall())
+      self.assertEqual(b'The quick ', f.readall())
+      self.assertEqual(b'brown fox', f.readall())
+      self.assertEqual(b'', f.readall())
+
+  def test_nonreadable_input_fail(self):
+    with tempfile.TemporaryFile('wb') as f:
+      with self.assertRaises(ValueError):
+        _ = _rewindable_input_stream.RewindableInputStream(cast(BinaryIO, f))
+
+
+if __name__ == '__main__':
+  absltest.main()
