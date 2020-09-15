662965c27f15da47cb8c063d388ed428daa783fe
juerg <juerg@google.com>
2020-09-15 04:21:18 -0700

    Add key rotation tests to hybrid encryption cross-language tests.
    PiperOrigin-RevId: 331739740

--- testing/cross_language/hybrid_encryption_test.py
+++ testing/cross_language/hybrid_encryption_test.py
@@ -12,7 +12,7 @@
 """Cross-language tests for Hybrid Encryption."""
 
 # Placeholder for import for type annotations
-from typing import Iterable, Text
+from typing import Iterable, Text, Tuple
 
 from absl.testing import absltest
 from absl.testing import parameterized
@@ -20,10 +20,24 @@ from absl.testing import parameterized
 import tink
 from tink import hybrid
 
+from tink.proto import tink_pb2
+from tink.testing import keyset_builder
 from util import supported_key_types
 from util import testing_servers
 
 SUPPORTED_LANGUAGES = testing_servers.SUPPORTED_LANGUAGES_BY_PRIMITIVE['hybrid']
+TEMPLATE = hybrid.hybrid_key_templates.ECIES_P256_HKDF_HMAC_SHA256_AES128_GCM
+KEY_ROTATION_TEMPLATES = [TEMPLATE,
+                          keyset_builder.raw_template(TEMPLATE)]
+
+
+def key_rotation_test_cases(
+) -> Iterable[Tuple[Text, Text, tink_pb2.KeyTemplate, tink_pb2.KeyTemplate]]:
+  for enc_lang in SUPPORTED_LANGUAGES:
+    for dec_lang in SUPPORTED_LANGUAGES:
+      for old_key_tmpl in KEY_ROTATION_TEMPLATES:
+        for new_key_tmpl in KEY_ROTATION_TEMPLATES:
+          yield (enc_lang, dec_lang, old_key_tmpl, new_key_tmpl)
 
 
 def setUpModule():
@@ -90,6 +104,65 @@ class HybridEncryptionTest(parameterized.TestCase):
     for enc in unsupported_encs:
       with self.assertRaises(tink.TinkError):
         enc.encrypt(b'plaintext', b'context_info')
+
+  @parameterized.parameters(key_rotation_test_cases())
+  def test_key_rotation(self, enc_lang, dec_lang, old_key_tmpl, new_key_tmpl):
+    # Do a key rotation from an old key generated from old_key_tmpl to a new
+    # key generated from new_key_tmpl. Encryption and decryption are done
+    # in languages enc_lang and dec_lang.
+    builder = keyset_builder.new_keyset_builder()
+    older_key_id = builder.add_new_key(old_key_tmpl)
+    builder.set_primary_key(older_key_id)
+    dec1 = testing_servers.hybrid_decrypt(enc_lang, builder.keyset())
+    enc1 = testing_servers.hybrid_encrypt(dec_lang, builder.public_keyset())
+    newer_key_id = builder.add_new_key(new_key_tmpl)
+    dec2 = testing_servers.hybrid_decrypt(enc_lang, builder.keyset())
+    enc2 = testing_servers.hybrid_encrypt(dec_lang, builder.public_keyset())
+
+    builder.set_primary_key(newer_key_id)
+    dec3 = testing_servers.hybrid_decrypt(enc_lang, builder.keyset())
+    enc3 = testing_servers.hybrid_encrypt(dec_lang, builder.public_keyset())
+
+    builder.disable_key(older_key_id)
+    dec4 = testing_servers.hybrid_decrypt(enc_lang, builder.keyset())
+    enc4 = testing_servers.hybrid_encrypt(dec_lang, builder.public_keyset())
+    self.assertNotEqual(older_key_id, newer_key_id)
+
+    # p1 encrypts with the older key. So p1, p2 and p3 can decrypt it,
+    # but not p4.
+    ciphertext1 = enc1.encrypt(b'plaintext', b'context')
+    self.assertEqual(dec1.decrypt(ciphertext1, b'context'), b'plaintext')
+    self.assertEqual(dec2.decrypt(ciphertext1, b'context'), b'plaintext')
+    self.assertEqual(dec3.decrypt(ciphertext1, b'context'), b'plaintext')
+    with self.assertRaises(tink.TinkError):
+      _ = dec4.decrypt(ciphertext1, b'context')
+
+    # p2 encrypts with the older key. So p1, p2 and p3 can decrypt it,
+    # but not p4.
+    ciphertext2 = enc2.encrypt(b'plaintext', b'context')
+    self.assertEqual(dec1.decrypt(ciphertext2, b'context'), b'plaintext')
+    self.assertEqual(dec2.decrypt(ciphertext2, b'context'), b'plaintext')
+    self.assertEqual(dec3.decrypt(ciphertext2, b'context'), b'plaintext')
+    with self.assertRaises(tink.TinkError):
+      _ = dec4.decrypt(ciphertext2, b'context')
+
+    # p3 encrypts with the newer key. So p2, p3 and p4 can decrypt it,
+    # but not p1.
+    ciphertext3 = enc3.encrypt(b'plaintext', b'context')
+    with self.assertRaises(tink.TinkError):
+      _ = dec1.decrypt(ciphertext3, b'context')
+    self.assertEqual(dec2.decrypt(ciphertext3, b'context'), b'plaintext')
+    self.assertEqual(dec3.decrypt(ciphertext3, b'context'), b'plaintext')
+    self.assertEqual(dec4.decrypt(ciphertext3, b'context'), b'plaintext')
+
+    # p4 encrypts with the newer key. So p2, p3 and p4 can decrypt it,
+    # but not p1.
+    ciphertext4 = enc4.encrypt(b'plaintext', b'context')
+    with self.assertRaises(tink.TinkError):
+      _ = dec1.decrypt(ciphertext4, b'context')
+    self.assertEqual(dec2.decrypt(ciphertext4, b'context'), b'plaintext')
+    self.assertEqual(dec3.decrypt(ciphertext4, b'context'), b'plaintext')
+    self.assertEqual(dec4.decrypt(ciphertext4, b'context'), b'plaintext')
 
 
 if __name__ == '__main__':
