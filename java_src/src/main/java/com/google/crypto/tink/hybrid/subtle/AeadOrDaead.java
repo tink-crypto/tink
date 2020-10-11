33de788ae3a500fa4d857799e2b14e7a67497dd4
mshavit <mshavit@google.com>
2020-10-10 17:31:12 -0700

    Define an AeadOrDaead helper class in Java. This will be used to support AES_SIV as the DEM for Hybrid encryption.
    PiperOrigin-RevId: 336491420

---
+++ java_src/src/main/java/com/google/crypto/tink/hybrid/subtle/AeadOrDaead.java
@@ -0,0 +1,80 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.hybrid.subtle;
+
+import com.google.crypto.tink.Aead;
+import com.google.crypto.tink.DeterministicAead;
+import java.security.GeneralSecurityException;
+
+/**
+ * A wrapper class that provides the functionality of an underlying Aead or Deterministic Aead
+ * primitive. This is useful for smoothing out the interface differences between those two primtives
+ * for cases where it isn't critical.
+ */
+public class AeadOrDaead {
+  private final Aead aead;
+  private final DeterministicAead deterministicAead;
+
+  public AeadOrDaead(Aead aead) {
+    this.aead = aead;
+    this.deterministicAead = null;
+  }
+
+  public AeadOrDaead(DeterministicAead deterministicAead) {
+    this.aead = null;
+    this.deterministicAead = deterministicAead;
+  }
+
+  /**
+   * Encrypts {@code plaintext} with {@code associatedData} as associated authenticated data. The
+   * resulting ciphertext allows for checking authenticity and integrity of associated data ({@code
+   * associatedData}), but does not guarantee its secrecy.
+   *
+   * @param plaintext the plaintext to be encrypted. It must be non-null, but can also be an empty
+   *     (zero-length) byte array
+   * @param associatedData associated data to be authenticated, but not encrypted. Associated data
+   *     is optional, so this parameter can be null. In this case the null value is equivalent to an
+   *     empty (zero-length) byte array. For successful decryption the same associatedData must be
+   *     provided along with the ciphertext.
+   * @return resulting ciphertext
+   */
+  public byte[] encrypt(final byte[] plaintext, final byte[] associatedData)
+      throws GeneralSecurityException {
+    if (aead != null) {
+      return this.aead.encrypt(plaintext, associatedData);
+    } else {
+      return this.deterministicAead.encryptDeterministically(plaintext, associatedData);
+    }
+  }
+  /**
+   * Decrypts {@code ciphertext} with {@code associatedData} as associated authenticated data. The
+   * decryption verifies the authenticity and integrity of the associated data, but there are no
+   * guarantees wrt. secrecy of that data.
+   *
+   * @param ciphertext the plaintext to be decrypted. It must be non-null.
+   * @param associatedData associated data to be authenticated. For successful decryption it must be
+   *     the same as associatedData used during encryption. Can be null, which is equivalent to an
+   *     empty (zero-length) byte array.
+   * @return resulting plaintext
+   */
+  public byte[] decrypt(final byte[] ciphertext, final byte[] associatedData)
+      throws GeneralSecurityException {
+    if (aead != null) {
+      return this.aead.decrypt(ciphertext, associatedData);
+    } else {
+      return this.deterministicAead.decryptDeterministically(ciphertext, associatedData);
+    }
+  }
+}
---
+++ java_src/src/test/java/com/google/crypto/tink/hybrid/subtle/AeadOrDaeadTest.java
@@ -0,0 +1,85 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.hybrid.subtle;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.DeterministicAead;
+import com.google.crypto.tink.testing.TestUtil.DummyAead;
+import java.security.GeneralSecurityException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for AeadOrDaead */
+@RunWith(JUnit4.class)
+public final class AeadOrDaeadTest {
+
+  static final byte[] TEST_BYTE_VECTOR = {0, 1, 2, 3, 4, 5, 6, 7};
+
+  /** A dummy Deterministic Aead-implementation that just throws exception. */
+  private static class DummyDeterministicAead implements DeterministicAead {
+    public DummyDeterministicAead() {}
+
+    @Override
+    public byte[] encryptDeterministically(byte[] plaintext, byte[] aad)
+        throws GeneralSecurityException {
+      throw new GeneralSecurityException("dummy deterministic encrypt");
+    }
+
+    @Override
+    public byte[] decryptDeterministically(byte[] ciphertext, byte[] aad)
+        throws GeneralSecurityException {
+      throw new GeneralSecurityException("dummy deterministic decrypt");
+    }
+  }
+
+  @Test
+  public void testWithAeadPrimitive() {
+    AeadOrDaead aeadOrDaead = new AeadOrDaead(new DummyAead());
+
+    // Test that encrypt and decrypt is called on the DummyAead.
+    GeneralSecurityException thrown =
+        assertThrows(
+            GeneralSecurityException.class,
+            () -> aeadOrDaead.encrypt(TEST_BYTE_VECTOR, TEST_BYTE_VECTOR));
+    assertThat(thrown).hasMessageThat().contains("dummy");
+
+    thrown =
+        assertThrows(
+            GeneralSecurityException.class,
+            () -> aeadOrDaead.decrypt(TEST_BYTE_VECTOR, TEST_BYTE_VECTOR));
+    assertThat(thrown).hasMessageThat().contains("dummy");
+  }
+
+  @Test
+  public void testWithDeterministicAeadPrimitive() {
+    AeadOrDaead aeadOrDaead = new AeadOrDaead(new DummyDeterministicAead());
+
+    // Test that encrypt and decrypt is called on the DummyDeterministicAead.
+    GeneralSecurityException thrown =
+        assertThrows(
+            GeneralSecurityException.class,
+            () -> aeadOrDaead.encrypt(TEST_BYTE_VECTOR, TEST_BYTE_VECTOR));
+    assertThat(thrown).hasMessageThat().contains("dummy deterministic encrypt");
+
+    thrown =
+        assertThrows(
+            GeneralSecurityException.class,
+            () -> aeadOrDaead.decrypt(TEST_BYTE_VECTOR, TEST_BYTE_VECTOR));
+    assertThat(thrown).hasMessageThat().contains("dummy deterministic decrypt");
+  }
+}
