f8ba5c86a936eac2828da17c02ab80520a675586
juerg <juerg@google.com>
2020-10-28 10:11:30 -0700

    Use testdata to test MAC KeyTemplates in Go.
    PiperOrigin-RevId: 339483340

--- go/mac/mac_key_templates_test.go
+++ go/mac/mac_key_templates_test.go
@@ -15,91 +15,58 @@
 package mac_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/google/tink/go/core/registry"
+	"github.com/google/tink/go/keyset"
 	"github.com/google/tink/go/mac"
 	"github.com/google/tink/go/testutil"
-	cmacpb "github.com/google/tink/go/proto/aes_cmac_go_proto"
-	commonpb "github.com/google/tink/go/proto/common_go_proto"
-	hmacpb "github.com/google/tink/go/proto/hmac_go_proto"
 	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
 )
 
-func TestTemplates(t *testing.T) {
-	template := mac.HMACSHA256Tag128KeyTemplate()
-	if err := checkHMACTemplate(template, 32, 16, commonpb.HashType_SHA256); err != nil {
-		t.Errorf("incorrect HMACSHA256Tag128KeyTemplate: %s", err)
+func TestKeyTemplates(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		template *tinkpb.KeyTemplate
+	}{
+		{name: "HMAC_SHA256_128BITTAG",
+			template: mac.HMACSHA256Tag128KeyTemplate()},
+		{name: "HMAC_SHA256_256BITTAG",
+			template: mac.HMACSHA256Tag256KeyTemplate()},
+		{name: "HMAC_SHA512_256BITTAG",
+			template: mac.HMACSHA512Tag256KeyTemplate()},
+		{name: "HMAC_SHA512_512BITTAG",
+			template: mac.HMACSHA512Tag512KeyTemplate()},
+		{name: "AES_CMAC",
+			template: mac.AESCMACTag128KeyTemplate()},
 	}
-	template = mac.HMACSHA256Tag256KeyTemplate()
-	if err := checkHMACTemplate(template, 32, 32, commonpb.HashType_SHA256); err != nil {
-		t.Errorf("incorrect HMACSHA256Tag256KeyTemplate: %s", err)
-	}
-	template = mac.HMACSHA512Tag256KeyTemplate()
-	if err := checkHMACTemplate(template, 64, 32, commonpb.HashType_SHA512); err != nil {
-		t.Errorf("incorrect HMACSHA512Tag256KeyTemplate: %s", err)
-	}
-	template = mac.HMACSHA512Tag512KeyTemplate()
-	if err := checkHMACTemplate(template, 64, 64, commonpb.HashType_SHA512); err != nil {
-		t.Errorf("incorrect HMACSHA512Tag512KeyTemplate: %s", err)
-	}
-	template = mac.AESCMACTag128KeyTemplate()
-	if err := checkCMACTemplate(template, 32, 16); err != nil {
-		t.Errorf("incorrect AESCMACTag128KeyTemplate: %s", err)
-	}
-}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			want, err := testutil.KeyTemplateProto("mac", tc.name)
+			if err != nil {
+				t.Fatalf("testutil.KeyTemplateProto('mac', tc.name) failed: %s", err)
+			}
+			if !proto.Equal(want, tc.template) {
+				t.Errorf("template %s is not equal to '%s'", tc.name, tc.template)
+			}
 
-func checkHMACTemplate(template *tinkpb.KeyTemplate,
-	keySize uint32,
-	tagSize uint32,
-	hashType commonpb.HashType) error {
-	if template.TypeUrl != testutil.HMACTypeURL {
-		return fmt.Errorf("TypeUrl is incorrect")
-	}
-	if template.OutputPrefixType != tinkpb.OutputPrefixType_TINK {
-		return fmt.Errorf("OutputPrefixType is incorrect")
-	}
-	format := new(hmacpb.HmacKeyFormat)
-	if err := proto.Unmarshal(template.Value, format); err != nil {
-		return fmt.Errorf("unable to unmarshal serialized key format")
-	}
-	if format.KeySize != keySize ||
-		format.Params.Hash != hashType ||
-		format.Params.TagSize != tagSize {
-		return fmt.Errorf("KeyFormat is incorrect")
-	}
-	keymanager, err := registry.GetKeyManager(testutil.HMACTypeURL)
-	if err != nil {
-		return fmt.Errorf("Could not obtain HMAC key manager: %v", err)
-	}
-	_, err = keymanager.NewKey(template.Value)
-	if err != nil {
-		return fmt.Errorf("HMAC key manager cannot create key: %v", err)
-	}
-	return nil
-}
+			handle, err := keyset.NewHandle(tc.template)
+			if err != nil {
+				t.Fatalf("keyset.NewHandle(tc.template) failed: %v", err)
+			}
+			primitive, err := mac.New(handle)
+			if err != nil {
+				t.Fatalf("mac.New(handle) failed: %v", err)
+			}
 
-func checkCMACTemplate(template *tinkpb.KeyTemplate, keySize uint32, tagSize uint32) error {
-	if template.TypeUrl != testutil.AESCMACTypeURL {
-		return fmt.Errorf("TypeUrl is incorrect")
+			msg := []byte("this data needs to be authenticated")
+			tag, err := primitive.ComputeMAC(msg)
+			if err != nil {
+				t.Fatalf("primitive.ComputeMAC(msg) failed: %v", err)
+			}
+			if primitive.VerifyMAC(tag, msg); err != nil {
+				t.Errorf("primitive.VerifyMAC(tag, msg) failed: %v", err)
+			}
+		})
 	}
-	format := new(cmacpb.AesCmacKeyFormat)
-	if err := proto.Unmarshal(template.Value, format); err != nil {
-		return fmt.Errorf("unable to unmarshal serialized key format")
-	}
-	if format.KeySize != keySize ||
-		format.Params.TagSize != tagSize {
-		return fmt.Errorf("KeyFormat is incorrect")
-	}
-	keymanager, err := registry.GetKeyManager(testutil.AESCMACTypeURL)
-	if err != nil {
-		return fmt.Errorf("Could not obtain AES CMAC key manager: %v", err)
-	}
-	_, err = keymanager.NewKey(template.Value)
-	if err != nil {
-		return fmt.Errorf("AES CMAC key manager cannot create key: %v", err)
-	}
-	return nil
 }
