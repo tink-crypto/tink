b4e33167c74f22f241b808371c3442024f430f12
juerg <juerg@google.com>
2020-10-28 03:41:04 -0700

    Explicitly set key format in AESSIVKeyTemplate in Go.
    PiperOrigin-RevId: 339428915

--- go/daead/daead_key_templates.go
+++ go/daead/daead_key_templates.go
@@ -14,13 +14,21 @@
 
 package daead
 
-import tinkpb "github.com/google/tink/go/proto/tink_go_proto"
+import (
+	"github.com/golang/protobuf/proto"
+	aspb "github.com/google/tink/go/proto/aes_siv_go_proto"
+	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
+)
 
 // AESSIVKeyTemplate is a KeyTemplate that generates a AES-SIV key.
 func AESSIVKeyTemplate() *tinkpb.KeyTemplate {
+	format := &aspb.AesSivKeyFormat{
+		KeySize: 64,
+	}
+	serializedFormat, _ := proto.Marshal(format)
 	return &tinkpb.KeyTemplate{
-		// Don't set value because KeyFormat is not required.
 		TypeUrl:          aesSIVTypeURL,
 		OutputPrefixType: tinkpb.OutputPrefixType_TINK,
+		Value:            serializedFormat,
 	}
 }
