89e09b21f2bac4916cd5db0dd5f7916e0b5aba7d
juerg <juerg@google.com>
2020-10-29 01:28:45 -0700

    Use testdata to test AEAD KeyTemplates in Go.
    PiperOrigin-RevId: 339620781

--- go/aead/aead_key_templates_test.go
+++ go/aead/aead_key_templates_test.go
@@ -16,155 +16,92 @@ package aead_test
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"testing"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/google/tink/go/aead"
-	"github.com/google/tink/go/core/registry"
+	"github.com/google/tink/go/keyset"
 	"github.com/google/tink/go/testutil"
-	"github.com/google/tink/go/tink"
-	ctrhmacpb "github.com/google/tink/go/proto/aes_ctr_hmac_aead_go_proto"
-	gcmpb "github.com/google/tink/go/proto/aes_gcm_go_proto"
-	commonpb "github.com/google/tink/go/proto/common_go_proto"
 	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
 )
 
-func TestAESGCMKeyTemplates(t *testing.T) {
-	// AES-GCM 128 bit
-	template := aead.AES128GCMKeyTemplate()
-	if err := checkAESGCMKeyTemplate(template, uint32(16), tinkpb.OutputPrefixType_TINK); err != nil {
-		t.Errorf("invalid AES-128 GCM key template: %s", err)
+func TestKeyTemplates(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		template *tinkpb.KeyTemplate
+	}{
+		{
+			name:     "AES128_GCM",
+			template: aead.AES128GCMKeyTemplate(),
+		}, {
+			name:     "AES256_GCM",
+			template: aead.AES256GCMKeyTemplate(),
+		}, {
+			name:     "AES128_CTR_HMAC_SHA256",
+			template: aead.AES128CTRHMACSHA256KeyTemplate(),
+		}, {
+			name:     "AES256_CTR_HMAC_SHA256",
+			template: aead.AES256CTRHMACSHA256KeyTemplate(),
+		}, {
+			name:     "CHACHA20_POLY1305",
+			template: aead.ChaCha20Poly1305KeyTemplate(),
+		}, {
+			name:     "XCHACHA20_POLY1305",
+			template: aead.XChaCha20Poly1305KeyTemplate(),
+		},
 	}
-	if err := testEncryptDecrypt(template, testutil.AESGCMTypeURL); err != nil {
-		t.Errorf("%v", err)
-	}
-
-	// AES-GCM 256 bit
-	template = aead.AES256GCMKeyTemplate()
-	if err := checkAESGCMKeyTemplate(template, uint32(32), tinkpb.OutputPrefixType_TINK); err != nil {
-		t.Errorf("invalid AES-256 GCM key template: %s", err)
-	}
-	if err := testEncryptDecrypt(template, testutil.AESGCMTypeURL); err != nil {
-		t.Errorf("%v", err)
-	}
-
-	// AES-GCM 256 bit No Prefix
-	template = aead.AES256GCMNoPrefixKeyTemplate()
-	if err := checkAESGCMKeyTemplate(template, uint32(32), tinkpb.OutputPrefixType_RAW); err != nil {
-		t.Errorf("invalid AES-256 GCM No Prefix key template: %s", err)
-	}
-	if err := testEncryptDecrypt(template, testutil.AESGCMTypeURL); err != nil {
-		t.Errorf("%v", err)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			want, err := testutil.KeyTemplateProto("aead", tc.name)
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+			if !proto.Equal(want, tc.template) {
+				t.Errorf("template %s is not equal to '%s'", tc.name, tc.template)
+			}
+			if err := testEncryptDecrypt(tc.template); err != nil {
+				t.Errorf("%v", err)
+			}
+		})
 	}
 }
 
-func checkAESGCMKeyTemplate(template *tinkpb.KeyTemplate, keySize uint32, outputPrefixType tinkpb.OutputPrefixType) error {
-	if template.TypeUrl != testutil.AESGCMTypeURL {
-		return fmt.Errorf("incorrect type url")
+func TestNoPrefixKeyTemplates(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		template *tinkpb.KeyTemplate
+	}{
+		{
+			name:     "AES256_GCM",
+			template: aead.AES256GCMNoPrefixKeyTemplate(),
+		},
 	}
-	if template.OutputPrefixType != outputPrefixType {
-		return fmt.Errorf("incorrect output prefix type")
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			want, err := testutil.KeyTemplateProto("aead", tc.name)
+			if err != nil {
+				t.Fatalf("testutil.KeyTemplateProto('aead', tc.name) failed: %s", err)
+			}
+			want.OutputPrefixType = tinkpb.OutputPrefixType_RAW
+			if !proto.Equal(want, tc.template) {
+				t.Errorf("template %s is not equal to '%s'", tc.name, tc.template)
+			}
+			if err := testEncryptDecrypt(tc.template); err != nil {
+				t.Errorf("%v", err)
+			}
+		})
 	}
-	keyFormat := new(gcmpb.AesGcmKeyFormat)
-	err := proto.Unmarshal(template.Value, keyFormat)
+}
+
+func testEncryptDecrypt(template *tinkpb.KeyTemplate) error {
+	handle, err := keyset.NewHandle(template)
 	if err != nil {
-		return fmt.Errorf("cannot deserialize key format: %s", err)
+		return fmt.Errorf("keyset.NewHandle(template) failed: %v", err)
 	}
-	if keyFormat.KeySize != keySize {
-		return fmt.Errorf("incorrect key size, expect %d, got %d", keySize, keyFormat.KeySize)
-	}
-	return nil
-}
-
-func TestAESCTRHMACAEADKeyTemplates(t *testing.T) {
-	// AES-CTR 128 bit with HMAC SHA-256
-	template := aead.AES128CTRHMACSHA256KeyTemplate()
-	if err := checkAESCTRHMACAEADKeyTemplate(template, 16, 16, 16); err != nil {
-		t.Errorf("invalid AES-128 CTR HMAC SHA256 key template: %s", err)
-	}
-
-	if err := testEncryptDecrypt(template, testutil.AESCTRHMACAEADTypeURL); err != nil {
-		t.Errorf("%v", err)
-	}
-
-	// AES-CTR 256 bit with HMAC SHA-256
-	template = aead.AES256CTRHMACSHA256KeyTemplate()
-	if err := checkAESCTRHMACAEADKeyTemplate(template, 32, 16, 32); err != nil {
-		t.Errorf("invalid AES-256 CTR HMAC SHA256 key template: %s", err)
-	}
-	if err := testEncryptDecrypt(template, testutil.AESCTRHMACAEADTypeURL); err != nil {
-		t.Errorf("%v", err)
-	}
-}
-
-func checkAESCTRHMACAEADKeyTemplate(template *tinkpb.KeyTemplate, keySize, ivSize, tagSize uint32) error {
-	if template.TypeUrl != testutil.AESCTRHMACAEADTypeURL {
-		return fmt.Errorf("incorrect type url")
-	}
-	keyFormat := new(ctrhmacpb.AesCtrHmacAeadKeyFormat)
-	err := proto.Unmarshal(template.Value, keyFormat)
+	primitive, err := aead.New(handle)
 	if err != nil {
-		return fmt.Errorf("cannot deserialize key format: %s", err)
-	}
-	if keyFormat.AesCtrKeyFormat.KeySize != keySize {
-		return fmt.Errorf("incorrect key size, expect %d, got %d", keySize, keyFormat.AesCtrKeyFormat.KeySize)
-	}
-	if keyFormat.AesCtrKeyFormat.Params.IvSize != ivSize {
-		return fmt.Errorf("incorrect IV size, expect %d, got %d", ivSize, keyFormat.AesCtrKeyFormat.Params.IvSize)
-	}
-	if keyFormat.HmacKeyFormat.KeySize != 32 {
-		return fmt.Errorf("incorrect HMAC key size, expect 32, got %d", keyFormat.HmacKeyFormat.KeySize)
-	}
-	if keyFormat.HmacKeyFormat.Params.TagSize != tagSize {
-		return fmt.Errorf("incorrect HMAC tag size, expect %d, got %d", tagSize, keyFormat.HmacKeyFormat.Params.TagSize)
-	}
-	if keyFormat.HmacKeyFormat.Params.Hash != commonpb.HashType_SHA256 {
-		return fmt.Errorf("incorrect HMAC hash, expect %q, got %q", commonpb.HashType_SHA256, keyFormat.HmacKeyFormat.Params.Hash)
-	}
-	return nil
-}
-
-func TestChaCha20Poly1305KeyTemplate(t *testing.T) {
-	template := aead.ChaCha20Poly1305KeyTemplate()
-	if template.TypeUrl != testutil.ChaCha20Poly1305TypeURL {
-		t.Errorf("incorrect type url: %v, expected %v", template.TypeUrl, testutil.ChaCha20Poly1305TypeURL)
-	}
-	if err := testEncryptDecrypt(template, testutil.ChaCha20Poly1305TypeURL); err != nil {
-		t.Errorf("%v", err)
-	}
-}
-
-func TestXChaCha20Poly1305KeyTemplate(t *testing.T) {
-	template := aead.XChaCha20Poly1305KeyTemplate()
-	if template.TypeUrl != testutil.XChaCha20Poly1305TypeURL {
-		t.Errorf("incorrect type url: %v, expected %v", template.TypeUrl, testutil.XChaCha20Poly1305TypeURL)
-	}
-	if err := testEncryptDecrypt(template, testutil.XChaCha20Poly1305TypeURL); err != nil {
-		t.Errorf("%v", err)
-	}
-}
-
-func testEncryptDecrypt(template *tinkpb.KeyTemplate, typeURL string) error {
-	key, err := registry.NewKey(template)
-	if err != nil {
-		return fmt.Errorf("failed to get key from template, error: %v", err)
-	}
-
-	sk, err := proto.Marshal(key)
-	if err != nil {
-		return fmt.Errorf("failed to serialize key, error: %v", err)
-	}
-
-	p, err := registry.Primitive(typeURL, sk)
-	if err != nil {
-		return fmt.Errorf("failed to get primitive from serialized key, error: %v", err)
-	}
-
-	primitive, ok := p.(tink.AEAD)
-	if !ok {
-		return errors.New("failed to convert AEAD primitive")
+		return fmt.Errorf("aead.New(handle) failed: %v", err)
 	}
 
 	plaintext := []byte("some data to encrypt")
