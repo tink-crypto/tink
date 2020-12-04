beb90fcd64cad536f957fa85b3a2f152c505d78d
juerg <juerg@google.com>
2020-12-04 02:30:15 -0800

    Remove all non-test bindings from tink testonly bindings.
    PiperOrigin-RevId: 345637632

--- python/tink/cc/pybind/tink_testonly_bindings.cc
+++ python/tink/cc/pybind/tink_testonly_bindings.cc
@@ -13,48 +13,13 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "pybind11/pybind11.h"
-#include "tink/aead.h"
-#include "tink/util/statusor.h"
-#include "tink/cc/pybind/aead.h"
-#include "tink/cc/pybind/cc_key_manager.h"
-#include "tink/cc/pybind/cc_streaming_aead_wrappers.h"
-#include "tink/cc/pybind/cc_tink_config.h"
-#include "tink/cc/pybind/deterministic_aead.h"
-#include "tink/cc/pybind/hybrid_decrypt.h"
-#include "tink/cc/pybind/hybrid_encrypt.h"
-#include "tink/cc/pybind/input_stream_adapter.h"
-#include "tink/cc/pybind/mac.h"
-#include "tink/cc/pybind/output_stream_adapter.h"
-#include "tink/cc/pybind/prf.h"
-#include "tink/cc/pybind/public_key_sign.h"
-#include "tink/cc/pybind/public_key_verify.h"
-#include "tink/cc/pybind/python_file_object_adapter.h"
-#include "tink/cc/pybind/status.h"
-#include "tink/cc/pybind/status_casters.h"
 #include "tink/cc/pybind/status_injector.h"
-#include "tink/cc/pybind/streaming_aead.h"
 
 namespace crypto {
 namespace tink {
 
 PYBIND11_MODULE(tink_testonly_bindings, m) {
-  PybindRegisterCcStreamingAeadWrappers(&m);
-  PybindRegisterAead(&m);
-  PybindRegisterHybridEncrypt(&m);
-  PybindRegisterCcTinkConfig(&m);
-  PybindRegisterStreamingAead(&m);
-  PybindRegisterDeterministicAead(&m);
-  PybindRegisterPublicKeySign(&m);
-  PybindRegisterMac(&m);
-  PybindRegisterPrf(&m);
   pybind11::test::PybindRegisterStatusInjector(&m);
-  pybind11::google_tink::PybindRegisterStatus(&m);
-  PybindRegisterHybridDecrypt(&m);
-  PybindRegisterOutputStreamAdapter(&m);
-  PybindRegisterCcKeyManager(&m);
-  PybindRegisterPythonFileObjectAdapter(&m);
-  PybindRegisterInputStreamAdapter(&m);
-  PybindRegisterPublicKeyVerify(&m);
 }
 
 }  // namespace tink
