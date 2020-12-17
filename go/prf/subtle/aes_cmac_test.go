06b2eaa8da51671130b6edcb640a590a82397e7c
ckl <ckl@google.com>
2020-12-16 21:53:00 -0800

    Clean up prf/subtle package.
     * use testutil provided Wycheproof functionality
     * create subtle.go/subtle_test.go to contain the package description and reusable constructs (e.g. test suite structs)
     * move the tests into a separate subtle_test package, to facilitate depending on the testutil package without circular dependencies
    
    PiperOrigin-RevId: 347957830

--- go/prf/subtle/aes_cmac_test.go
+++ go/prf/subtle/aes_cmac_test.go
@@ -12,14 +12,16 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-package subtle
+package subtle_test
 
 import (
+	"bytes"
 	"encoding/hex"
-	"encoding/json"
-	"os"
-	"path/filepath"
+	"fmt"
 	"testing"
+
+	"github.com/google/tink/go/prf/subtle"
+	"github.com/google/tink/go/testutil"
 )
 
 func TestVectorsRFC4493(t *testing.T) {
@@ -38,7 +40,7 @@ func TestVectorsRFC4493(t *testing.T) {
 		40: "dfa66747de9ae63030ca32611497c827",
 		64: "51f0bebf7e3b9d92fc49741779363cfe",
 	}
-	a, err := NewAESCMACPRF(key)
+	a, err := subtle.NewAESCMACPRF(key)
 	if err != nil {
 		t.Errorf("Could not create cmac.AES object: %v", err)
 	}
@@ -53,106 +55,79 @@ func TestVectorsRFC4493(t *testing.T) {
 	}
 }
 
-type testdata struct {
-	Algorithm        string
-	GeneratorVersion string
-	NumberOfTests    uint32
-	TestGroups       []*testgroup
-}
-
-type testgroup struct {
-	KeySize uint32
-	TagSize uint32
-	Type    string
-	Tests   []*testcase
-}
-
-type testcase struct {
-	Comment string
-	Key     string
-	Msg     string
-	Result  string
-	Tag     string
-	TcID    uint32
-}
-
-func TestVectorsWycheproof(t *testing.T) {
-	srcDir, ok := os.LookupEnv("TEST_SRCDIR")
-	if !ok {
-		t.Skip("TEST_SRCDIR not set")
+func TestAESCMACPRFWycheproofCases(t *testing.T) {
+	testutil.SkipTestIfTestSrcDirIsNotSet(t)
+	suite := new(macSuite)
+	if err := testutil.PopulateSuite(suite, "aes_cmac_test.json"); err != nil {
+		t.Fatalf("Failed populating suite: %s", err)
 	}
-	f, err := os.Open(filepath.Join(srcDir, "wycheproof/testvectors/aes_cmac_test.json"))
-	if err != nil {
-		t.Fatalf("cannot open file: %s", err)
-	}
-	parser := json.NewDecoder(f)
-	data := new(testdata)
-	if err := parser.Decode(data); err != nil {
-		t.Fatalf("cannot decode test data: %s", err)
-	}
+	for _, group := range suite.TestGroups {
+		groupName := fmt.Sprintf("%s-%s(%d)", suite.Algorithm, group.Type, group.KeySize)
+		if group.TagSize%8 != 0 {
+			t.Errorf("For %s, requested tag size is not a multiple of 8, but %d", groupName, group.TagSize)
+			continue
+		}
 
-	for _, g := range data.TestGroups {
-		for _, tc := range g.Tests {
-			key, err := hex.DecodeString(tc.Key)
-			if err != nil || uint32(len(key))*8 != g.KeySize {
-				t.Errorf("Could not decode key for test case %d (%s): %v", tc.TcID, tc.Comment, err)
-				continue
-			}
-			msg, err := hex.DecodeString(tc.Msg)
-			if err != nil {
-				t.Errorf("Could not decode message for test case %d (%s): %v", tc.TcID, tc.Comment, err)
-				continue
-			}
-			aes, err := NewAESCMACPRF(key)
-			valid := tc.Result == "valid"
-			if valid && err != nil {
-				t.Errorf("Could not create cmac.AES for test case %d (%s): %v", tc.TcID, tc.Comment, err)
-				continue
-			}
-			if !valid && err != nil {
-				continue
-			}
-			if g.TagSize%8 != 0 {
-				t.Errorf("Requested tag size for test case %d (%s) is not a multiple of 8, but %d", tc.TcID, tc.Comment, g.TagSize)
-				continue
-			}
-			res, err := aes.ComputePRF(msg, g.TagSize/8)
-			if valid && err != nil {
-				t.Errorf("Could not compute AES-CMAC for test case %d (%s): %v", tc.TcID, tc.Comment, err)
-				continue
-			}
-			if !valid && err != nil {
-				continue
-			}
-			if valid && hex.EncodeToString(res) != tc.Tag {
-				t.Errorf("Compute AES-CMAC and expected for test case %d (%s) do not match:\nComputed: %q\nExpected: %q", tc.TcID, tc.Comment, hex.EncodeToString(res), tc.Tag)
-			}
-			if !valid && hex.EncodeToString(res) == tc.Tag {
-				t.Errorf("Compute AES-CMAC and invalid expected for test case %d (%s) match:\nComputed: %q\nExpected: %q", tc.TcID, tc.Comment, hex.EncodeToString(res), tc.Tag)
-			}
+		for _, test := range group.Tests {
+			caseName := fmt.Sprintf("%s:Case-%d", groupName, test.CaseID)
+			t.Run(caseName, func(t *testing.T) {
+				if uint32(len(test.Key))*8 != group.KeySize {
+					t.Fatalf("Invalid key length: %s", test.Comment)
+				}
+
+				aes, err := subtle.NewAESCMACPRF(test.Key)
+				switch test.Result {
+				case "valid":
+					if err != nil {
+						t.Fatalf("NewAESCMACPRF failed (case: %s):  %v", test.Comment, err)
+					}
+					res, err := aes.ComputePRF(test.Message, group.TagSize/8)
+					if err != nil {
+						t.Fatalf("ComputePRF() failed: %v", err)
+					}
+					if !bytes.Equal(res, test.Tag) {
+						t.Errorf("ComputePRF() result and expected result do not match:\nComputed: %q\nExpected: %q", hex.EncodeToString(res), test.Tag)
+					}
+
+				case "invalid":
+					if err != nil {
+						return
+					}
+					res, err := aes.ComputePRF(test.Message, group.TagSize/8)
+					if err != nil {
+						return
+					}
+					if bytes.Equal(res, test.Tag) {
+						t.Errorf("ComputePRF() result and invalid expected result match:\nComputed: %q\nExpected: %q", hex.EncodeToString(res), test.Tag)
+					}
+
+				default:
+					t.Fatalf("Unsupported test result: %q", test.Result)
+				}
+			})
 		}
 	}
 }
 
 func TestValidateAESCMACPRFParams(t *testing.T) {
-	if err := ValidateAESCMACPRFParams(32); err != nil {
+	if err := subtle.ValidateAESCMACPRFParams(32); err != nil {
 		t.Errorf("Unexpected error validating AES CMAC PRF Params: %v", err)
 	}
-	if err := ValidateAESCMACPRFParams(2); err == nil {
+	if err := subtle.ValidateAESCMACPRFParams(2); err == nil {
 		t.Errorf("Unexpected validation of too short key for AES CMAC PRF Params")
 	}
 }
 
 func TestKeyLength(t *testing.T) {
-	if _, err := NewAESCMACPRF([]byte{0x01, 0x02}); err == nil {
+	if _, err := subtle.NewAESCMACPRF([]byte{0x01, 0x02}); err == nil {
 		t.Errorf("Expected NewAESCMACPRF to fail on short key")
 	}
-	if _, err := NewAESCMACPRF([]byte{
+	if _, err := subtle.NewAESCMACPRF([]byte{
 		0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
 		0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10}); err != nil {
 		t.Errorf("Expected NewAESCMACPRF to work on 16 byte key")
 	}
-	if _, err := NewAESCMACPRF([]byte{
+	if _, err := subtle.NewAESCMACPRF([]byte{
 		0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
 		0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
 		0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
@@ -162,7 +137,7 @@ func TestKeyLength(t *testing.T) {
 }
 
 func TestAESCMACPRFOutputLength(t *testing.T) {
-	prf, err := NewAESCMACPRF([]byte{
+	prf, err := subtle.NewAESCMACPRF([]byte{
 		0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
 		0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
 		0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
