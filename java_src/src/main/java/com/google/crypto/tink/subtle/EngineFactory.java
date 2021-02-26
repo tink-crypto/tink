925d976313ce4d72b8a620e23c5c9b36a53f3e06
kste <kste@google.com>
2021-02-26 06:26:28 -0800

    Remove getCustom methods from EngineFactory.
    PiperOrigin-RevId: 359744491

--- java_src/src/main/java/com/google/crypto/tink/subtle/EngineFactory.java
+++ java_src/src/main/java/com/google/crypto/tink/subtle/EngineFactory.java
@@ -55,76 +55,34 @@ public final class EngineFactory<T_WRAPPER extends EngineWrapper<T_ENGINE>, T_EN
               "GmsCore_OpenSSL" /* Conscrypt in GmsCore, updatable thus preferrable */,
               "AndroidOpenSSL" /* Conscrypt in AOSP, not updatable but still better than BC */);
     } else {
-      defaultPolicy = new ArrayList<Provider>();
+      defaultPolicy = new ArrayList<>();
     }
   }
 
   public static final EngineFactory<EngineWrapper.TCipher, Cipher> CIPHER =
       new EngineFactory<>(new EngineWrapper.TCipher());
 
-  public static final EngineFactory<EngineWrapper.TCipher, Cipher> getCustomCipherProvider(
-      boolean letFallbackToDefault, String... providerNames) {
-    return new EngineFactory<EngineWrapper.TCipher, Cipher>(
-        new EngineWrapper.TCipher(), toProviderList(providerNames), letFallbackToDefault);
-  }
-
   public static final EngineFactory<EngineWrapper.TMac, Mac> MAC =
       new EngineFactory<>(new EngineWrapper.TMac());
-
-  public static final EngineFactory<EngineWrapper.TMac, Mac> getCustomMacProvider(
-      boolean letFallbackToDefault, String... providerNames) {
-    return new EngineFactory<EngineWrapper.TMac, Mac>(
-        new EngineWrapper.TMac(), toProviderList(providerNames), letFallbackToDefault);
-  }
 
   public static final EngineFactory<EngineWrapper.TSignature, Signature> SIGNATURE =
       new EngineFactory<>(new EngineWrapper.TSignature());
 
-  public static final EngineFactory<EngineWrapper.TSignature, Signature> getCustomSignatureProvider(
-      boolean letFallbackToDefault, String... providerNames) {
-    return new EngineFactory<EngineWrapper.TSignature, Signature>(
-        new EngineWrapper.TSignature(), toProviderList(providerNames), letFallbackToDefault);
-  }
-
   public static final EngineFactory<EngineWrapper.TMessageDigest, MessageDigest> MESSAGE_DIGEST =
       new EngineFactory<>(new EngineWrapper.TMessageDigest());
-
-  public static final EngineFactory<EngineWrapper.TMessageDigest, MessageDigest>
-      getCustomMessageDigestProvider(boolean letFallbackToDefault, String... providerNames) {
-    return new EngineFactory<EngineWrapper.TMessageDigest, MessageDigest>(
-        new EngineWrapper.TMessageDigest(), toProviderList(providerNames), letFallbackToDefault);
-  }
 
   public static final EngineFactory<EngineWrapper.TKeyAgreement, KeyAgreement> KEY_AGREEMENT =
       new EngineFactory<>(new EngineWrapper.TKeyAgreement());
 
-  public static final EngineFactory<EngineWrapper.TKeyAgreement, KeyAgreement>
-      getCustomKeyAgreementProvider(boolean letFallbackToDefault, String... providerNames) {
-    return new EngineFactory<EngineWrapper.TKeyAgreement, KeyAgreement>(
-        new EngineWrapper.TKeyAgreement(), toProviderList(providerNames), letFallbackToDefault);
-  }
-
   public static final EngineFactory<EngineWrapper.TKeyPairGenerator, KeyPairGenerator>
       KEY_PAIR_GENERATOR = new EngineFactory<>(new EngineWrapper.TKeyPairGenerator());
-
-  public static final EngineFactory<EngineWrapper.TKeyPairGenerator, KeyPairGenerator>
-      getCustomKeyPairGeneratorProvider(boolean letFallbackToDefault, String... providerNames) {
-    return new EngineFactory<EngineWrapper.TKeyPairGenerator, KeyPairGenerator>(
-        new EngineWrapper.TKeyPairGenerator(), toProviderList(providerNames), letFallbackToDefault);
-  }
 
   public static final EngineFactory<EngineWrapper.TKeyFactory, KeyFactory> KEY_FACTORY =
       new EngineFactory<>(new EngineWrapper.TKeyFactory());
 
-  public static final EngineFactory<EngineWrapper.TKeyFactory, KeyFactory>
-      getCustomKeyFactoryProvider(boolean letFallbackToDefault, String... providerNames) {
-    return new EngineFactory<EngineWrapper.TKeyFactory, KeyFactory>(
-        new EngineWrapper.TKeyFactory(), toProviderList(providerNames), letFallbackToDefault);
-  }
-
   /** Helper function to get a list of Providers from names. */
   public static List<Provider> toProviderList(String... providerNames) {
-    List<Provider> providers = new ArrayList<Provider>();
+    List<Provider> providers = new ArrayList<>();
     for (String s : providerNames) {
       Provider p = Security.getProvider(s);
       if (p != null) {
@@ -140,18 +98,6 @@ public final class EngineFactory<T_WRAPPER extends EngineWrapper<T_ENGINE>, T_EN
     this.instanceBuilder = instanceBuilder;
     this.policy = defaultPolicy;
     this.letFallback = DEFAULT_LET_FALLBACK;
-  }
-
-  public EngineFactory(T_WRAPPER instanceBuilder, List<Provider> policy) {
-    this.instanceBuilder = instanceBuilder;
-    this.policy = policy;
-    this.letFallback = DEFAULT_LET_FALLBACK;
-  }
-
-  public EngineFactory(T_WRAPPER instanceBuilder, List<Provider> policy, boolean letFallback) {
-    this.instanceBuilder = instanceBuilder;
-    this.policy = policy;
-    this.letFallback = letFallback;
   }
 
   public T_ENGINE getInstance(String algorithm) throws GeneralSecurityException {
@@ -171,7 +117,7 @@ public final class EngineFactory<T_WRAPPER extends EngineWrapper<T_ENGINE>, T_EN
     throw new GeneralSecurityException("No good Provider found.", cause);
   }
 
-  private T_WRAPPER instanceBuilder;
-  private List<Provider> policy;
-  private boolean letFallback;
+  private final T_WRAPPER instanceBuilder;
+  private final List<Provider> policy;
+  private final boolean letFallback;
 }
--- java_src/src/test/java/com/google/crypto/tink/subtle/EngineFactoryTest.java
+++ java_src/src/test/java/com/google/crypto/tink/subtle/EngineFactoryTest.java
@@ -16,13 +16,6 @@
 
 package com.google.crypto.tink.subtle;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
-import java.security.GeneralSecurityException;
-import java.security.KeyPairGenerator;
-import java.security.Security;
-import javax.crypto.Cipher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -35,61 +28,6 @@ public class EngineFactoryTest {
   public void testAtLeastGetsACipherByDefault() throws Exception {
     EngineFactory.CIPHER.getInstance("AES");
     // didn't throw
-  }
-
-  @Test // A bunch of default settings for a regular JVM.
-  public void testCustomListOfProviders() throws Exception {
-    if (SubtleUtil.isAndroid()) {
-      // Android doesn't have SUN, SunJCE, or SunEC provider.
-      return;
-    }
-
-    Cipher c = EngineFactory.getCustomCipherProvider(true, "X", "Y").getInstance("AES");
-    KeyPairGenerator kpg =
-        EngineFactory.getCustomKeyPairGeneratorProvider(true, "X", "Y").getInstance("EC");
-    assertEquals("SUN", Security.getProviders()[0].getName()); // The first provider
-    assertEquals("SunJCE", c.getProvider().getName()); // The first one to implement AES
-    assertEquals("SunEC", kpg.getProvider().getName()); // The first one to implement EC stuff
-
-    kpg = EngineFactory.getCustomKeyPairGeneratorProvider(false, "SunEC").getInstance("EC");
-    c = EngineFactory.getCustomCipherProvider(false, "SunJCE").getInstance("AES");
-
-    try {
-      EngineFactory.getCustomCipherProvider(false, "SunEC").getInstance("AES");
-      fail();
-    } catch (GeneralSecurityException e) {
-      // expected
-    }
-    try {
-      EngineFactory.getCustomKeyPairGeneratorProvider(false, "SunJCE").getInstance("AES");
-      fail();
-    } catch (GeneralSecurityException e) {
-      // expected
-    }
-  }
-
-  @Test
-  public void testNoProviders() throws Exception {
-    try {
-      EngineFactory.getCustomCipherProvider(false).getInstance("AES");
-      fail();
-    } catch (GeneralSecurityException e) {
-      // expected
-    }
-
-    try {
-      EngineFactory.getCustomCipherProvider(true).getInstance("I don't exist, no point trying");
-      fail();
-    } catch (GeneralSecurityException e) {
-      // expected
-    }
-
-    try {
-      EngineFactory.getCustomKeyPairGeneratorProvider(false, "SunJCE").getInstance("EC");
-      fail();
-    } catch (GeneralSecurityException e) {
-      // expected
-    }
   }
 
   @Test
