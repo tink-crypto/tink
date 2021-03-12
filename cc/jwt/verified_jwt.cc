3ad98f7527fe8e4373d3beae90ff165b617e771a
juerg <juerg@google.com>
2021-03-12 01:02:13 -0800

    Add verified_jwt.
    PiperOrigin-RevId: 362469334

---
+++ cc/jwt/verified_jwt.cc
@@ -0,0 +1,140 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "tink/jwt/verified_jwt.h"
+
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_format.h"
+#include "absl/strings/substitute.h"
+#include "tink/jwt/jwt_names.h"
+#include "tink/jwt/internal/json_util.h"
+
+namespace crypto {
+namespace tink {
+
+VerifiedJwt::VerifiedJwt(RawJwt raw_jwt) {
+  raw_jwt_ = raw_jwt;
+}
+
+bool VerifiedJwt::HasIssuer() const {
+  return raw_jwt_.HasIssuer();
+}
+
+util::StatusOr<std::string> VerifiedJwt::GetIssuer() const {
+  return raw_jwt_.GetIssuer();
+}
+
+bool VerifiedJwt::HasSubject() const {
+  return raw_jwt_.HasSubject();
+}
+
+util::StatusOr<std::string> VerifiedJwt::GetSubject() const {
+  return raw_jwt_.GetSubject();
+}
+
+bool VerifiedJwt::HasAudiences() const {
+  return raw_jwt_.HasAudiences();
+}
+
+util::StatusOr<std::vector<std::string>> VerifiedJwt::GetAudiences() const {
+  return raw_jwt_.GetAudiences();
+}
+
+bool VerifiedJwt::HasJwtId() const {
+  return raw_jwt_.HasJwtId();
+}
+
+util::StatusOr<std::string> VerifiedJwt::GetJwtId() const {
+  return raw_jwt_.GetJwtId();
+}
+
+bool VerifiedJwt::HasExpiration() const {
+  return raw_jwt_.HasExpiration();
+}
+
+util::StatusOr<absl::Time> VerifiedJwt::GetExpiration() const {
+  return raw_jwt_.GetExpiration();
+}
+
+bool VerifiedJwt::HasNotBefore() const {
+  return raw_jwt_.HasNotBefore();
+}
+
+util::StatusOr<absl::Time> VerifiedJwt::GetNotBefore() const {
+  return raw_jwt_.GetNotBefore();
+}
+
+bool VerifiedJwt::HasIssuedAt() const {
+  return raw_jwt_.HasIssuedAt();
+}
+
+util::StatusOr<absl::Time> VerifiedJwt::GetIssuedAt() const {
+  return raw_jwt_.GetIssuedAt();
+}
+
+bool VerifiedJwt::IsNullClaim(absl::string_view name) const {
+  return raw_jwt_.IsNullClaim(name);
+}
+
+bool VerifiedJwt::HasBooleanClaim(absl::string_view name) const {
+  return raw_jwt_.HasBooleanClaim(name);
+}
+
+util::StatusOr<bool> VerifiedJwt::GetBooleanClaim(
+    absl::string_view name) const {
+  return raw_jwt_.GetBooleanClaim(name);
+}
+
+bool VerifiedJwt::HasStringClaim(absl::string_view name) const {
+  return raw_jwt_.HasStringClaim(name);
+}
+
+util::StatusOr<std::string> VerifiedJwt::GetStringClaim(
+    absl::string_view name) const {
+  return raw_jwt_.GetStringClaim(name);
+}
+
+bool VerifiedJwt::HasNumberClaim(absl::string_view name) const {
+  return raw_jwt_.HasNumberClaim(name);
+}
+
+util::StatusOr<double> VerifiedJwt::GetNumberClaim(
+    absl::string_view name) const {
+  return raw_jwt_.GetNumberClaim(name);
+}
+
+bool VerifiedJwt::HasJsonObjectClaim(absl::string_view name) const {
+  return raw_jwt_.HasJsonObjectClaim(name);
+}
+
+util::StatusOr<std::string> VerifiedJwt::GetJsonObjectClaim(
+    absl::string_view name) const {
+  return raw_jwt_.GetJsonObjectClaim(name);
+}
+
+bool VerifiedJwt::HasJsonArrayClaim(absl::string_view name) const {
+  return raw_jwt_.HasJsonArrayClaim(name);
+}
+
+util::StatusOr<std::string> VerifiedJwt::GetJsonArrayClaim(
+    absl::string_view name) const {
+  return raw_jwt_.GetJsonArrayClaim(name);
+}
+
+util::StatusOr<std::string> VerifiedJwt::ToString() {
+  return raw_jwt_.ToString();
+}
+
+}  // namespace tink
+}  // namespace crypto
---
+++ cc/jwt/verified_jwt.h
@@ -0,0 +1,87 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef TINK_JWT_VERIFIED_JWT_H_
+#define TINK_JWT_VERIFIED_JWT_H_
+
+#include "google/protobuf/struct.pb.h"
+#include "absl/strings/string_view.h"
+#include "absl/time/clock.h"
+#include "absl/time/time.h"
+#include "tink/util/status.h"
+#include "tink/util/statusor.h"
+#include "tink/jwt/raw_jwt.h"
+
+namespace crypto {
+namespace tink {
+
+///////////////////////////////////////////////////////////////////////////////
+// A read-only JSON Web Token</a> (JWT), https://tools.ietf.org/html/rfc7519.
+//
+// A new instance of this class is returned as the result of a sucessfully
+// verification of a JWT. It contains the payload of the token, but no header
+// information (typ, cty, alg and kid).
+class VerifiedJwt {
+ public:
+  // TODO(juerg): Restrict access to this constructor.
+  // VerifiedJwt object should only be constructed by JWT primitives and maybe
+  // the VerifedJwt Tests. I first have to figure out what's the best way to do
+  // this.
+  explicit VerifiedJwt(RawJwt raw_jwt);
+
+  // VerifiedJwt objects are copiable and movable.
+  VerifiedJwt(const VerifiedJwt&) = default;
+  VerifiedJwt& operator=(const VerifiedJwt&) = default;
+  VerifiedJwt(VerifiedJwt&& other) = default;
+  VerifiedJwt& operator=(VerifiedJwt&& other) = default;
+
+  bool HasIssuer() const;
+  util::StatusOr<std::string> GetIssuer() const;
+  bool HasSubject() const;
+  util::StatusOr<std::string> GetSubject() const;
+  bool HasAudiences() const;
+  util::StatusOr<std::vector<std::string>> GetAudiences() const;
+  bool HasJwtId() const;
+  util::StatusOr<std::string> GetJwtId() const;
+  bool HasExpiration() const;
+  util::StatusOr<absl::Time> GetExpiration() const;
+  bool HasNotBefore() const;
+  util::StatusOr<absl::Time> GetNotBefore() const;
+  bool HasIssuedAt() const;
+  util::StatusOr<absl::Time> GetIssuedAt() const;
+
+  bool IsNullClaim(absl::string_view name) const;
+  bool HasBooleanClaim(absl::string_view name) const;
+  util::StatusOr<bool> GetBooleanClaim(absl::string_view name) const;
+  bool HasStringClaim(absl::string_view name) const;
+  util::StatusOr<std::string> GetStringClaim(absl::string_view name) const;
+  bool HasNumberClaim(absl::string_view name) const;
+  util::StatusOr<double> GetNumberClaim(absl::string_view name) const;
+  bool HasJsonObjectClaim(absl::string_view name) const;
+  util::StatusOr<std::string> GetJsonObjectClaim(absl::string_view name) const;
+  bool HasJsonArrayClaim(absl::string_view name) const;
+  util::StatusOr<std::string> GetJsonArrayClaim(absl::string_view name) const;
+
+  util::StatusOr<std::string> ToString();
+
+
+
+ private:
+  RawJwt raw_jwt_;
+};
+
+}  // namespace tink
+}  // namespace crypto
+
+#endif  // TINK_JWT_VERIFIED_JWT_H_
