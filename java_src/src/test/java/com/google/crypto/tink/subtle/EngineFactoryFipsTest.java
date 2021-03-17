2656c0f2a95e710913bf7930e7cbca943f4ad074
kste <kste@google.com>
2021-03-17 08:02:52 -0700

    Add additional tests to check that EngineFactory uses the correct provider in FIPS mode.
    PiperOrigin-RevId: 363420098

--- java_src/src/test/java/com/google/crypto/tink/subtle/EngineFactoryFipsTest.java
+++ java_src/src/test/java/com/google/crypto/tink/subtle/EngineFactoryFipsTest.java
@@ -20,7 +20,6 @@ import com.google.crypto.tink.config.TinkFips;
 import java.security.GeneralSecurityException;
 import java.security.Provider;
 import java.security.Security;
-import javax.crypto.Cipher;
 import org.conscrypt.Conscrypt;
 import org.junit.Assume;
 import org.junit.Before;
@@ -36,21 +35,71 @@ public final class EngineFactoryFipsTest {
   public void setUp() throws Exception {
     // All tests here require that Tink is build in FIPS-mode.
     Assume.assumeTrue(TinkFips.useOnlyFips());
+
+    // Register conscrypt
+    Conscrypt.checkAvailability();
   }
 
   @Test
-  public void testConscryptUsedAsProvider() throws Exception {
-    Conscrypt.checkAvailability();
+  public void testConscryptUsedAsProviderForCiphers() throws Exception {
     Provider p = Conscrypt.newProvider();
     Security.addProvider(p);
-    Cipher c = EngineFactory.CIPHER.getInstance("AES/GCM/NoPadding");
-    assertThat(c.getProvider().getName()).isEqualTo(p.getName());
+    String conscryptName = p.getName();
+
+    assertThat(EngineFactory.CIPHER.getInstance("AES/GCM/NoPadding").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.CIPHER.getInstance("AES/CTR/NoPadding").getProvider().getName())
+        .isEqualTo(conscryptName);
+  }
+
+  @Test
+  public void testConscryptUsedAsProviderForMac() throws Exception {
+    Provider p = Conscrypt.newProvider();
+    Security.addProvider(p);
+    String conscryptName = p.getName();
+
+    assertThat(EngineFactory.MAC.getInstance("HMACSHA1").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.MAC.getInstance("HMACSHA224").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.MAC.getInstance("HMACSHA256").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.MAC.getInstance("HMACSHA384").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.MAC.getInstance("HMACSHA512").getProvider().getName())
+        .isEqualTo(conscryptName);
+  }
+
+  @Test
+  public void testConscryptUsedAsProviderForSigner() throws Exception {
+    Provider p = Conscrypt.newProvider();
+    Security.addProvider(p);
+    String conscryptName = p.getName();
+
+    assertThat(EngineFactory.SIGNATURE.getInstance("SHA256withRSA").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.SIGNATURE.getInstance("SHA384withRSA").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.SIGNATURE.getInstance("SHA512withRSA").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.SIGNATURE.getInstance("SHA256withRSAandMGF1").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.SIGNATURE.getInstance("SHA384withRSAandMGF1").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.SIGNATURE.getInstance("SHA512withRSAandMGF1").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.SIGNATURE.getInstance("SHA256withECDSA").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.SIGNATURE.getInstance("SHA384withECDSA").getProvider().getName())
+        .isEqualTo(conscryptName);
+    assertThat(EngineFactory.SIGNATURE.getInstance("SHA512withECDSA").getProvider().getName())
+        .isEqualTo(conscryptName);
   }
 
   @Test
   public void testNoFallback() throws Exception {
-    Conscrypt.checkAvailability();
-    Security.addProvider(Conscrypt.newProvider());
+    Provider p = Conscrypt.newProvider();
+    Security.addProvider(p);
 
     // Conscrypt does not provide "AES", so this must fail and not use another provider.
     assertThrows(GeneralSecurityException.class, () -> EngineFactory.CIPHER.getInstance("AES"));
