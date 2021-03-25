cc79d6a9addcda16d625335239f922775a0a4584
juerg <juerg@google.com>
2021-03-25 02:58:50 -0700

    Add hmac jwt_key_templates and jwt_mac_config in C++.
    PiperOrigin-RevId: 364995867

---
+++ cc/jwt/jwt_key_templates.cc
@@ -0,0 +1,61 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "tink/jwt/jwt_key_templates.h"
+
+#include "proto/common.pb.h"
+#include "proto/jwt_hmac.pb.h"
+#include "proto/tink.pb.h"
+
+using google::crypto::tink::HashType;
+using google::crypto::tink::JwtHmacKeyFormat;
+using google::crypto::tink::KeyTemplate;
+using google::crypto::tink::OutputPrefixType;
+
+namespace crypto {
+namespace tink {
+
+namespace {
+
+KeyTemplate* NewHmacKeyTemplate(HashType hash_type) {
+  KeyTemplate* key_template = new KeyTemplate;
+  key_template->set_type_url(
+      "type.googleapis.com/google.crypto.tink.JwtHmacKey");
+  key_template->set_output_prefix_type(OutputPrefixType::RAW);
+  JwtHmacKeyFormat key_format;
+  key_format.set_key_size(32);
+  key_format.set_hash_type(hash_type);
+  key_format.SerializeToString(key_template->mutable_value());
+  return key_template;
+}
+
+}  // anonymous namespace
+
+const KeyTemplate& JwtHs256Template() {
+  static const KeyTemplate* key_template = NewHmacKeyTemplate(HashType::SHA256);
+  return *key_template;
+}
+
+const KeyTemplate& JwtHs384Template() {
+  static const KeyTemplate* key_template = NewHmacKeyTemplate(HashType::SHA384);
+  return *key_template;
+}
+
+const KeyTemplate& JwtHs512Template() {
+  static const KeyTemplate* key_template = NewHmacKeyTemplate(HashType::SHA512);
+  return *key_template;
+}
+
+}  // namespace tink
+}  // namespace crypto
---
+++ cc/jwt/jwt_key_templates.h
@@ -0,0 +1,41 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef TINK_JWT_JWT_KEY_TEMPLATES_H_
+#define TINK_JWT_JWT_KEY_TEMPLATES_H_
+
+#include "proto/tink.pb.h"
+
+namespace crypto {
+namespace tink {
+
+///////////////////////////////////////////////////////////////////////////////
+// Pre-generated KeyTemplate for Jwt key types. One can use these templates
+// to generate new KeysetHandle object with fresh keys.
+// To generate a new keyset that contains a single AesGcmKey, one can do:
+//
+//   auto status = JwtMacRegister();
+//   if (!status.ok()) { /* fail with error */ }
+//   auto handle_result =
+//       KeysetHandle::GenerateNew(JwtHs256Template());
+//   if (!handle_result.ok()) { /* fail with error */ }
+//   auto keyset_handle = std::move(handle_result.ValueOrDie());
+const google::crypto::tink::KeyTemplate& JwtHs256Template();
+const google::crypto::tink::KeyTemplate& JwtHs384Template();
+const google::crypto::tink::KeyTemplate& JwtHs512Template();
+
+}  // namespace tink
+}  // namespace crypto
+
+#endif  // TINK_JWT_JWT_KEY_TEMPLATES_H_
