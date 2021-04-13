ddbb3298ac8a06c62b766aa8f96a4a6d5e681abd
juerg <juerg@google.com>
2021-04-13 05:59:40 -0700

    Fix typo in _jwt_mac_wrapper_test.py.
    PiperOrigin-RevId: 368198876

--- python/tink/jwt/_jwt_mac_wrapper_test.py
+++ python/tink/jwt/_jwt_mac_wrapper_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for tink.python.tink.jwt._jwt_hmac_key_manager."""
+"""Tests for tink.python.tink.jwt._jwt_mac_wrapper."""
 
 from absl.testing import absltest
 from absl.testing import parameterized
