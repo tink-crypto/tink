420bbdb2e8da47cf8fd6246a547e69759d71b30d
juerg <juerg@google.com>
2021-05-06 01:04:28 -0700

    Internal change.
    PiperOrigin-RevId: 372292114

--- testing/java_src/javatests/com/google/crypto/tink/testing/TestingServicesTest.java
+++ testing/java_src/javatests/com/google/crypto/tink/testing/TestingServicesTest.java
@@ -75,6 +75,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+
 @RunWith(JUnit4.class)
 public final class TestingServicesTest {
   private Server server;
