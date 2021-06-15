87e1788f0d86d46030cf828781d16b7d3d8c7008
ckl <ckl@google.com>
2021-06-15 15:39:34 -0700

    Add Python 3.9 classifier to the package configuration.
    PiperOrigin-RevId: 379589592

--- python/setup.py
+++ python/setup.py
@@ -284,6 +284,7 @@ def main():
       classifiers=[
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
           'Topic :: Software Development :: Libraries',
       ],
       license='Apache 2.0',
