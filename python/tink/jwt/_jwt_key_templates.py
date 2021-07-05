61797db5382e280b09e81467d6cb46c22cf76877
juerg <juerg@google.com>
2021-07-05 06:24:18 -0700

    Add TINK output prefix type JWT templates in Python.
    PiperOrigin-RevId: 383126248

--- python/tink/jwt/_jwt_key_templates.py
+++ python/tink/jwt/_jwt_key_templates.py
@@ -24,24 +24,26 @@ _F4 = 65537
 # TODO(juerg): Add TINK key templates.
 
 
-def _create_jwt_hmac_template(algorithm: jwt_hmac_pb2.JwtHmacAlgorithm,
-                              key_size: int) -> tink_pb2.KeyTemplate:
+def _create_jwt_hmac_template(
+    algorithm: jwt_hmac_pb2.JwtHmacAlgorithm, key_size: int,
+    output_prefix_type: tink_pb2.OutputPrefixType) -> tink_pb2.KeyTemplate:
   key_format = jwt_hmac_pb2.JwtHmacKeyFormat(
       algorithm=algorithm, key_size=key_size)
   return tink_pb2.KeyTemplate(
       type_url='type.googleapis.com/google.crypto.tink.JwtHmacKey',
       value=key_format.SerializeToString(),
-      output_prefix_type=tink_pb2.RAW)
+      output_prefix_type=output_prefix_type)
 
 
 def _create_jwt_ecdsa_template(
-    algorithm: jwt_ecdsa_pb2.JwtEcdsaAlgorithm) -> tink_pb2.KeyTemplate:
+    algorithm: jwt_ecdsa_pb2.JwtEcdsaAlgorithm,
+    output_prefix_type: tink_pb2.OutputPrefixType) -> tink_pb2.KeyTemplate:
   key_format = jwt_ecdsa_pb2.JwtEcdsaKeyFormat(
       algorithm=algorithm)
   return tink_pb2.KeyTemplate(
       type_url='type.googleapis.com/google.crypto.tink.JwtEcdsaPrivateKey',
       value=key_format.SerializeToString(),
-      output_prefix_type=tink_pb2.RAW)
+      output_prefix_type=output_prefix_type)
 
 
 # TODO(juerg): Move this function into a util lib.
@@ -59,9 +61,8 @@ def _num_to_bytes(n: int) -> bytes:
 
 
 def _create_jwt_rsa_ssa_pkcs1_template(
-    algorithm: jwt_rsa_ssa_pkcs1_pb2.JwtRsaSsaPkcs1Algorithm,
-    modulus_size: int
-) -> tink_pb2.KeyTemplate:
+    algorithm: jwt_rsa_ssa_pkcs1_pb2.JwtRsaSsaPkcs1Algorithm, modulus_size: int,
+    output_prefix_type: tink_pb2.OutputPrefixType) -> tink_pb2.KeyTemplate:
   key_format = jwt_rsa_ssa_pkcs1_pb2.JwtRsaSsaPkcs1KeyFormat(
       algorithm=algorithm,
       modulus_size_in_bits=modulus_size,
@@ -69,13 +70,12 @@ def _create_jwt_rsa_ssa_pkcs1_template(
   return tink_pb2.KeyTemplate(
       type_url='type.googleapis.com/google.crypto.tink.JwtRsaSsaPkcs1PrivateKey',
       value=key_format.SerializeToString(),
-      output_prefix_type=tink_pb2.RAW)
+      output_prefix_type=output_prefix_type)
 
 
 def _create_jwt_rsa_ssa_pss_template(
-    algorithm: jwt_rsa_ssa_pss_pb2.JwtRsaSsaPssAlgorithm,
-    modulus_size: int
-) -> tink_pb2.KeyTemplate:
+    algorithm: jwt_rsa_ssa_pss_pb2.JwtRsaSsaPssAlgorithm, modulus_size: int,
+    output_prefix_type: tink_pb2.OutputPrefixType) -> tink_pb2.KeyTemplate:
   key_format = jwt_rsa_ssa_pss_pb2.JwtRsaSsaPssKeyFormat(
       algorithm=algorithm,
       modulus_size_in_bits=modulus_size,
@@ -83,64 +83,136 @@ def _create_jwt_rsa_ssa_pss_template(
   return tink_pb2.KeyTemplate(
       type_url='type.googleapis.com/google.crypto.tink.JwtRsaSsaPssPrivateKey',
       value=key_format.SerializeToString(),
-      output_prefix_type=tink_pb2.RAW)
+      output_prefix_type=output_prefix_type)
 
 
 # Hmac Templates
 def jwt_hs256_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_hmac_template(jwt_hmac_pb2.HS256, 32)
+  return _create_jwt_hmac_template(jwt_hmac_pb2.HS256, 32, tink_pb2.TINK)
+
+
+def raw_jwt_hs256_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_hmac_template(jwt_hmac_pb2.HS256, 32, tink_pb2.RAW)
 
 
 def jwt_hs384_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_hmac_template(jwt_hmac_pb2.HS384, 48)
+  return _create_jwt_hmac_template(jwt_hmac_pb2.HS384, 48, tink_pb2.TINK)
+
+
+def raw_jwt_hs384_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_hmac_template(jwt_hmac_pb2.HS384, 48, tink_pb2.RAW)
 
 
 def jwt_hs512_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_hmac_template(jwt_hmac_pb2.HS512, 64)
+  return _create_jwt_hmac_template(jwt_hmac_pb2.HS512, 64, tink_pb2.TINK)
+
+
+def raw_jwt_hs512_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_hmac_template(jwt_hmac_pb2.HS512, 64, tink_pb2.RAW)
 
 
 # ECDSA Templates
 def jwt_es256_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_ecdsa_template(jwt_ecdsa_pb2.ES256)
+  return _create_jwt_ecdsa_template(jwt_ecdsa_pb2.ES256, tink_pb2.TINK)
+
+
+def raw_jwt_es256_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_ecdsa_template(jwt_ecdsa_pb2.ES256, tink_pb2.RAW)
 
 
 def jwt_es384_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_ecdsa_template(jwt_ecdsa_pb2.ES384)
+  return _create_jwt_ecdsa_template(jwt_ecdsa_pb2.ES384, tink_pb2.TINK)
+
+
+def raw_jwt_es384_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_ecdsa_template(jwt_ecdsa_pb2.ES384, tink_pb2.RAW)
 
 
 def jwt_es512_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_ecdsa_template(jwt_ecdsa_pb2.ES512)
+  return _create_jwt_ecdsa_template(jwt_ecdsa_pb2.ES512, tink_pb2.TINK)
+
+
+def raw_jwt_es512_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_ecdsa_template(jwt_ecdsa_pb2.ES512, tink_pb2.RAW)
 
 
 # RSA SSA PKCS1 Templates
 def jwt_rs256_2048_f4_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS256, 2048)
+  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS256, 2048,
+                                            tink_pb2.TINK)
+
+
+def raw_jwt_rs256_2048_f4_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS256, 2048,
+                                            tink_pb2.RAW)
 
 
 def jwt_rs256_3072_f4_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS256, 3072)
+  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS256, 3072,
+                                            tink_pb2.TINK)
+
+
+def raw_jwt_rs256_3072_f4_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS256, 3072,
+                                            tink_pb2.RAW)
 
 
 def jwt_rs384_3072_f4_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS384, 3072)
+  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS384, 3072,
+                                            tink_pb2.TINK)
+
+
+def raw_jwt_rs384_3072_f4_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS384, 3072,
+                                            tink_pb2.RAW)
 
 
 def jwt_rs512_4096_f4_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS512, 4096)
+  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS512, 4096,
+                                            tink_pb2.TINK)
+
+
+def raw_jwt_rs512_4096_f4_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_rsa_ssa_pkcs1_template(jwt_rsa_ssa_pkcs1_pb2.RS512, 4096,
+                                            tink_pb2.RAW)
 
 
 # RSA SSA PSS Templates
 def jwt_ps256_2048_f4_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS256, 2048)
+  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS256, 2048,
+                                          tink_pb2.TINK)
+
+
+def raw_jwt_ps256_2048_f4_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS256, 2048,
+                                          tink_pb2.RAW)
 
 
 def jwt_ps256_3072_f4_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS256, 3072)
+  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS256, 3072,
+                                          tink_pb2.TINK)
+
+
+def raw_jwt_ps256_3072_f4_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS256, 3072,
+                                          tink_pb2.RAW)
 
 
 def jwt_ps384_3072_f4_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS384, 3072)
+  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS384, 3072,
+                                          tink_pb2.TINK)
+
+
+def raw_jwt_ps384_3072_f4_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS384, 3072,
+                                          tink_pb2.RAW)
 
 
 def jwt_ps512_4096_f4_template() -> tink_pb2.KeyTemplate:
-  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS512, 4096)
+  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS512, 4096,
+                                          tink_pb2.TINK)
+
+
+def raw_jwt_ps512_4096_f4_template() -> tink_pb2.KeyTemplate:
+  return _create_jwt_rsa_ssa_pss_template(jwt_rsa_ssa_pss_pb2.PS512, 4096,
+                                          tink_pb2.RAW)
--- python/tink/jwt/_jwt_key_templates_test.py
+++ python/tink/jwt/_jwt_key_templates_test.py
@@ -34,19 +34,33 @@ class JwtKeyTemplatesTest(parameterized.TestCase):
 
   @parameterized.parameters([
       ('JWT_HS256', jwt.jwt_hs256_template()),
+      ('JWT_HS256_RAW', jwt.raw_jwt_hs256_template()),
       ('JWT_HS384', jwt.jwt_hs384_template()),
+      ('JWT_HS384_RAW', jwt.raw_jwt_hs384_template()),
       ('JWT_HS512', jwt.jwt_hs512_template()),
+      ('JWT_HS512_RAW', jwt.raw_jwt_hs512_template()),
       ('JWT_ES256', jwt.jwt_es256_template()),
+      ('JWT_ES256_RAW', jwt.raw_jwt_es256_template()),
       ('JWT_ES384', jwt.jwt_es384_template()),
+      ('JWT_ES384_RAW', jwt.raw_jwt_es384_template()),
       ('JWT_ES512', jwt.jwt_es512_template()),
+      ('JWT_ES512_RAW', jwt.raw_jwt_es512_template()),
       ('JWT_RS256_2048_F4', jwt.jwt_rs256_2048_f4_template()),
+      ('JWT_RS256_2048_F4_RAW', jwt.raw_jwt_rs256_2048_f4_template()),
       ('JWT_RS256_3072_F4', jwt.jwt_rs256_3072_f4_template()),
+      ('JWT_RS256_3072_F4_RAW', jwt.raw_jwt_rs256_3072_f4_template()),
       ('JWT_RS384_3072_F4', jwt.jwt_rs384_3072_f4_template()),
+      ('JWT_RS384_3072_F4_RAW', jwt.raw_jwt_rs384_3072_f4_template()),
       ('JWT_RS512_4096_F4', jwt.jwt_rs512_4096_f4_template()),
+      ('JWT_RS512_4096_F4_RAW', jwt.raw_jwt_rs512_4096_f4_template()),
       ('JWT_PS256_2048_F4', jwt.jwt_ps256_2048_f4_template()),
+      ('JWT_PS256_2048_F4_RAW', jwt.raw_jwt_ps256_2048_f4_template()),
       ('JWT_PS256_3072_F4', jwt.jwt_ps256_3072_f4_template()),
+      ('JWT_PS256_3072_F4_RAW', jwt.raw_jwt_ps256_3072_f4_template()),
       ('JWT_PS384_3072_F4', jwt.jwt_ps384_3072_f4_template()),
+      ('JWT_PS384_3072_F4_RAW', jwt.raw_jwt_ps384_3072_f4_template()),
       ('JWT_PS512_4096_F4', jwt.jwt_ps512_4096_f4_template()),
+      ('JWT_PS512_4096_F4_RAW', jwt.raw_jwt_ps512_4096_f4_template()),
   ])
   def test_template(self, template_name, template):
     self.assertEqual(template,
@@ -60,8 +74,11 @@ class JwtKeyTemplatesTest(parameterized.TestCase):
 
   @parameterized.named_parameters([
       ('JWT_HS256', jwt.jwt_hs256_template()),
+      ('JWT_HS256_RAW', jwt.raw_jwt_hs256_template()),
       ('JWT_HS384', jwt.jwt_hs384_template()),
+      ('JWT_HS384_RAW', jwt.raw_jwt_hs384_template()),
       ('JWT_HS512', jwt.jwt_hs512_template()),
+      ('JWT_HS512_RAW', jwt.raw_jwt_hs512_template()),
   ])
   def test_mac_success(self, key_template):
     keyset_handle = tink.new_keyset_handle(key_template)
@@ -80,8 +97,11 @@ class JwtKeyTemplatesTest(parameterized.TestCase):
 
   @parameterized.named_parameters([
       ('JWT_ES256', jwt.jwt_es256_template()),
+      ('JWT_ES256_RAW', jwt.raw_jwt_es256_template()),
       ('JWT_ES384', jwt.jwt_es384_template()),
+      ('JWT_ES384_RAW', jwt.raw_jwt_es384_template()),
       ('JWT_ES512', jwt.jwt_es512_template()),
+      ('JWT_ES512_RAW', jwt.raw_jwt_es512_template()),
   ])
   def test_new_keydata_primitive_success(self, template):
     private_handle = tink.new_keyset_handle(template)
