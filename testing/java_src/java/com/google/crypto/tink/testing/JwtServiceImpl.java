f9026ce659aadaace5af6e83ef2fd2f28dc5868f
juerg <juerg@google.com>
2021-09-06 09:12:24 -0700

    Add Jwk Set Convertion functions to Java testing API server.
    PiperOrigin-RevId: 395107954

--- testing/java_src/java/com/google/crypto/tink/testing/JwtServiceImpl.java
+++ testing/java_src/java/com/google/crypto/tink/testing/JwtServiceImpl.java
@@ -17,8 +17,10 @@
 package com.google.crypto.tink.testing;
 
 import com.google.crypto.tink.BinaryKeysetReader;
+import com.google.crypto.tink.BinaryKeysetWriter;
 import com.google.crypto.tink.CleartextKeysetHandle;
 import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.jwt.JwkSetConverter;
 import com.google.crypto.tink.jwt.JwtInvalidException;
 import com.google.crypto.tink.jwt.JwtMac;
 import com.google.crypto.tink.jwt.JwtMacConfig;
@@ -28,19 +30,27 @@ import com.google.crypto.tink.jwt.JwtSignatureConfig;
 import com.google.crypto.tink.jwt.JwtValidator;
 import com.google.crypto.tink.jwt.RawJwt;
 import com.google.crypto.tink.jwt.VerifiedJwt;
+import com.google.crypto.tink.proto.Keyset;
 import com.google.crypto.tink.proto.testing.JwtClaimValue;
+import com.google.crypto.tink.proto.testing.JwtFromJwkSetRequest;
+import com.google.crypto.tink.proto.testing.JwtFromJwkSetResponse;
 import com.google.crypto.tink.proto.testing.JwtGrpc.JwtImplBase;
 import com.google.crypto.tink.proto.testing.JwtSignRequest;
 import com.google.crypto.tink.proto.testing.JwtSignResponse;
+import com.google.crypto.tink.proto.testing.JwtToJwkSetRequest;
+import com.google.crypto.tink.proto.testing.JwtToJwkSetResponse;
 import com.google.crypto.tink.proto.testing.JwtToken;
 import com.google.crypto.tink.proto.testing.JwtVerifyRequest;
 import com.google.crypto.tink.proto.testing.JwtVerifyResponse;
 import com.google.crypto.tink.proto.testing.NullValue;
 import com.google.crypto.tink.proto.testing.StringValue;
 import com.google.crypto.tink.proto.testing.Timestamp;
+import com.google.crypto.tink.tinkkey.SecretKeyAccess;
+import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.time.Clock;
@@ -329,4 +339,53 @@ public final class JwtServiceImpl extends JwtImplBase {
     responseObserver.onCompleted();
   }
 
+  /** Converts a Tink JWT Keyset to a JWK set. */
+  @Override
+  public void toJwkSet(
+      JwtToJwkSetRequest request, StreamObserver<JwtToJwkSetResponse> responseObserver) {
+    JwtToJwkSetResponse response;
+    try {
+      KeysetHandle keysetHandle =
+          CleartextKeysetHandle.read(
+              BinaryKeysetReader.withBytes(request.getKeyset().toByteArray()));
+      String jwkSet =
+          JwkSetConverter.fromKeysetHandle(keysetHandle, SecretKeyAccess.insecureSecretAccess());
+      response = JwtToJwkSetResponse.newBuilder().setJwkSet(jwkSet).build();
+    } catch (GeneralSecurityException | InvalidProtocolBufferException e) {
+      response = JwtToJwkSetResponse.newBuilder().setErr(e.toString()).build();
+    } catch (IOException e) {
+      responseObserver.onError(Status.UNKNOWN.withDescription(e.getMessage()).asException());
+      return;
+    }
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
+  }
+
+  /** Converts a JWK set to a Tink JWT Keyset. */
+  @Override
+  public void fromJwkSet(
+      JwtFromJwkSetRequest request, StreamObserver<JwtFromJwkSetResponse> responseObserver) {
+    JwtFromJwkSetResponse response;
+    try {
+      KeysetHandle keysetHandle =
+          JwkSetConverter.toKeysetHandle(
+              request.getJwkSet(), SecretKeyAccess.insecureSecretAccess());
+
+      Keyset keyset = CleartextKeysetHandle.getKeyset(keysetHandle);
+      ByteArrayOutputStream keysetStream = new ByteArrayOutputStream();
+      BinaryKeysetWriter.withOutputStream(keysetStream).write(keyset);
+      keysetStream.close();
+      response =
+          JwtFromJwkSetResponse.newBuilder()
+              .setKeyset(ByteString.copyFrom(keysetStream.toByteArray()))
+              .build();
+    } catch (GeneralSecurityException | InvalidProtocolBufferException e) {
+      response = JwtFromJwkSetResponse.newBuilder().setErr(e.toString()).build();
+    } catch (IOException e) {
+      responseObserver.onError(Status.UNKNOWN.withDescription(e.getMessage()).asException());
+      return;
+    }
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
+  }
 }
