f302e1e6244e611052ba2afb0686310a8236693d
juerg <juerg@google.com>
2021-09-10 08:37:06 -0700

    Add conversion of RSA SSA PKCS1 JWK keysets to keyset handles in Python.
    PiperOrigin-RevId: 395935121

--- python/tink/jwt/_jwk_set_converter_test.py
+++ python/tink/jwt/_jwk_set_converter_test.py
@@ -397,7 +397,11 @@ class JwkSetConverterTest(parameterized.TestCase):
   @parameterized.named_parameters([('ES256_RAW', ES256_JWK_SET),
                                    ('ES384_RAW', ES384_JWK_SET),
                                    ('ES512_RAW', ES512_JWK_SET),
-                                   ('ES256_TINK', ES256_JWK_SET_KID)])
+                                   ('ES256_TINK', ES256_JWK_SET_KID),
+                                   ('RS256_RAW', RS256_JWK_SET),
+                                   ('RS384_RAW', RS384_JWK_SET),
+                                   ('RS512_RAW', RS512_JWK_SET),
+                                   ('RS256_TINK', RS256_JWK_SET_KID)])
   def test_convert_jwk_set_to_keyset_handle_and_back(self, jwk_set):
     keyset_handle = jwt.jwk_set_to_keyset_handle(jwk_set)
     output_jwk_set = jwt.jwk_set_from_keyset_handle(keyset_handle)
@@ -630,6 +634,107 @@ class JwkSetConverterTest(parameterized.TestCase):
            "use":"sig",
            "key_ops":"verify"
         }]}"""
+    with self.assertRaises(tink.TinkError):
+      jwt.jwk_set_to_keyset_handle(jwk_set)
+
+  def test_rsa_without_use_and_key_ops_to_keyset_handle_success(self):
+    jwk_set = RS256_JWK_SET.replace(',"use":"sig"',
+                                    '').replace(',"key_ops":["verify"]', '')
+    keyset_handle = jwt.jwk_set_to_keyset_handle(jwk_set)
+    output_jwk_set = jwt.jwk_set_from_keyset_handle(keyset_handle)
+    self.assertEqual(output_jwk_set, RS256_JWK_SET)
+
+  def test_rsa_with_unknown_property_keyset_handle_success(self):
+    jwk_set = RS256_JWK_SET.replace(',"use":"sig"',
+                                    ',"use":"sig","unknown":1234')
+    keyset_handle = jwt.jwk_set_to_keyset_handle(jwk_set)
+    output_jwk_set = jwt.jwk_set_from_keyset_handle(keyset_handle)
+    self.assertEqual(output_jwk_set, RS256_JWK_SET)
+
+  def test_rsa_private_key_to_keyset_handle_fails(self):
+    # Example from https://datatracker.ietf.org/doc/html/rfc7517#appendix-A.2
+    jwk_set = """
+     {"keys":
+       [
+         {"kty":"RSA",
+          "n":"0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4
+     cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMst
+     n64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2Q
+     vzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbIS
+     D08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw
+     0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
+          "e":"AQAB",
+          "d":"X4cTteJY_gn4FYPsXB8rdXix5vwsg1FLN5E3EaG6RJoVH-HLLKD9
+     M7dx5oo7GURknchnrRweUkC7hT5fJLM0WbFAKNLWY2vv7B6NqXSzUvxT0_YSfqij
+     wp3RTzlBaCxWp4doFk5N2o8Gy_nHNKroADIkJ46pRUohsXywbReAdYaMwFs9tv8d
+     _cPVY3i07a3t8MN6TNwm0dSawm9v47UiCl3Sk5ZiG7xojPLu4sbg1U2jx4IBTNBz
+     nbJSzFHK66jT8bgkuqsk0GjskDJk19Z4qwjwbsnn4j2WBii3RL-Us2lGVkY8fkFz
+     me1z0HbIkfz0Y6mqnOYtqc0X4jfcKoAC8Q",
+          "p":"83i-7IvMGXoMXCskv73TKr8637FiO7Z27zv8oj6pbWUQyLPQBQxtPV
+     nwD20R-60eTDmD2ujnMt5PoqMrm8RfmNhVWDtjjMmCMjOpSXicFHj7XOuVIYQyqV
+     WlWEh6dN36GVZYk93N8Bc9vY41xy8B9RzzOGVQzXvNEvn7O0nVbfs",
+          "q":"3dfOR9cuYq-0S-mkFLzgItgMEfFzB2q3hWehMuG0oCuqnb3vobLyum
+     qjVZQO1dIrdwgTnCdpYzBcOfW5r370AFXjiWft_NGEiovonizhKpo9VVS78TzFgx
+     kIdrecRezsZ-1kYd_s1qDbxtkDEgfAITAG9LUnADun4vIcb6yelxk",
+          "dp":"G4sPXkc6Ya9y8oJW9_ILj4xuppu0lzi_H7VTkS8xj5SdX3coE0oim
+     YwxIi2emTAue0UOa5dpgFGyBJ4c8tQ2VF402XRugKDTP8akYhFo5tAA77Qe_Nmtu
+     YZc3C3m3I24G2GvR5sSDxUyAN2zq8Lfn9EUms6rY3Ob8YeiKkTiBj0",
+          "dq":"s9lAH9fggBsoFR8Oac2R_E2gw282rT2kGOAhvIllETE1efrA6huUU
+     vMfBcMpn8lqeW6vzznYY5SSQF7pMdC_agI3nG8Ibp1BUb0JUiraRNqUfLhcQb_d9
+     GF4Dh7e74WbRsobRonujTYN1xCaP6TO61jvWrX-L18txXw494Q_cgk",
+          "qi":"GyM_p6JrXySiz1toFgKbWV-JdI3jQ4ypu9rbMWx3rQJBfmt0FoYzg
+     UIZEVFEcOqwemRN81zoDAaa-Bk0KWNGDjJHZDdDmFhW3AN7lI-puxk_mHZGJ11rx
+     yR8O55XLSe3SPmRfKwZI6yU24ZxvQKFYItdldUKGzO6Ia6zTKhAVRU",
+          "alg":"RS256",
+          "kid":"2011-04-29"}
+       ]
+     }"""
+    # remove spaces and line breaks
+    jwk_set = jwk_set.replace(' ', '').replace('\n', '')
+    with self.assertRaises(tink.TinkError):
+      jwt.jwk_set_to_keyset_handle(jwk_set)
+
+  def test_rsa_without_alg_fails(self):
+    jwk_set = RS256_JWK_SET.replace(',"alg":"RS256"', '')
+    with self.assertRaises(tink.TinkError):
+      jwt.jwk_set_to_keyset_handle(jwk_set)
+
+  def test_rsa_without_kty_fails(self):
+    jwk_set = RS256_JWK_SET.replace('"kty":"RSA",', '')
+    with self.assertRaises(tink.TinkError):
+      jwt.jwk_set_to_keyset_handle(jwk_set)
+
+  def test_rsa_with_small_n_primitive_fails(self):
+    jwk_set = """{"keys":[{
+        "kty":"RSA",
+        "n":"AAAwOQ",
+        "e":"AQAB",
+        "use":"sig",
+        "alg":"RS256",
+        "key_ops":["verify"]}]}"""
+    handle = jwt.jwk_set_to_keyset_handle(jwk_set)
+    with self.assertRaises(tink.TinkError):
+      handle.primitive(jwt.JwtPublicKeyVerify)
+
+  def test_rsa_with_invalid_kty_fails(self):
+    jwk_set = RS256_JWK_SET.replace('"kty":"RSA"', '"kty":"EC"')
+    with self.assertRaises(tink.TinkError):
+      jwt.jwk_set_to_keyset_handle(jwk_set)
+
+  def test_rsa_with_invalid_use_fails(self):
+    jwk_set = RS256_JWK_SET.replace('"use":"sig"', '"use":"invalid"')
+    with self.assertRaises(tink.TinkError):
+      jwt.jwk_set_to_keyset_handle(jwk_set)
+
+  def test_rsa_with_invalid_key_ops_fails(self):
+    jwk_set = RS256_JWK_SET.replace('"key_ops":["verify"]',
+                                    '"key_ops":["invalid"]')
+    with self.assertRaises(tink.TinkError):
+      jwt.jwk_set_to_keyset_handle(jwk_set)
+
+  def test_rsa_with_string_key_ops_fails(self):
+    jwk_set = RS256_JWK_SET.replace('"key_ops":["verify"]',
+                                    '"key_ops":"verify"')
     with self.assertRaises(tink.TinkError):
       jwt.jwk_set_to_keyset_handle(jwk_set)
 
