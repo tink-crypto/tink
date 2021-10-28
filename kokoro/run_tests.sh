1768791830c7a04e3b254a082ffdb4dd1b0edd17
ckl <ckl@google.com>
2021-10-28 02:00:12 -0700

    Fix Javascript build on macOS.
    PiperOrigin-RevId: 406085733

--- kokoro/run_tests.sh
+++ kokoro/run_tests.sh
@@ -35,6 +35,17 @@ run_linux_tests() {
   shift 1
   local manual_targets=("$@")
 
+  # This is needed to handle recent Chrome distributions on macOS which have
+  # paths with spaces.
+  #
+  # Context:
+  # https://github.com/bazelbuild/bazel/issues/4327#issuecomment-627422865
+  local -a BAZEL_FLAGS
+  if [[ "${PLATFORM}" == 'darwin' && "${workspace_dir}" == 'javascript' ]]; then
+    BAZEL_FLAGS+=( --experimental_inprocess_symlink_creation )
+  fi
+  readonly BAZEL_FLAGS
+
   local -a TEST_FLAGS=( --strategy=TestRunner=standalone --test_output=all )
   if [[ "${PLATFORM}" == 'darwin' ]]; then
     TEST_FLAGS+=( --jvmopt="-Djava.net.preferIPv6Addresses=true" )
@@ -42,8 +53,8 @@ run_linux_tests() {
   readonly TEST_FLAGS
   (
     cd "${workspace_dir}"
-    time bazel build -- ... || fail_with_debug_output
-    time bazel test "${TEST_FLAGS[@]}" -- ... || fail_with_debug_output
+    time bazel build "${BAZEL_FLAGS[@]}" -- ... || fail_with_debug_output
+    time bazel test "${BAZEL_FLAGS[@]}" "${TEST_FLAGS[@]}" -- ... || fail_with_debug_output
     if (( ${#manual_targets[@]} > 0 )); then
       time bazel test "${TEST_FLAGS[@]}"  -- "${manual_targets[@]}" \
         || fail_with_debug_output
