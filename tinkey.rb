ce579bab6a89daf2ac128ad4b3743677d2c31ea8
Christian Miller <christian.miller@uber.com>
2021-11-04 10:04:01 -0700

    remove unneeded directive for homebrew

--- tinkey.rb
+++ tinkey.rb
@@ -9,8 +9,6 @@ class Tinkey < Formula
   url "https://storage.googleapis.com/tinkey/tinkey-1.6.0.tar.gz"
   sha256 "51d9694a704d00fbac04862a6427ad5f17bf59f91d5e963517d8799141e737c0"
 
-  bottle :unneeded
-
   def install
     bin.install "tinkey"
     bin.install "tinkey_deploy.jar"
