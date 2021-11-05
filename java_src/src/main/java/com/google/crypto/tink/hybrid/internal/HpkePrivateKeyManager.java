b596f9571d3e2b9339cd5188e24d90a04b3b48e5
wconner <wconner@google.com>
2021-11-05 12:52:03 -0700

    Added HPKE private key manager.
    PiperOrigin-RevId: 407888287

---
+++ java_src/src/main/java/com/google/crypto/tink/hybrid/internal/HpkePrivateKeyManager.java
@@ -0,0 +1,165 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.hybrid.internal;
+
+import com.google.crypto.tink.HybridDecrypt;
+import com.google.crypto.tink.KeyTemplate;
+import com.google.crypto.tink.KeyTypeManager;
+import com.google.crypto.tink.PrivateKeyTypeManager;
+import com.google.crypto.tink.proto.HpkeAead;
+import com.google.crypto.tink.proto.HpkeKdf;
+import com.google.crypto.tink.proto.HpkeKem;
+import com.google.crypto.tink.proto.HpkeKeyFormat;
+import com.google.crypto.tink.proto.HpkeParams;
+import com.google.crypto.tink.proto.HpkePrivateKey;
+import com.google.crypto.tink.proto.HpkePublicKey;
+import com.google.crypto.tink.proto.KeyData.KeyMaterialType;
+import com.google.crypto.tink.subtle.Validators;
+import com.google.crypto.tink.subtle.X25519;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.ExtensionRegistryLite;
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Key manager that generates new {@link HpkePrivateKey} keys and produces new instances of {@link
+ * HpkeDecrypt} primitives.
+ */
+public final class HpkePrivateKeyManager
+    extends PrivateKeyTypeManager<HpkePrivateKey, HpkePublicKey> {
+  public HpkePrivateKeyManager() {
+    super(
+        HpkePrivateKey.class,
+        HpkePublicKey.class,
+        new KeyTypeManager.PrimitiveFactory<HybridDecrypt, HpkePrivateKey>(HybridDecrypt.class) {
+          @Override
+          public HybridDecrypt getPrimitive(HpkePrivateKey recipientPrivateKey)
+              throws GeneralSecurityException {
+            return HpkeDecrypt.createHpkeDecrypt(recipientPrivateKey);
+          }
+        });
+  }
+
+  @Override
+  public String getKeyType() {
+    return "type.googleapis.com/google.crypto.tink.HpkePrivateKey";
+  }
+
+  @Override
+  public int getVersion() {
+    return 0;
+  }
+
+  @Override
+  public HpkePublicKey getPublicKey(HpkePrivateKey key) {
+    return key.getPublicKey();
+  }
+
+  @Override
+  public KeyMaterialType keyMaterialType() {
+    return KeyMaterialType.ASYMMETRIC_PRIVATE;
+  }
+
+  @Override
+  public HpkePrivateKey parseKey(ByteString byteString) throws InvalidProtocolBufferException {
+    return HpkePrivateKey.parseFrom(byteString, ExtensionRegistryLite.getEmptyRegistry());
+  }
+
+  @Override
+  public void validateKey(HpkePrivateKey key) throws GeneralSecurityException {
+    if (key.getPrivateKey().isEmpty()) {
+      throw new GeneralSecurityException("Private key is empty.");
+    }
+    if (!key.hasPublicKey()) {
+      throw new GeneralSecurityException("Missing public key.");
+    }
+    Validators.validateVersion(key.getVersion(), getVersion());
+    HpkeUtil.validateParams(key.getPublicKey().getParams());
+  }
+
+  @Override
+  public KeyFactory<HpkeKeyFormat, HpkePrivateKey> keyFactory() {
+    return new KeyFactory<HpkeKeyFormat, HpkePrivateKey>(HpkeKeyFormat.class) {
+      @Override
+      public void validateKeyFormat(HpkeKeyFormat keyFormat) throws GeneralSecurityException {
+        HpkeUtil.validateParams(keyFormat.getParams());
+      }
+
+      @Override
+      public HpkeKeyFormat parseKeyFormat(ByteString byteString)
+          throws InvalidProtocolBufferException {
+        return HpkeKeyFormat.parseFrom(byteString, ExtensionRegistryLite.getEmptyRegistry());
+      }
+
+      @Override
+      public HpkePrivateKey createKey(HpkeKeyFormat keyFormat) throws GeneralSecurityException {
+        byte[] privateKeyBytes = X25519.generatePrivateKey();
+        byte[] publicKeyBytes = X25519.publicFromPrivate(privateKeyBytes);
+
+        HpkePublicKey publicKey =
+            HpkePublicKey.newBuilder()
+                .setVersion(getVersion())
+                .setParams(keyFormat.getParams())
+                .setPublicKey(ByteString.copyFrom(publicKeyBytes))
+                .build();
+
+        return HpkePrivateKey.newBuilder()
+            .setVersion(getVersion())
+            .setPublicKey(publicKey)
+            .setPrivateKey(ByteString.copyFrom(privateKeyBytes))
+            .build();
+      }
+
+      @Override
+      public Map<String, KeyFactory.KeyFormat<HpkeKeyFormat>> keyFormats() {
+        Map<String, KeyFactory.KeyFormat<HpkeKeyFormat>> result = new HashMap<>();
+        result.put(
+            "DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM",
+            createKeyFormat(
+                HpkeKem.DHKEM_X25519_HKDF_SHA256,
+                HpkeKdf.HKDF_SHA256,
+                HpkeAead.AES_128_GCM,
+                KeyTemplate.OutputPrefixType.TINK));
+        result.put(
+            "DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_256_GCM",
+            createKeyFormat(
+                HpkeKem.DHKEM_X25519_HKDF_SHA256,
+                HpkeKdf.HKDF_SHA256,
+                HpkeAead.AES_256_GCM,
+                KeyTemplate.OutputPrefixType.TINK));
+        result.put(
+            "DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_CHACHA20_POLY1305",
+            createKeyFormat(
+                HpkeKem.DHKEM_X25519_HKDF_SHA256,
+                HpkeKdf.HKDF_SHA256,
+                HpkeAead.CHACHA20_POLY1305,
+                KeyTemplate.OutputPrefixType.TINK));
+        return Collections.unmodifiableMap(result);
+      }
+    };
+  }
+
+  private static KeyFactory.KeyFormat<HpkeKeyFormat> createKeyFormat(
+      HpkeKem kem, HpkeKdf kdf, HpkeAead aead, KeyTemplate.OutputPrefixType prefixType) {
+    HpkeParams params = HpkeParams.newBuilder().setKem(kem).setKdf(kdf).setAead(aead).build();
+    return new KeyFactory.KeyFormat<>(
+        HpkeKeyFormat.newBuilder().setParams(params).build(), prefixType);
+  }
+}
---
+++ java_src/src/test/java/com/google/crypto/tink/hybrid/internal/HpkePrivateKeyManagerTest.java
@@ -0,0 +1,214 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.hybrid.internal;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.HybridDecrypt;
+import com.google.crypto.tink.HybridEncrypt;
+import com.google.crypto.tink.KeyTypeManager;
+import com.google.crypto.tink.proto.HpkeAead;
+import com.google.crypto.tink.proto.HpkeKdf;
+import com.google.crypto.tink.proto.HpkeKem;
+import com.google.crypto.tink.proto.HpkeKeyFormat;
+import com.google.crypto.tink.proto.HpkeParams;
+import com.google.crypto.tink.proto.HpkePrivateKey;
+import com.google.crypto.tink.proto.HpkePublicKey;
+import com.google.crypto.tink.proto.KeyData.KeyMaterialType;
+import com.google.crypto.tink.subtle.Random;
+import java.security.GeneralSecurityException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Unit tests for {@link HpkePrivateKeyManager}. */
+@RunWith(JUnit4.class)
+public final class HpkePrivateKeyManagerTest {
+  private HpkePrivateKeyManager manager;
+  private KeyTypeManager.KeyFactory<HpkeKeyFormat, HpkePrivateKey> factory;
+
+  @Before
+  public void setUp() {
+    manager = new HpkePrivateKeyManager();
+    factory = manager.keyFactory();
+  }
+
+  @Test
+  public void basics() throws Exception {
+    assertThat(manager.getKeyType())
+        .isEqualTo("type.googleapis.com/google.crypto.tink.HpkePrivateKey");
+    assertThat(manager.getVersion()).isEqualTo(0);
+    assertThat(manager.keyMaterialType()).isEqualTo(KeyMaterialType.ASYMMETRIC_PRIVATE);
+  }
+
+  private static HpkeKeyFormat createKeyFormat(HpkeKem kem, HpkeKdf kdf, HpkeAead aead) {
+    HpkeParams params = HpkeParams.newBuilder().setKem(kem).setKdf(kdf).setAead(aead).build();
+    return HpkeKeyFormat.newBuilder().setParams(params).build();
+  }
+
+  @Test
+  public void validateKeyFormat_succeeds() throws Exception {
+    HpkeKeyFormat format =
+        createKeyFormat(
+            HpkeKem.DHKEM_X25519_HKDF_SHA256, HpkeKdf.HKDF_SHA256, HpkeAead.AES_128_GCM);
+
+    factory.validateKeyFormat(format);
+  }
+
+  @Test
+  public void validateKeyFormat_failsWithMissingParams() throws Exception {
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> factory.validateKeyFormat(HpkeKeyFormat.getDefaultInstance()));
+  }
+
+  @Test
+  public void validateKeyFormat_failsWithInvalidKem() throws Exception {
+    HpkeKeyFormat format =
+        createKeyFormat(HpkeKem.KEM_UNKNOWN, HpkeKdf.HKDF_SHA256, HpkeAead.AES_128_GCM);
+
+    assertThrows(GeneralSecurityException.class, () -> factory.validateKeyFormat(format));
+  }
+
+  @Test
+  public void validateKeyFormat_failsWithInvalidKdf() throws Exception {
+    HpkeKeyFormat format =
+        createKeyFormat(
+            HpkeKem.DHKEM_X25519_HKDF_SHA256, HpkeKdf.KDF_UNKNOWN, HpkeAead.AES_128_GCM);
+
+    assertThrows(GeneralSecurityException.class, () -> factory.validateKeyFormat(format));
+  }
+
+  @Test
+  public void validateKeyFormat_failsWithInvalidAead() throws Exception {
+    HpkeKeyFormat format =
+        createKeyFormat(
+            HpkeKem.DHKEM_X25519_HKDF_SHA256, HpkeKdf.HKDF_SHA256, HpkeAead.AEAD_UNKNOWN);
+
+    assertThrows(GeneralSecurityException.class, () -> factory.validateKeyFormat(format));
+  }
+
+  @Test
+  public void keyFormats() throws Exception {
+    factory.validateKeyFormat(
+        factory.keyFormats().get("DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM").keyFormat);
+    factory.validateKeyFormat(
+        factory.keyFormats().get("DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_256_GCM").keyFormat);
+    factory.validateKeyFormat(
+        factory
+            .keyFormats()
+            .get("DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_CHACHA20_POLY1305")
+            .keyFormat);
+  }
+
+  @Test
+  public void createKey_succeeds() throws Exception {
+    HpkeKeyFormat format =
+        factory.keyFormats().get("DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM").keyFormat;
+    HpkePrivateKey key = factory.createKey(format);
+
+    assertThat(key.getVersion()).isEqualTo(manager.getVersion());
+    assertThat(key.getPublicKey().getParams()).isEqualTo(format.getParams());
+    assertThat(key.getPublicKey().getPublicKey()).isNotEmpty();
+    assertThat(key.getPrivateKey()).isNotEmpty();
+  }
+
+  @Test
+  public void validateKey_succeeds() throws Exception {
+    HpkeKeyFormat format =
+        factory.keyFormats().get("DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM").keyFormat;
+
+    manager.validateKey(factory.createKey(format));
+  }
+
+  @Test
+  public void validateKey_failsWithInvalidVersion() throws Exception {
+    HpkeKeyFormat format =
+        factory.keyFormats().get("DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM").keyFormat;
+    HpkePrivateKey key = HpkePrivateKey.newBuilder(factory.createKey(format)).setVersion(1).build();
+
+    assertThrows(GeneralSecurityException.class, () -> manager.validateKey(key));
+  }
+
+  @Test
+  public void validateKey_failsWithMissingPublicKey() throws Exception {
+    HpkeKeyFormat format =
+        factory.keyFormats().get("DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM").keyFormat;
+    HpkePrivateKey key =
+        HpkePrivateKey.newBuilder(factory.createKey(format)).clearPublicKey().build();
+
+    assertThrows(GeneralSecurityException.class, () -> manager.validateKey(key));
+  }
+
+  @Test
+  public void validateKey_failsWithEmptyPrivateKey() throws Exception {
+    HpkeKeyFormat format =
+        factory.keyFormats().get("DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM").keyFormat;
+    HpkePrivateKey key =
+        HpkePrivateKey.newBuilder(factory.createKey(format)).clearPrivateKey().build();
+
+    assertThrows(GeneralSecurityException.class, () -> manager.validateKey(key));
+  }
+
+  @Test
+  public void getPublicKey() throws Exception {
+    HpkeKeyFormat format =
+        factory.keyFormats().get("DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM").keyFormat;
+    HpkePrivateKey key = factory.createKey(format);
+
+    assertThat(manager.getPublicKey(key)).isEqualTo(key.getPublicKey());
+  }
+
+  @Test
+  public void parseKey() throws Exception {
+    HpkeKeyFormat format =
+        createKeyFormat(
+            HpkeKem.DHKEM_X25519_HKDF_SHA256, HpkeKdf.HKDF_SHA256, HpkeAead.AES_128_GCM);
+    HpkePrivateKey privateKey = factory.createKey(format);
+
+    assertThat(manager.parseKey(privateKey.toByteString())).isEqualTo(privateKey);
+  }
+
+  @Test
+  public void parseKeyFormat() throws Exception {
+    HpkeKeyFormat format =
+        createKeyFormat(
+            HpkeKem.DHKEM_X25519_HKDF_SHA256, HpkeKdf.HKDF_SHA256, HpkeAead.AES_128_GCM);
+
+    assertThat(factory.parseKeyFormat(format.toByteString())).isEqualTo(format);
+  }
+
+  @Test
+  public void createPrimitive() throws Exception {
+    HpkeKeyFormat format =
+        createKeyFormat(
+            HpkeKem.DHKEM_X25519_HKDF_SHA256, HpkeKdf.HKDF_SHA256, HpkeAead.AES_128_GCM);
+    HpkePrivateKey privateKey = factory.createKey(format);
+    HpkePublicKey publicKey = manager.getPublicKey(privateKey);
+    HybridDecrypt hybridDecrypt = manager.getPrimitive(privateKey, HybridDecrypt.class);
+    HybridEncrypt hybridEncrypt = HpkeEncrypt.createHpkeEncrypt(publicKey);
+
+    byte[] input = Random.randBytes(200);
+    byte[] contextInfo = Random.randBytes(100);
+    byte[] ciphertext = hybridEncrypt.encrypt(input, contextInfo);
+    byte[] plaintext = hybridDecrypt.decrypt(ciphertext, contextInfo);
+
+    assertThat(plaintext).isEqualTo(input);
+  }
+}
