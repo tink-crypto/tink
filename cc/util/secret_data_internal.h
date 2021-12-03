60be32c0f31bb1d41193aae756f26347b88b2699
tholenst <tholenst@google.com>
2021-12-03 08:14:25 -0800

    Change the SanitizingAllocator to respect alignments when __cpp_aligned_new is defined. Otherwise, make it reject types which have larger than default aligment requirements.
    PiperOrigin-RevId: 413939339

--- cc/util/secret_data_internal.h
+++ cc/util/secret_data_internal.h
@@ -19,6 +19,7 @@
 
 #include <cstddef>
 #include <memory>
+#include <new>
 
 #include "absl/base/attributes.h"
 #include "openssl/crypto.h"
@@ -37,18 +38,39 @@ template <typename T>
 struct SanitizingAllocator {
   typedef T value_type;
 
+  // If aligned operator new is not supported this only supports under aligned
+  // types.
+#ifndef __cpp_aligned_new
+  static_assert(alignof(T) <= alignof(std::max_align_t),
+                "SanitizingAllocator<T> only supports fundamental alignment "
+                "before C++17");
+#endif
+
   SanitizingAllocator() = default;
   template <class U>
   explicit constexpr SanitizingAllocator(
       const SanitizingAllocator<U>&) noexcept {}
 
   ABSL_MUST_USE_RESULT T* allocate(std::size_t n) {
-    return std::allocator<T>().allocate(n);
+    if (n > std::numeric_limits<std::size_t>::max() / sizeof(T)) {
+      throw std::bad_array_new_length();
+    }
+    std::size_t size = n * sizeof(T);
+#ifdef __cpp_aligned_new
+    void* result = ::operator new(size, std::align_val_t(alignof(T)));
+#else
+    void* result = ::operator new(size);
+#endif
+    return static_cast<T*>(result);
   }
 
   void deallocate(T* ptr, std::size_t n) noexcept {
     SafeZeroMemory(ptr, n * sizeof(T));
-    std::allocator<T>().deallocate(ptr, n);
+#ifdef __cpp_aligned_new
+    ::operator delete(ptr, std::align_val_t(alignof(T)));
+#else
+    ::operator delete(ptr);
+#endif
   }
 
   // Allocator requirements mandate definition of eq and neq operators
