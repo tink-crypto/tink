16609810e30eda3d7611bb46a6f360cf24f2ef67
cinlin <cinlin@google.com>
2022-01-18 12:01:29 -0800

    Separate out test vector parsing functions for HPKE Go.
    PiperOrigin-RevId: 422614014

--- go/hybrid/internal/hkdf_hpke_kdf_test.go
+++ go/hybrid/internal/hkdf_hpke_kdf_test.go
@@ -19,7 +19,6 @@ package internal
 import (
 	"bytes"
 	"encoding/hex"
-	"errors"
 	"fmt"
 	"math"
 	"testing"
@@ -27,103 +26,25 @@ import (
 	"github.com/google/tink/go/subtle"
 )
 
-// TODO(b/201070904): Expand tests to use x25519HkdfSha256BaseModeTestVectors
-// after it's in a separate package.
-
-func internetDraftTestVector(t *testing.T) (vector, error) {
-	t.Helper()
-
-	// Test vector from HPKE I-D
-	// https://www.ietf.org/archive/id/draft-irtf-cfrg-hpke-12.html#appendix-A.1.1.
-	v := struct {
-		mode                                                                                                                uint8
-		kemID, kdfID, aeadID                                                                                                uint16
-		info, ikmE, pkEm, skEm, ikmR, pkRm, skRm, enc, sharedSecret, keyScheduleCtx, secret, key, baseNonce, exporterSecret string
-	}{
-		mode:           0,
-		kemID:          32,
-		kdfID:          1,
-		aeadID:         1,
-		info:           "4f6465206f6e2061204772656369616e2055726e",
-		ikmE:           "7268600d403fce431561aef583ee1613527cff655c1343f29812e66706df3234",
-		pkEm:           "37fda3567bdbd628e88668c3c8d7e97d1d1253b6d4ea6d44c150f741f1bf4431",
-		skEm:           "52c4a758a802cd8b936eceea314432798d5baf2d7e9235dc084ab1b9cfa2f736",
-		ikmR:           "6db9df30aa07dd42ee5e8181afdb977e538f5e1fec8a06223f33f7013e525037",
-		pkRm:           "3948cfe0ad1ddb695d780e59077195da6c56506b027329794ab02bca80815c4d",
-		skRm:           "4612c550263fc8ad58375df3f557aac531d26850903e55a9f23f21d8534e8ac8",
-		enc:            "37fda3567bdbd628e88668c3c8d7e97d1d1253b6d4ea6d44c150f741f1bf4431",
-		sharedSecret:   "fe0e18c9f024ce43799ae393c7e8fe8fce9d218875e8227b0187c04e7d2ea1fc",
-		keyScheduleCtx: "00725611c9d98c07c03f60095cd32d400d8347d45ed67097bbad50fc56da742d07cb6cffde367bb0565ba28bb02c90744a20f5ef37f30523526106f637abb05449",
-		secret:         "12fff91991e93b48de37e7daddb52981084bd8aa64289c3788471d9a9712f397",
-		key:            "4531685d41d65f03dc48f6b8302c05b0",
-		baseNonce:      "56d890e5accaaf011cff4b7d",
-		exporterSecret: "45ff1c2e220db587171952c0592d5f5ebe103f1561a2614e38f2ffd47e99e3f8",
-	}
-
-	var info, senderPubKey, senderPrivKey, recipientPubKey, sharedSecret, keyScheduleCtx, secret, key, baseNonce []byte
-	var err error
-	if info, err = hex.DecodeString(v.info); err != nil {
-		return vector{}, errors.New("hex.DecodeString(info) failed")
-	}
-	if senderPubKey, err = hex.DecodeString(v.pkEm); err != nil {
-		return vector{}, errors.New("hex.DecodeString(pkEm) failed")
-	}
-	if senderPrivKey, err = hex.DecodeString(v.skEm); err != nil {
-		return vector{}, errors.New("hex.DecodeString(skEm) failed")
-	}
-	if recipientPubKey, err = hex.DecodeString(v.pkRm); err != nil {
-		return vector{}, errors.New("hex.DecodeString(pkRm) failed")
-	}
-	if sharedSecret, err = hex.DecodeString(v.sharedSecret); err != nil {
-		return vector{}, errors.New("hex.DecodeString(sharedSecret) failed")
-	}
-	if keyScheduleCtx, err = hex.DecodeString(v.keyScheduleCtx); err != nil {
-		return vector{}, errors.New("hex.DecodeString(keyScheduleCtx) failed")
-	}
-	if secret, err = hex.DecodeString(v.secret); err != nil {
-		return vector{}, errors.New("hex.DecodeString(secret) failed")
-	}
-	if key, err = hex.DecodeString(v.key); err != nil {
-		return vector{}, errors.New("hex.DecodeString(key) failed")
-	}
-	if baseNonce, err = hex.DecodeString(v.baseNonce); err != nil {
-		return vector{}, errors.New("hex.DecodeString(baseNonce) failed")
-	}
-
-	return vector{
-		mode:            v.mode,
-		kemID:           v.kemID,
-		kdfID:           v.kdfID,
-		aeadID:          v.aeadID,
-		info:            info,
-		senderPubKey:    senderPubKey,
-		senderPrivKey:   senderPrivKey,
-		recipientPubKey: recipientPubKey,
-		sharedSecret:    sharedSecret,
-		keyScheduleCtx:  keyScheduleCtx,
-		secret:          secret,
-		key:             key,
-		baseNonce:       baseNonce,
-	}, nil
-}
-
-func TestHkdfHpkeKdfLabeledExtractWithInternetDraftTestVector(t *testing.T) {
+// TODO(b/201070904): Write tests using hpkeX25519HkdfSha256BaseModeTestVectors
+// with hpkeAeadIds.
+func TestHkdfHpkeKdfLabeledExtractWithHpkeInternetDraftTestVector(t *testing.T) {
 	kdf, err := newHkdfHpkeKdf(sha256)
 	if err != nil {
 		t.Fatalf("newHkdfHpkeKdf(SHA256): got err %q, want success", err)
 	}
-	v, err := internetDraftTestVector(t)
+	id, v, err := hpkeInternetDraftTestVector(t)
 	if err != nil {
 		t.Fatal(err)
 	}
-	suiteID := hpkeSuiteID(v.kemID, v.kdfID, v.aeadID)
+	suiteID := hpkeSuiteID(id.kemID, id.kdfID, id.aeadID)
 
 	// Base mode uses a default empty value for the pre-shared key (PSK), see
 	// https://www.ietf.org/archive/id/draft-irtf-cfrg-hpke-12.html#section-5.1-2.4.
 	pskIDHash := kdf.labeledExtract(emptySalt, []byte{} /*=defaultPskId*/, "psk_id_hash", suiteID)
 	infoHash := kdf.labeledExtract(emptySalt, v.info, "info_hash", suiteID)
 	keyScheduleCtx := []byte{}
-	keyScheduleCtx = append(keyScheduleCtx, v.mode)
+	keyScheduleCtx = append(keyScheduleCtx, id.mode)
 	keyScheduleCtx = append(keyScheduleCtx, pskIDHash...)
 	keyScheduleCtx = append(keyScheduleCtx, infoHash...)
 	if !bytes.Equal(keyScheduleCtx, v.keyScheduleCtx) {
@@ -141,11 +62,11 @@ func TestHkdfHpkeKdfLabeledExpand(t *testing.T) {
 	if err != nil {
 		t.Fatalf("newHkdfHpkeKdf(SHA256): got err %q, want success", err)
 	}
-	v, err := internetDraftTestVector(t)
+	id, v, err := hpkeInternetDraftTestVector(t)
 	if err != nil {
 		t.Fatal(err)
 	}
-	suiteID := hpkeSuiteID(x25519HkdfSha256, hkdfSha256, aes128GCM)
+	suiteID := hpkeSuiteID(id.kemID, id.kdfID, id.aeadID)
 
 	tests := []struct {
 		infoLabel string
@@ -247,7 +168,7 @@ func TestHkdfHpkeKdfExtractAndExpand(t *testing.T) {
 	if err != nil {
 		t.Fatalf("newHkdfHpkeKdf(SHA256): got err %q, want success", err)
 	}
-	v, err := internetDraftTestVector(t)
+	_, v, err := hpkeInternetDraftTestVector(t)
 	if err != nil {
 		t.Fatal(err)
 	}
