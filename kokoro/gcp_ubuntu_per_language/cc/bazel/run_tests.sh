3f5914e81bd30bd13ec037b6a0c28a20ac297401
ambrosin <ambrosin@google.com>
2022-01-20 01:14:10 -0800

    Create separate Kokoro test for Tink C++ with BoringSSL FIPS
    PiperOrigin-RevId: 423005816

--- kokoro/gcp_ubuntu_per_language/cc/bazel/run_tests.sh
+++ kokoro/gcp_ubuntu_per_language/cc/bazel/run_tests.sh
@@ -24,27 +24,3 @@ cd cc
 use_bazel.sh $(cat .bazelversion)
 bazel build ...
 bazel test --test_output=errors -- ...
-
-# Run build and tests with the BoringSSL FIPS module
-bazel clean
-APPEND_AFTER='workspace(name = "tink_cc")'
-NUM_MATCHES="$(grep -c "${APPEND_AFTER}" WORKSPACE)"
-if (( $? != 0 || NUM_MATCHES != 1)); then
-  echo "ERROR: Could not patch WORKSPACE to build BoringSSL with FIPS module"
-  exit 1
-fi
-
-mapfile LOCAL_FIPS_REPOSITORY <<EOM
-local_repository(
-  name = "boringssl",
-  path = "third_party/boringssl_fips",
-)
-EOM
-
-printf -v INSERT_TEXT '\\n%s' "${LOCAL_FIPS_REPOSITORY[@]//$'\n'/}"
-sed -i.bak "/${APPEND_AFTER}/a \\${INSERT_TEXT}" WORKSPACE
-
-bazel build --//config:use_only_fips=True --build_tag_filters=fips,-requires_boringcrypto_update -- ...
-bazel test --//config:use_only_fips=True --build_tests_only --build_tag_filters=fips,-requires_boringcrypto_update --test_tag_filters=fips,-requires_boringcrypto_update -- ...
-
-mv WORKSPACE.bak WORKSPACE
