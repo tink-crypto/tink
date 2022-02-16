d0bc84bcffc8ccced0e32cfc1908d0288d8ff39b
felobato <felobato@google.com>
2022-02-15 16:42:28 -0800

    JWT ECDSA Key Templates
    PiperOrigin-RevId: 428906764

--- go/jwt/jwt_key_templates.go
+++ go/jwt/jwt_key_templates.go
@@ -18,6 +18,7 @@ package jwt
 
 import (
 	"google.golang.org/protobuf/proto"
+	jepb "github.com/google/tink/go/proto/jwt_ecdsa_go_proto"
 	jwtmacpb "github.com/google/tink/go/proto/jwt_hmac_go_proto"
 	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
 )
@@ -36,32 +37,87 @@ func createJWTHMACKeyTemplate(keySize uint32, algorithm jwtmacpb.JwtHmacAlgorith
 	}
 }
 
-// HS256Template creates a JWT key template for JWA algorithm "HS256", which is a HMAC-SHA256 with a 32 byte key. It will set a key ID header "kid" in the token.
+func createJWTECDSAKeyTemplate(algorithm jepb.JwtEcdsaAlgorithm, outputPrefixType tinkpb.OutputPrefixType) *tinkpb.KeyTemplate {
+	format := &jepb.JwtEcdsaKeyFormat{
+		Version:   jwtECDSASignerKeyVersion,
+		Algorithm: algorithm,
+	}
+	serializedFormat, _ := proto.Marshal(format)
+	return &tinkpb.KeyTemplate{
+		TypeUrl:          jwtECDSASignerTypeURL,
+		Value:            serializedFormat,
+		OutputPrefixType: outputPrefixType,
+	}
+}
+
+// HS256Template creates a JWT key template for JWA algorithm "HS256", which is a
+// HMAC-SHA256 with a 32 byte key. It will set a key ID header "kid" in the token.
 func HS256Template() *tinkpb.KeyTemplate {
 	return createJWTHMACKeyTemplate(32, jwtmacpb.JwtHmacAlgorithm_HS256, tinkpb.OutputPrefixType_TINK)
 }
 
-// RawHS256Template creates a JWT key template for JWA algorithm "HS256", which is a HMAC-SHA256 with a 32 byte key. It will not set a key ID header "kid" in the token.
+// RawHS256Template creates a JWT key template for JWA algorithm "HS256", which is a
+// HMAC-SHA256 with a 32 byte key. It will not set a key ID header "kid" in the token.
 func RawHS256Template() *tinkpb.KeyTemplate {
 	return createJWTHMACKeyTemplate(32, jwtmacpb.JwtHmacAlgorithm_HS256, tinkpb.OutputPrefixType_RAW)
 }
 
-// HS384Template creates a JWT key template for JWA algorithm "HS384", which is a HMAC-SHA384 with a 48 byte key. It will set a key ID header "kid" in the token.
+// HS384Template creates a JWT key template for JWA algorithm "HS384", which is a
+// HMAC-SHA384 with a 48 byte key. It will set a key ID header "kid" in the token.
 func HS384Template() *tinkpb.KeyTemplate {
 	return createJWTHMACKeyTemplate(48, jwtmacpb.JwtHmacAlgorithm_HS384, tinkpb.OutputPrefixType_TINK)
 }
 
-// RawHS384Template creates a JWT key template for JWA algorithm "HS384", which is a HMAC-SHA384 with a 48 byte key. It will not set a key ID header "kid" in the token.
+// RawHS384Template creates a JWT key template for JWA algorithm "HS384", which is a
+// HMAC-SHA384 with a 48 byte key. It will not set a key ID header "kid" in the token.
 func RawHS384Template() *tinkpb.KeyTemplate {
 	return createJWTHMACKeyTemplate(48, jwtmacpb.JwtHmacAlgorithm_HS384, tinkpb.OutputPrefixType_RAW)
 }
 
-// HS512Template creates a JWT key template for JWA algorithm "HS512", which is a HMAC-SHA512 with a 64 byte key. It will set a key ID header "kid" in the token.
+// HS512Template creates a JWT key template for JWA algorithm "HS512", which is a
+// HMAC-SHA512 with a 64 byte key. It will set a key ID header "kid" in the token.
 func HS512Template() *tinkpb.KeyTemplate {
 	return createJWTHMACKeyTemplate(64, jwtmacpb.JwtHmacAlgorithm_HS512, tinkpb.OutputPrefixType_TINK)
 }
 
-// RawHS512Template creates a JWT key template for JWA algorithm "HS512", which is a HMAC-SHA512 with a 64 byte key. It will not set a key ID header "kid" in the token.
+// RawHS512Template creates a JWT key template for JWA algorithm "HS512", which is a
+// HMAC-SHA512 with a 64 byte key. It will not set a key ID header "kid" in the token.
 func RawHS512Template() *tinkpb.KeyTemplate {
 	return createJWTHMACKeyTemplate(64, jwtmacpb.JwtHmacAlgorithm_HS512, tinkpb.OutputPrefixType_RAW)
+}
+
+// ES256Template creates a JWT key template for JWA algorithm "ES256", which is digital
+// signature with the NIST P-256 curve. It will set a key ID header "kid" in the token.
+func ES256Template() *tinkpb.KeyTemplate {
+	return createJWTECDSAKeyTemplate(jepb.JwtEcdsaAlgorithm_ES256, tinkpb.OutputPrefixType_TINK)
+}
+
+// RawES256Template creates a JWT key template for JWA algorithm "ES256", which is digital
+// signature with the NIST P-256 curve. It will not set a key ID header "kid" in the token.
+func RawES256Template() *tinkpb.KeyTemplate {
+	return createJWTECDSAKeyTemplate(jepb.JwtEcdsaAlgorithm_ES256, tinkpb.OutputPrefixType_RAW)
+}
+
+// ES384Template creates a JWT key template for JWA algorithm "ES384", which is digital
+// signature with the NIST P-384 curve. It will set a key ID header "kid" in the token.
+func ES384Template() *tinkpb.KeyTemplate {
+	return createJWTECDSAKeyTemplate(jepb.JwtEcdsaAlgorithm_ES384, tinkpb.OutputPrefixType_TINK)
+}
+
+// RawES384Template creates a JWT key template for JWA algorithm "ES384", which is digital
+// signature with the NIST P-384 curve. It will not set a key ID header "kid" in the token.
+func RawES384Template() *tinkpb.KeyTemplate {
+	return createJWTECDSAKeyTemplate(jepb.JwtEcdsaAlgorithm_ES384, tinkpb.OutputPrefixType_RAW)
+}
+
+// ES512Template creates a JWT key template for JWA algorithm "ES512", which is digital
+// signature with the NIST P-521 curve. It will set a key ID header "kid" in the token.
+func ES512Template() *tinkpb.KeyTemplate {
+	return createJWTECDSAKeyTemplate(jepb.JwtEcdsaAlgorithm_ES512, tinkpb.OutputPrefixType_TINK)
+}
+
+// RawES512Template creates a JWT key template for JWA algorithm "ES512", which is digital
+// signature with the NIST P-521 curve. It will not set a key ID header "kid" in the token.
+func RawES512Template() *tinkpb.KeyTemplate {
+	return createJWTECDSAKeyTemplate(jepb.JwtEcdsaAlgorithm_ES512, tinkpb.OutputPrefixType_RAW)
 }
