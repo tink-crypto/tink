6c2b006960042093478e11cdcb09eaec4435d9a5
tholenst <tholenst@google.com>
2022-04-21 01:57:15 -0700

    Add SecretKeyAccess and InsecureCreateSecretKeyAccess.
    These will be helpful to restrict visibility in the future (i.e., in order to access secret key material, in the future, users will have to call InsecureCreateSecretKeyAccess.get()).
    
    PiperOrigin-RevId: 443312063

---
+++ java_src/src/main/java/com/google/crypto/tink/SecretKeyAccess.java
@@ -0,0 +1,67 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink;
+
+import com.google.crypto.tink.annotations.Alpha;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.errorprone.annotations.CheckReturnValue;
+import com.google.errorprone.annotations.Immutable;
+import java.security.GeneralSecurityException;
+import java.util.Optional;
+
+/**
+ * Represents access to secret key material.
+ *
+ * <p>Tink restricts access to secret key material, and users who require such access need to have
+ * an object of the class {@code SecretKeyAccess} to do this. For example, a function that outputs
+ * individiual key bytes might look like this:
+ *
+ * <pre>
+ *   class HmacKey {
+ *      ...
+ *      public byte[] getKeyMaterial(SecretKeyAccess access) {
+ *        checkNotNull(access);
+ *        return keyMaterial;
+ *      }
+ *   }
+ * </pre>
+ *
+ * Users who want to call {@code getKeyMaterial} then need to get a {@code SecretKeyAccess} object
+ * via {@code InsecureSecretKeyAccess.get()}.
+ */
+@CheckReturnValue
+@Alpha
+@Immutable
+public final class SecretKeyAccess {
+  private SecretKeyAccess() {}
+
+  private static final SecretKeyAccess INSTANCE = new SecretKeyAccess();
+
+  /**
+   *  Package visibility restricted for {@link InsecureSecretKeyAccess}.
+   */
+  static SecretKeyAccess instance() {
+    return INSTANCE;
+  }
+
+  /** Throws an exception if the passed in optional is empty, otherwise returns the contents. */
+  @CanIgnoreReturnValue
+  public static SecretKeyAccess requireAccess(Optional<SecretKeyAccess> in)
+      throws GeneralSecurityException {
+    return in.orElseThrow(() -> new GeneralSecurityException("SecretKeyAccess is required"));
+  }
+}
---
+++ java_src/src/test/java/com/google/crypto/tink/SecretKeyAccessTest.java
@@ -0,0 +1,51 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import java.security.GeneralSecurityException;
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link SecretKeyAccess} */
+@RunWith(JUnit4.class)
+public final class SecretKeyAccessTest {
+  @Test
+  public void testGet_notNull() {
+    assertThat(InsecureSecretKeyAccess.get()).isNotNull();
+  }
+
+  @Test
+  public void testRequireAccess_worksAndReturnsObject() throws Exception {
+    assertThat(SecretKeyAccess.requireAccess(Optional.of(InsecureSecretKeyAccess.get())))
+        .isEqualTo(InsecureSecretKeyAccess.get());
+  }
+
+  @Test
+  public void testRequireAccess_throwsIfEmpty() throws Exception {
+    assertThrows(
+        GeneralSecurityException.class, () -> SecretKeyAccess.requireAccess(Optional.empty()));
+    // Optionals cannot be null: Optional.ofNullable(null) returns an absent value.
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> SecretKeyAccess.requireAccess(Optional.ofNullable(null)));
+  }
+}
