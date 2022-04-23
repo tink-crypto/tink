37d8a65ae4d6f75774d489485f55952f17ed3454
Tink Team <tink-dev@google.com>
2022-04-22 19:06:11 -0700

    Allow buffering and re-reading data when trying to find the correct StreamingAEAD key, but don't keep the buffer around once the key is found.
    This replaces the previous implementation that buffered the entire stream into
    a `bytes.Buffer` using `io.TeeReader`. The buffer could be re-read when trying
    a new key, but it was still referenced and written to when a key was found and
    it was no longer needed. That caused the entire stream to be held in the buffer
    instead of just the current block.
    
    Fixes #594.
    
    PiperOrigin-RevId: 443813811

--- go/streamingaead/decrypt_reader_test.go
+++ go/streamingaead/decrypt_reader_test.go
@@ -17,7 +17,11 @@
 package streamingaead
 
 import (
+	"bytes"
+	"crypto/rand"
+	"fmt"
 	"io"
+	"strings"
 	"testing"
 
 	"github.com/google/tink/go/subtle/random"
@@ -109,5 +113,110 @@ func BenchmarkDecryptReader(b *testing.B) {
 				b.Fatalf("Error decrypting data: %v", err)
 			}
 		}
+	}
+}
+
+func TestUnreaderUnread(t *testing.T) {
+	original := make([]byte, 4096)
+	if _, err := io.ReadFull(rand.Reader, original); err != nil {
+		t.Fatalf("Failed to fill buffer with random bytes: %v", err)
+	}
+
+	u := &unreader{r: bytes.NewReader(original)}
+	got, err := io.ReadAll(u)
+	if err != nil {
+		t.Errorf("First io.ReadAll(%T) failed unexpectedly: %v", u, err)
+	}
+	if !bytes.Equal(got, original) {
+		t.Errorf("First io.ReadAll(%T) got %d bytes, want %d bytes that match the original random data.\nGot: %X\nWant: %X", u, len(got), len(original), got, original)
+	}
+
+	u.unread()
+	got, err = io.ReadAll(u)
+	if err != nil {
+		t.Errorf("After %T.unread(), io.ReadAll(%T) failed unexpectedly: %v", u, u, err)
+	}
+	if !bytes.Equal(got, original) {
+		t.Errorf("After %T.unread(), io.ReadAll(%T) got %d bytes, want %d bytes that match the original random data.\nGot: %X\nWant: %X", u, u, len(got), len(original), got, original)
+	}
+}
+
+func TestUnreader(t *testing.T) {
+	// Repeating sequence of characters '0' through '9' makes it easy to see
+	// holes or repeated data.
+	original := make([]byte, 100)
+	for i := range original {
+		original[i] = '0' + byte(i%10)
+	}
+
+	type step struct {
+		read    int  // If set, read the given number of bytes exactly.
+		unread  bool // If true, call unread().
+		disable bool // If true, call disable().
+	}
+	tcs := []struct {
+		name  string
+		steps []step
+	}{
+		{"Read2UnreadRead4Unread", []step{{read: 2}, {unread: true}, {read: 4}, {unread: true}}},
+		{"Read4UnreadRead2Unread", []step{{read: 4}, {unread: true}, {read: 2}, {unread: true}}},
+		{"Read3UnreadRead3Unread", []step{{read: 3}, {unread: true}, {read: 3}, {unread: true}}},
+		{"Read3Disable", []step{{read: 3}, {disable: true}}},
+		{"Read2UnreadRead4Disable", []step{{read: 2}, {unread: true}, {read: 4}, {disable: true}}},
+		{"Read4UnreadRead2Disable", []step{{read: 4}, {unread: true}, {read: 2}, {disable: true}}},
+		{"Read3UnreadRead3Disable", []step{{read: 3}, {unread: true}, {read: 3}, {disable: true}}},
+		{"Read2UnreadDisable", []step{{read: 2}, {unread: true}, {disable: true}}},
+		{"Read4UnreadDisable", []step{{read: 4}, {unread: true}, {disable: true}}},
+		{"ReadAllUnread", []step{{read: len(original)}, {unread: true}}},
+		{"ReadAllDisable", []step{{read: len(original)}, {disable: true}}},
+		{"Unread", []step{{unread: true}}},
+		{"Disable", []step{{disable: true}}},
+		{"UnreadDisable", []step{{unread: true}, {disable: true}}},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			u := &unreader{r: bytes.NewReader(original)}
+			var (
+				after []string
+				pos   int
+			)
+			// Explains what happened before the failure.
+			prefix := func() string {
+				if after == nil {
+					return ""
+				}
+				return fmt.Sprintf("After %s, ", strings.Join(after, "+"))
+			}
+			for _, s := range tc.steps {
+				if s.read != 0 {
+					buf := make([]byte, s.read)
+					if _, err := io.ReadFull(u, buf); err != nil {
+						t.Fatalf("%sio.ReadFull(%T, %d byte buffer) failed unexpectedly: %v", prefix(), u, s.read, err)
+					}
+					if want := original[pos : pos+s.read]; !bytes.Equal(buf, want) {
+						t.Fatalf("%sio.ReadFull(%T, %d byte buffer) got %q, want %q", prefix(), u, s.read, buf, want)
+					}
+					after = append(after, fmt.Sprintf("Read(%d bytes)", s.read))
+					pos += s.read
+				}
+				if s.disable {
+					u.disable()
+					after = append(after, "disable()")
+				}
+				if s.unread {
+					u.unread()
+					after = append(after, "unread()")
+					pos = 0
+				}
+			}
+			got, err := io.ReadAll(u)
+			if err != nil {
+				t.Fatalf("%sio.ReadAll(%T) failed unexpectedly: %v", prefix(), u, err)
+			}
+			if want := original[pos:]; !bytes.Equal(want, got) {
+				t.Errorf("%sio.ReadAll(%T) got %q, want %q", prefix(), u, got, want)
+			}
+		})
 	}
 }
