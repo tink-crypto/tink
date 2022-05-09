9205e1c84ef691b9fa9af2726158b788446efaa0
juerg <juerg@google.com>
2022-05-09 10:47:58 -0700

    Rename additional data to associated data in cord_aes_gcm_boringssl.
    PiperOrigin-RevId: 447510142

--- cc/aead/internal/cord_aes_gcm_boringssl.h
+++ cc/aead/internal/cord_aes_gcm_boringssl.h
@@ -38,10 +38,10 @@ class CordAesGcmBoringSsl : public CordAead {
       util::SecretData key_value);
 
   crypto::tink::util::StatusOr<absl::Cord> Encrypt(
-      absl::Cord plaintext, absl::Cord additional_data) const override;
+      absl::Cord plaintext, absl::Cord associated_data) const override;
 
   crypto::tink::util::StatusOr<absl::Cord> Decrypt(
-      absl::Cord ciphertext, absl::Cord additional_data) const override;
+      absl::Cord ciphertext, absl::Cord associated_data) const override;
 
  private:
   explicit CordAesGcmBoringSsl(internal::SslUniquePtr<EVP_CIPHER_CTX> context)
