f59a31a6b217a6b5bb71fc669b326de788601766
ambrosin <ambrosin@google.com>
2022-05-24 07:18:25 -0700

    Add Kokoro test build script for Tink Java.
    PiperOrigin-RevId: 450675061

---
+++ java_src/kokoro/gcp_ubuntu/bazel/run_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+set -euo pipefail
+
+
+if [[ -n "${KOKORO_ROOT:-}" ]] ; then
+  cd "${KOKORO_ARTIFACTS_DIR}/git/tink"
+  use_bazel.sh "$(cat .bazelversion)"
+fi
+
+./kokoro/testutils/update_android_sdk.sh
+./kokoro/testutils/run_bazel_tests.sh .
