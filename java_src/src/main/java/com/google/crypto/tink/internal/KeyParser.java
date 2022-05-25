0ae04ed2febf6d88bf87e1b60cd01902243debd0
tholenst <tholenst@google.com>
2022-05-25 08:46:34 -0700

    Add a class KeyParser.
    PiperOrigin-RevId: 450932771

---
+++ java_src/src/main/java/com/google/crypto/tink/internal/KeyParser.java
@@ -0,0 +1,113 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.internal;
+
+import com.google.crypto.tink.Key;
+import com.google.crypto.tink.SecretKeyAccess;
+import java.security.GeneralSecurityException;
+import java.util.Optional;
+
+/**
+ * Parses {@code Serialization} objects into {@code Key} objects of a certain kind.
+ *
+ * <p>This class should eventually be in Tinks public API -- however, it might still change before
+ * that.
+ */
+public abstract class KeyParser<SerializationT extends Serialization> {
+  /**
+   * A function which parses a key.
+   *
+   * <p>This interface exists only so we have a type we can reference in {@link #create}. Users
+   * should not use this directly; see the explanation in {@link #create}.
+   */
+  public interface KeyParsingFunction<SerializationT extends Serialization> {
+    Key parseKey(SerializationT serialization, Optional<SecretKeyAccess> access)
+        throws GeneralSecurityException;
+  }
+
+  private final ByteArray objectIdentifier;
+  private final Class<SerializationT> serializationClass;
+
+  private KeyParser(ByteArray objectIdentifier, Class<SerializationT> serializationClass) {
+    this.objectIdentifier = objectIdentifier;
+    this.serializationClass = serializationClass;
+  }
+
+  /**
+   * Parses a serialization into a key.
+   *
+   * <p>This function is usually called with a Serialization matching the result of {@link
+   * getObjectIdentifier}. However, implementations should check that this is the case.
+   */
+  public abstract Key parseKey(SerializationT serialization, Optional<SecretKeyAccess> access)
+      throws GeneralSecurityException;
+
+  /**
+   * Returns the {@code objectIdentifier} for this serialization.
+   *
+   * <p>The object identifier is a unique identifier per registry for this object (in the standard
+   * proto serialization, it is the typeUrl). In other words, when registering a {@code KeyParser},
+   * the registry will invoke this to get the handled object identifier. In order to parse an object
+   * of type {@code SerializationT}, the registry will then obtain the {@code objectIdentifier} of
+   * this serialization object, and call the parser corresponding to this object.
+   */
+  public final ByteArray getObjectIdentifier() {
+    return objectIdentifier;
+  }
+
+  public final Class<SerializationT> getSerializationClass() {
+    return serializationClass;
+  }
+
+  /**
+   * Creates a KeyParser object.
+   *
+   * <p>In order to create a KeyParser object, one typically writes a function
+   *
+   * <pre>{@code
+   * class MyClass {
+   *   private static MyKey parse(MySerialization key, Optional<SecretKeyAccess> access)
+   *             throws GeneralSecurityException {
+   *     ...
+   *   }
+   * }
+   * }</pre>
+   *
+   * This function can then be used to create a {@code KeyParser}:
+   *
+   * <pre>{@code
+   * KeyParser<MyKey, MySerialization> parser =
+   *       KeyParser.create(MyClass::parse, objectIdentifier, MySerialization.class);
+   * }</pre>
+   *
+   * @param function The function used to parse a Key
+   * @param objectIdentifier The identifier to be returned by {@link #getObjectIdentifier}
+   * @param serializationClass The class object corresponding to {@code SerializationT}
+   */
+  public static <SerializationT extends Serialization> KeyParser<SerializationT> create(
+      KeyParsingFunction<SerializationT> function,
+      ByteArray objectIdentifier,
+      Class<SerializationT> serializationClass) {
+    return new KeyParser<SerializationT>(objectIdentifier, serializationClass) {
+      @Override
+      public Key parseKey(SerializationT serialization, Optional<SecretKeyAccess> access)
+          throws GeneralSecurityException {
+        return function.parseKey(serialization, access);
+      }
+    };
+  }
+}
---
+++ java_src/src/test/java/com/google/crypto/tink/internal/KeyParserTest.java
@@ -0,0 +1,99 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.internal;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.InsecureSecretKeyAccess;
+import com.google.crypto.tink.Key;
+import com.google.crypto.tink.KeyFormat;
+import com.google.crypto.tink.SecretKeyAccess;
+import com.google.errorprone.annotations.Immutable;
+import java.security.GeneralSecurityException;
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link KeyParser}. */
+@RunWith(JUnit4.class)
+public final class KeyParserTest {
+
+  @Immutable
+  private static class ExampleKey extends Key {
+    @Override
+    public boolean equalsKey(Key k) {
+      return k == this;
+    }
+
+    @Override
+    public Optional<Integer> getIdRequirement() {
+      return Optional.empty();
+    }
+
+    @Override
+    public KeyFormat getKeyFormat() {
+      throw new UnsupportedOperationException("Not needed in test");
+    }
+  }
+
+  @Immutable
+  private static class ExampleSerialization implements Serialization {
+    @Override
+    public ByteArray getObjectIdentifier() {
+      return ByteArray.copyOf(new byte[0]);
+    }
+  }
+
+  private static ExampleKey parse(
+      ExampleSerialization serialization, Optional<SecretKeyAccess> access)
+      throws GeneralSecurityException {
+    SecretKeyAccess.requireAccess(access);
+    return new ExampleKey();
+  }
+
+  @Test
+  public void createParser_works() throws Exception {
+    KeyParser.create(
+        KeyParserTest::parse, ByteArray.copyOf(new byte[0]), ExampleSerialization.class);
+  }
+
+  @Test
+  public void createParser_parseKey_works() throws Exception {
+    KeyParser<ExampleSerialization> parser =
+        KeyParser.create(
+            KeyParserTest::parse, ByteArray.copyOf(new byte[0]), ExampleSerialization.class);
+    assertThat(
+            parser.parseKey(new ExampleSerialization(), Optional.of(InsecureSecretKeyAccess.get())))
+        .isNotNull();
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> parser.parseKey(new ExampleSerialization(), Optional.empty()));
+  }
+
+  @Test
+  public void createParser_classes_work() throws Exception {
+    KeyParser<ExampleSerialization> parser =
+        KeyParser.create(
+            KeyParserTest::parse,
+            ByteArray.copyOf(new byte[] {1, 2, 3}),
+            ExampleSerialization.class);
+    assertThat(parser.getObjectIdentifier()).isEqualTo(ByteArray.copyOf(new byte[] {1, 2, 3}));
+    assertThat(parser.getSerializationClass()).isEqualTo(ExampleSerialization.class);
+  }
+}
