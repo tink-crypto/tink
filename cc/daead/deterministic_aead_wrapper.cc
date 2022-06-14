4a6e9545455239649a32cdc74b4cd0d688916e67
kste <kste@google.com>
2022-06-14 02:44:32 -0700

    Add monitoring to deterministic AEAD wrapper.
    If a MonitoringClientFactory is registered, then we create the DeterministicAeadWrapper with the corresponding MonitoringClients to report events from the EncryptDeterministically and DecryptDeterministically APIs.
    
    PiperOrigin-RevId: 454808935

--- cc/daead/deterministic_aead_wrapper.cc
+++ cc/daead/deterministic_aead_wrapper.cc
@@ -22,7 +22,10 @@
 #include "absl/status/status.h"
 #include "tink/crypto_format.h"
 #include "tink/deterministic_aead.h"
+#include "tink/internal/monitoring_util.h"
+#include "tink/internal/registry_impl.h"
 #include "tink/internal/util.h"
+#include "tink/monitoring/monitoring.h"
 #include "tink/primitive_set.h"
 #include "tink/util/status.h"
 #include "tink/util/statusor.h"
@@ -31,6 +34,10 @@ namespace crypto {
 namespace tink {
 
 namespace {
+
+constexpr absl::string_view kPrimitive = "daead";
+constexpr absl::string_view kEncryptApi = "encrypt";
+constexpr absl::string_view kDecryptApi = "decrypt";
 
 util::Status Validate(PrimitiveSet<DeterministicAead>* daead_set) {
   if (daead_set == nullptr) {
@@ -47,8 +54,13 @@ util::Status Validate(PrimitiveSet<DeterministicAead>* daead_set) {
 class  DeterministicAeadSetWrapper : public DeterministicAead {
  public:
   explicit DeterministicAeadSetWrapper(
-      std::unique_ptr<PrimitiveSet<DeterministicAead>> daead_set)
-      : daead_set_(std::move(daead_set)) {}
+      std::unique_ptr<PrimitiveSet<DeterministicAead>> daead_set,
+      std::unique_ptr<MonitoringClient> monitoring_encryption_client = nullptr,
+      std::unique_ptr<MonitoringClient> monitoring_decryption_client = nullptr)
+      : daead_set_(std::move(daead_set)),
+        monitoring_encryption_client_(std::move(monitoring_encryption_client)),
+        monitoring_decryption_client_(std::move(monitoring_decryption_client))
+        {}
 
   crypto::tink::util::StatusOr<std::string> EncryptDeterministically(
       absl::string_view plaintext,
@@ -62,6 +74,8 @@ class  DeterministicAeadSetWrapper : public DeterministicAead {
 
  private:
   std::unique_ptr<PrimitiveSet<DeterministicAead>> daead_set_;
+  std::unique_ptr<MonitoringClient> monitoring_encryption_client_;
+  std::unique_ptr<MonitoringClient> monitoring_decryption_client_;
 };
 
 util::StatusOr<std::string>
@@ -75,7 +89,16 @@ DeterministicAeadSetWrapper::EncryptDeterministically(
   auto encrypt_result =
       daead_set_->get_primary()->get_primitive().EncryptDeterministically(
           plaintext, associated_data);
-  if (!encrypt_result.ok()) return encrypt_result.status();
+  if (!encrypt_result.ok()) {
+    if (monitoring_encryption_client_ != nullptr) {
+      monitoring_encryption_client_->LogFailure();
+    }
+    return encrypt_result.status();
+  }
+  if (monitoring_encryption_client_ != nullptr) {
+    monitoring_encryption_client_->Log(daead_set_->get_primary()->get_key_id(),
+                                       plaintext.size());
+  }
   const std::string& key_id = daead_set_->get_primary()->get_identifier();
   return key_id + encrypt_result.value();
 }
@@ -94,11 +117,15 @@ DeterministicAeadSetWrapper::DecryptDeterministically(
     if (primitives_result.ok()) {
       absl::string_view raw_ciphertext =
           ciphertext.substr(CryptoFormat::kNonRawPrefixSize);
-      for (auto& daead_entry : *(primitives_result.value())) {
+      for (const auto& daead_entry : *(primitives_result.value())) {
         DeterministicAead& daead = daead_entry->get_primitive();
         auto decrypt_result =
             daead.DecryptDeterministically(raw_ciphertext, associated_data);
         if (decrypt_result.ok()) {
+          if (monitoring_decryption_client_ != nullptr) {
+            monitoring_decryption_client_->Log(daead_entry->get_key_id(),
+                                               raw_ciphertext.size());
+          }
           return std::move(decrypt_result.value());
         } else {
           // LOG that a matching key didn't decrypt the ciphertext.
@@ -110,14 +137,21 @@ DeterministicAeadSetWrapper::DecryptDeterministically(
   // No matching key succeeded with decryption, try all RAW keys.
   auto raw_primitives_result = daead_set_->get_raw_primitives();
   if (raw_primitives_result.ok()) {
-    for (auto& daead_entry : *(raw_primitives_result.value())) {
+    for (const auto& daead_entry : *(raw_primitives_result.value())) {
       DeterministicAead& daead = daead_entry->get_primitive();
       auto decrypt_result =
           daead.DecryptDeterministically(ciphertext, associated_data);
       if (decrypt_result.ok()) {
+        if (monitoring_decryption_client_ != nullptr) {
+          monitoring_decryption_client_->Log(daead_entry->get_key_id(),
+                                             ciphertext.size());
+        }
         return std::move(decrypt_result.value());
       }
     }
+  }
+  if (monitoring_decryption_client_ != nullptr) {
+    monitoring_decryption_client_->LogFailure();
   }
   return util::Status(absl::StatusCode::kInvalidArgument, "decryption failed");
 }
@@ -129,9 +163,39 @@ DeterministicAeadWrapper::Wrap(
     std::unique_ptr<PrimitiveSet<DeterministicAead>> primitive_set) const {
   util::Status status = Validate(primitive_set.get());
   if (!status.ok()) return status;
-  std::unique_ptr<DeterministicAead> daead(
-      new DeterministicAeadSetWrapper(std::move(primitive_set)));
-  return std::move(daead);
+
+  MonitoringClientFactory* const monitoring_factory =
+      internal::RegistryImpl::GlobalInstance().GetMonitoringClientFactory();
+
+  // Monitoring is not enabled. Create a wrapper without monitoring clients.
+  if (monitoring_factory == nullptr) {
+    return {absl::make_unique<DeterministicAeadSetWrapper>(
+        std::move(primitive_set))};
+  }
+
+  util::StatusOr<MonitoringKeySetInfo> keyset_info =
+      internal::MonitoringKeySetInfoFromPrimitiveSet(*primitive_set);
+  if (!keyset_info.ok()) {
+    return keyset_info.status();
+  }
+
+  util::StatusOr<std::unique_ptr<MonitoringClient>>
+      monitoring_encryption_client = monitoring_factory->New(
+          MonitoringContext(kPrimitive, kEncryptApi, *keyset_info));
+  if (!monitoring_encryption_client.ok()) {
+    return monitoring_encryption_client.status();
+  }
+
+  util::StatusOr<std::unique_ptr<MonitoringClient>>
+      monitoring_decryption_client = monitoring_factory->New(
+          MonitoringContext(kPrimitive, kDecryptApi, *keyset_info));
+  if (!monitoring_decryption_client.ok()) {
+    return monitoring_decryption_client.status();
+  }
+
+  return {absl::make_unique<DeterministicAeadSetWrapper>(
+      std::move(primitive_set), *std::move(monitoring_encryption_client),
+      *std::move(monitoring_decryption_client))};
 }
 
 }  // namespace tink
