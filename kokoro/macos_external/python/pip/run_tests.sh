c7e0882b8e087a27b6ca739489c4889a0f5fdc9b
ambrosin <ambrosin@google.com>
2022-06-15 01:00:30 -0700

    Some refactoring of Tink Python kokoro test scripts.
     * Move protoc installation into a testutils script.
     * Use install_tink_via_pip.sh instead of repeating the same code in multiple scripts.
     * Modify install_tink_via_pip.sh to take the path to Tink python as a parameter instead of Tink base path.
    
    PiperOrigin-RevId: 455059462

--- kokoro/macos_external/python/pip/run_tests.sh
+++ kokoro/macos_external/python/pip/run_tests.sh
@@ -15,57 +15,26 @@
 ################################################################################
 
 set -euo pipefail
-cd "${KOKORO_ARTIFACTS_DIR}/git/tink"
+
+# If we are running on Kokoro cd into the repository.
+if [[ -n "${KOKORO_ROOT:-}" ]]; then
+  cd "${KOKORO_ARTIFACTS_DIR}/git/tink"
+  use_bazel.sh "$(cat python/.bazelversion)"
+fi
 
 ./kokoro/testutils/copy_credentials.sh "python/testdata"
+source ./kokoro/testutils/install_protoc.sh
+source ./kokoro/testutils/install_tink_via_pip.sh "${PWD}/python"
 
-install_temp_protoc() {
-  local protoc_version='3.19.3'
-  local protoc_zip="protoc-${protoc_version}-osx-x86_64.zip"
-  local protoc_url="https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/${protoc_zip}"
-  local -r protoc_tmpdir="$(mktemp -dt tink-protoc.XXXXXX)"
-  (
-    cd "${protoc_tmpdir}"
-    curl -OLsS "${protoc_url}"
-    unzip "${protoc_zip}" bin/protoc
-  )
-  export PATH="${protoc_tmpdir}/bin:${PATH}"
-}
+# Get root certificates for gRPC
+curl -OLsS https://raw.githubusercontent.com/grpc/grpc/master/etc/roots.pem
+export GRPC_DEFAULT_SSL_ROOTS_FILE_PATH="${PWD}/roots.pem"
 
-install_pip_package() {
-  # Check if we can build Tink python package.
-  (
-    cd python
+# Set path to Tink base folder
+export TINK_SRC_PATH="${PWD}"
 
-    # Needed for setuptools
-    use_bazel.sh "$(cat .bazelversion)"
-
-    # Set path to Tink base folder
-    export TINK_PYTHON_SETUPTOOLS_OVERRIDE_BASE_PATH="${PWD}/.."
-
-    # Update pip and install all requirements. Note that on MacOS we need to
-    # use the --user flag as otherwise pip will complain about permissions.
-    python3 -m pip install --user --upgrade pip
-    # TODO(b/219813176): Remove once Kokoro environment is compatible.
-    python3 -m pip install --user --upgrade 'setuptools==60.9.0'
-    python3 -m pip install --user .
-  )
-}
-
-run_tests_with_package() {
-  # Get root certificates for gRPC
-  curl -OLsS https://raw.githubusercontent.com/grpc/grpc/master/etc/roots.pem
-  export GRPC_DEFAULT_SSL_ROOTS_FILE_PATH="${PWD}/roots.pem"
-
-  # Set path to Tink base folder
-  export TINK_SRC_PATH="${PWD}"
-
-  # Run Python tests directly so the package is used.
-  # We exclude tests in tink/cc/pybind: they are implementation details and may
-  # depend on a testonly shared object.
-  find python/tink/ -not -path "*cc/pybind*" -type f -name "*_test.py" -print0 | xargs -0 -n1 python3
-}
-
-install_temp_protoc
-install_pip_package
-run_tests_with_package
+# Run Python tests directly so the package is used.
+# We exclude tests in tink/cc/pybind: they are implementation details and may
+# depend on a testonly shared object.
+find python/tink/ -not -path "*cc/pybind*" -type f -name "*_test.py" -print0 \
+  | xargs -0 -n1 python3
