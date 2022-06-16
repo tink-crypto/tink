0ccd8df2c2740b12249940e5f725c4c5f542af3b
ambrosin <ambrosin@google.com>
2022-06-16 01:47:23 -0700

    Move logic to verify Go generated files in kokoro tests to a testutils script.
    PiperOrigin-RevId: 455324712

--- kokoro/macos_external/go/bazel/run_tests.sh
+++ kokoro/macos_external/go/bazel/run_tests.sh
@@ -18,30 +18,21 @@ set -euo pipefail
 
 if [[ -n "${KOKORO_ROOT:-}" ]]; then
   cd "${KOKORO_ARTIFACTS_DIR}/git/tink"
+  use_bazel.sh "$(cat go/.bazelversion)"
 fi
+
 ./kokoro/testutils/copy_credentials.sh "go/testdata"
 # Sourcing required to update callers environment.
 source ./kokoro/testutils/install_go.sh
 
 echo "Using go binary from $(which go): $(go version)"
 
-
-if [[ -n "${KOKORO_ROOT:-}" ]]; then
-  use_bazel.sh "$(cat go/.bazelversion)"
-fi
-
-# TODO(b/219879042): Add tests that build files are up-to-date.
-# We already do these tests in gcp_ubuntu_per_language/go/bazel/run_tests.sh.
-# Since macos uses an older version of bash, these tests don't work here yet.
-# We either need to use a different version of bash, or replace readarray with
-# while loops.
+./kokoro/testutils/check_go_generated_files_up_to_date.sh go/
 
 declare -a MANUAL_TARGETS
 # Run manual tests which rely on test data only available via Bazel.
 if [[ -n "${KOKORO_ROOT:-}" ]]; then
-  MANUAL_TARGETS=(
-    "//integration/gcpkms:gcpkms_test"
-  )
+  MANUAL_TARGETS=( "//integration/gcpkms:gcpkms_test" )
 fi
 readonly MANUAL_TARGETS
 
