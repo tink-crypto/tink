d894e705410bed4c5bc70e3d09c68dc3106422c5
wconner <wconner@google.com>
2022-07-19 10:12:26 -0700

    Register ChunkedMac wrapper during Mac registration.
    PiperOrigin-RevId: 461912594

--- cc/mac/mac_config_test.cc
+++ cc/mac/mac_config_test.cc
@@ -17,14 +17,18 @@
 #include "tink/mac/mac_config.h"
 
 #include <list>
+#include <memory>
+#include <string>
 #include <utility>
 
 #include "gtest/gtest.h"
 #include "absl/status/status.h"
+#include "tink/chunked_mac.h"
 #include "tink/config.h"
 #include "tink/config/tink_fips.h"
 #include "tink/keyset_handle.h"
 #include "tink/mac.h"
+#include "tink/mac/aes_cmac_key_manager.h"
 #include "tink/mac/hmac_key_manager.h"
 #include "tink/mac/mac_key_templates.h"
 #include "tink/registry.h"
@@ -39,6 +43,11 @@ namespace {
 using ::crypto::tink::test::DummyMac;
 using ::crypto::tink::test::IsOk;
 using ::crypto::tink::test::StatusIs;
+using ::google::crypto::tink::KeysetInfo;
+using ::google::crypto::tink::KeyStatusType;
+using ::google::crypto::tink::KeyTemplate;
+using ::google::crypto::tink::OutputPrefixType;
+using ::testing::Values;
 
 class MacConfigTest : public ::testing::Test {
  protected:
@@ -55,25 +64,49 @@ TEST_F(MacConfigTest, Basic) {
   EXPECT_THAT(
       Registry::get_key_manager<Mac>(HmacKeyManager().get_key_type()).status(),
       StatusIs(absl::StatusCode::kNotFound));
+  EXPECT_THAT(
+      Registry::get_key_manager<ChunkedMac>(HmacKeyManager().get_key_type())
+          .status(),
+      StatusIs(absl::StatusCode::kNotFound));
+  EXPECT_THAT(Registry::get_key_manager<Mac>(AesCmacKeyManager().get_key_type())
+                  .status(),
+              StatusIs(absl::StatusCode::kNotFound));
+  EXPECT_THAT(
+      Registry::get_key_manager<ChunkedMac>(AesCmacKeyManager().get_key_type())
+          .status(),
+      StatusIs(absl::StatusCode::kNotFound));
+
   ASSERT_THAT(MacConfig::Register(), IsOk());
+
   EXPECT_THAT(
       Registry::get_key_manager<Mac>(HmacKeyManager().get_key_type()).status(),
+      IsOk());
+  EXPECT_THAT(
+      Registry::get_key_manager<ChunkedMac>(HmacKeyManager().get_key_type())
+          .status(),
+      IsOk());
+  EXPECT_THAT(Registry::get_key_manager<Mac>(AesCmacKeyManager().get_key_type())
+                  .status(),
+              IsOk());
+  EXPECT_THAT(
+      Registry::get_key_manager<ChunkedMac>(AesCmacKeyManager().get_key_type())
+          .status(),
       IsOk());
 }
 
 // Tests that the MacWrapper has been properly registered and we can wrap
 // primitives.
-TEST_F(MacConfigTest, WrappersRegistered) {
+TEST_F(MacConfigTest, MacWrappersRegistered) {
   if (IsFipsModeEnabled()) {
     GTEST_SKIP() << "Not supported in FIPS-only mode";
   }
 
   ASSERT_TRUE(MacConfig::Register().ok());
 
-  google::crypto::tink::KeysetInfo::KeyInfo key_info;
-  key_info.set_status(google::crypto::tink::KeyStatusType::ENABLED);
+  KeysetInfo::KeyInfo key_info;
+  key_info.set_status(KeyStatusType::ENABLED);
   key_info.set_key_id(1234);
-  key_info.set_output_prefix_type(google::crypto::tink::OutputPrefixType::RAW);
+  key_info.set_output_prefix_type(OutputPrefixType::RAW);
   auto primitive_set = absl::make_unique<PrimitiveSet<Mac>>();
   ASSERT_TRUE(
       primitive_set
@@ -93,6 +126,48 @@ TEST_F(MacConfigTest, WrappersRegistered) {
       DummyMac("dummy").VerifyMac(mac_result.value(), "verified text").ok());
   EXPECT_FALSE(
       DummyMac("dummy").VerifyMac(mac_result.value(), "faked text").ok());
+}
+
+class ChunkedMacConfigTest : public ::testing::TestWithParam<KeyTemplate> {
+ protected:
+  void SetUp() override { Registry::Reset(); }
+};
+
+INSTANTIATE_TEST_SUITE_P(ChunkedMacConfigTestSuite, ChunkedMacConfigTest,
+                         Values(MacKeyTemplates::AesCmac(),
+                                MacKeyTemplates::HmacSha256()));
+
+// Tests that the ChunkedMacWrapper has been properly registered and we can get
+// primitives.
+TEST_P(ChunkedMacConfigTest, ChunkedMacWrappersRegistered) {
+  if (IsFipsModeEnabled()) {
+    GTEST_SKIP() << "Not supported in FIPS-only mode";
+  }
+
+  ASSERT_THAT(MacConfig::Register(), IsOk());
+
+  KeyTemplate key_template = GetParam();
+  util::StatusOr<std::unique_ptr<KeysetHandle>> key =
+      KeysetHandle::GenerateNew(key_template);
+  ASSERT_THAT(key, IsOk());
+
+  util::StatusOr<std::unique_ptr<ChunkedMac>> chunked_mac =
+      (*key)->GetPrimitive<ChunkedMac>();
+  ASSERT_THAT(chunked_mac, IsOk());
+
+  util::StatusOr<std::unique_ptr<ChunkedMacComputation>> computation =
+      (*chunked_mac)->CreateComputation();
+  ASSERT_THAT(computation, IsOk());
+  ASSERT_THAT((*computation)->Update("verified text"), IsOk());
+  util::StatusOr<std::string> tag = (*computation)->ComputeMac();
+  ASSERT_THAT(tag, IsOk());
+
+  util::StatusOr<std::unique_ptr<ChunkedMacVerification>> verification =
+      (*chunked_mac)->CreateVerification(*tag);
+  ASSERT_THAT(verification, IsOk());
+  ASSERT_THAT((*verification)->Update("verified text"), IsOk());
+
+  EXPECT_THAT((*verification)->VerifyMac(), IsOk());
 }
 
 // FIPS-only mode tests
