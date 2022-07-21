1a6adf7f2330c17e5f3f3eaebf6c05aee9436da2
juerg <juerg@google.com>
2022-07-21 00:51:47 -0700

    Fix some lint warnings.
    PiperOrigin-RevId: 462321424

--- java_src/src/main/java/com/google/crypto/tink/KeysetHandle.java
+++ java_src/src/main/java/com/google/crypto/tink/KeysetHandle.java
@@ -470,7 +470,7 @@ public final class KeysetHandle {
     Keyset.Builder keysetBuilder = Keyset.newBuilder();
     for (Keyset.Key key : keyset.getKeyList()) {
       KeyData keyData = createPublicKeyData(key.getKeyData());
-      keysetBuilder.addKey(Keyset.Key.newBuilder().mergeFrom(key).setKeyData(keyData).build());
+      keysetBuilder.addKey(key.toBuilder().setKeyData(keyData).build());
     }
     keysetBuilder.setPrimaryKeyId(keyset.getPrimaryKeyId());
     return new KeysetHandle(keysetBuilder.build());
