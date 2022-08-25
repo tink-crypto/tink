e05f07767510d0d60d5fab42071e3d01a4414eeb
juerg <juerg@google.com>
2022-08-25 09:30:23 -0700

    Add monitoring to PrfSet in Java
    PiperOrigin-RevId: 470008715

--- java_src/src/main/java/com/google/crypto/tink/prf/PrfSetWrapper.java
+++ java_src/src/main/java/com/google/crypto/tink/prf/PrfSetWrapper.java
@@ -18,6 +18,10 @@ package com.google.crypto.tink.prf;
 import com.google.crypto.tink.PrimitiveSet;
 import com.google.crypto.tink.PrimitiveWrapper;
 import com.google.crypto.tink.Registry;
+import com.google.crypto.tink.internal.MonitoringUtil;
+import com.google.crypto.tink.internal.MutableMonitoringRegistry;
+import com.google.crypto.tink.monitoring.MonitoringClient;
+import com.google.crypto.tink.monitoring.MonitoringKeysetInfo;
 import com.google.crypto.tink.proto.OutputPrefixType;
 import com.google.errorprone.annotations.Immutable;
 import java.security.GeneralSecurityException;
@@ -41,12 +45,47 @@ public class PrfSetWrapper implements PrimitiveWrapper<Prf, PrfSet> {
 
     private final int primaryKeyId;
 
+    @Immutable
+    private static class PrfWithMonitoring implements Prf {
+      private final Prf prf;
+      private final int keyId;
+
+      @SuppressWarnings("Immutable")
+      private final MonitoringClient.Logger logger;
+
+      @Override
+      public byte[] compute(byte[] input, int outputLength) throws GeneralSecurityException {
+        try {
+          byte[] output = prf.compute(input, outputLength);
+          logger.log(keyId, input.length);
+          return output;
+        } catch (GeneralSecurityException e) {
+          logger.logFailure();
+          throw e;
+        }
+      }
+
+      public PrfWithMonitoring(Prf prf, int keyId, MonitoringClient.Logger logger) {
+        this.prf = prf;
+        this.keyId = keyId;
+        this.logger = logger;
+      }
+    }
+
     private WrappedPrfSet(PrimitiveSet<Prf> primitives) throws GeneralSecurityException {
       if (primitives.getRawPrimitives().isEmpty()) {
         throw new GeneralSecurityException("No primitives provided.");
       }
       if (primitives.getPrimary() == null) {
         throw new GeneralSecurityException("Primary key not set.");
+      }
+      MonitoringClient.Logger logger;
+      if (primitives.hasAnnotations()) {
+        MonitoringClient client = MutableMonitoringRegistry.globalInstance().getMonitoringClient();
+        MonitoringKeysetInfo keysetInfo = MonitoringUtil.getMonitoringKeysetInfo(primitives);
+        logger = client.createLogger(keysetInfo, "prf", "compute");
+      } else {
+        logger = MonitoringUtil.DO_NOTHING_LOGGER;
       }
 
       primaryKeyId = primitives.getPrimary().getKeyId();
@@ -58,7 +97,9 @@ public class PrfSetWrapper implements PrimitiveWrapper<Prf, PrfSet> {
               "Key " + entry.getKeyId() + " has non raw prefix type");
         }
         // Likewise, the key IDs of the PrfSet passed
-        mutablePrfMap.put(entry.getKeyId(), entry.getPrimitive());
+        mutablePrfMap.put(
+            entry.getKeyId(),
+            new PrfWithMonitoring(entry.getPrimitive(), entry.getKeyId(), logger));
       }
       keyIdToPrfMap = Collections.unmodifiableMap(mutablePrfMap);
     }
--- java_src/src/test/java/com/google/crypto/tink/prf/PrfSetWrapperTest.java
+++ java_src/src/test/java/com/google/crypto/tink/prf/PrfSetWrapperTest.java
@@ -22,12 +22,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 import com.google.crypto.tink.PrimitiveSet;
+import com.google.crypto.tink.internal.MutableMonitoringRegistry;
+import com.google.crypto.tink.internal.testing.FakeMonitoringClient;
+import com.google.crypto.tink.monitoring.MonitoringAnnotations;
 import com.google.crypto.tink.proto.KeyStatusType;
 import com.google.crypto.tink.proto.Keyset;
 import com.google.crypto.tink.proto.OutputPrefixType;
 import com.google.crypto.tink.subtle.Random;
 import com.google.crypto.tink.testing.TestUtil;
+import com.google.errorprone.annotations.Immutable;
 import java.security.GeneralSecurityException;
+import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.function.ThrowingRunnable;
@@ -141,5 +146,149 @@ public class PrfSetWrapperTest {
             new PrfSetWrapper().wrap(primitiveSet);
           }
         });
+  }
+
+  @Test
+  public void testWithEmptyAnnotations_noMonitoring() throws Exception {
+    FakeMonitoringClient fakeMonitoringClient = new FakeMonitoringClient();
+    MutableMonitoringRegistry.globalInstance().clear();
+    MutableMonitoringRegistry.globalInstance().registerMonitoringClient(fakeMonitoringClient);
+
+    Keyset.Key primary =
+        TestUtil.createKey(
+            TestUtil.createPrfKeyData(Random.randBytes(KEY_SIZE)),
+            /* keyId= */ 5,
+            KeyStatusType.ENABLED,
+            OutputPrefixType.RAW);
+    PrimitiveSet<Prf> primitives =
+        TestUtil.createPrimitiveSetWithAnnotations(
+            TestUtil.createKeyset(primary), MonitoringAnnotations.EMPTY, Prf.class);
+    byte[] plaintext = "blah".getBytes(UTF_8);
+
+    PrfSet prfSet = new PrfSetWrapper().wrap(primitives);
+    byte[] prsPrimary = prfSet.computePrimary(plaintext, 12);
+    byte[] prs5 = prfSet.getPrfs().get(5).compute(plaintext, 12);
+
+    assertThat(prfSet.getPrimaryId()).isEqualTo(5);
+    assertThat(prfSet.getPrfs()).hasSize(1);
+    assertThat(prsPrimary).hasLength(12);
+    assertThat(prs5).hasLength(12);
+
+    // Without annotations, nothing gets logged.
+    assertThat(fakeMonitoringClient.getLogEntries()).isEmpty();
+    assertThat(fakeMonitoringClient.getLogFailureEntries()).isEmpty();
+  }
+
+  @Test
+  public void testWithAnnotations_hasMonitoring() throws Exception {
+    FakeMonitoringClient fakeMonitoringClient = new FakeMonitoringClient();
+    MutableMonitoringRegistry.globalInstance().clear();
+    MutableMonitoringRegistry.globalInstance().registerMonitoringClient(fakeMonitoringClient);
+
+    byte[] primaryKeyValue = Random.randBytes(KEY_SIZE);
+    Keyset.Key primary =
+        TestUtil.createKey(
+            TestUtil.createPrfKeyData(primaryKeyValue),
+            /* keyId= */ 5,
+            KeyStatusType.ENABLED,
+            OutputPrefixType.RAW);
+    byte[] secondaryKeyValue = Random.randBytes(KEY_SIZE);
+    Keyset.Key secondary =
+        TestUtil.createKey(
+            TestUtil.createPrfKeyData(secondaryKeyValue),
+            /* keyId= */ 6,
+            KeyStatusType.ENABLED,
+            OutputPrefixType.RAW);
+    MonitoringAnnotations annotations =
+        MonitoringAnnotations.newBuilder().add("annotation_name", "annotation_value").build();
+    PrimitiveSet<Prf> primitives =
+        TestUtil.createPrimitiveSetWithAnnotations(
+            TestUtil.createKeyset(primary, secondary), annotations, Prf.class);
+    byte[] plaintext = "blah".getBytes(UTF_8);
+
+    PrfSet prfSet = new PrfSetWrapper().wrap(primitives);
+    byte[] prsPrimary = prfSet.computePrimary(plaintext, 12);
+    byte[] prs5 = prfSet.getPrfs().get(5).compute(plaintext, 12);
+    byte[] prs6 = prfSet.getPrfs().get(6).compute(plaintext, 12);
+
+    assertThat(prfSet.getPrimaryId()).isEqualTo(5);
+
+    assertThat(prfSet.getPrfs()).hasSize(2);
+    assertThat(prsPrimary).hasLength(12);
+    assertThat(prs5).isEqualTo(prsPrimary);
+    assertThat(prsPrimary).isNotEqualTo(prs6);
+
+    assertThat(fakeMonitoringClient.getLogFailureEntries()).isEmpty();
+
+    List<FakeMonitoringClient.LogEntry> logEntries = fakeMonitoringClient.getLogEntries();
+    assertThat(logEntries).hasSize(3);
+    FakeMonitoringClient.LogEntry entry0 = logEntries.get(0);
+    assertThat(entry0.getKeyId()).isEqualTo(5);
+    assertThat(entry0.getPrimitive()).isEqualTo("prf");
+    assertThat(entry0.getApi()).isEqualTo("compute");
+    assertThat(entry0.getKeysetInfo().getAnnotations()).isEqualTo(annotations);
+    FakeMonitoringClient.LogEntry entry1 = logEntries.get(1);
+    assertThat(entry1.getKeyId()).isEqualTo(5);
+    assertThat(entry1.getPrimitive()).isEqualTo("prf");
+    assertThat(entry1.getApi()).isEqualTo("compute");
+    assertThat(entry1.getKeysetInfo().getAnnotations()).isEqualTo(annotations);
+    FakeMonitoringClient.LogEntry entry2 = logEntries.get(2);
+    assertThat(entry2.getKeyId()).isEqualTo(6);
+    assertThat(entry2.getPrimitive()).isEqualTo("prf");
+    assertThat(entry2.getApi()).isEqualTo("compute");
+    assertThat(entry2.getKeysetInfo().getAnnotations()).isEqualTo(annotations);
+  }
+
+  @Immutable
+  private static class AlwaysFailingPrf implements Prf {
+
+    @Override
+    public byte[] compute(byte[] input, int outputLength) throws GeneralSecurityException {
+      throw new GeneralSecurityException("fail");
+    }
+  }
+
+  @Test
+  public void testAlwaysFailingPrfWithAnnotations_hasMonitoring() throws Exception {
+    FakeMonitoringClient fakeMonitoringClient = new FakeMonitoringClient();
+    MutableMonitoringRegistry.globalInstance().clear();
+    MutableMonitoringRegistry.globalInstance().registerMonitoringClient(fakeMonitoringClient);
+
+    MonitoringAnnotations annotations =
+        MonitoringAnnotations.newBuilder().add("annotation_name", "annotation_value").build();
+    PrimitiveSet<Prf> primitives =
+        PrimitiveSet.newBuilder(Prf.class)
+            .setAnnotations(annotations)
+            .addPrimaryPrimitive(
+                new AlwaysFailingPrf(),
+                TestUtil.createKey(
+                    TestUtil.createPrfKeyData(Random.randBytes(KEY_SIZE)),
+                    /* keyId= */ 5,
+                    KeyStatusType.ENABLED,
+                    OutputPrefixType.RAW))
+            .build();
+    PrfSet prfSet = new PrfSetWrapper().wrap(primitives);
+
+    byte[] plaintext = "blah".getBytes(UTF_8);
+
+    assertThrows(GeneralSecurityException.class, () -> prfSet.computePrimary(plaintext, 12));
+    assertThrows(
+        GeneralSecurityException.class, () -> prfSet.getPrfs().get(5).compute(plaintext, 12));
+
+    assertThat(fakeMonitoringClient.getLogEntries()).isEmpty();
+
+    List<FakeMonitoringClient.LogFailureEntry> failures =
+        fakeMonitoringClient.getLogFailureEntries();
+    assertThat(failures).hasSize(2);
+    FakeMonitoringClient.LogFailureEntry failure0 = failures.get(0);
+    assertThat(failure0.getPrimitive()).isEqualTo("prf");
+    assertThat(failure0.getApi()).isEqualTo("compute");
+    assertThat(failure0.getKeysetInfo().getPrimaryKeyId()).isEqualTo(5);
+    assertThat(failure0.getKeysetInfo().getAnnotations()).isEqualTo(annotations);
+    FakeMonitoringClient.LogFailureEntry failure1 = failures.get(1);
+    assertThat(failure1.getPrimitive()).isEqualTo("prf");
+    assertThat(failure1.getApi()).isEqualTo("compute");
+    assertThat(failure1.getKeysetInfo().getPrimaryKeyId()).isEqualTo(5);
+    assertThat(failure1.getKeysetInfo().getAnnotations()).isEqualTo(annotations);
   }
 }
