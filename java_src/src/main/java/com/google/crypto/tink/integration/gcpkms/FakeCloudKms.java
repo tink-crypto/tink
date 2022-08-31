52012c006b1c3438ddbdcaeb8c24221bcda177bc
juerg <juerg@google.com>
2022-08-31 04:18:05 -0700

    Add a FakeCloudKms.
    This will be useful to add unit tests for GcpKmsClient.
    
    PiperOrigin-RevId: 471223397

---
+++ java_src/src/main/java/com/google/crypto/tink/integration/gcpkms/FakeCloudKms.java
@@ -0,0 +1,164 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+package com.google.crypto.tink.integration.gcpkms;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.LowLevelHttpRequest;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.cloudkms.v1.CloudKMS;
+import com.google.api.services.cloudkms.v1.model.DecryptRequest;
+import com.google.api.services.cloudkms.v1.model.DecryptResponse;
+import com.google.api.services.cloudkms.v1.model.EncryptRequest;
+import com.google.api.services.cloudkms.v1.model.EncryptResponse;
+import com.google.crypto.tink.Aead;
+import com.google.crypto.tink.KeyTemplates;
+import com.google.crypto.tink.KeysetHandle;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A partial, fake implementation of {@link com.google.api.services.cloudkms.v1.CloudKMS}.
+ *
+ * <p>It creates a new AEAD for every valid key ID. CryptoKeys use them to encrypt and decrypt.
+ */
+final class FakeCloudKms extends CloudKMS {
+  private final Map<String, Aead> aeads = new HashMap<>();
+
+  public FakeCloudKms(List<String> validKeyIds)
+      throws GeneralSecurityException {
+    super(
+        new HttpTransport() {
+          @Override
+          protected LowLevelHttpRequest buildRequest(String method, String url) throws IOException {
+            throw new IOException("Test should not have interacted with HttpTransport.");
+          }
+        },
+        new GsonFactory(),
+        new GoogleCredential());
+    for (String keyId : validKeyIds) {
+      Aead aead = KeysetHandle.generateNew(KeyTemplates.get("AES128_GCM")).getPrimitive(Aead.class);
+      aeads.put(keyId, aead);
+    }
+  }
+
+  private final Projects projects = new Projects();
+
+  @Override
+  public Projects projects() {
+    return projects;
+  }
+
+  final class Projects extends CloudKMS.Projects {
+
+    private final Locations locations = new Locations();
+
+    @Override
+    public Locations locations() {
+      return locations;
+    }
+
+    final class Locations extends CloudKMS.Projects.Locations {
+
+      private final KeyRings keyRings = new KeyRings();
+
+      @Override
+      public KeyRings keyRings() {
+        return keyRings;
+      }
+
+      final class KeyRings extends CloudKMS.Projects.Locations.KeyRings {
+
+        private final CryptoKeys cryptoKeys = new CryptoKeys();
+
+        @Override
+        public CryptoKeys cryptoKeys() {
+          return cryptoKeys;
+        }
+
+        final class CryptoKeys extends CloudKMS.Projects.Locations.KeyRings.CryptoKeys {
+          @Override
+          public Encrypt encrypt(String name, EncryptRequest request) {
+            return new Encrypt(name, request);
+          }
+
+          @Override
+          public Decrypt decrypt(String name, DecryptRequest request) {
+            return new Decrypt(name, request);
+          }
+
+          final class Encrypt extends CloudKMS.Projects.Locations.KeyRings.CryptoKeys.Encrypt {
+            String name;
+            EncryptRequest request;
+
+            Encrypt(String name, EncryptRequest request) {
+              super(name, request);
+              this.name = name;
+              this.request = request;
+            }
+
+            @Override
+            public EncryptResponse execute() throws IOException {
+              if (!aeads.containsKey(name)) {
+                throw new IOException(
+                    "Unknown key ID : " + name + " is not in " + aeads.keySet());
+              }
+              try {
+                Aead aead = aeads.get(name);
+                byte[] ciphertext =
+                    aead.encrypt(
+                        request.decodePlaintext(), request.decodeAdditionalAuthenticatedData());
+                return new EncryptResponse().encodeCiphertext(ciphertext);
+              } catch (GeneralSecurityException e) {
+                throw new IOException(e.getMessage());
+              }
+            }
+          }
+
+          final class Decrypt extends CloudKMS.Projects.Locations.KeyRings.CryptoKeys.Decrypt {
+            String name;
+            DecryptRequest request;
+
+            Decrypt(String name, DecryptRequest request) {
+              super(name, request);
+              this.name = name;
+              this.request = request;
+            }
+
+            @Override
+            public DecryptResponse execute() throws IOException {
+              if (!aeads.containsKey(name)) {
+                throw new IOException("Unknown key ID : " + name + " is not in " + aeads.keySet());
+              }
+              try {
+                Aead aead = aeads.get(name);
+                byte[] plaintext =
+                    aead.decrypt(
+                        request.decodeCiphertext(), request.decodeAdditionalAuthenticatedData());
+                return new DecryptResponse().encodePlaintext(plaintext);
+              } catch (GeneralSecurityException e) {
+                throw new IOException(e.getMessage());
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ java_src/src/test/java/com/google/crypto/tink/integration/gcpkms/FakeCloudKmsTest.java
@@ -0,0 +1,237 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+package com.google.crypto.tink.integration.gcpkms;
+
+import static com.google.common.truth.Truth.assertThat;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertThrows;
+
+import com.google.api.services.cloudkms.v1.CloudKMS;
+import com.google.api.services.cloudkms.v1.model.DecryptRequest;
+import com.google.api.services.cloudkms.v1.model.DecryptResponse;
+import com.google.api.services.cloudkms.v1.model.EncryptRequest;
+import com.google.api.services.cloudkms.v1.model.EncryptResponse;
+import com.google.crypto.tink.aead.AeadConfig;
+import java.io.IOException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class FakeCloudKmsTest {
+
+  private static final String KEY_ID =
+      "projects/tink-test/locations/global/keyRings/unit-test/cryptoKeys/aead-key";
+  private static final String KEY_ID_2 =
+      "projects/tink-test/locations/global/keyRings/unit-test/cryptoKeys/aead-key-2";
+
+  @BeforeClass
+  public static void setUpClass() throws Exception {
+    AeadConfig.register();
+  }
+
+  @Test
+  public void testEncryptDecryptWithValidKeyId_success() throws Exception {
+    CloudKMS kms = new FakeCloudKms(asList(KEY_ID));
+
+    byte[] plaintext = "plaintext".getBytes(UTF_8);
+    byte[] associatedData = "associatedData".getBytes(UTF_8);
+
+    EncryptRequest encRequest =
+        new EncryptRequest()
+            .encodePlaintext(plaintext)
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    EncryptResponse encResponse =
+        kms.projects().locations().keyRings().cryptoKeys().encrypt(KEY_ID, encRequest).execute();
+
+    DecryptRequest decRequest =
+        new DecryptRequest()
+            .encodeCiphertext(encResponse.decodeCiphertext())
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    DecryptResponse decResponse =
+        kms.projects().locations().keyRings().cryptoKeys().decrypt(KEY_ID, decRequest).execute();
+
+    assertThat(decResponse.decodePlaintext()).isEqualTo(plaintext);
+  }
+
+  @Test
+  public void testEncryptWithUnknownKeyId_executeFails() throws Exception {
+    CloudKMS kms = new FakeCloudKms(asList(KEY_ID));
+
+    byte[] plaintext = "plaintext".getBytes(UTF_8);
+    byte[] associatedData = "associatedData".getBytes(UTF_8);
+
+    EncryptRequest encRequest =
+        new EncryptRequest()
+            .encodePlaintext(plaintext)
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    // The RPC to the KMS is done when execute is called. Therefore, calling encrypt for a
+    // valid but unknown key id does not (yet) fail.
+    CloudKMS.Projects.Locations.KeyRings.CryptoKeys.Encrypt enc =
+        kms.projects().locations().keyRings().cryptoKeys().encrypt(KEY_ID_2, encRequest);
+    assertThrows(IOException.class, enc::execute);
+  }
+
+  @Test
+  public void testEncryptWithInvalidKeyId_encryptFails() throws Exception {
+    String invalidId = "invalid";
+
+    CloudKMS kms = new FakeCloudKms(asList(invalidId));
+
+    byte[] plaintext = "plaintext".getBytes(UTF_8);
+    byte[] associatedData = "associatedData".getBytes(UTF_8);
+
+    EncryptRequest encRequest =
+        new EncryptRequest()
+            .encodePlaintext(plaintext)
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    // Encrypts validates the format of the key id, and fails if it is invalid.
+    CloudKMS.Projects.Locations.KeyRings.CryptoKeys cryptoKeys =
+        kms.projects().locations().keyRings().cryptoKeys();
+    assertThrows(IllegalArgumentException.class, () -> cryptoKeys.encrypt(invalidId, encRequest));
+  }
+
+  @Test
+  public void testDecryptWithInvalidKeyId_decryptFails() throws Exception {
+    String invalidId = "invalid";
+
+    CloudKMS kms = new FakeCloudKms(asList(KEY_ID, invalidId));
+
+    byte[] plaintext = "plaintext".getBytes(UTF_8);
+    byte[] associatedData = "associatedData".getBytes(UTF_8);
+
+    EncryptRequest encRequest =
+        new EncryptRequest()
+            .encodePlaintext(plaintext)
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    EncryptResponse encResponse =
+        kms.projects().locations().keyRings().cryptoKeys().encrypt(KEY_ID, encRequest).execute();
+
+    DecryptRequest decRequest =
+        new DecryptRequest()
+            .encodeCiphertext(encResponse.decodeCiphertext())
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    // Decrypt validates the format of the key id, and fails if it is invalid.
+    CloudKMS.Projects.Locations.KeyRings.CryptoKeys cryptoKeys =
+        kms.projects().locations().keyRings().cryptoKeys();
+    assertThrows(IllegalArgumentException.class, () -> cryptoKeys.decrypt(invalidId, decRequest));
+  }
+
+
+  @Test
+  public void testDecryptWithWrongKeyId_decryptFails() throws Exception {
+    CloudKMS kms = new FakeCloudKms(asList(KEY_ID, KEY_ID_2));
+
+    byte[] plaintext = "plaintext".getBytes(UTF_8);
+    byte[] associatedData = "associatedData".getBytes(UTF_8);
+
+    EncryptRequest encRequest =
+        new EncryptRequest()
+            .encodePlaintext(plaintext)
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    EncryptResponse encResponse =
+        kms.projects().locations().keyRings().cryptoKeys().encrypt(KEY_ID, encRequest).execute();
+
+    DecryptRequest decRequest =
+        new DecryptRequest()
+            .encodeCiphertext(encResponse.decodeCiphertext())
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    // The RPC to the KMS is done when execute is called. Therefore, calling decrypt with a wrong
+    // key id does not (yet) fail.
+    CloudKMS.Projects.Locations.KeyRings.CryptoKeys.Decrypt dec =
+        kms.projects()
+            .locations()
+            .keyRings()
+            .cryptoKeys()
+            .decrypt(KEY_ID_2, decRequest);
+    assertThrows(IOException.class, dec::execute);
+  }
+
+  @Test
+  public void testDecryptExecuteWithInvalidCiphertext_executeFails() throws Exception {
+    CloudKMS kms = new FakeCloudKms(asList(KEY_ID));
+
+    byte[] invalidCiphertext = "invalid".getBytes(UTF_8);
+    byte[] associatedData = "associatedData".getBytes(UTF_8);
+
+    DecryptRequest decRequestWithInvalidCiphertext =
+        new DecryptRequest()
+            .encodeCiphertext(invalidCiphertext)
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    CloudKMS.Projects.Locations.KeyRings.CryptoKeys.Decrypt dec =
+        kms.projects()
+            .locations()
+            .keyRings()
+            .cryptoKeys()
+            .decrypt(KEY_ID, decRequestWithInvalidCiphertext);
+    assertThrows(IOException.class, dec::execute);
+  }
+
+  @Test
+  public void testEncryptDecryptWithTwoValidKeyId_success() throws Exception {
+    CloudKMS kms = new FakeCloudKms(asList(KEY_ID, KEY_ID_2));
+
+    byte[] plaintext = "plaintext".getBytes(UTF_8);
+    byte[] plaintext2 = "plaintext2".getBytes(UTF_8);
+    byte[] associatedData = "associatedData".getBytes(UTF_8);
+
+    EncryptRequest encRequest =
+        new EncryptRequest()
+            .encodePlaintext(plaintext)
+            .encodeAdditionalAuthenticatedData(associatedData);
+    EncryptResponse encResponse =
+        kms.projects().locations().keyRings().cryptoKeys().encrypt(KEY_ID, encRequest).execute();
+
+    EncryptRequest encRequest2 =
+        new EncryptRequest()
+            .encodePlaintext(plaintext2)
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    EncryptResponse encResponse2 =
+        kms.projects().locations().keyRings().cryptoKeys().encrypt(KEY_ID, encRequest2).execute();
+
+    DecryptRequest decRequest =
+        new DecryptRequest()
+            .encodeCiphertext(encResponse.decodeCiphertext())
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    DecryptResponse decResponse =
+        kms.projects().locations().keyRings().cryptoKeys().decrypt(KEY_ID, decRequest).execute();
+
+    assertThat(decResponse.decodePlaintext()).isEqualTo(plaintext);
+
+    DecryptRequest decRequest2 =
+        new DecryptRequest()
+            .encodeCiphertext(encResponse2.decodeCiphertext())
+            .encodeAdditionalAuthenticatedData(associatedData);
+
+    DecryptResponse decResponse2 =
+        kms.projects().locations().keyRings().cryptoKeys().decrypt(KEY_ID, decRequest2).execute();
+
+    assertThat(decResponse2.decodePlaintext()).isEqualTo(plaintext2);
+  }
+}
