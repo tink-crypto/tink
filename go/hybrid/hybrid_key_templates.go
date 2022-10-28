2da1d5c6b58d37f2472dc5c13498a092116a2376
ckl <ckl@google.com>
2022-10-28 10:58:49 -0700

    Clean up godoc formatting.
    - Fix list indentation
    - Remove nested lists (not supported by godoc)
    - Make wording consistent between newer and older entries
    - Also, rename 'empty' variable to 'salt', to better describe what it is vs
      what it contains
    
    PiperOrigin-RevId: 484572678

--- go/hybrid/hybrid_key_templates.go
+++ go/hybrid/hybrid_key_templates.go
@@ -25,14 +25,15 @@ import (
 	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
 )
 
-// This file contains pre-generated KeyTemplates for HybridEncrypt keys. One can use these templates
-// to generate new Keysets.
+// This file contains pre-generated KeyTemplates for HybridEncrypt keys. One
+// can use these templates to generate new Keysets.
 
 // DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM_Key_Template creates a HPKE
-// key template with
-//  - KEM: DHKEM_X25519_HKDF_SHA256,
-//  - KDF: HKDF_SHA256, and
-//  - AEAD: AES_128_GCM.
+// key template with:
+//   - KEM: DHKEM_X25519_HKDF_SHA256,
+//   - KDF: HKDF_SHA256, and
+//   - AEAD: AES_128_GCM.
+//
 // It adds the 5-byte Tink prefix to ciphertexts.
 func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM_Key_Template() *tinkpb.KeyTemplate {
 	return createHPKEKeyTemplate(
@@ -44,10 +45,11 @@ func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM_Key_Template() *tinkpb.Key
 }
 
 // DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM_Raw_Key_Template creates a
-// HPKE key template with
-//  - KEM: DHKEM_X25519_HKDF_SHA256,
-//  - KDF: HKDF_SHA256, and
-//  - AEAD: AES_128_GCM.
+// HPKE key template with:
+//   - KEM: DHKEM_X25519_HKDF_SHA256,
+//   - KDF: HKDF_SHA256, and
+//   - AEAD: AES_128_GCM.
+//
 // It does not add a prefix to ciphertexts.
 func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM_Raw_Key_Template() *tinkpb.KeyTemplate {
 	return createHPKEKeyTemplate(
@@ -59,10 +61,11 @@ func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_128_GCM_Raw_Key_Template() *tinkpb
 }
 
 // DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_256_GCM_Key_Template creates a HPKE
-// key template with
-//  - KEM: DHKEM_X25519_HKDF_SHA256,
-//  - KDF: HKDF_SHA256, and
-//  - AEAD: AES_256_GCM.
+// key template with:
+//   - KEM: DHKEM_X25519_HKDF_SHA256,
+//   - KDF: HKDF_SHA256, and
+//   - AEAD: AES_256_GCM.
+//
 // It adds the 5-byte Tink prefix to ciphertexts.
 func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_256_GCM_Key_Template() *tinkpb.KeyTemplate {
 	return createHPKEKeyTemplate(
@@ -74,10 +77,11 @@ func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_256_GCM_Key_Template() *tinkpb.Key
 }
 
 // DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_256_GCM_Raw_Key_Template creates a
-// HPKE key template with
-//  - KEM: DHKEM_X25519_HKDF_SHA256,
-//  - KDF: HKDF_SHA256, and
-//  - AEAD: AES_256_GCM.
+// HPKE key template with:
+//   - KEM: DHKEM_X25519_HKDF_SHA256,
+//   - KDF: HKDF_SHA256, and
+//   - AEAD: AES_256_GCM.
+//
 // It does not add a prefix to ciphertexts.
 func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_256_GCM_Raw_Key_Template() *tinkpb.KeyTemplate {
 	return createHPKEKeyTemplate(
@@ -88,11 +92,12 @@ func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_AES_256_GCM_Raw_Key_Template() *tinkpb
 	)
 }
 
-// DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_CHACHA20_POLY1305_Key_Template creates a HPKE
-// key template with
-//  - KEM: DHKEM_X25519_HKDF_SHA256,
-//  - KDF: HKDF_SHA256, and
-//  - AEAD: CHACHA20_POLY1305.
+// DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_CHACHA20_POLY1305_Key_Template creates
+// a HPKE key template with:
+//   - KEM: DHKEM_X25519_HKDF_SHA256,
+//   - KDF: HKDF_SHA256, and
+//   - AEAD: CHACHA20_POLY1305.
+//
 // It adds the 5-byte Tink prefix to ciphertexts.
 func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_CHACHA20_POLY1305_Key_Template() *tinkpb.KeyTemplate {
 	return createHPKEKeyTemplate(
@@ -103,11 +108,12 @@ func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_CHACHA20_POLY1305_Key_Template() *tink
 	)
 }
 
-// DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_CHACHA20_POLY1305_Raw_Key_Template creates a
-// HPKE key template with
-//  - KEM: DHKEM_X25519_HKDF_SHA256,
-//  - KDF: HKDF_SHA256, and
-//  - AEAD: CHACHA20_POLY1305.
+// DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_CHACHA20_POLY1305_Raw_Key_Template creates
+// a HPKE key template with:
+//   - KEM: DHKEM_X25519_HKDF_SHA256,
+//   - KDF: HKDF_SHA256, and
+//   - AEAD: CHACHA20_POLY1305.
+//
 // It does not add a prefix to ciphertexts.
 func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_CHACHA20_POLY1305_Raw_Key_Template() *tinkpb.KeyTemplate {
 	return createHPKEKeyTemplate(
@@ -118,7 +124,8 @@ func DHKEM_X25519_HKDF_SHA256_HKDF_SHA256_CHACHA20_POLY1305_Raw_Key_Template() *
 	)
 }
 
-// createHPKEKeyTemplate creates a new HPKE key template with the given parameters.
+// createHPKEKeyTemplate creates a new HPKE key template with the given
+// parameters.
 func createHPKEKeyTemplate(kem hpkepb.HpkeKem, kdf hpkepb.HpkeKdf, aead hpkepb.HpkeAead, outputPrefixType tinkpb.OutputPrefixType) *tinkpb.KeyTemplate {
 	format := &hpkepb.HpkeKeyFormat{
 		Params: &hpkepb.HpkeParams{
@@ -135,32 +142,33 @@ func createHPKEKeyTemplate(kem hpkepb.HpkeKem, kdf hpkepb.HpkeKdf, aead hpkepb.H
 	}
 }
 
-// ECIESHKDFAES128GCMKeyTemplate is a KeyTemplate that generates an ECDH P-256 and decapsulation key
-// AES128-GCM key with the following parameters:
-//  - KEM: ECDH over NIST P-256
-//  - DEM: AES128-GCM
-//  - KDF: HKDF-HMAC-SHA256 with an empty salt
+// ECIESHKDFAES128GCMKeyTemplate creates an ECIES-AEAD-HKDF key template with:
+//   - KEM: ECDH over NIST P-256
+//   - DEM: AES128-GCM
+//   - KDF: HKDF-HMAC-SHA256 with an empty salt
 func ECIESHKDFAES128GCMKeyTemplate() *tinkpb.KeyTemplate {
-	empty := []byte{}
-	return createECIESAEADHKDFKeyTemplate(commonpb.EllipticCurveType_NIST_P256, commonpb.HashType_SHA256, commonpb.EcPointFormat_UNCOMPRESSED, aead.AES128GCMKeyTemplate(), empty)
+	salt := []byte{}
+	return createECIESAEADHKDFKeyTemplate(commonpb.EllipticCurveType_NIST_P256, commonpb.HashType_SHA256, commonpb.EcPointFormat_UNCOMPRESSED, aead.AES128GCMKeyTemplate(), salt)
 }
 
-// ECIESHKDFAES128CTRHMACSHA256KeyTemplate is a KeyTemplate that generates an ECDH P-256 and
-// decapsulation key AES128-CTR-HMAC-SHA256 with the following parameters:
-//  - KEM: ECDH over NIST P-256
-//  - DEM: AES128-CTR-HMAC-SHA256 with the following parameters
-//      - AES key size: 16 bytes
-//      - AES CTR IV size: 16 bytes
-//      - HMAC key size: 32 bytes
-//      - HMAC tag size: 16 bytes
-//  - KDF: HKDF-HMAC-SHA256 with an empty salt
+// ECIESHKDFAES128CTRHMACSHA256KeyTemplate creates an ECIES-AEAD-HKDF key
+// template with:
+//   - KEM: ECDH over NIST P-256
+//   - DEM: AES128-CTR-HMAC-SHA256
+//   - KDF: HKDF-HMAC-SHA256 with an empty salt
+//
+// The DEM parameters are:
+//   - AES key size: 16 bytes
+//   - AES CTR IV size: 16 bytes
+//   - HMAC key size: 32 bytes
+//   - HMAC tag size: 16 bytes
 func ECIESHKDFAES128CTRHMACSHA256KeyTemplate() *tinkpb.KeyTemplate {
-	empty := []byte{}
-	return createECIESAEADHKDFKeyTemplate(commonpb.EllipticCurveType_NIST_P256, commonpb.HashType_SHA256, commonpb.EcPointFormat_UNCOMPRESSED, aead.AES128CTRHMACSHA256KeyTemplate(), empty)
+	salt := []byte{}
+	return createECIESAEADHKDFKeyTemplate(commonpb.EllipticCurveType_NIST_P256, commonpb.HashType_SHA256, commonpb.EcPointFormat_UNCOMPRESSED, aead.AES128CTRHMACSHA256KeyTemplate(), salt)
 }
 
-// createEciesAEADHKDFKeyTemplate creates a new ECIES-AEAD-HKDF key template with the given key size
-// in bytes.
+// createEciesAEADHKDFKeyTemplate creates a new ECIES-AEAD-HKDF key template
+// with the given parameters.
 func createECIESAEADHKDFKeyTemplate(c commonpb.EllipticCurveType, ht commonpb.HashType, ptfmt commonpb.EcPointFormat, dekT *tinkpb.KeyTemplate, salt []byte) *tinkpb.KeyTemplate {
 	format := &eciespb.EciesAeadHkdfKeyFormat{
 		Params: &eciespb.EciesAeadHkdfParams{
