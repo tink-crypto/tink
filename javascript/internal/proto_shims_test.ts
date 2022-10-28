b085156837ebe558e407c7affc85f23d154ecc54
wconner <wconner@google.com>
2022-10-28 13:11:08 -0700

    Add unit test for proto shims.
    PiperOrigin-RevId: 484603856

---
+++ javascript/internal/proto_shims_test.ts
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as random from '../subtle/random';
+
+import {PbKeyData} from './proto';
+import {bytesAsU8, bytesLength} from './proto_shims';
+
+describe('proto shims test', () => {
+  it('transforms bytes to uint8array and returns byte length', () => {
+    const rawKey = random.randBytes(32);
+
+    const keyData =
+        new PbKeyData()
+            .setTypeUrl('key_type_url')
+            .setValue(rawKey)
+            .setKeyMaterialType(PbKeyData.KeyMaterialType.SYMMETRIC);
+
+    expect(bytesAsU8(keyData.getValue())).toEqual(rawKey);
+    expect(bytesLength(keyData.getValue())).toEqual(32);
+  });
+});
