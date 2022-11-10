75cc1276e2ac9ee78d1988d035c170fb12b279b6
Tink Team <tink-dev@google.com>
2022-11-09 16:28:14 -0800

    Adding Kdf interface for HPKE Typescript
    PiperOrigin-RevId: 487378125

---
+++ javascript/hybrid/internal/hpke/hpke_kdf.ts
@@ -0,0 +1,61 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Interface for Hybrid Public Key Encryption (HPKE) key derivation function
+ * (KDF).
+ *
+ * HPKE RFC is available at https://www.rfc-editor.org/rfc/rfc9180.html.
+ */
+
+export interface HpkeKdf {
+  /**
+   * Extracts pseudorandom key from `salt` and `ikm` using the
+   * HPKE-specific values `ikmLabel` and `suiteId` to facilitate domain
+   * separation and context binding.
+   *
+   * More details available at
+   * https://www.rfc-editor.org/rfc/rfc9180.html#section-4-9.
+   *
+   */
+  labeledExtract(
+      ikm: Uint8Array, ikmLabel: string, suiteId: Uint8Array,
+      salt?: Uint8Array): Promise<Uint8Array>;
+
+  /**
+   * Expands pseudorandom key `prk` into `length` pseudorandom bytes
+   * using `info` along with the HPKE-specific values `infoLabel`
+   * and `suiteId` to facilitate domain separation and context binding.
+   *
+   * More details available at
+   * https://www.rfc-editor.org/rfc/rfc9180.html#section-4-10.
+   *
+   */
+  labeledExpand(
+      prk: Uint8Array, info: Uint8Array, infoLabel: string, suiteId: Uint8Array,
+      length: number): Promise<Uint8Array>;
+
+  /**
+   * Combines `labeledExtract` and `labeledExpand` into a single method.
+   *
+   * More details available at
+   * https://www.rfc-editor.org/rfc/rfc9180.html#section-4.1-3.
+   *
+   */
+  extractAndExpand(
+      ikm: Uint8Array, ikmLabel: string, info: Uint8Array, infoLabel: string,
+      suiteId: Uint8Array, length: number,
+      salt?: Uint8Array): Promise<Uint8Array>;
+
+  /**
+   * Returns the HPKE KDF algorithm identifier for the underlying
+   * KDF implementation.
+   *
+   * More details at
+   * https://www.rfc-editor.org/rfc/rfc9180.html#name-key-derivation-functions-kd.
+   */
+  getKdfId(): Uint8Array;
+}
