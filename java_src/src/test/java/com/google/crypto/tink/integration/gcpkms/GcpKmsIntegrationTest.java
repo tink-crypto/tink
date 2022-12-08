dbee9605e7a0ed0c408f9bb9dca5b5691ca801a9
juerg <juerg@google.com>
2022-12-08 04:41:47 -0800

    Also test BigQuery data with CleartextKeysetHandle and BinaryKeysetReader.
    PiperOrigin-RevId: 493859828

--- java_src/src/test/java/com/google/crypto/tink/integration/gcpkms/GcpKmsIntegrationTest.java
+++ java_src/src/test/java/com/google/crypto/tink/integration/gcpkms/GcpKmsIntegrationTest.java
@@ -21,6 +21,8 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertThrows;
 
 import com.google.crypto.tink.Aead;
+import com.google.crypto.tink.BinaryKeysetReader;
+import com.google.crypto.tink.CleartextKeysetHandle;
 import com.google.crypto.tink.InsecureSecretKeyAccess;
 import com.google.crypto.tink.KeyTemplate;
 import com.google.crypto.tink.KeyTemplates;
@@ -65,8 +67,7 @@ public class GcpKmsIntegrationTest {
   @Test
   public void kmsAead_encryptDecrypt() throws Exception {
     KeysetHandle keysetHandle =
-        KeysetHandle.generateNew(
-            KmsAeadKeyManager.createKeyTemplate(GCP_KMS_TEST_KEY_URI));
+        KeysetHandle.generateNew(KmsAeadKeyManager.createKeyTemplate(GCP_KMS_TEST_KEY_URI));
 
     Aead aead = keysetHandle.getPrimitive(Aead.class);
 
@@ -88,8 +89,7 @@ public class GcpKmsIntegrationTest {
   @Test
   public void kmsAeadEncryptAndDecryptWithoutAssociatedData_success() throws Exception {
     KeysetHandle keysetHandle =
-        KeysetHandle.generateNew(
-            KmsAeadKeyManager.createKeyTemplate(GCP_KMS_TEST_KEY_URI));
+        KeysetHandle.generateNew(KmsAeadKeyManager.createKeyTemplate(GCP_KMS_TEST_KEY_URI));
     Aead aead = keysetHandle.getPrimitive(Aead.class);
 
     byte[] plaintext = "plaintext".getBytes(UTF_8);
@@ -107,13 +107,11 @@ public class GcpKmsIntegrationTest {
   @Test
   public void kmsAeadDecryptWithDifferentKeyUri_fails() throws Exception {
     KeysetHandle keysetHandle =
-        KeysetHandle.generateNew(
-            KmsAeadKeyManager.createKeyTemplate(GCP_KMS_TEST_KEY_URI));
+        KeysetHandle.generateNew(KmsAeadKeyManager.createKeyTemplate(GCP_KMS_TEST_KEY_URI));
     Aead aead = keysetHandle.getPrimitive(Aead.class);
 
     KeysetHandle keysetHandle2 =
-        KeysetHandle.generateNew(
-            KmsAeadKeyManager.createKeyTemplate(GCP_KMS_TEST_KEY_URI_2));
+        KeysetHandle.generateNew(KmsAeadKeyManager.createKeyTemplate(GCP_KMS_TEST_KEY_URI_2));
     Aead aead2 = keysetHandle2.getPrimitive(Aead.class);
 
     byte[] plaintext = "plaintext".getBytes(UTF_8);
@@ -160,13 +158,13 @@ public class GcpKmsIntegrationTest {
     KeysetHandle keysetHandle =
         KeysetHandle.generateNew(
             KmsEnvelopeAeadKeyManager.createKeyTemplate(
-            GCP_KMS_TEST_KEY_URI, KeyTemplates.get("AES128_CTR_HMAC_SHA256")));
+                GCP_KMS_TEST_KEY_URI, KeyTemplates.get("AES128_CTR_HMAC_SHA256")));
     Aead aead = keysetHandle.getPrimitive(Aead.class);
 
     KeysetHandle keysetHandle2 =
         KeysetHandle.generateNew(
             KmsEnvelopeAeadKeyManager.createKeyTemplate(
-            GCP_KMS_TEST_KEY_URI_2, KeyTemplates.get("AES128_CTR_HMAC_SHA256")));
+                GCP_KMS_TEST_KEY_URI_2, KeyTemplates.get("AES128_CTR_HMAC_SHA256")));
     Aead aead2 = keysetHandle2.getPrimitive(Aead.class);
 
     byte[] plaintext = "plaintext".getBytes(UTF_8);
@@ -225,10 +223,17 @@ public class GcpKmsIntegrationTest {
     byte[] decrypted = aead.decrypt(ciphertext, /* associatedData= */ "animal".getBytes(UTF_8));
     assertThat(decrypted).isEqualTo("elephant".getBytes(UTF_8));
 
-    // BigQuery's wrappedKeyset is not compatible with TinkProtoKeysetFormat.parseEncryptedKeyset
-    assertThrows(GeneralSecurityException.class,
-        () -> TinkProtoKeysetFormat.parseEncryptedKeyset(
-            wrappedKeyset, kmsAead, "".getBytes(UTF_8)));
-  }
+    // CleartextKeysetHandle and BinaryKeysetReader instead of TinkProtoKeysetFormat also works
+    KeysetHandle handle2 =
+        CleartextKeysetHandle.read(BinaryKeysetReader.withBytes(unwrappedKeyset));
+    Aead aead2 = handle2.getPrimitive(Aead.class);
+    byte[] decrypted2 = aead2.decrypt(ciphertext, /* associatedData= */ "animal".getBytes(UTF_8));
+    assertThat(decrypted2).isEqualTo("elephant".getBytes(UTF_8));
 
+    // BigQuery's wrappedKeyset is not compatible with TinkProtoKeysetFormat.parseEncryptedKeyset
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            TinkProtoKeysetFormat.parseEncryptedKeyset(wrappedKeyset, kmsAead, "".getBytes(UTF_8)));
+  }
 }
