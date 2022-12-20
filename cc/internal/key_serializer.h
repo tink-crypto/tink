a612a61ad26a0f3b30a789a8ba9224d42823a2f8
wconner <wconner@google.com>
2022-12-20 07:15:53 -0800

    Add key serializer.
    PiperOrigin-RevId: 496653972

---
+++ cc/internal/key_serializer.h
@@ -0,0 +1,71 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef TINK_INTERNAL_KEY_SERIALIZER_H_
+#define TINK_INTERNAL_KEY_SERIALIZER_H_
+
+#include <functional>
+#include <typeindex>
+
+#include "absl/functional/function_ref.h"
+#include "tink/secret_key_access_token.h"
+#include "tink/util/statusor.h"
+
+namespace crypto {
+namespace tink {
+namespace internal {
+
+// Non-template base class that can be used with internal registry map.
+class KeySerializerBase {
+ public:
+  // Returns an index that can be used to look up the `KeySerializer`
+  // object registered for the `KeyT` type in a registry.
+  virtual std::type_index TypeIndex() const = 0;
+
+  virtual ~KeySerializerBase() = default;
+};
+
+// Serializes `KeyT` objects into `SerializationT` objects.
+template <typename KeyT, typename SerializationT>
+class KeySerializer : public KeySerializerBase {
+ public:
+  // Creates a key serializer with serialization `function`. The referenced
+  // `function` should outlive the created key serializer object.
+  explicit KeySerializer(absl::FunctionRef<util::StatusOr<SerializationT>(
+                             KeyT, SecretKeyAccessToken)>
+                             function)
+      : function_(function) {}
+
+  // Returns the serialization of `key`.
+  util::StatusOr<SerializationT> SerializeKey(
+      KeyT key, SecretKeyAccessToken token) const {
+    return function_(key, token);
+  }
+
+  std::type_index TypeIndex() const override {
+    return std::type_index(typeid(KeyT));
+  }
+
+ private:
+  std::function<util::StatusOr<SerializationT>(KeyT, SecretKeyAccessToken)>
+      function_;
+};
+
+}  // namespace internal
+}  // namespace tink
+}  // namespace crypto
+
+#endif  // TINK_INTERNAL_KEY_SERIALIZER_H_
---
+++ cc/internal/key_serializer_test.cc
@@ -0,0 +1,83 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/internal/key_serializer.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "tink/insecure_secret_key_access.h"
+#include "tink/internal/serialization.h"
+#include "tink/key.h"
+#include "tink/parameters.h"
+#include "tink/secret_key_access_token.h"
+#include "tink/util/test_matchers.h"
+
+namespace crypto {
+namespace tink {
+namespace internal {
+
+using ::crypto::tink::test::IsOk;
+using ::testing::Eq;
+
+class ExampleParameters : public Parameters {
+ public:
+  bool HasIdRequirement() const override { return false; }
+
+  bool operator==(const Parameters& other) const override { return true; }
+};
+
+class ExampleKey : public Key {
+ public:
+  const Parameters& GetParameters() const override { return parameters_; }
+
+  absl::optional<int> GetIdRequirement() const override { return 123; }
+
+  bool operator==(const Key& other) const override { return true; }
+
+ private:
+  ExampleParameters parameters_;
+};
+
+class ExampleSerialization : public Serialization {
+ public:
+  absl::string_view ObjectIdentifier() const override {
+    return "example_type_url";
+  }
+};
+
+util::StatusOr<ExampleSerialization> Serialize(ExampleKey key,
+                                               SecretKeyAccessToken token) {
+  return ExampleSerialization();
+}
+
+TEST(KeyParserTest, Create) {
+  KeySerializer<ExampleKey, ExampleSerialization> serializer(Serialize);
+
+  EXPECT_THAT(serializer.TypeIndex(), Eq(std::type_index(typeid(ExampleKey))));
+}
+
+TEST(KeyParserTest, SerializeKey) {
+  KeySerializer<ExampleKey, ExampleSerialization> serializer(Serialize);
+
+  util::StatusOr<ExampleSerialization> serialization =
+      serializer.SerializeKey(ExampleKey(), InsecureSecretKeyAccess::Get());
+  ASSERT_THAT(serialization, IsOk());
+  EXPECT_THAT(serialization->ObjectIdentifier(), Eq("example_type_url"));
+}
+
+}  // namespace internal
+}  // namespace tink
+}  // namespace crypto
