e2372ba6f85739fdefb597e2ff823bdeef5f6b48
cinlin <cinlin@google.com>
2022-12-29 11:15:46 -0800

    Clean up XChaCha20Poly1305 Go key manager.
    PiperOrigin-RevId: 498419664

--- go/aead/xchacha20poly1305_key_manager.go
+++ go/aead/xchacha20poly1305_key_manager.go
@@ -26,7 +26,7 @@ import (
 	"github.com/google/tink/go/keyset"
 	"github.com/google/tink/go/subtle/random"
 
-	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
+	tpb "github.com/google/tink/go/proto/tink_go_proto"
 	xcppb "github.com/google/tink/go/proto/xchacha20_poly1305_go_proto"
 )
 
@@ -35,17 +35,17 @@ const (
 	xChaCha20Poly1305TypeURL    = "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key"
 )
 
-// Common errors.
 var errInvalidXChaCha20Poly1305Key = fmt.Errorf("xchacha20poly1305_key_manager: invalid key")
 
-// xChaCha20Poly1305KeyManager is an implementation of KeyManager interface.
-// It generates new XChaCha20Poly1305Key keys and produces new instances of XChaCha20Poly1305 subtle.
+// xChaCha20Poly1305KeyManager generates XChaCha20Poly1305Key keys and produces
+// instances of XChaCha20Poly1305.
 type xChaCha20Poly1305KeyManager struct{}
 
 // Assert that xChaCha20Poly1305KeyManager implements the KeyManager interface.
 var _ registry.KeyManager = (*xChaCha20Poly1305KeyManager)(nil)
 
-// Primitive creates an XChaCha20Poly1305 subtle for the given serialized XChaCha20Poly1305Key proto.
+// Primitive constructs a XChaCha20Poly1305 for the given serialized
+// XChaCha20Poly1305Key.
 func (km *xChaCha20Poly1305KeyManager) Primitive(serializedKey []byte) (interface{}, error) {
 	if len(serializedKey) == 0 {
 		return nil, errInvalidXChaCha20Poly1305Key
@@ -59,60 +59,58 @@ func (km *xChaCha20Poly1305KeyManager) Primitive(serializedKey []byte) (interfac
 	}
 	ret, err := subtle.NewXChaCha20Poly1305(key.KeyValue)
 	if err != nil {
-		return nil, fmt.Errorf("xchacha20poly1305_key_manager: cannot create new primitive: %s", err)
+		return nil, fmt.Errorf("xchacha20poly1305_key_manager: cannot create new primitive: %v", err)
 	}
 	return ret, nil
 }
 
-// NewKey creates a new key, ignoring the specification in the given serialized key format
+// NewKey generates a new XChaCha20Poly1305Key. It ignores serializedKeyFormat
 // because the key size and other params are fixed.
 func (km *xChaCha20Poly1305KeyManager) NewKey(serializedKeyFormat []byte) (proto.Message, error) {
-	return km.newXChaCha20Poly1305Key(), nil
+	return &xcppb.XChaCha20Poly1305Key{
+		Version:  xChaCha20Poly1305KeyVersion,
+		KeyValue: random.GetRandomBytes(chacha20poly1305.KeySize),
+	}, nil
 }
 
-// NewKeyData creates a new KeyData, ignoring the specification in the given serialized key format
-// because the key size and other params are fixed.
-// It should be used solely by the key management API.
-func (km *xChaCha20Poly1305KeyManager) NewKeyData(serializedKeyFormat []byte) (*tinkpb.KeyData, error) {
-	key := km.newXChaCha20Poly1305Key()
+// NewKeyData generates a new KeyData. It ignores serializedKeyFormat because
+// the key size and other params are fixed. This should be used solely by the
+// key management API.
+func (km *xChaCha20Poly1305KeyManager) NewKeyData(serializedKeyFormat []byte) (*tpb.KeyData, error) {
+	key := &xcppb.XChaCha20Poly1305Key{
+		Version:  xChaCha20Poly1305KeyVersion,
+		KeyValue: random.GetRandomBytes(chacha20poly1305.KeySize),
+	}
 	serializedKey, err := proto.Marshal(key)
 	if err != nil {
 		return nil, err
 	}
-	return &tinkpb.KeyData{
+	return &tpb.KeyData{
 		TypeUrl:         xChaCha20Poly1305TypeURL,
 		Value:           serializedKey,
-		KeyMaterialType: tinkpb.KeyData_SYMMETRIC,
+		KeyMaterialType: tpb.KeyData_SYMMETRIC,
 	}, nil
 }
 
-// DoesSupport indicates if this key manager supports the given key type.
+// DoesSupport checks whether this key manager supports the given key type.
 func (km *xChaCha20Poly1305KeyManager) DoesSupport(typeURL string) bool {
 	return typeURL == xChaCha20Poly1305TypeURL
 }
 
-// TypeURL returns the key type of keys managed by this key manager.
+// TypeURL returns the type URL of keys managed by this key manager.
 func (km *xChaCha20Poly1305KeyManager) TypeURL() string {
 	return xChaCha20Poly1305TypeURL
-}
-
-func (km *xChaCha20Poly1305KeyManager) newXChaCha20Poly1305Key() *xcppb.XChaCha20Poly1305Key {
-	keyValue := random.GetRandomBytes(chacha20poly1305.KeySize)
-	return &xcppb.XChaCha20Poly1305Key{
-		Version:  xChaCha20Poly1305KeyVersion,
-		KeyValue: keyValue,
-	}
 }
 
 // validateKey validates the given XChaCha20Poly1305Key.
 func (km *xChaCha20Poly1305KeyManager) validateKey(key *xcppb.XChaCha20Poly1305Key) error {
 	err := keyset.ValidateKeyVersion(key.Version, xChaCha20Poly1305KeyVersion)
 	if err != nil {
-		return fmt.Errorf("xchacha20poly1305_key_manager: %s", err)
+		return fmt.Errorf("xchacha20poly1305_key_manager: %v", err)
 	}
 	keySize := uint32(len(key.KeyValue))
 	if keySize != chacha20poly1305.KeySize {
-		return fmt.Errorf("xchacha20poly1305_key_manager: keySize != %d", chacha20poly1305.KeySize)
+		return fmt.Errorf("xchacha20poly1305_key_manager: key size != %d", chacha20poly1305.KeySize)
 	}
 	return nil
 }
