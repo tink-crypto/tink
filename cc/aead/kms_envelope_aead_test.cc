a914f347f2c87d5b7957cbbec6e399cba300442f
ambrosin <ambrosin@google.com>
2023-01-09 06:46:39 -0800

    Some fixes to the KMS envelope AEAD test
     - Use a test fixture to make sure Registry is reset before executing each test case
     - Use more descriptive names for test cases
     - Use ASSERT_* for checking invariants that are critical to the execution of the test
     - Make `auto` explicit when not obvious
     - Add test case that verifies a new key is generated each time encryption is performed
     - Other minor fixes
    
    PiperOrigin-RevId: 500700407

--- cc/aead/kms_envelope_aead_test.cc
+++ cc/aead/kms_envelope_aead_test.cc
@@ -21,15 +21,18 @@
 #include <memory>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "absl/base/internal/endian.h"
 #include "absl/memory/memory.h"
 #include "absl/status/status.h"
+#include "absl/strings/string_view.h"
 #include "tink/aead.h"
 #include "tink/aead/aead_config.h"
 #include "tink/aead/aead_key_templates.h"
+#include "tink/keyset_handle.h"
 #include "tink/mac/mac_key_templates.h"
 #include "tink/registry.h"
 #include "tink/util/status.h"
@@ -42,140 +45,194 @@ namespace crypto {
 namespace tink {
 namespace {
 
-using crypto::tink::test::DummyAead;
-using crypto::tink::test::IsOk;
-using crypto::tink::test::StatusIs;
-using testing::HasSubstr;
+using ::crypto::tink::Aead;
+using ::crypto::tink::test::DummyAead;
+using ::crypto::tink::test::IsOk;
+using ::crypto::tink::test::IsOkAndHolds;
+using ::crypto::tink::test::StatusIs;
+using ::google::crypto::tink::KeyTemplate;
+using ::testing::Eq;
+using ::testing::HasSubstr;
+using ::testing::Not;
+using ::testing::SizeIs;
+using ::testing::Test;
 
+constexpr int kEncryptedDekPrefixSize = 4;
+constexpr absl::string_view kRemoteAeadName = "kms-backed-aead";
 
-TEST(KmsEnvelopeAeadTest, BasicEncryptDecrypt) {
-  EXPECT_THAT(AeadConfig::Register(), IsOk());
+class KmsEnvelopeAeadTest : public Test {
+ protected:
+  void TearDown() override { Registry::Reset(); }
+};
 
-  auto dek_template = AeadKeyTemplates::Aes128Eax();
-  std::string remote_aead_name = "kms-backed-aead";
-  auto remote_aead = absl::make_unique<DummyAead>(remote_aead_name);
+TEST_F(KmsEnvelopeAeadTest, EncryptDecryptSucceed) {
+  ASSERT_THAT(AeadConfig::Register(), IsOk());
 
-  auto aead_result = KmsEnvelopeAead::New(dek_template, std::move(remote_aead));
-  EXPECT_THAT(aead_result, IsOk());
-  auto aead = std::move(aead_result.value());
+  // Use an AES-128-GCM primitive as the remote one.
+  util::StatusOr<std::unique_ptr<KeysetHandle>> keyset_handle =
+      KeysetHandle::GenerateNew(AeadKeyTemplates::Aes128Gcm());
+  ASSERT_THAT(keyset_handle, IsOk());
+  KeyTemplate dek_template = AeadKeyTemplates::Aes128Eax();
+  util::StatusOr<std::unique_ptr<Aead>> remote_aead =
+      (*keyset_handle)->GetPrimitive<Aead>();
+
+  util::StatusOr<std::unique_ptr<Aead>> envelope_aead =
+      KmsEnvelopeAead::New(dek_template, *std::move(remote_aead));
+  ASSERT_THAT(envelope_aead, IsOk());
+
   std::string message = "Some data to encrypt.";
-  std::string aad = "Some data to authenticate.";
-  auto encrypt_result = aead->Encrypt(message, aad);
-  EXPECT_THAT(encrypt_result, IsOk());
-  auto decrypt_result = aead->Decrypt(encrypt_result.value(), aad);
-  EXPECT_THAT(decrypt_result, IsOk());
-  EXPECT_EQ(decrypt_result.value(), message);
+  std::string aad = "Some associated data.";
+  util::StatusOr<std::string> encrypt_result =
+      (*envelope_aead)->Encrypt(message, aad);
+  ASSERT_THAT(encrypt_result, IsOk());
+  util::StatusOr<std::string> decrypt_result =
+      (*envelope_aead)->Decrypt(encrypt_result.value(), aad);
+  EXPECT_THAT(decrypt_result, IsOkAndHolds(message));
 }
 
-TEST(KmsEnvelopeAeadTest, NullAead) {
-  auto dek_template = AeadKeyTemplates::Aes128Eax();
-  auto aead_result = KmsEnvelopeAead::New(dek_template, nullptr);
-  EXPECT_THAT(aead_result.status(), StatusIs(absl::StatusCode::kInvalidArgument,
-                                             HasSubstr("non-null")));
-}
-
-TEST(KmsEnvelopeAeadTest, MissingDekKeyManager) {
-  Registry::Reset();
-  auto dek_template = AeadKeyTemplates::Aes128Eax();
-  std::string remote_aead_name = "kms-backed-aead";
-  auto remote_aead = absl::make_unique<DummyAead>(remote_aead_name);
-  auto aead_result = KmsEnvelopeAead::New(dek_template, std::move(remote_aead));
-  EXPECT_THAT(aead_result.status(),
-              StatusIs(absl::StatusCode::kNotFound, HasSubstr("AesEaxKey")));
-}
-
-TEST(KmsEnvelopeAeadTest, WrongDekPrimitive) {
-  EXPECT_THAT(AeadConfig::Register(), IsOk());
-  auto dek_template = MacKeyTemplates::HmacSha256();
-  std::string remote_aead_name = "kms-backed-aead";
-  auto remote_aead = absl::make_unique<DummyAead>(remote_aead_name);
-  auto aead_result = KmsEnvelopeAead::New(dek_template, std::move(remote_aead));
-  EXPECT_THAT(aead_result.status(),
-              StatusIs(absl::StatusCode::kInvalidArgument,
-                       HasSubstr("not among supported primitives")));
-}
-
-TEST(KmsEnvelopeAeadTest, DecryptionErrors) {
-  EXPECT_THAT(AeadConfig::Register(), IsOk());
-
-  auto dek_template = AeadKeyTemplates::Aes128Gcm();
-  std::string remote_aead_name = "kms-backed-aead";
-  auto remote_aead = absl::make_unique<DummyAead>(remote_aead_name);
-
-  auto aead_result = KmsEnvelopeAead::New(dek_template, std::move(remote_aead));
-  EXPECT_THAT(aead_result, IsOk());
-  auto aead = std::move(aead_result.value());
-  std::string message = "Some data to encrypt.";
-  std::string aad = "Some data to authenticate.";
-  auto encrypt_result = aead->Encrypt(message, aad);
-  EXPECT_THAT(encrypt_result, IsOk());
-  auto ct = encrypt_result.value();
-
-  // Empty ciphertext.
-  auto decrypt_result = aead->Decrypt("", aad);
+TEST_F(KmsEnvelopeAeadTest, NewFailsIfReamoteAeadIsNull) {
+  KeyTemplate dek_template = AeadKeyTemplates::Aes128Eax();
   EXPECT_THAT(
-      decrypt_result.status(),
+      KmsEnvelopeAead::New(dek_template, /*remote_aead=*/nullptr).status(),
+      StatusIs(absl::StatusCode::kInvalidArgument, HasSubstr("non-null")));
+}
+
+TEST_F(KmsEnvelopeAeadTest, NewFailsIfDekKeyManagerIsNotRegistered) {
+  KeyTemplate dek_template = AeadKeyTemplates::Aes128Eax();
+  auto remote_aead = absl::make_unique<DummyAead>(kRemoteAeadName);
+  EXPECT_THAT(
+      KmsEnvelopeAead::New(dek_template, std::move(remote_aead)).status(),
+      StatusIs(absl::StatusCode::kNotFound, HasSubstr("AesEaxKey")));
+}
+
+TEST_F(KmsEnvelopeAeadTest, NewFailsIfUsingWrongDekPrimitive) {
+  ASSERT_THAT(AeadConfig::Register(), IsOk());
+  KeyTemplate dek_template = MacKeyTemplates::HmacSha256();
+  auto remote_aead = absl::make_unique<DummyAead>(kRemoteAeadName);
+  EXPECT_THAT(
+      KmsEnvelopeAead::New(dek_template, std::move(remote_aead)).status(),
+      StatusIs(absl::StatusCode::kInvalidArgument,
+               HasSubstr("not among supported primitives")));
+}
+
+TEST_F(KmsEnvelopeAeadTest, DecryptFailsWithInvalidCiphertextOrAad) {
+  ASSERT_THAT(AeadConfig::Register(), IsOk());
+
+  KeyTemplate dek_template = AeadKeyTemplates::Aes128Gcm();
+  auto remote_aead = absl::make_unique<DummyAead>(kRemoteAeadName);
+  util::StatusOr<std::unique_ptr<Aead>> aead =
+      KmsEnvelopeAead::New(dek_template, std::move(remote_aead));
+  ASSERT_THAT(aead, IsOk());
+
+  std::string message = "Some data to encrypt.";
+  std::string aad = "Some associated data.";
+  util::StatusOr<std::string> encrypt_result = (*aead)->Encrypt(message, aad);
+  ASSERT_THAT(encrypt_result, IsOk());
+  auto ciphertext = absl::string_view(*encrypt_result);
+
+  // Ciphertext has size zero or smaller than 4 bytes.
+  EXPECT_THAT(
+      (*aead)->Decrypt(/*ciphertext=*/"", aad).status(),
+      StatusIs(absl::StatusCode::kInvalidArgument, HasSubstr("too short")));
+  EXPECT_THAT(
+      (*aead)->Decrypt(/*ciphertext=*/"sh", aad).status(),
       StatusIs(absl::StatusCode::kInvalidArgument, HasSubstr("too short")));
 
-  // Short ciphertext.
-  decrypt_result = aead->Decrypt("sh", aad);
+  // Ciphertext is smaller than the size of the key.
+  const int dek_encrypted_key_size = absl::big_endian::Load32(
+      reinterpret_cast<const uint8_t*>(ciphertext.data()));
+  // We leave only key size and key truncated by one.
   EXPECT_THAT(
-      decrypt_result.status(),
-      StatusIs(absl::StatusCode::kInvalidArgument, HasSubstr("too short")));
-
-  // Truncated ciphertext.
-  decrypt_result = aead->Decrypt(ct.substr(2), aad);
-  EXPECT_THAT(
-      decrypt_result.status(),
+      (*aead)
+          ->Decrypt(ciphertext.substr(0, 4 + dek_encrypted_key_size - 1), aad)
+          .status(),
       StatusIs(absl::StatusCode::kInvalidArgument, HasSubstr("invalid")));
 
-  // Corrupted ciphertext.
-  auto ct_copy = ct;
-  ct_copy[4] = 'a';  // corrupt serialized DEK.
-  decrypt_result = aead->Decrypt(ct_copy, aad);
+  std::string corrupted_ciphertext = *encrypt_result;
+  // Corrupt the serialized DEK.
+  corrupted_ciphertext[4] = 'a';
   EXPECT_THAT(
-      decrypt_result.status(),
+      (*aead)->Decrypt(corrupted_ciphertext, aad).status(),
       StatusIs(absl::StatusCode::kInvalidArgument, HasSubstr("invalid")));
 
   // Wrong associated data.
-  decrypt_result = aead->Decrypt(ct, "wrong aad");
-  EXPECT_THAT(decrypt_result.status(),
+  EXPECT_THAT((*aead)->Decrypt(ciphertext, "wrong aad").status(),
               StatusIs(absl::StatusCode::kInternal,
                        HasSubstr("Authentication failed")));
 }
 
-TEST(KmsEnvelopeAeadTest, KeyFormat) {
-  EXPECT_THAT(AeadConfig::Register(), IsOk());
+TEST_F(KmsEnvelopeAeadTest, DekMaintainsCorrectKeyFormat) {
+  ASSERT_THAT(AeadConfig::Register(), IsOk());
 
-  auto dek_template = AeadKeyTemplates::Aes128Gcm();
+  KeyTemplate dek_template = AeadKeyTemplates::Aes128Gcm();
+  auto kms_remote_aead = absl::make_unique<DummyAead>(kRemoteAeadName);
+  util::StatusOr<std::unique_ptr<Aead>> aead =
+      KmsEnvelopeAead::New(dek_template, std::move(kms_remote_aead));
+  ASSERT_THAT(aead, IsOk());
 
-  // Construct a remote AEAD which uses same key template for this test.
-  std::string remote_aead_name = "kms-backed-aead";
-  auto remote_aead = absl::make_unique<DummyAead>(remote_aead_name);
-
-  // Create envelope AEAD and encrypt some data.
-  auto aead_result = KmsEnvelopeAead::New(dek_template, std::move(remote_aead));
-  EXPECT_THAT(aead_result, IsOk());
-
-  auto aead = std::move(aead_result.value());
   std::string message = "Some data to encrypt.";
-  std::string aad = "Some data to authenticate.";
-  auto encrypt_result = aead->Encrypt(message, aad);
-  EXPECT_THAT(encrypt_result, IsOk());
-  auto ct = encrypt_result.value();
+  std::string aad = "Some associated data.";
+  util::StatusOr<std::string> ciphertext = (*aead)->Encrypt(message, aad);
+  ASSERT_THAT(ciphertext, IsOk());
 
-  // Recover DEK from ciphertext
-  auto enc_dek_size =
-      absl::big_endian::Load32(reinterpret_cast<const uint8_t*>(ct.data()));
-
-  remote_aead = absl::make_unique<DummyAead>(remote_aead_name);
-  auto dek_decrypt_result =
-      remote_aead->Decrypt(ct.substr(4, enc_dek_size), "");
+  // Recover DEK from ciphertext (see
+  // https://developers.google.com/tink/wire-format#envelope_encryption).
+  auto enc_dek_size = absl::big_endian::Load32(
+      reinterpret_cast<const uint8_t*>(ciphertext->data()));
+  DummyAead remote_aead = DummyAead(kRemoteAeadName);
+  absl::string_view encrypted_dek =
+      absl::string_view(*ciphertext)
+          .substr(kEncryptedDekPrefixSize, enc_dek_size);
+  util::StatusOr<std::string> dek_proto_bytes =
+      remote_aead.Decrypt(encrypted_dek,
+                          /*associated_data=*/"");
+  ASSERT_THAT(dek_proto_bytes, IsOk());
 
   // Check if we can deserialize a GCM key proto from the decrypted DEK.
   google::crypto::tink::AesGcmKey key;
-  EXPECT_THAT(key.ParseFromString(dek_decrypt_result.value()), true);
-  EXPECT_THAT(key.key_value().size(), testing::Eq(16));
+  EXPECT_TRUE(key.ParseFromString(dek_proto_bytes.value()));
+  EXPECT_THAT(key.key_value(), SizeIs(16));
+}
+
+TEST_F(KmsEnvelopeAeadTest, MultipleEncryptionsProduceDifferentDeks) {
+  ASSERT_THAT(AeadConfig::Register(), IsOk());
+
+  KeyTemplate dek_template = AeadKeyTemplates::Aes128Gcm();
+  auto kms_remote_aead = absl::make_unique<DummyAead>(kRemoteAeadName);
+  util::StatusOr<std::unique_ptr<Aead>> aead =
+      KmsEnvelopeAead::New(dek_template, std::move(kms_remote_aead));
+  ASSERT_THAT(aead, IsOk());
+
+  std::string message = "Some data to encrypt.";
+  std::string aad = "Some associated data.";
+
+  constexpr int kNumIterations = 2;
+  std::vector<google::crypto::tink::AesGcmKey> ciphertexts;
+  ciphertexts.reserve(kNumIterations);
+  for (int i = 0; i < kNumIterations; i++) {
+    util::StatusOr<std::string> ciphertext = (*aead)->Encrypt(message, aad);
+    ASSERT_THAT(ciphertext, IsOk());
+
+    auto enc_dek_size = absl::big_endian::Load32(
+        reinterpret_cast<const uint8_t*>(ciphertext->data()));
+    DummyAead remote_aead = DummyAead(kRemoteAeadName);
+    util::StatusOr<std::string> dek_proto_bytes = remote_aead.Decrypt(
+        ciphertext->substr(kEncryptedDekPrefixSize, enc_dek_size),
+        /*associated_data=*/"");
+    ASSERT_THAT(dek_proto_bytes, IsOk());
+
+    google::crypto::tink::AesGcmKey key;
+    ASSERT_TRUE(key.ParseFromString(dek_proto_bytes.value()));
+    ASSERT_THAT(key.key_value(), SizeIs(16));
+    ciphertexts.push_back(key);
+  }
+
+  for (int i = 0; i < ciphertexts.size() - 1; i++) {
+    for (int j = i + 1; j < ciphertexts.size(); j++) {
+      EXPECT_THAT(ciphertexts[i].SerializeAsString(),
+                  Not(Eq(ciphertexts[j].SerializeAsString())));
+    }
+  }
 }
 
 }  // namespace
