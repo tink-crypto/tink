e8ed1d422aea8d921e2826216725733233b3d95b
tholenst <tholenst@google.com>
2023-02-07 05:10:20 -0800

    Add classes for ChaCha20Poly1305Keys.
    PiperOrigin-RevId: 507747642

---
+++ java_src/src/main/java/com/google/crypto/tink/aead/ChaCha20Poly1305Parameters.java
@@ -0,0 +1,90 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.aead;
+
+import com.google.errorprone.annotations.Immutable;
+import java.util.Objects;
+
+/** Describes the parameters of an {@link ChaChaPoly1305Key}. */
+public final class ChaCha20Poly1305Parameters extends AeadParameters {
+  /**
+   * Describes how the prefix is computed. For AEAD there are three main possibilities: NO_PREFIX
+   * (empty prefix), TINK (prefix the ciphertext with 0x01 followed by a 4-byte key id in big endian
+   * format) or CRUNCHY (prefix the ciphertext with 0x00 followed by a 4-byte key id in big endian
+   * format)
+   */
+  @Immutable
+  public static final class Variant {
+    public static final Variant TINK = new Variant("TINK");
+    public static final Variant CRUNCHY = new Variant("CRUNCHY");
+    public static final Variant NO_PREFIX = new Variant("NO_PREFIX");
+
+    private final String name;
+
+    private Variant(String name) {
+      this.name = name;
+    }
+
+    @Override
+    public String toString() {
+      return name;
+    }
+  }
+
+  public static ChaCha20Poly1305Parameters create() {
+    return new ChaCha20Poly1305Parameters(Variant.NO_PREFIX);
+  }
+
+  public static ChaCha20Poly1305Parameters create(Variant variant) {
+    return new ChaCha20Poly1305Parameters(variant);
+  }
+
+  private final Variant variant;
+
+  private ChaCha20Poly1305Parameters(Variant variant) {
+    this.variant = variant;
+  }
+
+  /** Returns a variant object. */
+  public Variant getVariant() {
+    return variant;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof ChaCha20Poly1305Parameters)) {
+      return false;
+    }
+    ChaCha20Poly1305Parameters that = (ChaCha20Poly1305Parameters) o;
+    return that.getVariant() == getVariant();
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(variant);
+  }
+
+  @Override
+  public boolean hasIdRequirement() {
+    return variant != Variant.NO_PREFIX;
+  }
+
+  @Override
+  public String toString() {
+    return "ChaCha20Poly1305 Parameters (variant: " + variant + ")";
+  }
+}
---
+++ java_src/src/test/java/com/google/crypto/tink/aead/ChaCha20Poly1305ParametersTest.java
@@ -0,0 +1,122 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.aead;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class ChaCha20Poly1305ParametersTest {
+  private static final ChaCha20Poly1305Parameters.Variant NO_PREFIX =
+      ChaCha20Poly1305Parameters.Variant.NO_PREFIX;
+  private static final ChaCha20Poly1305Parameters.Variant TINK =
+      ChaCha20Poly1305Parameters.Variant.TINK;
+  private static final ChaCha20Poly1305Parameters.Variant CRUNCHY =
+      ChaCha20Poly1305Parameters.Variant.CRUNCHY;
+
+  @Test
+  public void buildParametersAndGetProperties() throws Exception {
+    ChaCha20Poly1305Parameters parameters = ChaCha20Poly1305Parameters.create();
+    assertThat(parameters.getVariant()).isEqualTo(NO_PREFIX);
+    assertThat(parameters.hasIdRequirement()).isFalse();
+  }
+
+  @Test
+  public void buildParameters_setVariantExplicitly() throws Exception {
+    ChaCha20Poly1305Parameters parameters =
+        ChaCha20Poly1305Parameters.create(ChaCha20Poly1305Parameters.Variant.NO_PREFIX);
+    assertThat(parameters.getVariant()).isEqualTo(NO_PREFIX);
+    assertThat(parameters.hasIdRequirement()).isFalse();
+  }
+
+  @Test
+  public void buildParameters_tink() throws Exception {
+    ChaCha20Poly1305Parameters parameters =
+        ChaCha20Poly1305Parameters.create(ChaCha20Poly1305Parameters.Variant.TINK);
+    assertThat(parameters.getVariant()).isEqualTo(TINK);
+    assertThat(parameters.hasIdRequirement()).isTrue();
+  }
+
+  @Test
+  public void buildParameters_crunchy() throws Exception {
+    ChaCha20Poly1305Parameters parameters =
+        ChaCha20Poly1305Parameters.create(ChaCha20Poly1305Parameters.Variant.CRUNCHY);
+    assertThat(parameters.getVariant()).isEqualTo(CRUNCHY);
+    assertThat(parameters.hasIdRequirement()).isTrue();
+  }
+
+  @Test
+  public void testEqualsAndEqualHashCode_noPrefix() throws Exception {
+    ChaCha20Poly1305Parameters parametersNoPrefix0 = ChaCha20Poly1305Parameters.create();
+    ChaCha20Poly1305Parameters parametersNoPrefix1 =
+        ChaCha20Poly1305Parameters.create(ChaCha20Poly1305Parameters.Variant.NO_PREFIX);
+    assertThat(parametersNoPrefix0).isEqualTo(parametersNoPrefix1);
+    assertThat(parametersNoPrefix0.hashCode()).isEqualTo(parametersNoPrefix1.hashCode());
+  }
+
+  @Test
+  public void testEqualsAndEqualHashCode_tink() throws Exception {
+    ChaCha20Poly1305Parameters parametersTink0 =
+        ChaCha20Poly1305Parameters.create(ChaCha20Poly1305Parameters.Variant.TINK);
+    ChaCha20Poly1305Parameters parametersTink1 =
+        ChaCha20Poly1305Parameters.create(ChaCha20Poly1305Parameters.Variant.TINK);
+    assertThat(parametersTink0).isEqualTo(parametersTink1);
+    assertThat(parametersTink0.hashCode()).isEqualTo(parametersTink1.hashCode());
+  }
+
+  @Test
+  public void testEqualsAndEqualHashCode_crunchy() throws Exception {
+    ChaCha20Poly1305Parameters parametersCrunchy0 =
+        ChaCha20Poly1305Parameters.create(ChaCha20Poly1305Parameters.Variant.CRUNCHY);
+    ChaCha20Poly1305Parameters parametersCrunchy1 =
+        ChaCha20Poly1305Parameters.create(ChaCha20Poly1305Parameters.Variant.CRUNCHY);
+    assertThat(parametersCrunchy0).isEqualTo(parametersCrunchy1);
+    assertThat(parametersCrunchy0.hashCode()).isEqualTo(parametersCrunchy1.hashCode());
+  }
+
+  @Test
+  public void testEqualsAndEqualHashCode_different() throws Exception {
+    ChaCha20Poly1305Parameters parametersNoPrefix = ChaCha20Poly1305Parameters.create();
+
+    ChaCha20Poly1305Parameters parametersTink =
+        ChaCha20Poly1305Parameters.create(ChaCha20Poly1305Parameters.Variant.TINK);
+
+    ChaCha20Poly1305Parameters parametersCrunchy =
+        ChaCha20Poly1305Parameters.create(ChaCha20Poly1305Parameters.Variant.CRUNCHY);
+
+    assertThat(parametersNoPrefix).isNotEqualTo(parametersTink);
+    assertThat(parametersNoPrefix.hashCode()).isNotEqualTo(parametersTink.hashCode());
+
+    assertThat(parametersNoPrefix).isNotEqualTo(parametersCrunchy);
+    assertThat(parametersNoPrefix.hashCode()).isNotEqualTo(parametersCrunchy.hashCode());
+
+    assertThat(parametersTink).isNotEqualTo(parametersNoPrefix);
+    assertThat(parametersTink.hashCode()).isNotEqualTo(parametersNoPrefix.hashCode());
+
+    assertThat(parametersTink).isNotEqualTo(parametersCrunchy);
+    assertThat(parametersTink.hashCode()).isNotEqualTo(parametersCrunchy.hashCode());
+
+    assertThat(parametersCrunchy).isNotEqualTo(parametersNoPrefix);
+    assertThat(parametersCrunchy.hashCode()).isNotEqualTo(parametersNoPrefix.hashCode());
+
+    assertThat(parametersCrunchy).isNotEqualTo(parametersTink);
+    assertThat(parametersCrunchy.hashCode()).isNotEqualTo(parametersTink.hashCode());
+  }
+}
