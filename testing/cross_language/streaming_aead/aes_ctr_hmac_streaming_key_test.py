e3ef7464cb065835edb5c3b52fd432401d16ef82
tholenst <tholenst@google.com>
2023-02-27 04:00:33 -0800

    Add a manually created test with all steps for AesCtrHmacStreaming.
    PiperOrigin-RevId: 512585731

--- testing/cross_language/streaming_aead/aes_ctr_hmac_streaming_key_test.py
+++ testing/cross_language/streaming_aead/aes_ctr_hmac_streaming_key_test.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import binascii
 import io
 
 from absl.testing import absltest
@@ -326,6 +327,100 @@ class AesCtrHmacStreamingKeyTest(parameterized.TestCase):
           lang, keyset.SerializeToString(), streaming_aead.StreamingAead
       )
 
+  @parameterized.parameters(
+      tink_config.supported_languages_for_key_type('AesCtrHmacStreamingKey')
+  )
+  def test_manually_created_test_vector(self, lang: str):
+    """Tests using a ciphertext created by looking at the documentation.
+
+    See https://developers.google.com/tink/streaming-aead/aes_ctr_hmac_streaming
+    for the documentation. The goal is to ensure that the documentation is
+    clear; we expect readers to read this with the documentation.
+
+    Args:
+      lang: The language to test.
+    """
+
+    def xor(b1: bytes, b2: bytes) -> bytes:
+      return bytes(i ^ j for (i, j) in zip(b1, b2))
+
+    h2b = binascii.a2b_hex
+
+    key = aes_ctr_hmac_streaming_pb2.AesCtrHmacStreamingKey(
+        version=0,
+        params=aes_ctr_hmac_streaming_pb2.AesCtrHmacStreamingParams(
+            ciphertext_segment_size=64,
+            derived_key_size=16,
+            hkdf_hash_type=common_pb2.HashType.SHA1,
+            hmac_params=hmac_pb2.HmacParams(
+                hash=common_pb2.HashType.SHA256, tag_size=32
+            ),
+        ),
+        key_value=h2b('6eb56cdc726dfbe5d57f2fcdc6e9345b')
+    )
+    # We set the message to be:
+    msg = b'This is a fairly long plaintext. However, it is not crazy long.'
+    #
+    # We set the associated data to be:
+    aad = b'aad'
+
+    # We picked the header at random: Note the length is 24 = 0x18.
+    header_length = h2b('18')
+    salt = h2b('93b3af5e14ab378d065addfc8484da64')
+    nonce_prefix = h2b('2c0862877baea8')
+    header = header_length + salt + nonce_prefix
+    # hkdf.hkdf_sha1(ikm=key_value, salt=header_salt, info=aad, size=48) gives
+    # '66dd511791296a6cfc94a24041fcab9f' +
+    # '0f736d6e85c448c2c8cc30f094d7e2d89e1a4c6a2dea4e9c8d1d2015e54c609a'
+    # aes_key = h2b('66dd511791296a6cfc94a24041fcab9f')
+    # hmac_key = h2b(
+    #        '0f736d6e85c448c2c8cc30f094d7e2d89e1a4c6a2dea4e9c8d1d2015e54c609a')
+
+    # We next split the message:
+    # len(msg) = 63.
+    # len(M_0) = 8 = CiphertextSegmentSize(64) - Headerlength(24) - TagSize(32)
+    # len(M_1) = 32 = CiphertextSegmentSize(64) - TagSize(32)
+    # len(M_2) = 23 < CiphertextSegmentSize(64) - TagSize(32)
+    msg_0 = msg[:8]
+    msg_1 = msg[8:40]
+    msg_2 = msg[40:]
+
+    # Relevant AES computations with key = 66dd511791296a6cfc94a24041fcab9f
+    #
+    # nonce_prefix + segment_nr + b + i   | Out
+    # -----------------------------------------------------------------------
+    # 2c0862877baea8 00000000 00 00000000 | ea8e18301bd57bfdd2f903025950c827
+    # 2c0862877baea8 00000001 00 00000000 | 2999c8ea5401704243c8cd77929fd526
+    # 2c0862877baea8 00000001 00 00000001 | 17fec5542a842446251bb2f3a81f6249
+    # 2c0862877baea8 00000002 01 00000000 | 70fe58e44835a6602952749e763637d9
+    # 2c0862877baea8 00000002 01 00000001 | d973bca83580867766f38b056d735902
+    #
+    c0 = xor(msg_0, h2b(b'ea8e18301bd57bfd'))
+    c1 = xor(msg_1[:16], h2b('2999c8ea5401704243c8cd77929fd526')) + xor(
+        msg_1[16:32], h2b('17fec5542a842446251bb2f3a81f6249')
+    )
+    c2 = xor(msg_2[:16], h2b('70fe58e44835a6602952749e763637d9')) + xor(
+        msg_2[16:], h2b('d973bca8358086')
+    )
+
+    # T0 = hmac(key = hmac_key, h2b('2c0862877baea8000000000000000000' + c0)
+    t0 = h2b('8303ca71c04d8e06e1b01cff7c1178af47dac031517b1f6a2d9be84105677a68')
+    # T1 = hmac(key = hmac_key, h2b('2c0862877baea8000000010000000000' + c1)
+    t1 = h2b('834d890839f37f762caddc029cc673300ff107fd51f9a62058fcd00befc362e5')
+    # T2 = hmac(key = hmac_key, h2b('2c0862877baea8000000020100000000' + c2)
+    t2 = h2b('5fb0c893903271af38380c2f355cb85e5ec571648513123321bde0c6042f43c7')
+
+    ciphertext = header + c0 + t0 + c1 + t1 + c2 + t2
+
+    keyset = to_keyset(key)
+    saead = testing_servers.remote_primitive(
+        lang, keyset.SerializeToString(), streaming_aead.StreamingAead
+    )
+
+    self.assertEqual(
+        saead.new_decrypting_stream(io.BytesIO(ciphertext), aad).read(),
+        msg,
+    )
 
 if __name__ == '__main__':
   absltest.main()
