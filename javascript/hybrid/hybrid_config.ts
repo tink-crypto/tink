02cf8e10bcd8e2409aa93d0447332d0a726aebd4
Tink Team <tink-dev@google.com>
2023-03-08 09:11:16 -0800

    Add hybrid key templates for HPKE implementation.
    PiperOrigin-RevId: 515054867

--- javascript/hybrid/hybrid_config.ts
+++ javascript/hybrid/hybrid_config.ts
@@ -5,12 +5,14 @@
  */
 
 import {AeadConfig} from '../aead/aead_config';
-import * as Registry from '../internal/registry';
+import * as registry from '../internal/registry';
 
 import {EciesAeadHkdfPrivateKeyManager} from './ecies_aead_hkdf_private_key_manager';
 import {EciesAeadHkdfPublicKeyManager} from './ecies_aead_hkdf_public_key_manager';
 import {HybridDecryptWrapper} from './hybrid_decrypt_wrapper';
 import {HybridEncryptWrapper} from './hybrid_encrypt_wrapper';
+import {HpkePrivateKeyManager} from './internal/hpke/hpke_private_key_manager';
+import {HpkePublicKeyManager} from './internal/hpke/hpke_public_key_manager';
 
 // Static methods and constants for registering with the Registry all instances
 // of key types for hybrid encryption and decryption supported in a particular
@@ -27,18 +29,21 @@ import {HybridEncryptWrapper} from './hybrid_encrypt_wrapper';
  */
 export function register() {
   AeadConfig.register();
-  Registry.registerKeyManager(new EciesAeadHkdfPrivateKeyManager());
-  Registry.registerKeyManager(new EciesAeadHkdfPublicKeyManager());
-  Registry.registerPrimitiveWrapper(new HybridEncryptWrapper());
-  Registry.registerPrimitiveWrapper(new HybridDecryptWrapper());
+  registry.registerKeyManager(new EciesAeadHkdfPrivateKeyManager());
+  registry.registerKeyManager(new EciesAeadHkdfPublicKeyManager());
+  registry.registerKeyManager(new HpkePrivateKeyManager());
+  registry.registerKeyManager(new HpkePublicKeyManager());
+  registry.registerPrimitiveWrapper(new HybridEncryptWrapper());
+  registry.registerPrimitiveWrapper(new HybridDecryptWrapper());
 }
 
-export const ENCRYPT_PRIMITIVE_NAME: string = 'HybridEncrypt';
-
+// Suppressing enforced comments since the names are self-explanatory.
+// tslint:disable:enforce-comments-on-exported-symbols
+export const ENCRYPT_PRIMITIVE_NAME = 'HybridEncrypt';
 export const ECIES_AEAD_HKDF_PUBLIC_KEY_TYPE: string =
     EciesAeadHkdfPublicKeyManager.KEY_TYPE;
-
-export const DECRYPT_PRIMITIVE_NAME: string = 'HybridDecrypt';
-
+export const HPKE_PUBLIC_KEY_TYPE: string = HpkePublicKeyManager.KEY_TYPE;
+export const DECRYPT_PRIMITIVE_NAME = 'HybridDecrypt';
 export const ECIES_AEAD_HKDF_PRIVATE_KEY_TYPE: string =
     EciesAeadHkdfPrivateKeyManager.KEY_TYPE;
+export const HPKE_PRIVATE_KEY_TYPE: string = HpkePrivateKeyManager.KEY_TYPE;
