fcfaf6f91f9e8fda15915a388504e9882da348d9
juerg <juerg@google.com>
2023-03-13 07:39:23 -0700

    Convert keyset_test.go into an example.
    The example can then be used on the dev site:
    https://developers.google.com/tink/generate-encrypted-keyset
    
    I have also changed the comments a bit, and I now use WriteWithAssociatedData.
    
    PiperOrigin-RevId: 516205988

--- go/keyset/keyset_test.go
+++ go/keyset/keyset_test.go
@@ -16,73 +16,80 @@
 
 package keyset_test
 
+// [START encrypted-keyset-example]
+
 import (
 	"bytes"
-	"testing"
+	"fmt"
+	"log"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/google/tink/go/aead"
 	"github.com/google/tink/go/keyset"
 	"github.com/google/tink/go/testing/fakekms"
 )
 
+// The fake KMS should only be used in tests. It is not secure.
 const keyURI = "fake-kms://CM2b3_MDElQKSAowdHlwZS5nb29nbGVhcGlzLmNvbS9nb29nbGUuY3J5cHRvLnRpbmsuQWVzR2NtS2V5EhIaEIK75t5L-adlUwVhWvRuWUwYARABGM2b3_MDIAE"
 
-func TestKMSEncryptedKeysetHandle(t *testing.T) {
-	// We use a fake KMS to avoid making RPCs.
+func Example_encryptedKeyset() {
+	// Get a KEK (key encryption key) AEAD. This is usually a remote AEAD to a KMS. In this example,
+	// we use a fake KMS to avoid making RPCs.
 	client, err := fakekms.NewClient(keyURI)
 	if err != nil {
-		t.Fatalf("fakekms.NewClient(keyURI) failed: %v", err)
+		log.Fatal(err)
 	}
-	keyEncryptionAEAD, err := client.GetAEAD(keyURI)
+	kekAEAD, err := client.GetAEAD(keyURI)
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
 
 	// Generate a new keyset handle for the primitive we want to use.
-	handle, err := keyset.NewHandle(aead.AES256GCMKeyTemplate())
+	newHandle, err := keyset.NewHandle(aead.AES256GCMKeyTemplate())
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
 
-	// Encrypt the keyset handle using the KMS AEAD.
+	// Choose some associated data. This is the context in which the keyset will be used.
+	keysetAssociatedData := []byte("keyset encryption example")
+
+	// Encrypt the keyset with the KEK AEAD and the associated data.
 	buf := new(bytes.Buffer)
 	writer := keyset.NewBinaryWriter(buf)
-	err = handle.Write(writer, keyEncryptionAEAD)
+	err = newHandle.WriteWithAssociatedData(writer, kekAEAD, keysetAssociatedData)
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
 	encryptedKeyset := buf.Bytes()
 
-	// Use the keyset handle.
-	a, err := aead.New(handle)
-	if err != nil {
-		t.Fatal(err)
-	}
-	plaintext := []byte("plaintext")
-	associatedData := []byte("associatedData")
-	ciphertext, err := a.Encrypt(plaintext, associatedData)
-	if err != nil {
-		t.Fatal(err)
-	}
+	// The encrypted keyset can now be stored.
 
-	// Get the keyset handle from the encrypted keyset and the KMS.
+	// To use the primitive, we first need to decrypt the keyset. We use the same
+	// KEK AEAD and the same associated data that we used to encrypt it.
 	reader := keyset.NewBinaryReader(bytes.NewReader(encryptedKeyset))
-	handle2, err := keyset.Read(reader, keyEncryptionAEAD)
+	handle, err := keyset.ReadWithAssociatedData(reader, kekAEAD, keysetAssociatedData)
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
 
-	// Use the keyset handle2.
-	a2, err := aead.New(handle2)
+	// Get the primitive.
+	primitive, err := aead.New(handle)
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
-	decrypted, err := a2.Decrypt(ciphertext, associatedData)
+
+	// Use the primitive.
+	plaintext := []byte("message")
+	associatedData := []byte("example encryption")
+	ciphertext, err := primitive.Encrypt(plaintext, associatedData)
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
-	if !cmp.Equal(decrypted, plaintext) {
-		t.Errorf("got = %v, want = %v", decrypted, plaintext)
+	decrypted, err := primitive.Decrypt(ciphertext, associatedData)
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Println(string(decrypted))
+	// Output: message
 }
+
+// [END encrypted-keyset-example]
