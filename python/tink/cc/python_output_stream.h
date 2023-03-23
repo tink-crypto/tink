17eac0c0e462f6f3b50b6b927ed56f758ed34dfd
Tink Team <tink-dev@google.com>
2023-03-23 03:46:59 -0700

    Internal Code Change
    PiperOrigin-RevId: 518816624

--- python/tink/cc/python_output_stream.h
+++ python/tink/cc/python_output_stream.h
@@ -14,8 +14,8 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef TINK_PYTHON_CC_PYTHON_OUTPUT_STREAM_H_
-#define TINK_PYTHON_CC_PYTHON_OUTPUT_STREAM_H_
+#ifndef TINK_PYTHON_TINK_CC_PYTHON_OUTPUT_STREAM_H_
+#define TINK_PYTHON_TINK_CC_PYTHON_OUTPUT_STREAM_H_
 
 #include <memory>
 #include <string>
@@ -63,4 +63,4 @@ class PythonOutputStream : public OutputStream {
 }  // namespace tink
 }  // namespace crypto
 
-#endif  // TINK_PYTHON_CC_PYTHON_OUTPUT_STREAM_H_
+#endif  // TINK_PYTHON_TINK_CC_PYTHON_OUTPUT_STREAM_H_
