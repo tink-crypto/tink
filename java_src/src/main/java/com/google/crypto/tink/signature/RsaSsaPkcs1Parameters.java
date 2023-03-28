728f2b7ebb7750f7ce2655f2c561118cdf251366
juerg <juerg@google.com>
2023-03-28 02:46:35 -0700

    Add RsaSsaPkcs1Parameters in Java.
    DD: go/tink-rsassapkcs1-key-dd.
    PiperOrigin-RevId: 519971008

---
+++ java_src/src/main/java/com/google/crypto/tink/signature/RsaSsaPkcs1Parameters.java
@@ -0,0 +1,225 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.signature;
+
+import com.google.crypto.tink.annotations.Alpha;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.errorprone.annotations.Immutable;
+import java.math.BigInteger;
+import java.security.GeneralSecurityException;
+import java.security.InvalidAlgorithmParameterException;
+import java.util.Objects;
+import javax.annotation.Nullable;
+
+/**
+ * Describes the parameters of a {@link RsaSsaPkcs1PublicKey} and {@link RsaSsaPkcs1PrivateKey}.
+ *
+ * <p>Standard: https://www.rfc-editor.org/rfc/rfc8017.txt
+ *
+ * <p>This API is annotated with Alpha because it is not yet stable and might be changed in the
+ * future.
+ */
+@Alpha
+public final class RsaSsaPkcs1Parameters extends SignatureParameters {
+  /**
+   * Describes details of the signature.
+   *
+   * <p>The usual key is used for variant "NO_PREFIX". Other variants slightly change how the
+   * signature is computed, or add a prefix to every computation depending on the key id.
+   */
+  @Immutable
+  public static final class Variant {
+    public static final Variant TINK = new Variant("TINK");
+    public static final Variant CRUNCHY = new Variant("CRUNCHY");
+    public static final Variant LEGACY = new Variant("LEGACY");
+    public static final Variant NO_PREFIX = new Variant("NO_PREFIX");
+
+    private final String name;
+
+    private Variant(String name) {
+      this.name = name;
+    }
+
+    @Override
+    public String toString() {
+      return name;
+    }
+  }
+
+  /** The Hash algorithm used. */
+  @Immutable
+  public static final class HashType {
+    public static final HashType SHA256 = new HashType("SHA256");
+    public static final HashType SHA384 = new HashType("SHA384");
+    public static final HashType SHA512 = new HashType("SHA512");
+
+    private final String name;
+
+    private HashType(String name) {
+      this.name = name;
+    }
+
+    @Override
+    public String toString() {
+      return name;
+    }
+  }
+
+  public static final BigInteger F4 = BigInteger.valueOf(65537);
+  private static final BigInteger TWO = BigInteger.valueOf(2);
+
+  /** Builds a new RsaSsaPkcs1Parameters instance. */
+  public static final class Builder {
+    @Nullable private Integer modulusSizeBits = null;
+    @Nullable private BigInteger publicExponent = F4;
+    @Nullable private HashType hashType = null;
+    private Variant variant = Variant.NO_PREFIX;
+
+    private Builder() {}
+
+    @CanIgnoreReturnValue
+    public Builder setModulusSizeBits(int modulusSizeBits) {
+      this.modulusSizeBits = modulusSizeBits;
+      return this;
+    }
+
+    @CanIgnoreReturnValue
+    public Builder setPublicExponent(BigInteger e) {
+      this.publicExponent = e;
+      return this;
+    }
+
+    @CanIgnoreReturnValue
+    public Builder setVariant(Variant variant) {
+      this.variant = variant;
+      return this;
+    }
+
+    @CanIgnoreReturnValue
+    public Builder setHashType(HashType hashType) {
+      this.hashType = hashType;
+      return this;
+    }
+
+    private void validatePublicExponent(BigInteger publicExponent)
+        throws InvalidAlgorithmParameterException {
+      int c = publicExponent.compareTo(F4);
+      if (c == 0) {
+        // publicExponent is F4.
+        return;
+      }
+      if (c < 0) {
+        // publicExponent is smaller than F4.
+        throw new InvalidAlgorithmParameterException("Public exponent must be at least 65537.");
+      }
+      if (publicExponent.mod(TWO).equals(BigInteger.ZERO)) {
+        // publicExponent is even. This is invalid since it is not co-prime to p-1.
+        throw new InvalidAlgorithmParameterException("Invalid public exponent");
+      }
+    }
+
+    public RsaSsaPkcs1Parameters build() throws GeneralSecurityException {
+      if (modulusSizeBits == null) {
+        throw new GeneralSecurityException("key size is not set");
+      }
+      if (publicExponent == null) {
+        throw new GeneralSecurityException("publicExponent is not set");
+      }
+      if (hashType == null) {
+        throw new GeneralSecurityException("hash type is not set");
+      }
+      if (variant == null) {
+        throw new GeneralSecurityException("variant is not set");
+      }
+      if (modulusSizeBits < 2048) {
+        throw new InvalidAlgorithmParameterException(
+            String.format(
+                "Invalid key size in bytes %d; must be at least 2048 bits", modulusSizeBits));
+      }
+      validatePublicExponent(publicExponent);
+      return new RsaSsaPkcs1Parameters(modulusSizeBits, publicExponent, variant, hashType);
+    }
+  }
+
+  private final int modulusSizeBits;
+  private final BigInteger publicExponent;
+  private final Variant variant;
+  private final HashType hashType;
+
+  private RsaSsaPkcs1Parameters(
+      int modulusSizeBits, BigInteger publicExponent, Variant variant, HashType hashType) {
+    this.modulusSizeBits = modulusSizeBits;
+    this.publicExponent = publicExponent;
+    this.variant = variant;
+    this.hashType = hashType;
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public int getModulusSizeBits() {
+    return modulusSizeBits;
+  }
+
+  public BigInteger getPublicExponent() {
+    return publicExponent;
+  }
+
+  public Variant getVariant() {
+    return variant;
+  }
+
+  public HashType getHashType() {
+    return hashType;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof RsaSsaPkcs1Parameters)) {
+      return false;
+    }
+    RsaSsaPkcs1Parameters that = (RsaSsaPkcs1Parameters) o;
+    return that.getModulusSizeBits() == getModulusSizeBits()
+        && Objects.equals(that.getPublicExponent(), getPublicExponent())
+        && that.getVariant() == getVariant()
+        && that.getHashType() == getHashType();
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(modulusSizeBits, publicExponent, variant, hashType);
+  }
+
+  @Override
+  public boolean hasIdRequirement() {
+    return variant != Variant.NO_PREFIX;
+  }
+
+  @Override
+  public String toString() {
+    return "RSA SSA PKCS1 Parameters (variant: "
+        + variant
+        + ", hashType: "
+        + hashType
+        + ", publicExponent: "
+        + publicExponent
+        + ", and "
+        + modulusSizeBits
+        + "-bit modulus)";
+  }
+}
---
+++ java_src/src/test/java/com/google/crypto/tink/signature/RsaSsaPkcs1ParametersTest.java
@@ -0,0 +1,322 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.signature;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import java.math.BigInteger;
+import java.security.GeneralSecurityException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class RsaSsaPkcs1ParametersTest {
+
+  @Test
+  public void buildWithNoPrefixAndGetProperties() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+            .build();
+    assertThat(parameters.getModulusSizeBits()).isEqualTo(2048);
+    assertThat(parameters.getPublicExponent()).isEqualTo(RsaSsaPkcs1Parameters.F4);
+    assertThat(parameters.getHashType()).isEqualTo(RsaSsaPkcs1Parameters.HashType.SHA256);
+    assertThat(parameters.getVariant()).isEqualTo(RsaSsaPkcs1Parameters.Variant.NO_PREFIX);
+    assertThat(parameters.hasIdRequirement()).isFalse();
+  }
+
+  @Test
+  public void buildParametersWithoutSettingVariant_hasNoPrefix() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .build();
+    assertThat(parameters.getVariant()).isEqualTo(RsaSsaPkcs1Parameters.Variant.NO_PREFIX);
+    assertThat(parameters.hasIdRequirement()).isFalse();
+  }
+
+  @Test
+  public void buildParametersWithoutExponent() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+            .build();
+    assertThat(parameters.getPublicExponent()).isEqualTo(RsaSsaPkcs1Parameters.F4);
+  }
+
+  @Test
+  public void buildParametersWithTinkPrefix() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.TINK)
+            .build();
+    assertThat(parameters.getVariant()).isEqualTo(RsaSsaPkcs1Parameters.Variant.TINK);
+    assertThat(parameters.hasIdRequirement()).isTrue();
+  }
+
+  @Test
+  public void buildParametersWithLegacyPrefix() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.LEGACY)
+            .build();
+    assertThat(parameters.getVariant()).isEqualTo(RsaSsaPkcs1Parameters.Variant.LEGACY);
+    assertThat(parameters.hasIdRequirement()).isTrue();
+  }
+
+  @Test
+  public void buildParametersWithCrunchyPrefix() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.CRUNCHY)
+            .build();
+    assertThat(parameters.getVariant()).isEqualTo(RsaSsaPkcs1Parameters.Variant.CRUNCHY);
+    assertThat(parameters.hasIdRequirement()).isTrue();
+  }
+
+  @Test
+  public void buildParametersWithLargeModulusSize() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(16789)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+            .build();
+    assertThat(parameters.getModulusSizeBits()).isEqualTo(16789);
+  }
+
+  @Test
+  public void buildParametersWithValidNonF4PublicExponentSet() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setPublicExponent(BigInteger.valueOf(1234567))
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+            .build();
+    assertThat(parameters.getPublicExponent()).isEqualTo(BigInteger.valueOf(1234567));
+  }
+
+  @Test
+  public void buildParametersWithSmallPublicExponent_fails() throws Exception {
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            RsaSsaPkcs1Parameters.builder()
+                .setModulusSizeBits(2048)
+                .setPublicExponent(BigInteger.valueOf(3))
+                .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+                .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+                .build());
+  }
+
+  @Test
+  public void buildParametersWithEvenPublicExponent_fails() throws Exception {
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            RsaSsaPkcs1Parameters.builder()
+                .setModulusSizeBits(2048)
+                .setPublicExponent(BigInteger.valueOf(1234568))
+                .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+                .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+                .build());
+  }
+
+  @Test
+  public void buildParametersWithLargePublicExponent_works() throws Exception {
+    BigInteger largeE = BigInteger.valueOf(100000000001L);
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setPublicExponent(largeE)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+            .build();
+    assertThat(parameters.getPublicExponent()).isEqualTo(largeE);
+  }
+
+  @Test
+  public void buildParametersWithSha384() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(3072)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA384)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+            .build();
+    assertThat(parameters.getHashType()).isEqualTo(RsaSsaPkcs1Parameters.HashType.SHA384);
+  }
+
+  @Test
+  public void buildParametersWithSha512() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(4096)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA512)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+            .build();
+    assertThat(parameters.getHashType()).isEqualTo(RsaSsaPkcs1Parameters.HashType.SHA512);
+  }
+
+  @Test
+  public void buildWithoutSettingModulusSize_fails() throws Exception {
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            RsaSsaPkcs1Parameters.builder()
+                .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+                .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+                .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+                .build());
+  }
+
+  @Test
+  public void buildWithoutSettingHashType_fails() throws Exception {
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            RsaSsaPkcs1Parameters.builder()
+                .setModulusSizeBits(2048)
+                .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+                .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+                .build());
+  }
+
+  @Test
+  public void buildWithVariantSetToNull_fails() throws Exception {
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            RsaSsaPkcs1Parameters.builder()
+                .setModulusSizeBits(2048)
+                .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+                .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+                .setVariant(null)
+                .build());
+  }
+
+  @Test
+  public void buildWithExponentSetToNull_fails() throws Exception {
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            RsaSsaPkcs1Parameters.builder()
+                .setModulusSizeBits(2048)
+                .setPublicExponent(null)
+                .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+                .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+                .build());
+  }
+
+  @Test
+  public void buildWithTooSmallModulusSize_fails() throws Exception {
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            RsaSsaPkcs1Parameters.builder()
+                .setModulusSizeBits(2047)
+                .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+                .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+                .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+                .build());
+  }
+
+  @Test
+  public void testEqualsAndEqualHashCode() throws Exception {
+    RsaSsaPkcs1Parameters parameters1 =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+            .build();
+    RsaSsaPkcs1Parameters parameters2 =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+            .build();
+
+    assertThat(parameters1).isEqualTo(parameters2);
+    assertThat(parameters1.hashCode()).isEqualTo(parameters2.hashCode());
+  }
+
+  @Test
+  public void testNotEqual() throws Exception {
+    RsaSsaPkcs1Parameters parameters =
+        RsaSsaPkcs1Parameters.builder()
+            .setModulusSizeBits(2048)
+            .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+            .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+            .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+            .build();
+    assertThat(parameters)
+        .isNotEqualTo(
+            RsaSsaPkcs1Parameters.builder()
+                .setModulusSizeBits(2049)
+                .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+                .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+                .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+                .build());
+    assertThat(parameters)
+        .isNotEqualTo(
+            RsaSsaPkcs1Parameters.builder()
+                .setModulusSizeBits(2048)
+                .setPublicExponent(BigInteger.valueOf(65539))
+                .setHashType(RsaSsaPkcs1Parameters.HashType.SHA256)
+                .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+                .build());
+    assertThat(parameters)
+        .isNotEqualTo(
+            RsaSsaPkcs1Parameters.builder()
+                .setModulusSizeBits(2048)
+                .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+                .setHashType(RsaSsaPkcs1Parameters.HashType.SHA384)
+                .setVariant(RsaSsaPkcs1Parameters.Variant.NO_PREFIX)
+                .build());
+    assertThat(parameters)
+        .isNotEqualTo(
+            RsaSsaPkcs1Parameters.builder()
+                .setModulusSizeBits(2048)
+                .setPublicExponent(RsaSsaPkcs1Parameters.F4)
+                .setHashType(RsaSsaPkcs1Parameters.HashType.SHA384)
+                .setVariant(RsaSsaPkcs1Parameters.Variant.TINK)
+                .build());
+  }
+}
