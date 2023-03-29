c24631e71bc514d20ba2dde674df59100d0cbcb3
Tink Team <tink-dev@google.com>
2023-03-29 08:40:16 -0700

    Add Ed25519PublicKey class in Java.
    PiperOrigin-RevId: 520345902

---
+++ java_src/src/main/java/com/google/crypto/tink/signature/Ed25519PublicKey.java
@@ -0,0 +1,146 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.signature;
+
+import com.google.crypto.tink.AccessesPartialKey;
+import com.google.crypto.tink.Key;
+import com.google.crypto.tink.annotations.Alpha;
+import com.google.crypto.tink.util.Bytes;
+import com.google.crypto.tink.util.SecretBytes;
+import com.google.errorprone.annotations.Immutable;
+import com.google.errorprone.annotations.RestrictedApi;
+import java.nio.ByteBuffer;
+import java.security.GeneralSecurityException;
+import java.util.Objects;
+import javax.annotation.Nullable;
+
+/**
+ * Ed25519PublicKey represents the public portion of the Ed25519 signature primitive.
+ *
+ * <p>This API is annotated with Alpha because it is not yet stable and might be changed in the
+ * future.
+ */
+@Alpha
+@Immutable
+public final class Ed25519PublicKey extends SignaturePublicKey {
+  private final Ed25519Parameters parameters;
+  private final SecretBytes publicKeyBytes;
+  private final Bytes outputPrefix;
+  @Nullable private final Integer idRequirement;
+
+  private Ed25519PublicKey(
+      Ed25519Parameters parameters,
+      SecretBytes publicKeyBytes,
+      Bytes outputPrefix,
+      @Nullable Integer idRequirement) {
+    this.parameters = parameters;
+    this.publicKeyBytes = publicKeyBytes;
+    this.outputPrefix = outputPrefix;
+    this.idRequirement = idRequirement;
+  }
+
+  private static Bytes createOutputPrefix(
+      Ed25519Parameters parameters, @Nullable Integer idRequirement) {
+    if (parameters.getVariant() == Ed25519Parameters.Variant.NO_PREFIX) {
+      return Bytes.copyFrom(new byte[] {});
+    }
+    if (parameters.getVariant() == Ed25519Parameters.Variant.CRUNCHY
+        || parameters.getVariant() == Ed25519Parameters.Variant.LEGACY) {
+      return Bytes.copyFrom(
+          ByteBuffer.allocate(5).put((byte) 0).putInt(idRequirement.intValue()).array());
+    }
+    if (parameters.getVariant() == Ed25519Parameters.Variant.TINK) {
+      return Bytes.copyFrom(
+          ByteBuffer.allocate(5).put((byte) 1).putInt(idRequirement.intValue()).array());
+    }
+    throw new IllegalStateException("Unknown Variant: " + parameters.getVariant());
+  }
+
+  @Override
+  public Bytes getOutputPrefix() {
+    return outputPrefix;
+  }
+
+  @RestrictedApi(
+      explanation = "Accessing parts of keys can produce unexpected incompatibilities, annotate the function with @AccessesPartialKey",
+      link = "https://developers.google.com/tink/design/access_control#accessing_partial_keys",
+      allowedOnPath = ".*Test\\.java",
+      allowlistAnnotations = {AccessesPartialKey.class})
+  @AccessesPartialKey
+  public static Ed25519PublicKey create(SecretBytes secretBytes) throws GeneralSecurityException {
+    return create(Ed25519Parameters.Variant.NO_PREFIX, secretBytes, null);
+  }
+
+  @RestrictedApi(
+      explanation = "Accessing parts of keys can produce unexpected incompatibilities, annotate the function with @AccessesPartialKey",
+      link = "https://developers.google.com/tink/design/access_control#accessing_partial_keys",
+      allowedOnPath = ".*Test\\.java",
+      allowlistAnnotations = {AccessesPartialKey.class})
+  public static Ed25519PublicKey create(
+      Ed25519Parameters.Variant variant, SecretBytes secretBytes, @Nullable Integer idRequirement)
+      throws GeneralSecurityException {
+    Ed25519Parameters parameters = Ed25519Parameters.create(variant);
+    if (!variant.equals(Ed25519Parameters.Variant.NO_PREFIX) && idRequirement == null) {
+      throw new GeneralSecurityException(
+          "For given Variant " + variant + " the value of idRequirement must be non-null");
+      }
+
+    if (variant.equals(Ed25519Parameters.Variant.NO_PREFIX) && idRequirement != null) {
+      throw new GeneralSecurityException(
+          "For given Variant NO_PREFIX the value of idRequirement must be null");
+    }
+    if (secretBytes.size() != 32) {
+      throw new GeneralSecurityException(
+          "Ed25519 key must be constructed with key of length 32 bytes, not " + secretBytes.size());
+    }
+
+    return new Ed25519PublicKey(
+        parameters, secretBytes, createOutputPrefix(parameters, idRequirement), idRequirement);
+  }
+
+  @RestrictedApi(
+      explanation = "Accessing parts of keys can produce unexpected incompatibilities, annotate the function with @AccessesPartialKey",
+      link = "https://developers.google.com/tink/design/access_control#accessing_partial_keys",
+      allowedOnPath = ".*Test\\.java",
+      allowlistAnnotations = {AccessesPartialKey.class})
+  public SecretBytes getPublicKeyBytes() {
+    return publicKeyBytes;
+  }
+
+  @Override
+  public Ed25519Parameters getParameters() {
+    return parameters;
+  }
+
+  @Override
+  @Nullable
+  public Integer getIdRequirementOrNull() {
+    return idRequirement;
+  }
+
+  @Override
+  public boolean equalsKey(Key o) {
+    if (!(o instanceof Ed25519PublicKey)) {
+      return false;
+    }
+    Ed25519PublicKey that = (Ed25519PublicKey) o;
+    // Since outputPrefix is a function of parameters, we can ignore it here.
+    return that.parameters.equals(parameters)
+        && that.publicKeyBytes.equalsSecretBytes(publicKeyBytes)
+        && Objects.equals(that.idRequirement, idRequirement);
+  }
+}
---
+++ java_src/src/test/java/com/google/crypto/tink/signature/Ed25519PublicKeyTest.java
@@ -0,0 +1,198 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.signature;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.InsecureSecretKeyAccess;
+import com.google.crypto.tink.aead.ChaCha20Poly1305Key;
+import com.google.crypto.tink.internal.KeyTester;
+import com.google.crypto.tink.util.Bytes;
+import com.google.crypto.tink.util.SecretBytes;
+import java.security.GeneralSecurityException;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+@RunWith(Theories.class)
+public final class Ed25519PublicKeyTest {
+
+  @DataPoints("requireIdVariants")
+  public static final Ed25519Parameters.Variant[] REQUIRE_ID_VARIANTS =
+      new Ed25519Parameters.Variant[] {
+        Ed25519Parameters.Variant.TINK,
+        Ed25519Parameters.Variant.CRUNCHY,
+        Ed25519Parameters.Variant.LEGACY
+      };
+
+  @Test
+  public void buildNoPrefixVariantAndGetProperties() throws Exception {
+    SecretBytes keyBytes = SecretBytes.randomBytes(32);
+
+    Ed25519PublicKey key = Ed25519PublicKey.create(keyBytes);
+
+    assertThat(key.getParameters()).isEqualTo(Ed25519Parameters.create());
+    assertThat(key.getPublicKeyBytes()).isEqualTo(keyBytes);
+    assertThat(key.getOutputPrefix()).isEqualTo(Bytes.copyFrom(new byte[] {}));
+    assertThat(key.getIdRequirementOrNull()).isNull();
+  }
+
+  @Test
+  public void buildNoPrefixVariantExplicitAndGetProperties() throws Exception {
+    SecretBytes keyBytes = SecretBytes.randomBytes(32);
+
+    Ed25519PublicKey key =
+        Ed25519PublicKey.create(
+            Ed25519Parameters.Variant.NO_PREFIX, keyBytes, /* idRequirement= */ null);
+
+    assertThat(key.getParameters()).isEqualTo(Ed25519Parameters.create());
+    assertThat(key.getPublicKeyBytes()).isEqualTo(keyBytes);
+    assertThat(key.getOutputPrefix()).isEqualTo(Bytes.copyFrom(new byte[] {}));
+    assertThat(key.getIdRequirementOrNull()).isNull();
+  }
+
+  @Test
+  public void buildTinkVariantAndGetProperties() throws Exception {
+    SecretBytes keyBytes = SecretBytes.randomBytes(32);
+
+    Ed25519PublicKey key =
+        Ed25519PublicKey.create(
+            Ed25519Parameters.Variant.TINK, keyBytes, /* idRequirement= */ 0x0708090a);
+
+    assertThat(key.getParameters())
+        .isEqualTo(Ed25519Parameters.create(Ed25519Parameters.Variant.TINK));
+    assertThat(key.getPublicKeyBytes()).isEqualTo(keyBytes);
+    assertThat(key.getOutputPrefix())
+        .isEqualTo(Bytes.copyFrom(new byte[] {0x01, 0x07, 0x08, 0x09, 0x0a}));
+    assertThat(key.getIdRequirementOrNull()).isEqualTo(0x708090a);
+  }
+
+  @Test
+  public void buildCrunchyVariantAndGetProperties() throws Exception {
+    SecretBytes keyBytes = SecretBytes.randomBytes(32);
+
+    Ed25519PublicKey key =
+        Ed25519PublicKey.create(
+            Ed25519Parameters.Variant.CRUNCHY, keyBytes, /* idRequirement= */ 0x0708090a);
+
+    assertThat(key.getParameters())
+        .isEqualTo(Ed25519Parameters.create(Ed25519Parameters.Variant.CRUNCHY));
+    assertThat(key.getPublicKeyBytes()).isEqualTo(keyBytes);
+    assertThat(key.getOutputPrefix())
+        .isEqualTo(Bytes.copyFrom(new byte[] {0x00, 0x07, 0x08, 0x09, 0x0a}));
+    assertThat(key.getIdRequirementOrNull()).isEqualTo(0x708090a);
+  }
+
+  @Test
+  public void buildLegacyVariantAndGetProperties() throws Exception {
+    SecretBytes keyBytes = SecretBytes.randomBytes(32);
+
+    Ed25519PublicKey key =
+        Ed25519PublicKey.create(
+            Ed25519Parameters.Variant.LEGACY, keyBytes, /* idRequirement= */ 0x0708090a);
+
+    assertThat(key.getParameters())
+        .isEqualTo(Ed25519Parameters.create(Ed25519Parameters.Variant.LEGACY));
+    assertThat(key.getPublicKeyBytes()).isEqualTo(keyBytes);
+    assertThat(key.getOutputPrefix())
+        .isEqualTo(Bytes.copyFrom(new byte[] {0x00, 0x07, 0x08, 0x09, 0x0a}));
+    assertThat(key.getIdRequirementOrNull()).isEqualTo(0x708090a);
+  }
+
+  @Theory
+  public void requireIdButIdIsNotSet_fails(
+      @FromDataPoints("requireIdVariants") Ed25519Parameters.Variant variant) throws Exception {
+    SecretBytes keyBytes = SecretBytes.randomBytes(32);
+
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> Ed25519PublicKey.create(variant, keyBytes, /* idRequirement= */ null));
+  }
+
+  @Test
+  public void doesNotRequireIdButIdIsSet_fails() throws Exception {
+    SecretBytes keyBytes = SecretBytes.randomBytes(32);
+
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            Ed25519PublicKey.create(
+                Ed25519Parameters.Variant.NO_PREFIX, keyBytes, /* idRequirement= */ 1115));
+  }
+
+  @Test
+  public void invalidKeySize() throws Exception {
+    SecretBytes keyBytes = SecretBytes.randomBytes(64);
+
+    assertThrows(GeneralSecurityException.class, () -> Ed25519PublicKey.create(keyBytes));
+  }
+
+  @Test
+  public void testEqualities() throws Exception {
+    SecretBytes keyBytes = SecretBytes.randomBytes(32);
+    SecretBytes keyBytesCopy =
+        SecretBytes.copyFrom(
+            keyBytes.toByteArray(InsecureSecretKeyAccess.get()), InsecureSecretKeyAccess.get());
+    SecretBytes keyBytesDiff = SecretBytes.randomBytes(32);
+
+    new KeyTester()
+        .addEqualityGroup(
+            "No prefix, keyBytes",
+            Ed25519PublicKey.create(keyBytes),
+            Ed25519PublicKey.create(
+                Ed25519Parameters.Variant.NO_PREFIX, keyBytes, /* idRequirement= */ null),
+            Ed25519PublicKey.create(
+                Ed25519Parameters.Variant.NO_PREFIX, keyBytesCopy, /* idRequirement= */ null))
+        .addEqualityGroup(
+            "No prefix, different key bytes",
+            Ed25519PublicKey.create(
+                Ed25519Parameters.Variant.NO_PREFIX, keyBytesDiff, /* idRequirement= */ null))
+        .addEqualityGroup(
+            "Tink with key id 1907, keyBytes",
+            Ed25519PublicKey.create(
+                Ed25519Parameters.Variant.TINK, keyBytes, /* idRequirement= */ 1907),
+            Ed25519PublicKey.create(
+                Ed25519Parameters.Variant.TINK, keyBytesCopy, /* idRequirement= */ 1907))
+        .addEqualityGroup(
+            "Tink with key id 1908, keyBytes",
+            Ed25519PublicKey.create(
+                Ed25519Parameters.Variant.TINK, keyBytes, /* idRequirement= */ 1908))
+        .addEqualityGroup(
+            "Crunchy with key id 1907, keyBytes",
+            Ed25519PublicKey.create(
+                Ed25519Parameters.Variant.CRUNCHY, keyBytes, /* idRequirement= */ 1907))
+        .addEqualityGroup(
+            "Legacy with key id 1908, keyBytes",
+            Ed25519PublicKey.create(
+                Ed25519Parameters.Variant.LEGACY, keyBytes, /* idRequirement= */ 1907))
+        .doTests();
+  }
+
+  @Test
+  public void testDifferentKeyTypesEquality_fails() throws Exception {
+    SecretBytes keyBytes = SecretBytes.randomBytes(32);
+
+    Ed25519PublicKey ed25519Key = Ed25519PublicKey.create(keyBytes);
+    ChaCha20Poly1305Key chaCha20Poly1305Key = ChaCha20Poly1305Key.create(keyBytes);
+
+    assertThat(ed25519Key.equalsKey(chaCha20Poly1305Key)).isFalse();
+  }
+}
