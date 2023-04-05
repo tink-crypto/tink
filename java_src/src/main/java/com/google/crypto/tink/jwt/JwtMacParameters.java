7f285795e03bd921d85917304a2762d07a02c3fc
tholenst <tholenst@google.com>
2023-04-05 06:01:49 -0700

    Add JwtMacKey and JwtMacParameters.
    PiperOrigin-RevId: 522030213

---
+++ java_src/src/main/java/com/google/crypto/tink/jwt/JwtMacParameters.java
@@ -0,0 +1,25 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.jwt;
+
+import com.google.crypto.tink.Parameters;
+
+/** Represents a description of a {@link JwtMacKey} excluding the randomly chosen key material. */
+abstract class JwtMacParameters extends Parameters {
+  /** If true, no {@code "kid"} header is allowed when verifying a token. */
+  public abstract boolean allowKidAbsent();
+}
