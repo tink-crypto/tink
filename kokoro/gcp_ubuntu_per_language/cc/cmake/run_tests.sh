411e3cfce3219224f5916690f731112ab3751850
ambrosin <ambrosin@google.com>
2023-04-25 06:56:53 -0700

    Set minimum supported CMake version to 3.13
    The current minimum supported version in https://github.com/google/oss-policies-info/blob/main/foundational-cxx-support-matrix.md is 3.10, but this is going to change to 3.13 from May 1st because Ubuntu 18.04 will be no longer supoported. 3.13 is the older version that comes packaged with the supported distros (Debian 10 in this case).
    
    See https://opensource.google/documentation/policies/cplusplus-support#support_criteria_3
    
    PiperOrigin-RevId: 526954498

--- kokoro/gcp_ubuntu_per_language/cc/cmake/run_tests.sh
+++ kokoro/gcp_ubuntu_per_language/cc/cmake/run_tests.sh
@@ -22,7 +22,7 @@ fi
 
 ./kokoro/testutils/upgrade_gcc.sh
 # Sourcing is needed to update the caller environment.
-# Install CMake 3.10 which is the minimum required.
-source ./kokoro/testutils/install_cmake.sh "3.10.0" \
-  "6b5ea290042fac96023fd5387cd65cc69fdf4f48bad91177eb8697ba37e3deb3"
+# Install CMake 3.13 which is the minimum required.
+source ./kokoro/testutils/install_cmake.sh "3.13.5" \
+  "e2fd0080a6f0fc1ec84647acdcd8e0b4019770f48d83509e6a5b0b6ea27e5864"
 ./kokoro/testutils/run_cmake_tests.sh .
