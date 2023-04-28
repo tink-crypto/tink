d2b5a8977a61dc4f9e4a0d5284e6571be86619cc
ckl <ckl@google.com>
2023-04-28 01:25:26 -0700

    Document support for 3.10 in the package classifiers.
    PiperOrigin-RevId: 527815662

--- python/setup.py
+++ python/setup.py
@@ -262,6 +262,7 @@ def main():
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
           'Topic :: Software Development :: Libraries',
       ],
       license='Apache 2.0',
