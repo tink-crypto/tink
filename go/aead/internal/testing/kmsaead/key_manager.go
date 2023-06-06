944263329175d213eb25f38d47566a1abda83635
juerg <juerg@google.com>
2023-06-06 02:37:31 -0700

    Add internal test-only KMS AEAD key manager in go.
    Go currently doesn't support this key type. But it is needed for the cross-language tests. This change adds an internal-only key manager to be used by the tests.
    
    We can later decide if we want to make this part of the public API.
    
    PiperOrigin-RevId: 538121721

---
+++ go/aead/internal/testing/kmsaead/key_manager.go
@@ -0,0 +1,111 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+// Package kmsaead provides a keymanager for KmsAeadKey that may only be used in tests.
+//
+// Golang currently doesn't implement KmsAeadKey. This is an internal implementation
+// to be used by the cross-language tests.
+package kmsaead
+
+import (
+	"errors"
+	"fmt"
+
+	"google.golang.org/protobuf/proto"
+	"github.com/google/tink/go/core/registry"
+	"github.com/google/tink/go/keyset"
+	kmsaeadpb "github.com/google/tink/go/proto/kms_aead_go_proto"
+	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
+)
+
+const kmsAEADTypeURL = "type.googleapis.com/google.crypto.tink.KmsAeadKey"
+
+type keyManager struct{}
+
+func (km *keyManager) Primitive(protoSerializedKey []byte) (interface{}, error) {
+	if len(protoSerializedKey) == 0 {
+		return nil, errors.New("kmsaead.keyManager: invalid key")
+	}
+	key := new(kmsaeadpb.KmsAeadKey)
+	if err := proto.Unmarshal(protoSerializedKey, key); err != nil {
+		return nil, errors.New("kmsaead.keyManager: invalid key")
+	}
+	err := keyset.ValidateKeyVersion(key.Version, 0)
+	if err != nil {
+		return nil, errors.New("kmsaead.keyManager: invalid version")
+	}
+	uri := key.GetParams().GetKeyUri()
+	kmsClient, err := registry.GetKMSClient(uri)
+	if err != nil {
+		return nil, err
+	}
+	return kmsClient.GetAEAD(uri)
+}
+
+func (km *keyManager) NewKey(serializedKeyFormat []byte) (proto.Message, error) {
+	if len(serializedKeyFormat) == 0 {
+		return nil, errors.New("kmsaead.keyManager: invalid key format")
+	}
+	keyFormat := new(kmsaeadpb.KmsAeadKeyFormat)
+	if err := proto.Unmarshal(serializedKeyFormat, keyFormat); err != nil {
+		return nil, errors.New("kmsaead.keyManager: invalid key format")
+	}
+	return &kmsaeadpb.KmsAeadKey{
+		Version: 0,
+		Params:  keyFormat,
+	}, nil
+}
+
+func (km *keyManager) NewKeyData(serializedKeyFormat []byte) (*tinkpb.KeyData, error) {
+	key, err := km.NewKey(serializedKeyFormat)
+	if err != nil {
+		return nil, err
+	}
+	serializedKey, err := proto.Marshal(key)
+	if err != nil {
+		return nil, err
+	}
+	return &tinkpb.KeyData{
+		TypeUrl:         kmsAEADTypeURL,
+		Value:           serializedKey,
+		KeyMaterialType: tinkpb.KeyData_REMOTE,
+	}, nil
+}
+
+func (km *keyManager) DoesSupport(typeURL string) bool {
+	return typeURL == kmsAEADTypeURL
+}
+
+func (km *keyManager) TypeURL() string {
+	return kmsAEADTypeURL
+}
+
+// NewKeyManager returns a new KeyManager for the KMS AEAD key type.
+func NewKeyManager() registry.KeyManager { return new(keyManager) }
+
+// CreateKeyTemplate creates a new KMS AEAD key template.
+func CreateKeyTemplate(uri string) (*tinkpb.KeyTemplate, error) {
+	f := &kmsaeadpb.KmsAeadKeyFormat{KeyUri: uri}
+	serializedFormat, err := proto.Marshal(f)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal key format: %s", err)
+	}
+	return &tinkpb.KeyTemplate{
+		Value:            serializedFormat,
+		TypeUrl:          kmsAEADTypeURL,
+		OutputPrefixType: tinkpb.OutputPrefixType_RAW,
+	}, nil
+}
---
+++ go/aead/internal/testing/kmsaead/key_manager_test.go
@@ -0,0 +1,88 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package kmsaead_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/google/tink/go/aead"
+	"github.com/google/tink/go/aead/internal/testing/kmsaead"
+	"github.com/google/tink/go/core/registry"
+	"github.com/google/tink/go/keyset"
+	"github.com/google/tink/go/testing/fakekms"
+)
+
+// The fake KMS should only be used in tests. It is not secure.
+const keyURI = "fake-kms://CM2b3_MDElQKSAowdHlwZS5nb29nbGVhcGlzLmNvbS9nb29nbGUuY3J5cHRvLnRpbmsuQWVzR2NtS2V5EhIaEIK75t5L-adlUwVhWvRuWUwYARABGM2b3_MDIAE"
+
+func TestCreateEncryptDecrypt(t *testing.T) {
+	registry.RegisterKeyManager(kmsaead.NewKeyManager())
+
+	client, err := fakekms.NewClient(keyURI)
+	if err != nil {
+		t.Fatalf("fakekms.NewClient(keyURI) err = %q, want nil", err)
+	}
+	registry.RegisterKMSClient(client)
+
+	template, err := kmsaead.CreateKeyTemplate(keyURI)
+	if err != nil {
+		t.Fatalf("kmsaead.CreateKeyTemplate(keyURI) err = %q, want nil", err)
+	}
+	handle, err := keyset.NewHandle(template)
+	if err != nil {
+		t.Fatalf("keyset.NewHandle(template) err = %q, want nil", err)
+	}
+	primitive, err := aead.New(handle)
+	if err != nil {
+		t.Fatalf("aead.New(handle) err = %q, want nil", err)
+	}
+
+	plaintext := []byte("plaintext")
+	associatedData := []byte("associatedData")
+
+	ciphertext, err := primitive.Encrypt(plaintext, associatedData)
+	if err != nil {
+		t.Fatalf("primitive.Encrypt(plaintext, associatedData) err = %q, want nil", err)
+	}
+
+	gotPlaintext, err := primitive.Decrypt(ciphertext, associatedData)
+	if err != nil {
+		t.Fatalf("primitive.Decrypt(ciphertext, associatedData) err = %q, want nil", err)
+	}
+	if !bytes.Equal(gotPlaintext, plaintext) {
+		t.Fatalf("gotPlaintext = %q, want %q", gotPlaintext, plaintext)
+	}
+
+	_, err = primitive.Decrypt(ciphertext, []byte("invalidAssociatedData"))
+	if err == nil {
+		t.Fatalf("primitive.Decrypt(ciphertext, []byte(\"invalidAssociatedData\")) err = nil, want error")
+	}
+
+	// Verify that the AEAD primitive returned by client is also able to decrypt.
+	primitive2, err := client.GetAEAD(keyURI)
+	if err != nil {
+		t.Fatalf("client.GetAEAD(keyURI) err = %q, want nil", err)
+	}
+	gotPlaintext2, err := primitive2.Decrypt(ciphertext, associatedData)
+	if err != nil {
+		t.Fatalf("primitive2.Decrypt(ciphertext, associatedData) err = %q, want nil", err)
+	}
+	if !bytes.Equal(gotPlaintext2, plaintext) {
+		t.Fatalf("gotPlaintext2 = %q, want %q", gotPlaintext, plaintext)
+	}
+}
