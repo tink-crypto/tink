fbd77e2d296a2dc126b3ff6ff1acf004fe46ed63
ioannanedelcu <ioannanedelcu@google.com>
2023-07-07 06:40:40 -0700

    Add Parser and Serializer for EciesParameters.
    PiperOrigin-RevId: 546265787

---
+++ java_src/src/main/java/com/google/crypto/tink/hybrid/EciesProtoSerialization.java
@@ -0,0 +1,194 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.hybrid;
+
+import static com.google.crypto.tink.internal.Util.toBytesFromPrintableAscii;
+
+import com.google.crypto.tink.AccessesPartialKey;
+import com.google.crypto.tink.TinkProtoParametersFormat;
+import com.google.crypto.tink.internal.EnumTypeProtoConverter;
+import com.google.crypto.tink.internal.MutableSerializationRegistry;
+import com.google.crypto.tink.internal.ParametersParser;
+import com.google.crypto.tink.internal.ParametersSerializer;
+import com.google.crypto.tink.internal.ProtoParametersSerialization;
+import com.google.crypto.tink.proto.EcPointFormat;
+import com.google.crypto.tink.proto.EciesAeadHkdfKeyFormat;
+import com.google.crypto.tink.proto.EciesAeadHkdfParams;
+import com.google.crypto.tink.proto.EllipticCurveType;
+import com.google.crypto.tink.proto.HashType;
+import com.google.crypto.tink.proto.OutputPrefixType;
+import com.google.crypto.tink.util.Bytes;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.ExtensionRegistryLite;
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.security.GeneralSecurityException;
+
+/** Methods to serialize and parse {@link EciesParameters} objects. */
+@AccessesPartialKey
+@SuppressWarnings("UnnecessarilyFullyQualified") // Fully specifying proto types is more readable
+final class EciesProtoSerialization {
+  private static final String PRIVATE_TYPE_URL =
+      "type.googleapis.com/google.crypto.tink.EciesAeadHkdfPrivateKey";
+  private static final Bytes PRIVATE_TYPE_URL_BYTES = toBytesFromPrintableAscii(PRIVATE_TYPE_URL);
+
+  private static final ParametersSerializer<EciesParameters, ProtoParametersSerialization>
+      PARAMETERS_SERIALIZER =
+          ParametersSerializer.create(
+              EciesProtoSerialization::serializeParameters,
+              EciesParameters.class,
+              ProtoParametersSerialization.class);
+
+  private static final ParametersParser<ProtoParametersSerialization> PARAMETERS_PARSER =
+      ParametersParser.create(
+          EciesProtoSerialization::parseParameters,
+          PRIVATE_TYPE_URL_BYTES,
+          ProtoParametersSerialization.class);
+
+  private static final EnumTypeProtoConverter<OutputPrefixType, EciesParameters.Variant>
+      VARIANT_CONVERTER =
+          EnumTypeProtoConverter.<OutputPrefixType, EciesParameters.Variant>builder()
+              .add(OutputPrefixType.RAW, EciesParameters.Variant.NO_PREFIX)
+              .add(OutputPrefixType.TINK, EciesParameters.Variant.TINK)
+              .add(OutputPrefixType.CRUNCHY, EciesParameters.Variant.CRUNCHY)
+              .build();
+
+  private static final EnumTypeProtoConverter<HashType, EciesParameters.HashType>
+      HASH_TYPE_CONVERTER =
+          EnumTypeProtoConverter.<HashType, EciesParameters.HashType>builder()
+              .add(HashType.SHA1, EciesParameters.HashType.SHA1)
+              .add(HashType.SHA224, EciesParameters.HashType.SHA224)
+              .add(HashType.SHA256, EciesParameters.HashType.SHA256)
+              .add(HashType.SHA384, EciesParameters.HashType.SHA384)
+              .add(HashType.SHA512, EciesParameters.HashType.SHA512)
+              .build();
+
+  private static final EnumTypeProtoConverter<EllipticCurveType, EciesParameters.CurveType>
+      CURVE_TYPE_CONVERTER =
+          EnumTypeProtoConverter.<EllipticCurveType, EciesParameters.CurveType>builder()
+              .add(EllipticCurveType.NIST_P256, EciesParameters.CurveType.NIST_P256)
+              .add(EllipticCurveType.NIST_P384, EciesParameters.CurveType.NIST_P384)
+              .add(EllipticCurveType.NIST_P521, EciesParameters.CurveType.NIST_P521)
+              .add(EllipticCurveType.CURVE25519, EciesParameters.CurveType.X25519)
+              .build();
+
+  private static final EnumTypeProtoConverter<EcPointFormat, EciesParameters.PointFormat>
+      POINT_FORMAT_CONVERTER =
+          EnumTypeProtoConverter.<EcPointFormat, EciesParameters.PointFormat>builder()
+              .add(EcPointFormat.UNCOMPRESSED, EciesParameters.PointFormat.UNCOMPRESSED)
+              .add(EcPointFormat.COMPRESSED, EciesParameters.PointFormat.COMPRESSED)
+              .add(
+                  EcPointFormat.DO_NOT_USE_CRUNCHY_UNCOMPRESSED,
+                  EciesParameters.PointFormat.LEGACY_UNCOMPRESSED)
+              .build();
+
+  /**
+   * Registers previously defined parser/serializer objects into a global, mutable registry.
+   * Registration is public to enable custom configurations.
+   */
+  public static void register() throws GeneralSecurityException {
+    register(MutableSerializationRegistry.globalInstance());
+  }
+
+  /** Registers previously defined parser/serializer objects into a given registry. */
+  public static void register(MutableSerializationRegistry registry)
+      throws GeneralSecurityException {
+    registry.registerParametersSerializer(PARAMETERS_SERIALIZER);
+    registry.registerParametersParser(PARAMETERS_PARSER);
+  }
+
+  private static com.google.crypto.tink.proto.EciesAeadHkdfParams toProtoParameters(
+      EciesParameters parameters) throws GeneralSecurityException {
+    com.google.crypto.tink.proto.EciesHkdfKemParams.Builder kemProtoParamsBuilder =
+        com.google.crypto.tink.proto.EciesHkdfKemParams.newBuilder()
+            .setCurveType(CURVE_TYPE_CONVERTER.toProtoEnum(parameters.getCurveType()))
+            .setHkdfHashType(HASH_TYPE_CONVERTER.toProtoEnum(parameters.getHashType()));
+    if (parameters.getSalt() != null && parameters.getSalt().size() > 0) {
+      kemProtoParamsBuilder.setHkdfSalt(ByteString.copyFrom(parameters.getSalt().toByteArray()));
+    }
+    com.google.crypto.tink.proto.EciesHkdfKemParams kemProtoParams = kemProtoParamsBuilder.build();
+
+    com.google.crypto.tink.proto.EciesAeadDemParams demProtoParams;
+    try {
+      demProtoParams =
+          com.google.crypto.tink.proto.EciesAeadDemParams.newBuilder()
+              .setAeadDem(
+                  com.google.crypto.tink.proto.KeyTemplate.parseFrom(
+                      TinkProtoParametersFormat.serialize(parameters.getDemParameters()),
+                      ExtensionRegistryLite.getEmptyRegistry()))
+              .build();
+    } catch (InvalidProtocolBufferException e) {
+      throw new GeneralSecurityException("Parsing EciesParameters failed: ", e);
+    }
+
+    return com.google.crypto.tink.proto.EciesAeadHkdfParams.newBuilder()
+        .setKemParams(kemProtoParams)
+        .setDemParams(demProtoParams)
+        .setEcPointFormat(POINT_FORMAT_CONVERTER.toProtoEnum(parameters.getPointFormat()))
+        .build();
+  }
+
+  private static EciesParameters fromProtoParameters(
+      OutputPrefixType outputPrefixType, EciesAeadHkdfParams protoParams)
+      throws GeneralSecurityException {
+    com.google.crypto.tink.proto.KeyTemplate aeadKeyTemplate =
+        protoParams.getDemParams().getAeadDem();
+    return EciesParameters.builder()
+        .setVariant(VARIANT_CONVERTER.fromProtoEnum(outputPrefixType))
+        .setCurveType(CURVE_TYPE_CONVERTER.fromProtoEnum(protoParams.getKemParams().getCurveType()))
+        .setHashType(
+            HASH_TYPE_CONVERTER.fromProtoEnum(protoParams.getKemParams().getHkdfHashType()))
+        .setPointFormat(POINT_FORMAT_CONVERTER.fromProtoEnum(protoParams.getEcPointFormat()))
+        .setDemParameters(TinkProtoParametersFormat.parse(aeadKeyTemplate.toByteArray()))
+        .setSalt(Bytes.copyFrom(protoParams.getKemParams().getHkdfSalt().toByteArray()))
+        .build();
+  }
+
+  private static ProtoParametersSerialization serializeParameters(EciesParameters parameters)
+      throws GeneralSecurityException {
+    return ProtoParametersSerialization.create(
+        com.google.crypto.tink.proto.KeyTemplate.newBuilder()
+            .setTypeUrl(PRIVATE_TYPE_URL)
+            .setValue(
+                EciesAeadHkdfKeyFormat.newBuilder()
+                    .setParams(toProtoParameters(parameters))
+                    .build()
+                    .toByteString())
+            .setOutputPrefixType(VARIANT_CONVERTER.toProtoEnum(parameters.getVariant()))
+            .build());
+  }
+
+  private static EciesParameters parseParameters(ProtoParametersSerialization serialization)
+      throws GeneralSecurityException {
+    if (!serialization.getKeyTemplate().getTypeUrl().equals(PRIVATE_TYPE_URL)) {
+      throw new IllegalArgumentException(
+          "Wrong type URL in call to EciesProtoSerialization.parseParameters: "
+              + serialization.getKeyTemplate().getTypeUrl());
+    }
+    EciesAeadHkdfKeyFormat format;
+    try {
+      format =
+          EciesAeadHkdfKeyFormat.parseFrom(
+              serialization.getKeyTemplate().getValue(), ExtensionRegistryLite.getEmptyRegistry());
+    } catch (InvalidProtocolBufferException e) {
+      throw new GeneralSecurityException("Parsing EciesParameters failed: ", e);
+    }
+    return fromProtoParameters(
+        serialization.getKeyTemplate().getOutputPrefixType(), format.getParams());
+  }
+
+  private EciesProtoSerialization() {}
+}
---
+++ java_src/src/test/java/com/google/crypto/tink/hybrid/EciesProtoSerializationTest.java
@@ -0,0 +1,394 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.hybrid;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.crypto.tink.internal.TinkBugException.exceptionIsBug;
+import static com.google.crypto.tink.internal.testing.Asserts.assertEqualWhenValueParsed;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.Parameters;
+import com.google.crypto.tink.aead.AeadConfig;
+import com.google.crypto.tink.aead.XChaCha20Poly1305Parameters;
+import com.google.crypto.tink.internal.MutableSerializationRegistry;
+import com.google.crypto.tink.internal.ProtoParametersSerialization;
+import com.google.crypto.tink.proto.EcPointFormat;
+import com.google.crypto.tink.proto.EciesAeadDemParams;
+import com.google.crypto.tink.proto.EciesAeadHkdfKeyFormat;
+import com.google.crypto.tink.proto.EciesAeadHkdfParams;
+import com.google.crypto.tink.proto.EciesHkdfKemParams;
+import com.google.crypto.tink.proto.EllipticCurveType;
+import com.google.crypto.tink.proto.HashType;
+import com.google.crypto.tink.proto.KeyTemplate;
+import com.google.crypto.tink.proto.OutputPrefixType;
+import com.google.crypto.tink.proto.XChaCha20Poly1305KeyFormat;
+import com.google.crypto.tink.subtle.Hex;
+import com.google.crypto.tink.util.Bytes;
+import com.google.protobuf.ByteString;
+import java.security.GeneralSecurityException;
+import javax.annotation.Nullable;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+/** Unit tests for {@link EciesProtoSerialization}. */
+@RunWith(Theories.class)
+@SuppressWarnings("UnnecessarilyFullyQualified") // Fully specifying proto types is more readable
+public final class EciesProtoSerializationTest {
+  private static final String PRIVATE_TYPE_URL =
+      "type.googleapis.com/google.crypto.tink.EciesAeadHkdfPrivateKey";
+
+  private static final Bytes SALT = Bytes.copyFrom(Hex.decode("2023af"));
+  private static final Parameters DEM_PARAMETERS =
+      exceptionIsBug(() -> XChaCha20Poly1305Parameters.create());
+
+  private static final XChaCha20Poly1305KeyFormat DEM_KEY_FORMAT_PROTO =
+      XChaCha20Poly1305KeyFormat.newBuilder().setVersion(0).build();
+
+  private static final MutableSerializationRegistry registry = new MutableSerializationRegistry();
+
+  private static final class VariantTuple {
+    final EciesParameters.Variant variant;
+    final OutputPrefixType outputPrefixType;
+
+    VariantTuple(
+        EciesParameters.Variant variant,
+        OutputPrefixType outputPrefixType,
+        @Nullable Integer idRequirement) {
+      this.variant = variant;
+      this.outputPrefixType = outputPrefixType;
+    }
+  }
+
+  @DataPoints("variants")
+  public static final VariantTuple[] VARIANTS_TUPLES =
+      new VariantTuple[] {
+        new VariantTuple(
+            EciesParameters.Variant.NO_PREFIX, OutputPrefixType.RAW, /* idRequirement= */ null),
+        new VariantTuple(
+            EciesParameters.Variant.TINK, OutputPrefixType.TINK, /* idRequirement= */ 123),
+        new VariantTuple(
+            EciesParameters.Variant.CRUNCHY, OutputPrefixType.CRUNCHY, /* idRequirement= */ 456),
+      };
+
+  private static final EciesAeadHkdfParams createEciesProtoParams(
+      EllipticCurveType curveType,
+      HashType hashType,
+      EcPointFormat pointFormat,
+      @Nullable ByteString salt,
+      KeyTemplate demKeyTemplate) {
+    EciesHkdfKemParams.Builder kemProtoParamsBuilder =
+        com.google.crypto.tink.proto.EciesHkdfKemParams.newBuilder()
+            .setCurveType(curveType)
+            .setHkdfHashType(hashType);
+
+    if (salt != null) {
+      kemProtoParamsBuilder.setHkdfSalt(salt);
+    }
+
+    EciesHkdfKemParams kemProtoParams = kemProtoParamsBuilder.build();
+
+    EciesAeadDemParams demProtoParams =
+        com.google.crypto.tink.proto.EciesAeadDemParams.newBuilder()
+            .setAeadDem(demKeyTemplate)
+            .build();
+
+    return EciesAeadHkdfParams.newBuilder()
+        .setKemParams(kemProtoParams)
+        .setDemParams(demProtoParams)
+        .setEcPointFormat(pointFormat)
+        .build();
+  }
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    AeadConfig.register();
+    EciesProtoSerialization.register(registry);
+  }
+
+  @Test
+  public void register_calledTwice_succeedsAndSecondCallHasNoEffect() throws Exception {
+    EciesParameters parameters =
+        EciesParameters.builder()
+            .setCurveType(EciesParameters.CurveType.NIST_P256)
+            .setHashType(EciesParameters.HashType.SHA256)
+            .setPointFormat(EciesParameters.PointFormat.UNCOMPRESSED)
+            .setVariant(EciesParameters.Variant.NO_PREFIX)
+            .setDemParameters(DEM_PARAMETERS)
+            .setSalt(SALT)
+            .build();
+
+    EciesAeadHkdfParams protoParams =
+        createEciesProtoParams(
+            EllipticCurveType.NIST_P256,
+            HashType.SHA256,
+            EcPointFormat.UNCOMPRESSED,
+            ByteString.copyFrom(SALT.toByteArray()),
+            KeyTemplate.newBuilder()
+                .setTypeUrl("type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                .setValue(DEM_KEY_FORMAT_PROTO.toByteString())
+                .setOutputPrefixType(OutputPrefixType.RAW)
+                .build());
+    EciesAeadHkdfKeyFormat format =
+        EciesAeadHkdfKeyFormat.newBuilder().setParams(protoParams).build();
+    ProtoParametersSerialization serialization =
+        ProtoParametersSerialization.create(PRIVATE_TYPE_URL, OutputPrefixType.RAW, format);
+
+    MutableSerializationRegistry registry = new MutableSerializationRegistry();
+    assertThat(registry.hasParserForParameters(serialization)).isFalse();
+    assertThat(registry.hasSerializerForParameters(parameters, ProtoParametersSerialization.class))
+        .isFalse();
+
+    EciesProtoSerialization.register(registry);
+
+    assertThat(registry.hasParserForParameters(serialization)).isTrue();
+    assertThat(registry.hasSerializerForParameters(parameters, ProtoParametersSerialization.class))
+        .isTrue();
+
+    EciesProtoSerialization.register(registry);
+
+    assertThat(registry.hasParserForParameters(serialization)).isTrue();
+    assertThat(registry.hasSerializerForParameters(parameters, ProtoParametersSerialization.class))
+        .isTrue();
+  }
+
+  @Theory
+  public void serializeParseParameters(@FromDataPoints("variants") VariantTuple variantTuple)
+      throws Exception {
+    EciesParameters parameters =
+        EciesParameters.builder()
+            .setCurveType(EciesParameters.CurveType.NIST_P256)
+            .setHashType(EciesParameters.HashType.SHA256)
+            .setPointFormat(EciesParameters.PointFormat.UNCOMPRESSED)
+            .setVariant(variantTuple.variant)
+            .setDemParameters(DEM_PARAMETERS)
+            .setSalt(SALT)
+            .build();
+
+    EciesAeadHkdfParams protoParams =
+        createEciesProtoParams(
+            EllipticCurveType.NIST_P256,
+            HashType.SHA256,
+            EcPointFormat.UNCOMPRESSED,
+            ByteString.copyFrom(SALT.toByteArray()),
+            KeyTemplate.newBuilder()
+                .setTypeUrl("type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                .setValue(DEM_KEY_FORMAT_PROTO.toByteString())
+                .setOutputPrefixType(OutputPrefixType.RAW)
+                .build());
+    EciesAeadHkdfKeyFormat format =
+        EciesAeadHkdfKeyFormat.newBuilder().setParams(protoParams).build();
+
+    ProtoParametersSerialization serialization =
+        ProtoParametersSerialization.create(
+            PRIVATE_TYPE_URL, variantTuple.outputPrefixType, format);
+
+    ProtoParametersSerialization serialized =
+        registry.serializeParameters(parameters, ProtoParametersSerialization.class);
+    assertEqualWhenValueParsed(EciesAeadHkdfKeyFormat.parser(), serialized, serialization);
+
+    Parameters parsed = registry.parseParameters(serialization);
+    assertThat(parsed).isEqualTo(parameters);
+  }
+
+  @Theory
+  public void serializeParseParametersX25519() throws Exception {
+    EciesParameters parameters =
+        EciesParameters.builder()
+            .setCurveType(EciesParameters.CurveType.X25519)
+            .setHashType(EciesParameters.HashType.SHA256)
+            .setPointFormat(EciesParameters.PointFormat.COMPRESSED)
+            .setVariant(EciesParameters.Variant.NO_PREFIX)
+            .setDemParameters(DEM_PARAMETERS)
+            .build();
+
+    EciesAeadHkdfParams protoParams =
+        createEciesProtoParams(
+            EllipticCurveType.CURVE25519,
+            HashType.SHA256,
+            EcPointFormat.COMPRESSED,
+            null,
+            KeyTemplate.newBuilder()
+                .setTypeUrl("type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                .setValue(DEM_KEY_FORMAT_PROTO.toByteString())
+                .setOutputPrefixType(OutputPrefixType.RAW)
+                .build());
+    EciesAeadHkdfKeyFormat format =
+        EciesAeadHkdfKeyFormat.newBuilder().setParams(protoParams).build();
+
+    ProtoParametersSerialization serialization =
+        ProtoParametersSerialization.create(PRIVATE_TYPE_URL, OutputPrefixType.RAW, format);
+
+    ProtoParametersSerialization serialized =
+        registry.serializeParameters(parameters, ProtoParametersSerialization.class);
+    assertEqualWhenValueParsed(EciesAeadHkdfKeyFormat.parser(), serialized, serialization);
+
+    Parameters parsed = registry.parseParameters(serialization);
+    assertThat(parsed).isEqualTo(parameters);
+  }
+
+  @DataPoints("invalidParametersSerializations")
+  public static final ProtoParametersSerialization[] INVALID_PARAMETERS_SERIALIZATIONS =
+      new ProtoParametersSerialization[] {
+        // Unknown output prefix.
+        ProtoParametersSerialization.create(
+            PRIVATE_TYPE_URL,
+            OutputPrefixType.UNKNOWN_PREFIX,
+            EciesAeadHkdfKeyFormat.newBuilder()
+                .setParams(
+                    createEciesProtoParams(
+                        EllipticCurveType.NIST_P256,
+                        HashType.SHA256,
+                        EcPointFormat.UNCOMPRESSED,
+                        ByteString.copyFrom(SALT.toByteArray()),
+                        KeyTemplate.newBuilder()
+                            .setTypeUrl(
+                                "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                            .setValue(DEM_KEY_FORMAT_PROTO.toByteString())
+                            .setOutputPrefixType(OutputPrefixType.RAW)
+                            .build()))
+                .build()),
+        // Unknown Curve.
+        ProtoParametersSerialization.create(
+            PRIVATE_TYPE_URL,
+            OutputPrefixType.TINK,
+            EciesAeadHkdfKeyFormat.newBuilder()
+                .setParams(
+                    createEciesProtoParams(
+                        EllipticCurveType.UNKNOWN_CURVE,
+                        HashType.SHA256,
+                        EcPointFormat.UNCOMPRESSED,
+                        ByteString.copyFrom(SALT.toByteArray()),
+                        KeyTemplate.newBuilder()
+                            .setTypeUrl(
+                                "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                            .setValue(DEM_KEY_FORMAT_PROTO.toByteString())
+                            .setOutputPrefixType(OutputPrefixType.RAW)
+                            .build()))
+                .build()),
+        // Unknown HashType.
+        ProtoParametersSerialization.create(
+            PRIVATE_TYPE_URL,
+            OutputPrefixType.TINK,
+            EciesAeadHkdfKeyFormat.newBuilder()
+                .setParams(
+                    createEciesProtoParams(
+                        EllipticCurveType.NIST_P256,
+                        HashType.UNKNOWN_HASH,
+                        EcPointFormat.UNCOMPRESSED,
+                        ByteString.copyFrom(SALT.toByteArray()),
+                        KeyTemplate.newBuilder()
+                            .setTypeUrl(
+                                "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                            .setValue(DEM_KEY_FORMAT_PROTO.toByteString())
+                            .setOutputPrefixType(OutputPrefixType.RAW)
+                            .build()))
+                .build()),
+        // Unknown Point Format.
+        ProtoParametersSerialization.create(
+            PRIVATE_TYPE_URL,
+            OutputPrefixType.TINK,
+            EciesAeadHkdfKeyFormat.newBuilder()
+                .setParams(
+                    createEciesProtoParams(
+                        EllipticCurveType.NIST_P256,
+                        HashType.SHA256,
+                        EcPointFormat.UNKNOWN_FORMAT,
+                        ByteString.copyFrom(SALT.toByteArray()),
+                        KeyTemplate.newBuilder()
+                            .setTypeUrl(
+                                "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                            .setValue(DEM_KEY_FORMAT_PROTO.toByteString())
+                            .setOutputPrefixType(OutputPrefixType.RAW)
+                            .build()))
+                .build()),
+        // Bad dem key template Type URL.
+        ProtoParametersSerialization.create(
+            PRIVATE_TYPE_URL,
+            OutputPrefixType.TINK,
+            EciesAeadHkdfKeyFormat.newBuilder()
+                .setParams(
+                    createEciesProtoParams(
+                        EllipticCurveType.NIST_P256,
+                        HashType.SHA256,
+                        EcPointFormat.UNCOMPRESSED,
+                        ByteString.copyFrom(SALT.toByteArray()),
+                        KeyTemplate.newBuilder()
+                            .setTypeUrl("Non Existent Type Url")
+                            .setValue(DEM_KEY_FORMAT_PROTO.toByteString())
+                            .setOutputPrefixType(OutputPrefixType.RAW)
+                            .build()))
+                .build()),
+        // Bad dem key template value.
+        ProtoParametersSerialization.create(
+            PRIVATE_TYPE_URL,
+            OutputPrefixType.TINK,
+            EciesAeadHkdfKeyFormat.newBuilder()
+                .setParams(
+                    createEciesProtoParams(
+                        EllipticCurveType.NIST_P256,
+                        HashType.SHA256,
+                        EcPointFormat.UNCOMPRESSED,
+                        ByteString.copyFrom(SALT.toByteArray()),
+                        KeyTemplate.newBuilder()
+                            .setTypeUrl(
+                                "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                            .setValue(ByteString.copyFrom(new byte[] {(byte) 0x80}))
+                            .setOutputPrefixType(OutputPrefixType.RAW)
+                            .build()))
+                .build()),
+        // Unknown dem key template prefix.
+        ProtoParametersSerialization.create(
+            PRIVATE_TYPE_URL,
+            OutputPrefixType.TINK,
+            EciesAeadHkdfKeyFormat.newBuilder()
+                .setParams(
+                    createEciesProtoParams(
+                        EllipticCurveType.NIST_P256,
+                        HashType.SHA256,
+                        EcPointFormat.UNCOMPRESSED,
+                        ByteString.copyFrom(SALT.toByteArray()),
+                        KeyTemplate.newBuilder()
+                            .setTypeUrl(
+                                "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                            .setValue(DEM_KEY_FORMAT_PROTO.toByteString())
+                            .setOutputPrefixType(OutputPrefixType.UNKNOWN_PREFIX)
+                            .build()))
+                .build()),
+        // Proto messages start with a VarInt, which always ends with a byte with most
+        // significant bit unset. 0x80 is hence invalid.
+        ProtoParametersSerialization.create(
+            KeyTemplate.newBuilder()
+                .setTypeUrl(PRIVATE_TYPE_URL)
+                .setOutputPrefixType(OutputPrefixType.RAW)
+                .setValue(ByteString.copyFrom(new byte[] {(byte) 0x80}))
+                .build()),
+      };
+
+  @Theory
+  public void parseInvalidParameters_fails(
+      @FromDataPoints("invalidParametersSerializations")
+          ProtoParametersSerialization serializedParameters)
+      throws Exception {
+    assertThrows(
+        GeneralSecurityException.class, () -> registry.parseParameters(serializedParameters));
+  }
+}
