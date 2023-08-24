d15c647370a9e866aef2344aac848cc554eece30
wconner <wconner@google.com>
2023-08-24 06:49:07 -0700

    Add HPKE C++ parameters type.
    PiperOrigin-RevId: 559738782

---
+++ cc/hybrid/hpke_parameters.cc
@@ -0,0 +1,103 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/hybrid/hpke_parameters.h"
+
+#include <set>
+
+#include "absl/status/status.h"
+#include "tink/parameters.h"
+#include "tink/util/status.h"
+#include "tink/util/statusor.h"
+
+namespace crypto {
+namespace tink {
+
+HpkeParameters::Builder& HpkeParameters::Builder::SetKemId(KemId kem_id) {
+  kem_id_ = kem_id;
+  return *this;
+}
+
+HpkeParameters::Builder& HpkeParameters::Builder::SetKdfId(KdfId kdf_id) {
+  kdf_id_ = kdf_id;
+  return *this;
+}
+
+HpkeParameters::Builder& HpkeParameters::Builder::SetAeadId(AeadId aead_id) {
+  aead_id_ = aead_id;
+  return *this;
+}
+
+HpkeParameters::Builder& HpkeParameters::Builder::SetVariant(Variant variant) {
+  variant_ = variant;
+  return *this;
+}
+
+util::StatusOr<HpkeParameters> HpkeParameters::Builder::Build() {
+  static const std::set<KemId>* supported_kem_ids = new std::set<KemId>(
+      {KemId::kDhkemP256HkdfSha256, KemId::kDhkemP384HkdfSha384,
+       KemId::kDhkemP521HkdfSha512, KemId::kDhkemX25519HkdfSha256});
+  if (supported_kem_ids->find(kem_id_) == supported_kem_ids->end()) {
+    return util::Status(absl::StatusCode::kInvalidArgument,
+                        "Cannot create HPKE parameters with unknown KEM ID.");
+  }
+
+  static const std::set<KdfId>* supported_kdf_ids = new std::set<KdfId>(
+      {KdfId::kHkdfSha256, KdfId::kHkdfSha384, KdfId::kHkdfSha512});
+  if (supported_kdf_ids->find(kdf_id_) == supported_kdf_ids->end()) {
+    return util::Status(absl::StatusCode::kInvalidArgument,
+                        "Cannot create HPKE parameters with unknown KDF ID.");
+  }
+
+  static const std::set<AeadId>* supported_aead_ids = new std::set<AeadId>(
+      {AeadId::kAesGcm128, AeadId::kAesGcm256, AeadId::kChaChaPoly1305});
+  if (supported_aead_ids->find(aead_id_) == supported_aead_ids->end()) {
+    return util::Status(absl::StatusCode::kInvalidArgument,
+                        "Cannot create HPKE parameters with unknown AEAD ID.");
+  }
+
+  static const std::set<Variant>* supported_variants = new std::set<Variant>(
+      {Variant::kTink, Variant::kCrunchy, Variant::kNoPrefix});
+  if (supported_variants->find(variant_) == supported_variants->end()) {
+    return util::Status(absl::StatusCode::kInvalidArgument,
+                        "Cannot create HPKE parameters with unknown variant.");
+  }
+
+  return HpkeParameters(kem_id_, kdf_id_, aead_id_, variant_);
+}
+
+bool HpkeParameters::operator==(const Parameters& other) const {
+  const HpkeParameters* that = dynamic_cast<const HpkeParameters*>(&other);
+  if (that == nullptr) {
+    return false;
+  }
+  if (kem_id_ != that->kem_id_) {
+    return false;
+  }
+  if (kdf_id_ != that->kdf_id_) {
+    return false;
+  }
+  if (aead_id_ != that->aead_id_) {
+    return false;
+  }
+  if (variant_ != that->variant_) {
+    return false;
+  }
+  return true;
+}
+
+}  // namespace tink
+}  // namespace crypto
---
+++ cc/hybrid/hpke_parameters_test.cc
@@ -0,0 +1,341 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/hybrid/hpke_parameters.h"
+
+#include <tuple>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/status/status.h"
+#include "tink/util/statusor.h"
+#include "tink/util/test_matchers.h"
+
+namespace crypto {
+namespace tink {
+namespace {
+
+using ::crypto::tink::test::IsOk;
+using ::crypto::tink::test::StatusIs;
+using ::testing::Combine;
+using ::testing::Eq;
+using ::testing::IsTrue;
+using ::testing::TestWithParam;
+using ::testing::Values;
+
+struct VariantWithIdRequirement {
+  HpkeParameters::Variant variant;
+  bool has_id_requirement;
+};
+
+using HpkeParametersTest =
+    TestWithParam<std::tuple<HpkeParameters::KemId, HpkeParameters::KdfId,
+                             HpkeParameters::AeadId, VariantWithIdRequirement>>;
+
+INSTANTIATE_TEST_SUITE_P(
+    HpkeParametersTestSuite, HpkeParametersTest,
+    Combine(Values(HpkeParameters::KemId::kDhkemP256HkdfSha256,
+                   HpkeParameters::KemId::kDhkemP384HkdfSha384,
+                   HpkeParameters::KemId::kDhkemP521HkdfSha512,
+                   HpkeParameters::KemId::kDhkemX25519HkdfSha256),
+            Values(HpkeParameters::KdfId::kHkdfSha256,
+                   HpkeParameters::KdfId::kHkdfSha384,
+                   HpkeParameters::KdfId::kHkdfSha512),
+            Values(HpkeParameters::AeadId::kAesGcm128,
+                   HpkeParameters::AeadId::kAesGcm256,
+                   HpkeParameters::AeadId::kChaChaPoly1305),
+            Values(VariantWithIdRequirement{HpkeParameters::Variant::kTink,
+                                            /*has_id_requirement=*/true},
+                   VariantWithIdRequirement{HpkeParameters::Variant::kCrunchy,
+                                            /*has_id_requirement=*/true},
+                   VariantWithIdRequirement{HpkeParameters::Variant::kNoPrefix,
+                                            /*has_id_requirement=*/false})));
+
+TEST_P(HpkeParametersTest, Build) {
+  HpkeParameters::KemId kem_id;
+  HpkeParameters::KdfId kdf_id;
+  HpkeParameters::AeadId aead_id;
+  VariantWithIdRequirement variant;
+  std::tie(kem_id, kdf_id, aead_id, variant) = GetParam();
+
+  util::StatusOr<HpkeParameters> parameters = HpkeParameters::Builder()
+                                                  .SetVariant(variant.variant)
+                                                  .SetKemId(kem_id)
+                                                  .SetKdfId(kdf_id)
+                                                  .SetAeadId(aead_id)
+                                                  .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  EXPECT_THAT(parameters->GetKemId(), Eq(kem_id));
+  EXPECT_THAT(parameters->GetKdfId(), Eq(kdf_id));
+  EXPECT_THAT(parameters->GetAeadId(), Eq(aead_id));
+  EXPECT_THAT(parameters->GetVariant(), Eq(variant.variant));
+  EXPECT_THAT(parameters->HasIdRequirement(), Eq(variant.has_id_requirement));
+}
+
+TEST(HpkeParametersTest, BuildWithInvalidVariantFails) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::
+                          kDoNotUseInsteadUseDefaultWhenWritingSwitchStatements)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  EXPECT_THAT(parameters.status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(HpkeParametersTest, BuildWithoutVariantFails) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  EXPECT_THAT(parameters.status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(HpkeParametersTest, BuildWithInvalidKemIdFails) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::
+                        kDoNotUseInsteadUseDefaultWhenWritingSwitchStatements)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  EXPECT_THAT(parameters.status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(HpkeParametersTest, BuildWithoutKemIdFails) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  EXPECT_THAT(parameters.status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(HpkeParametersTest, BuildWithInvalidKdfIdFails) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::
+                        kDoNotUseInsteadUseDefaultWhenWritingSwitchStatements)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  EXPECT_THAT(parameters.status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(HpkeParametersTest, BuildWithoutKdfIdFails) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  EXPECT_THAT(parameters.status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(HpkeParametersTest, BuildWithInvalidAeadIdFails) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::
+                         kDoNotUseInsteadUseDefaultWhenWritingSwitchStatements)
+          .Build();
+  EXPECT_THAT(parameters.status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(HpkeParametersTest, BuildWithoutAeadIdFails) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .Build();
+  EXPECT_THAT(parameters.status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(Ed25519ParametersTest, CopyConstructor) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  HpkeParameters copy(*parameters);
+
+  EXPECT_THAT(copy.GetVariant(), Eq(HpkeParameters::Variant::kTink));
+  EXPECT_THAT(copy.HasIdRequirement(), IsTrue());
+}
+
+TEST(Ed25519ParametersTest, CopyAssignment) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  HpkeParameters copy = *parameters;
+
+  EXPECT_THAT(copy.GetVariant(), Eq(HpkeParameters::Variant::kTink));
+  EXPECT_THAT(copy.HasIdRequirement(), IsTrue());
+}
+
+TEST_P(HpkeParametersTest, ParametersEquals) {
+  HpkeParameters::KemId kem_id;
+  HpkeParameters::KdfId kdf_id;
+  HpkeParameters::AeadId aead_id;
+  VariantWithIdRequirement variant;
+  std::tie(kem_id, kdf_id, aead_id, variant) = GetParam();
+
+  util::StatusOr<HpkeParameters> parameters = HpkeParameters::Builder()
+                                                  .SetVariant(variant.variant)
+                                                  .SetKemId(kem_id)
+                                                  .SetKdfId(kdf_id)
+                                                  .SetAeadId(aead_id)
+                                                  .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  util::StatusOr<HpkeParameters> other_parameters =
+      HpkeParameters::Builder()
+          .SetVariant(variant.variant)
+          .SetKemId(kem_id)
+          .SetKdfId(kdf_id)
+          .SetAeadId(aead_id)
+          .Build();
+  ASSERT_THAT(other_parameters, IsOk());
+
+  EXPECT_TRUE(*parameters == *other_parameters);
+  EXPECT_TRUE(*other_parameters == *parameters);
+  EXPECT_FALSE(*parameters != *other_parameters);
+  EXPECT_FALSE(*other_parameters != *parameters);
+}
+
+TEST(HpkeParametersTest, VariantNotEqual) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  util::StatusOr<HpkeParameters> other_parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kCrunchy)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  ASSERT_THAT(other_parameters, IsOk());
+
+  EXPECT_TRUE(*parameters != *other_parameters);
+  EXPECT_FALSE(*parameters == *other_parameters);
+}
+
+TEST(HpkeParametersTest, KemIdNotEqual) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  util::StatusOr<HpkeParameters> other_parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemP256HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  ASSERT_THAT(other_parameters, IsOk());
+
+  EXPECT_TRUE(*parameters != *other_parameters);
+  EXPECT_FALSE(*parameters == *other_parameters);
+}
+
+TEST(HpkeParametersTest, KdfIdNotEqual) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha384)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  util::StatusOr<HpkeParameters> other_parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  ASSERT_THAT(other_parameters, IsOk());
+
+  EXPECT_TRUE(*parameters != *other_parameters);
+  EXPECT_FALSE(*parameters == *other_parameters);
+}
+
+TEST(HpkeParametersTest, AeadIdNotEqual) {
+  util::StatusOr<HpkeParameters> parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kAesGcm128)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  util::StatusOr<HpkeParameters> other_parameters =
+      HpkeParameters::Builder()
+          .SetVariant(HpkeParameters::Variant::kTink)
+          .SetKemId(HpkeParameters::KemId::kDhkemX25519HkdfSha256)
+          .SetKdfId(HpkeParameters::KdfId::kHkdfSha256)
+          .SetAeadId(HpkeParameters::AeadId::kChaChaPoly1305)
+          .Build();
+  ASSERT_THAT(other_parameters, IsOk());
+
+  EXPECT_TRUE(*parameters != *other_parameters);
+  EXPECT_FALSE(*parameters == *other_parameters);
+}
+
+}  // namespace
+}  // namespace tink
+}  // namespace crypto
