1799b1573d60fdb112c74008dd613805218f564b
tholenst <tholenst@google.com>
2023-09-12 01:20:19 -0700

    Add a MutableParametersRegistry.
    This will be used to get rid of KeyTypeManagers: instead of registering the parameters in the Registry internally, in each key manager class we just register these parameters.
    
    PiperOrigin-RevId: 564639556

---
+++ java_src/src/test/java/com/google/crypto/tink/internal/MutableParametersRegistryTest.java
@@ -0,0 +1,74 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.internal;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.aead.PredefinedAeadParameters;
+import java.security.GeneralSecurityException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class MutableParametersRegistryTest {
+  @Test
+  public void putAndGet_works() throws Exception {
+    MutableParametersRegistry registry = new MutableParametersRegistry();
+    registry.put("AES_128GCM", PredefinedAeadParameters.AES128_GCM);
+    assertThat(registry.get("AES_128GCM")).isEqualTo(PredefinedAeadParameters.AES128_GCM);
+  }
+
+  @Test
+  public void putAndGet_multipleElements_works() throws Exception {
+    MutableParametersRegistry registry = new MutableParametersRegistry();
+    registry.put("AES_128GCM", PredefinedAeadParameters.AES128_GCM);
+    registry.put("AES_256GCM", PredefinedAeadParameters.AES256_GCM);
+    assertThat(registry.get("AES_128GCM")).isEqualTo(PredefinedAeadParameters.AES128_GCM);
+    assertThat(registry.get("AES_256GCM")).isEqualTo(PredefinedAeadParameters.AES256_GCM);
+  }
+
+  @Test
+  public void putAndGet_sameElementMultipleTimes_works() throws Exception {
+    MutableParametersRegistry registry = new MutableParametersRegistry();
+    registry.put("AES_128GCM", PredefinedAeadParameters.AES128_GCM);
+    registry.put("AES_256GCM", PredefinedAeadParameters.AES256_GCM);
+    registry.put("AES_128GCM", PredefinedAeadParameters.AES128_GCM);
+    registry.put("AES_256GCM", PredefinedAeadParameters.AES256_GCM);
+    registry.put("AES_128GCM", PredefinedAeadParameters.AES128_GCM);
+    registry.put("AES_256GCM", PredefinedAeadParameters.AES256_GCM);
+    assertThat(registry.get("AES_128GCM")).isEqualTo(PredefinedAeadParameters.AES128_GCM);
+    assertThat(registry.get("AES_256GCM")).isEqualTo(PredefinedAeadParameters.AES256_GCM);
+  }
+
+  @Test
+  public void getNonexistentKey_throws() throws Exception {
+    MutableParametersRegistry registry = new MutableParametersRegistry();
+    registry.put("AES_128GCM", PredefinedAeadParameters.AES128_GCM);
+    assertThrows(GeneralSecurityException.class, () -> registry.get("AES_256GCM"));
+  }
+
+  @Test
+  public void putAndGet_sameKeyDifferentValue_throws() throws Exception {
+    MutableParametersRegistry registry = new MutableParametersRegistry();
+    registry.put("AES_128GCM", PredefinedAeadParameters.AES128_GCM);
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> registry.put("AES_128GCM", PredefinedAeadParameters.AES256_GCM));
+  }
+}
