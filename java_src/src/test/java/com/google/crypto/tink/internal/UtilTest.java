e591b23cc22a2abe5ffdd99a3ad528965404d4c8
tholenst <tholenst@google.com>
2023-09-18 03:33:17 -0700

    Add an internal utility function to read from an input stream to SecretBytes.
    This is very useful for key derivation with the planned changes.
    
    PiperOrigin-RevId: 566250256

--- java_src/src/test/java/com/google/crypto/tink/internal/UtilTest.java
+++ java_src/src/test/java/com/google/crypto/tink/internal/UtilTest.java
@@ -21,7 +21,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
+import com.google.crypto.tink.InsecureSecretKeyAccess;
 import com.google.crypto.tink.util.Bytes;
+import com.google.crypto.tink.util.SecretBytes;
+import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.util.stream.IntStream;
 import org.junit.Test;
@@ -109,5 +112,61 @@ public final class UtilTest {
     assertFalse(Util.isPrefix(new byte[] {5, 7, 9, 10}, new byte[] {5, 7, 9}));
     assertFalse(Util.isPrefix(new byte[] {5, 7, 10}, new byte[] {5, 7, 9}));
     assertFalse(Util.isPrefix(new byte[] {1}, new byte[] {}));
+  }
+
+  @Test
+  public void readIntoSecretBytes_works() throws Exception {
+    InputStream fragmentedInputStream =
+        new InputStream() {
+          byte nextNumber = 4;
+
+          @Override
+          public int read() {
+            return 0;
+          }
+
+          @Override
+          public int read(byte[] b, int off, int len) {
+            b[off] = nextNumber;
+            nextNumber++;
+            return 1;
+          }
+        };
+    SecretBytes result =
+        Util.readIntoSecretBytes(fragmentedInputStream, 4, InsecureSecretKeyAccess.get());
+
+    assertThat(result.toByteArray(InsecureSecretKeyAccess.get()))
+        .isEqualTo(new byte[] {4, 5, 6, 7});
+
+    result = Util.readIntoSecretBytes(fragmentedInputStream, 4, InsecureSecretKeyAccess.get());
+    assertThat(result.toByteArray(InsecureSecretKeyAccess.get()))
+        .isEqualTo(new byte[] {8, 9, 10, 11});
+  }
+
+  @Test
+  public void readIntoSecretBytes_throwsOnNotEnoughPseudorandomness() throws Exception {
+    byte randomness = 4;
+    InputStream shortInputStream =
+        new InputStream() {
+          int numReads = 3;
+
+          @Override
+          public int read() {
+            return 0;
+          }
+
+          @Override
+          public int read(byte[] b, int off, int len) {
+            if (numReads == 0) {
+              return -1;
+            }
+            --numReads;
+            b[off] = randomness;
+            return 1;
+          }
+        };
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> Util.readIntoSecretBytes(shortInputStream, 4, InsecureSecretKeyAccess.get()));
   }
 }
