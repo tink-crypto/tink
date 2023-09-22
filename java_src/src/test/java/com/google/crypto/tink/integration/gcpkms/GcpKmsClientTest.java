578a14cc71b9d48884a98637e301d4233d7b7f90
ambrosin <ambrosin@google.com>
2023-09-22 07:05:01 -0700

    Use the new KmsEnvelopeAead.create API in GcpKmsClientTest
    This was introduced in tink-java@1.11.0.
    
    PiperOrigin-RevId: 567611894

--- java_src/src/test/java/com/google/crypto/tink/integration/gcpkms/GcpKmsClientTest.java
+++ java_src/src/test/java/com/google/crypto/tink/integration/gcpkms/GcpKmsClientTest.java
@@ -30,7 +30,9 @@ import com.google.crypto.tink.KmsClients;
 import com.google.crypto.tink.KmsClientsTestUtil;
 import com.google.crypto.tink.aead.AeadConfig;
 import com.google.crypto.tink.aead.KmsAeadKeyManager;
+import com.google.crypto.tink.aead.KmsEnvelopeAead;
 import com.google.crypto.tink.aead.KmsEnvelopeAeadKeyManager;
+import com.google.crypto.tink.aead.PredefinedAeadParameters;
 import java.security.GeneralSecurityException;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -108,14 +110,10 @@ public final class GcpKmsClientTest {
     String keyUri =
         "gcp-kms://projects/tink-test/locations/global/keyRings/unit-test/cryptoKeys/aead-key";
 
-    // Register a client bound to a single key.
-    registerGcpKmsClient(keyUri, new FakeCloudKms(asList(keyId)));
-
-    // Create an envelope encryption AEAD primitive
-    KeyTemplate dekTemplate = KeyTemplates.get("AES128_CTR_HMAC_SHA256_RAW");
-    KeyTemplate envelopeTemplate = KmsEnvelopeAeadKeyManager.createKeyTemplate(keyUri, dekTemplate);
-    KeysetHandle handle = KeysetHandle.generateNew(envelopeTemplate);
-    Aead kmsEnvelopeAead = handle.getPrimitive(Aead.class);
+    KmsClient client = new GcpKmsClient(keyUri).withCloudKms(new FakeCloudKms(asList(keyId)));
+    Aead kmsEnvelopeAead =
+        KmsEnvelopeAead.create(
+            PredefinedAeadParameters.AES128_CTR_HMAC_SHA256, client.getAead(keyUri));
 
     byte[] plaintext = "plaintext".getBytes(UTF_8);
     byte[] associatedData = "associatedData".getBytes(UTF_8);
@@ -262,7 +260,8 @@ public final class GcpKmsClientTest {
     byte[] ciphertext = kmsAead.encrypt(plaintext, associatedData);
 
     Aead kmsAeadWithInvalidUri = client.getAead(invalidUri);
-    assertThrows(IllegalArgumentException.class,
+    assertThrows(
+        IllegalArgumentException.class,
         () -> kmsAeadWithInvalidUri.decrypt(ciphertext, associatedData));
   }
 }
