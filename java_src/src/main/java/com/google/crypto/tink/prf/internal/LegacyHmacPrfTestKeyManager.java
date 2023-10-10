66928eb9e0971f156a5877a6b3a0b90868130117
lizatretyakova <lizatretyakova@google.com>
2023-10-10 06:24:16 -0700

    Remove redundant methods in LegacyHmacPrfTestKeyManager, and add tests to ensure its correctness.
    PiperOrigin-RevId: 572227434

--- java_src/src/main/java/com/google/crypto/tink/prf/internal/LegacyHmacPrfTestKeyManager.java
+++ java_src/src/main/java/com/google/crypto/tink/prf/internal/LegacyHmacPrfTestKeyManager.java
@@ -21,14 +21,11 @@ import com.google.crypto.tink.Registry;
 import com.google.crypto.tink.prf.Prf;
 import com.google.crypto.tink.proto.HashType;
 import com.google.crypto.tink.proto.HmacPrfKey;
-import com.google.crypto.tink.proto.HmacPrfParams;
 import com.google.crypto.tink.proto.KeyData;
 import com.google.crypto.tink.subtle.PrfHmacJce;
-import com.google.crypto.tink.subtle.Validators;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.ExtensionRegistryLite;
 import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.MessageLite;
 import java.security.GeneralSecurityException;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -39,9 +36,6 @@ import javax.crypto.spec.SecretKeySpec;
 public class LegacyHmacPrfTestKeyManager implements KeyManager<Prf> {
   /** Type url that this manager does support. */
   public static final String TYPE_URL = "type.googleapis.com/google.crypto.tink.HmacPrfKey";
-
-  /** Current version of this key manager. Keys with version equal or smaller are supported. */
-  private static final int VERSION = 0;
 
   /** Minimum key size in bytes. */
   private static final int MIN_KEY_SIZE_IN_BYTES = 16;
@@ -54,64 +48,32 @@ public class LegacyHmacPrfTestKeyManager implements KeyManager<Prf> {
     try {
       HmacPrfKey keyProto =
           HmacPrfKey.parseFrom(serializedKey, ExtensionRegistryLite.getEmptyRegistry());
-      return getPrimitive(keyProto);
+      validate(keyProto);
+      HashType hash = keyProto.getParams().getHash();
+      byte[] keyValue = keyProto.getKeyValue().toByteArray();
+      SecretKeySpec keySpec = new SecretKeySpec(keyValue, "HMAC");
+      switch (hash) {
+        case SHA1:
+          return new PrfHmacJce("HMACSHA1", keySpec);
+        case SHA224:
+          return new PrfHmacJce("HMACSHA224", keySpec);
+        case SHA256:
+          return new PrfHmacJce("HMACSHA256", keySpec);
+        case SHA384:
+          return new PrfHmacJce("HMACSHA384", keySpec);
+        case SHA512:
+          return new PrfHmacJce("HMACSHA512", keySpec);
+        default:
+          throw new GeneralSecurityException("unknown hash");
+      }
     } catch (InvalidProtocolBufferException e) {
       throw new GeneralSecurityException("expected serialized HmacPrfKey proto", e);
     }
   }
 
-  /**
-   * @param key {@code HmacPrfKey} proto
-   */
-  @Override
-  public Prf getPrimitive(MessageLite key) throws GeneralSecurityException {
-    if (!(key instanceof HmacPrfKey)) {
-      throw new GeneralSecurityException("Expected HmacPrfKey proto");
-    }
-    HmacPrfKey keyProto = (HmacPrfKey) key;
-    validate(keyProto);
-    HashType hash = keyProto.getParams().getHash();
-    byte[] keyValue = keyProto.getKeyValue().toByteArray();
-    SecretKeySpec keySpec = new SecretKeySpec(keyValue, "HMAC");
-    switch (hash) {
-      case SHA1:
-        return new PrfHmacJce("HMACSHA1", keySpec);
-      case SHA224:
-        return new PrfHmacJce("HMACSHA224", keySpec);
-      case SHA256:
-        return new PrfHmacJce("HMACSHA256", keySpec);
-      case SHA384:
-        return new PrfHmacJce("HMACSHA384", keySpec);
-      case SHA512:
-        return new PrfHmacJce("HMACSHA512", keySpec);
-      default:
-        throw new GeneralSecurityException("unknown hash");
-    }
-  }
-
-  @Override
-  public MessageLite newKey(ByteString serializedKeyFormat) throws GeneralSecurityException {
-    throw new UnsupportedOperationException("not needed for tests");
-  }
-
-  @Override
-  public MessageLite newKey(MessageLite keyFormat) throws GeneralSecurityException {
-    throw new UnsupportedOperationException("not needed for tests");
-  }
-
-  @Override
-  public boolean doesSupport(String typeUrl) {
-    return typeUrl.equals(TYPE_URL);
-  }
-
   @Override
   public String getKeyType() {
     return TYPE_URL;
-  }
-
-  @Override
-  public int getVersion() {
-    return VERSION;
   }
 
   @Override
@@ -125,15 +87,10 @@ public class LegacyHmacPrfTestKeyManager implements KeyManager<Prf> {
   }
 
   private void validate(HmacPrfKey key) throws GeneralSecurityException {
-    Validators.validateVersion(key.getVersion(), VERSION);
     if (key.getKeyValue().size() < MIN_KEY_SIZE_IN_BYTES) {
       throw new GeneralSecurityException("key too short");
     }
-    validate(key.getParams());
-  }
-
-  private void validate(HmacPrfParams params) throws GeneralSecurityException {
-    switch (params.getHash()) {
+    switch (key.getParams().getHash()) {
       case SHA1:
       case SHA224:
       case SHA256:
