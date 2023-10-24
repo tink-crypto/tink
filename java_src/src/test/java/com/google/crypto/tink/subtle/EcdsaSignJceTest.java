77fdcc280137862d98fb498857d9d1aa7920ace7
tholenst <tholenst@google.com>
2023-10-23 17:16:20 -0700

    Add EcdsaSign/Verify#create.
    PiperOrigin-RevId: 575972559

--- java_src/src/test/java/com/google/crypto/tink/subtle/EcdsaSignJceTest.java
+++ java_src/src/test/java/com/google/crypto/tink/subtle/EcdsaSignJceTest.java
@@ -20,8 +20,14 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
+import com.google.crypto.tink.PublicKeySign;
+import com.google.crypto.tink.PublicKeyVerify;
 import com.google.crypto.tink.config.TinkFips;
 import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.signature.EcdsaPrivateKey;
+import com.google.crypto.tink.signature.EcdsaPublicKey;
+import com.google.crypto.tink.signature.internal.testing.EcdsaTestUtil;
+import com.google.crypto.tink.signature.internal.testing.SignatureTestVector;
 import com.google.crypto.tink.subtle.EllipticCurves.EcdsaEncoding;
 import com.google.crypto.tink.subtle.Enums.HashType;
 import com.google.crypto.tink.testing.TestUtil;
@@ -37,13 +43,14 @@ import org.conscrypt.Conscrypt;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-/**
- * Unit tests for EcdsaSignJce.
- */
-@RunWith(JUnit4.class)
+/** Unit tests for EcdsaSignJce. */
+@RunWith(Theories.class)
 public class EcdsaSignJceTest {
 
   @Before
@@ -150,4 +157,37 @@ public class EcdsaSignJceTest {
             new EcdsaSignJce(
                 (ECPrivateKey) keyPair.getPrivate(), HashType.SHA256, EcdsaEncoding.DER));
   }
+
+  @Theory
+  public void test_validateSignatureInTestVector(
+      @FromDataPoints("allTests") SignatureTestVector testVector) throws Exception {
+    PublicKeyVerify verifier =
+        EcdsaVerifyJce.create((EcdsaPublicKey) testVector.getPrivateKey().getPublicKey());
+    verifier.verify(testVector.getSignature(), testVector.getMessage());
+  }
+
+  @Theory
+  public void test_computeAndValidateFreshSignatureWithTestVector(
+      @FromDataPoints("allTests") SignatureTestVector testVector) throws Exception {
+    PublicKeySign signer = EcdsaSignJce.create((EcdsaPrivateKey) testVector.getPrivateKey());
+    byte[] signature = signer.sign(testVector.getMessage());
+    PublicKeyVerify verifier =
+        EcdsaVerifyJce.create((EcdsaPublicKey) testVector.getPrivateKey().getPublicKey());
+    verifier.verify(signature, testVector.getMessage());
+  }
+
+  @Theory
+  public void test_validateSignatureInTestVectorWithWrongMessage_throws(
+      @FromDataPoints("allTests") SignatureTestVector testVector) throws Exception {
+    PublicKeyVerify verifier =
+        EcdsaVerifyJce.create((EcdsaPublicKey) testVector.getPrivateKey().getPublicKey());
+    byte[] modifiedMessage = Bytes.concat(testVector.getMessage(), new byte[] {0x01});
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> verifier.verify(testVector.getSignature(), modifiedMessage));
+  }
+
+  @DataPoints("allTests")
+  public static final SignatureTestVector[] ALL_TEST_VECTORS =
+      EcdsaTestUtil.createEcdsaTestVectors();
 }
