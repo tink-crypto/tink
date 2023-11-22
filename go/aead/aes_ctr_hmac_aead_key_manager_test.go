27665d763ee818f7021a3c7be161c3eed6f328eb
ckl <ckl@google.com>
2023-11-22 09:35:58 -0800

    aead: Use Get* methods for chained proto field access.
    PiperOrigin-RevId: 584652165

--- go/aead/aes_ctr_hmac_aead_key_manager_test.go
+++ go/aead/aes_ctr_hmac_aead_key_manager_test.go
@@ -23,11 +23,14 @@ import (
 	"github.com/google/tink/go/aead"
 	"github.com/google/tink/go/core/registry"
 	"github.com/google/tink/go/testutil"
+	ctrpb "github.com/google/tink/go/proto/aes_ctr_go_proto"
+	aeadpb "github.com/google/tink/go/proto/aes_ctr_hmac_aead_go_proto"
 	ctrhmacpb "github.com/google/tink/go/proto/aes_ctr_hmac_aead_go_proto"
-	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
+	commonpb "github.com/google/tink/go/proto/common_go_proto"
+	hmacpb "github.com/google/tink/go/proto/hmac_go_proto"
 )
 
-func TestNewKeyMultipleTimes(t *testing.T) {
+func TestAESCTRHMACNewKeyMultipleTimes(t *testing.T) {
 	keyTemplate := aead.AES128CTRHMACSHA256KeyTemplate()
 	aeadKeyFormat := new(ctrhmacpb.AesCtrHmacAeadKeyFormat)
 	if err := proto.Unmarshal(keyTemplate.Value, aeadKeyFormat); err != nil {
@@ -65,24 +68,142 @@ func TestNewKeyMultipleTimes(t *testing.T) {
 	}
 }
 
-func TestNewKeyWithCorruptedFormat(t *testing.T) {
-	keyTemplate := new(tinkpb.KeyTemplate)
-
-	keyTemplate.TypeUrl = testutil.AESCTRHMACAEADTypeURL
-	keyTemplate.Value = make([]byte, 128)
-
+func TestAESCTRHMACNewKeyWithInvalidSerializedKeyFormat(t *testing.T) {
 	keyManager, err := registry.GetKeyManager(testutil.AESCTRHMACAEADTypeURL)
 	if err != nil {
 		t.Errorf("cannot obtain AES-CTR-HMAC-AEAD key manager: %s", err)
 	}
 
-	_, err = keyManager.NewKey(keyTemplate.Value)
-	if err == nil {
-		t.Error("NewKey got: success, want: error due to corrupted format")
+	testcases := []struct {
+		name                string
+		serializedKeyFormat []byte
+		keyFormat           *ctrhmacpb.AesCtrHmacAeadKeyFormat
+	}{
+		{
+			name:                "empty",
+			serializedKeyFormat: make([]byte, 128),
+		},
+		{
+			name: "params_unset",
+			keyFormat: &ctrhmacpb.AesCtrHmacAeadKeyFormat{
+				AesCtrKeyFormat: &ctrpb.AesCtrKeyFormat{
+					Params:  nil,
+					KeySize: 32,
+				},
+				HmacKeyFormat: &hmacpb.HmacKeyFormat{
+					Params:  nil,
+					KeySize: 32,
+				},
+			},
+		},
+		{
+			name: "nested_key_formats_unset",
+			keyFormat: &ctrhmacpb.AesCtrHmacAeadKeyFormat{
+				AesCtrKeyFormat: nil,
+				HmacKeyFormat:   nil,
+			},
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			serializedKeyFormat := tc.serializedKeyFormat
+			if serializedKeyFormat == nil {
+				var err error
+				serializedKeyFormat, err = proto.Marshal(tc.keyFormat)
+				if err != nil {
+					t.Fatalf("failed to marshal key format: %s", err)
+				}
+			}
+
+			_, err = keyManager.NewKey(serializedKeyFormat)
+			if err == nil {
+				t.Error("NewKey() err = nil, want not error")
+			}
+
+			_, err = keyManager.NewKeyData(serializedKeyFormat)
+			if err == nil {
+				t.Error("NewKeyData() err = nil, want error")
+			}
+		})
+	}
+}
+
+func TestAESCTRHMACPrimitive(t *testing.T) {
+	keyManager, err := registry.GetKeyManager(testutil.AESCTRHMACAEADTypeURL)
+	if err != nil {
+		t.Errorf("cannot obtain AES-CTR-HMAC-AEAD key manager: %s", err)
 	}
 
-	_, err = keyManager.NewKeyData(keyTemplate.Value)
-	if err == nil {
-		t.Error("NewKeyData got: success, want: error due to corrupted format")
+	key := &aeadpb.AesCtrHmacAeadKey{
+		Version: 0,
+		AesCtrKey: &ctrpb.AesCtrKey{
+			Version:  0,
+			KeyValue: make([]byte, 32),
+			Params:   &ctrpb.AesCtrParams{IvSize: 16},
+		},
+		HmacKey: &hmacpb.HmacKey{
+			Version:  0,
+			KeyValue: make([]byte, 32),
+			Params:   &hmacpb.HmacParams{Hash: commonpb.HashType_SHA256, TagSize: 32},
+		},
+	}
+	serializedKey, err := proto.Marshal(key)
+	if err != nil {
+		t.Fatalf("failed to marshal key: %s", err)
+	}
+
+	_, err = keyManager.Primitive(serializedKey)
+	if err != nil {
+		t.Errorf("Primitive() err = %v, want nil", err)
+	}
+}
+
+func TestAESCTRHMACPrimitiveWithInvalidKey(t *testing.T) {
+	keyManager, err := registry.GetKeyManager(testutil.AESCTRHMACAEADTypeURL)
+	if err != nil {
+		t.Errorf("cannot obtain AES-CTR-HMAC-AEAD key manager: %s", err)
+	}
+
+	testcases := []struct {
+		name string
+		key  *ctrhmacpb.AesCtrHmacAeadKey
+	}{
+		{
+			name: "nil_nested_keys",
+			key: &aeadpb.AesCtrHmacAeadKey{
+				Version:   0,
+				AesCtrKey: nil,
+				HmacKey:   nil,
+			},
+		},
+		{
+			name: "nil_key_params",
+			key: &aeadpb.AesCtrHmacAeadKey{
+				Version: 0,
+				AesCtrKey: &ctrpb.AesCtrKey{
+					Version:  0,
+					KeyValue: make([]byte, 32),
+					Params:   nil,
+				},
+				HmacKey: &hmacpb.HmacKey{
+					Version:  0,
+					KeyValue: make([]byte, 32),
+					Params:   nil,
+				},
+			},
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			serializedKey, err := proto.Marshal(tc.key)
+			if err != nil {
+				t.Fatalf("failed to marshal key: %s", err)
+			}
+
+			_, err = keyManager.Primitive(serializedKey)
+			if err == nil {
+				t.Error("Primitive() err = nil, want error")
+			}
+		})
 	}
 }
