fe1aa7501745d9d1c11ccb9ad32f9c5fe5f7f8de
juerg <juerg@google.com>
2023-12-19 05:43:11 -0800

    Add test for encrypted keyset serialization overhead in C++.
    PiperOrigin-RevId: 592207449

--- cc/core/binary_keyset_writer_test.cc
+++ cc/core/binary_keyset_writer_test.cc
@@ -22,17 +22,33 @@
 #include <string>
 #include <utility>
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "absl/memory/memory.h"
+#include "absl/status/status.h"
+#include "tink/aead.h"
+#include "tink/aead/aead_config.h"
+#include "tink/aead_key_templates.h"
+#include "tink/config/global_registry.h"
+#include "tink/insecure_secret_key_access.h"
+#include "tink/keyset_handle.h"
+#include "tink/proto_keyset_format.h"
+#include "tink/util/secret_data.h"
+#include "tink/util/statusor.h"
+#include "tink/util/test_matchers.h"
 #include "tink/util/test_util.h"
 #include "proto/tink.pb.h"
 
 using crypto::tink::test::AddRawKey;
 using crypto::tink::test::AddTinkKey;
 
+using ::crypto::tink::test::IsOk;
 using google::crypto::tink::EncryptedKeyset;
 using google::crypto::tink::KeyData;
 using google::crypto::tink::Keyset;
 using google::crypto::tink::KeyStatusType;
+using testing::Le;
+using testing::SizeIs;
 
 namespace crypto {
 namespace tink {
@@ -41,6 +57,8 @@ namespace {
 class BinaryKeysetWriterTest : public ::testing::Test {
  protected:
   void SetUp() override {
+    ASSERT_THAT(AeadConfig::Register(), IsOk());
+
     Keyset::Key key;
     AddTinkKey("some key type", 42, key, KeyStatusType::ENABLED,
                KeyData::SYMMETRIC, &keyset_);
@@ -120,6 +138,45 @@ TEST_F(BinaryKeysetWriterTest, testDestinationStreamErrors) {
     EXPECT_FALSE(status.ok()) << status;
     EXPECT_EQ(absl::StatusCode::kUnknown, status.code());
   }
+}
+
+TEST_F(BinaryKeysetWriterTest, EncryptedKeysetOverhead) {
+  util::StatusOr<std::unique_ptr<KeysetHandle>> keysetEncryptionHandle =
+      KeysetHandle::GenerateNew(AeadKeyTemplates::Aes128Gcm(),
+                                KeyGenConfigGlobalRegistry());
+  ASSERT_THAT(keysetEncryptionHandle, IsOk());
+  util::StatusOr<std::unique_ptr<Aead>> keyset_encryption_aead =
+      (*keysetEncryptionHandle)->GetPrimitive<Aead>(ConfigGlobalRegistry());
+  ASSERT_THAT(keyset_encryption_aead, IsOk());
+
+  util::StatusOr<std::unique_ptr<KeysetHandle>> handle =
+      KeysetHandle::GenerateNew(AeadKeyTemplates::Aes128Gcm(),
+                                KeyGenConfigGlobalRegistry());
+  ASSERT_THAT(handle, IsOk());
+
+  crypto::tink::util::StatusOr<util::SecretData> serialized_keyset =
+      SerializeKeysetToProtoKeysetFormat(**handle,
+                                         InsecureSecretKeyAccess::Get());
+  ASSERT_THAT(serialized_keyset, IsOk());
+  util::StatusOr<std::string> raw_encrypted_keyset =
+      (*keyset_encryption_aead)
+          ->Encrypt(util::SecretDataAsStringView(*serialized_keyset), "");
+  ASSERT_THAT(raw_encrypted_keyset, IsOk());
+
+  std::stringbuf encrypted_keyset;
+  crypto::tink::util::StatusOr<std::unique_ptr<BinaryKeysetWriter>> writer =
+      BinaryKeysetWriter::New(
+          absl::make_unique<std::ostream>(&encrypted_keyset));
+  ASSERT_THAT(writer, IsOk());
+
+  auto status = (*handle)->Write(writer->get(), **keyset_encryption_aead);
+  ASSERT_THAT(status, IsOk());
+
+  // encrypted_keyset is a serialized protocol buffer that only contains
+  // raw_encrypted_keyset in a field. So it should only be slightly larger than
+  // raw_encrypted_keyset.
+  EXPECT_THAT(encrypted_keyset.str(),
+              SizeIs(Le(raw_encrypted_keyset->size() + 6)));
 }
 
 }  // namespace
