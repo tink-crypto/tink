642def5fb5d80d636bd24c8e8d47ed4f1a7e8d84
juerg <juerg@google.com>
2023-12-20 01:50:37 -0800

    Add test vector to tink.proto_keyset_format test.
    PiperOrigin-RevId: 592485980

--- python/tink/_proto_keyset_format_test.py
+++ python/tink/_proto_keyset_format_test.py
@@ -22,6 +22,7 @@ import tink
 from tink import aead
 from tink import core
 from tink import hybrid
+from tink import mac
 from tink import secret_key_access
 from tink import tink_config
 
@@ -219,6 +220,60 @@ class TinkProtoKeysetFormatTest(absltest.TestCase):
     ciphertext = primitive1.encrypt(plaintext, associated_data)
     primitive2 = parsed_keyset_handle.primitive(aead.Aead)
     self.assertEqual(primitive2.decrypt(ciphertext, associated_data), plaintext)
+
+  def test_parse_keyset_from_test_vector(self):
+    # Same as in TinkProtoKeysetFormatTest.parseKeysetFromTestVector.
+    # It contains one HMAC key.
+    serialized_keyset = bytes.fromhex(
+        '0895e59bcc0612680a5c0a2e747970652e676f6f676c65617069732e636f6d2f676f6f'
+        '676c652e63727970746f2e74696e6b2e486d61634b657912281a20cca20f02278003b3'
+        '513f5d01759ac1302f7d883f2f4a40025532ee1b11f9e5871204101008031801100118'
+        '95e59bcc062001'
+    )
+    parsed_keyset_handle = tink.proto_keyset_format.parse(
+        serialized_keyset, secret_key_access.TOKEN
+    )
+    primitive = parsed_keyset_handle.primitive(mac.Mac)
+    valid_mac = bytes.fromhex('016986f2956092d259136923c6f4323557714ec499')
+    data = b'data'
+    primitive.verify_mac(valid_mac, data)
+
+  def test_parse_encrypted_keyset_from_test_vector(self):
+    # Same as in TinkProtoKeysetFormatTest.parseEncryptedKeysetFromTestVector.
+    # An AEAD key, with which we encrypted the mac keyset below.
+    serialized_keyset_encryption_keyset = bytes.fromhex(
+        '08b891f5a20412580a4c0a30747970652e676f6f676c65617069732e636f6d2f676f6f'
+        '676c652e63727970746f2e74696e6b2e4165734561784b65791216120208101a10e5d7'
+        'd0cdd649e81e7952260689b2e1971801100118b891f5a2042001'
+    )
+    keyset_encryption_handle = tink.proto_keyset_format.parse(
+        serialized_keyset_encryption_keyset, secret_key_access.TOKEN
+    )
+    keyset_encryption_aead = keyset_encryption_handle.primitive(aead.Aead)
+
+    # A keyset that contains one HMAC key, encrypted with the above, using
+    # associatedData
+    encrypted_serialized_keyset = bytes.fromhex(
+        '12950101445d48b8b5f591efaf73a46df9ebd7b6ac471cc0cf4f815a4f012fcaffc8f0'
+        'b2b10b30c33194f0b291614bd8e1d2e80118e5d6226b6c41551e104ef8cd8ee20f1c14'
+        'c1b87f6eed5fb04a91feafaacbf6f368519f36f97f7d08b24c8e71b5e620c4f69615ef'
+        '0479391666e2fb32e46b416893fc4e564ba927b22ebff2a77bd3b5b8d5afa162cbd35c'
+        '94c155cdfa13c8a9c964cde21a4208f5909ce901123a0a2e747970652e676f6f676c65'
+        '617069732e636f6d2f676f6f676c652e63727970746f2e74696e6b2e486d61634b6579'
+        '100118f5909ce9012001'
+    )
+    associated_data = bytes.fromhex('abcdef330012')
+
+    handle = tink.proto_keyset_format.parse_encrypted(
+        encrypted_serialized_keyset,
+        keyset_encryption_aead,
+        associated_data,
+    )
+
+    primitive = handle.primitive(mac.Mac)
+    message = b''
+    tag = bytes.fromhex('011d270875989dd6fbd5f54dbc9520bb41efd058d5')
+    primitive.verify_mac(tag, message)
 
   def test_encrypted_keyset_overhead(self):
     keyset_encryption_aead = tink.new_keyset_handle(
