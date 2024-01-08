470d07eac2206fc03b000d3271095d41389984fa
tholenst <tholenst@google.com>
2024-01-08 05:06:52 -0800

    Inline methods from HmacKeyManager.
    We will remove/deprecate the HmacKeyManager constructor. Hence we inline the usages of methods from there in this manager.
    
    PiperOrigin-RevId: 596559344

--- java_src/src/main/java/com/google/crypto/tink/aead/internal/LegacyAesCtrHmacTestKeyManager.java
+++ java_src/src/main/java/com/google/crypto/tink/aead/internal/LegacyAesCtrHmacTestKeyManager.java
@@ -20,18 +20,23 @@ import com.google.crypto.tink.Aead;
 import com.google.crypto.tink.KeyManager;
 import com.google.crypto.tink.Mac;
 import com.google.crypto.tink.Registry;
-import com.google.crypto.tink.mac.HmacKeyManager;
 import com.google.crypto.tink.proto.AesCtrHmacAeadKey;
 import com.google.crypto.tink.proto.AesCtrKey;
 import com.google.crypto.tink.proto.AesCtrParams;
+import com.google.crypto.tink.proto.HashType;
+import com.google.crypto.tink.proto.HmacKey;
+import com.google.crypto.tink.proto.HmacParams;
 import com.google.crypto.tink.proto.KeyData;
 import com.google.crypto.tink.subtle.AesCtrJceCipher;
 import com.google.crypto.tink.subtle.EncryptThenAuthenticate;
+import com.google.crypto.tink.subtle.PrfHmacJce;
+import com.google.crypto.tink.subtle.PrfMac;
 import com.google.crypto.tink.subtle.Validators;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.ExtensionRegistryLite;
 import com.google.protobuf.InvalidProtocolBufferException;
 import java.security.GeneralSecurityException;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Test helper key manager emulating a user that has a custom old KeyManager implementation with a
@@ -41,6 +46,70 @@ public class LegacyAesCtrHmacTestKeyManager implements KeyManager<Aead> {
 
   private static final String TYPE_URL = "type.googleapis.com/custom.AesCtrHmacAeadKey";
   private static final int MIN_AES_CTR_IV_SIZE_IN_BYTES = 12;
+
+  private static void validateHmacParams(HmacParams params) throws GeneralSecurityException {
+    if (params.getTagSize() < 10) {
+      throw new GeneralSecurityException("tag size too small");
+    }
+    switch (params.getHash()) {
+      case SHA1:
+        if (params.getTagSize() > 20) {
+          throw new GeneralSecurityException("tag size too big");
+        }
+        break;
+      case SHA224:
+        if (params.getTagSize() > 28) {
+          throw new GeneralSecurityException("tag size too big");
+        }
+        break;
+      case SHA256:
+        if (params.getTagSize() > 32) {
+          throw new GeneralSecurityException("tag size too big");
+        }
+        break;
+      case SHA384:
+        if (params.getTagSize() > 48) {
+          throw new GeneralSecurityException("tag size too big");
+        }
+        break;
+      case SHA512:
+        if (params.getTagSize() > 64) {
+          throw new GeneralSecurityException("tag size too big");
+        }
+        break;
+      default:
+        throw new GeneralSecurityException("unknown hash type");
+    }
+  }
+
+  private static void validateHmacKey(HmacKey key) throws GeneralSecurityException {
+    Validators.validateVersion(key.getVersion(), 0);
+    if (key.getKeyValue().size() < 16) {
+      throw new GeneralSecurityException("key too short");
+    }
+    validateHmacParams(key.getParams());
+  }
+
+  private static Mac getMacPrimitive(HmacKey key) throws GeneralSecurityException {
+    HashType hash = key.getParams().getHash();
+    byte[] keyValue = key.getKeyValue().toByteArray();
+    SecretKeySpec keySpec = new SecretKeySpec(keyValue, "HMAC");
+    int tagSize = key.getParams().getTagSize();
+    switch (hash) {
+      case SHA1:
+        return new PrfMac(new PrfHmacJce("HMACSHA1", keySpec), tagSize);
+      case SHA224:
+        return new PrfMac(new PrfHmacJce("HMACSHA224", keySpec), tagSize);
+      case SHA256:
+        return new PrfMac(new PrfHmacJce("HMACSHA256", keySpec), tagSize);
+      case SHA384:
+        return new PrfMac(new PrfHmacJce("HMACSHA384", keySpec), tagSize);
+      case SHA512:
+        return new PrfMac(new PrfHmacJce("HMACSHA512", keySpec), tagSize);
+      default:
+        throw new GeneralSecurityException("unknown hash");
+    }
+  }
 
   @Override
   public Aead getPrimitive(ByteString serializedKey) throws GeneralSecurityException {
@@ -55,7 +124,7 @@ public class LegacyAesCtrHmacTestKeyManager implements KeyManager<Aead> {
         new AesCtrJceCipher(
             keyProto.getAesCtrKey().getKeyValue().toByteArray(),
             keyProto.getAesCtrKey().getParams().getIvSize()),
-        new HmacKeyManager().getPrimitive(keyProto.getHmacKey(), Mac.class),
+        getMacPrimitive(keyProto.getHmacKey()),
         keyProto.getHmacKey().getParams().getTagSize());
   }
 
@@ -73,7 +142,7 @@ public class LegacyAesCtrHmacTestKeyManager implements KeyManager<Aead> {
     }
 
     // Validate HmacKey.
-    new HmacKeyManager().validateKey(key.getHmacKey());
+    validateHmacKey(key.getHmacKey());
   }
 
   @Override
