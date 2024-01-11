7cfd2c5adbb6ba07b55ebf25ceefd381b4db85f5
wiktorg <wiktorg@google.com>
2024-01-11 03:56:05 -0800

    Make CallWithCoreDumpProtection C++14 compatible
    Add a basic test.
    
    PiperOrigin-RevId: 597510458

--- cc/internal/call_with_core_dump_protection_test.cc
+++ cc/internal/call_with_core_dump_protection_test.cc
@@ -1,4 +1,4 @@
-// Copyright 2023 Google LLC
+// Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,27 +13,30 @@
 // limitations under the License.
 //
 ///////////////////////////////////////////////////////////////////////////////
+#include "tink/internal/call_with_core_dump_protection.h"
 
-#ifndef TINK_INTERNAL_CALL_WITH_CORE_DUMP_PROTECTION_H_
-#define TINK_INTERNAL_CALL_WITH_CORE_DUMP_PROTECTION_H_
+#include "gtest/gtest.h"
 
 namespace crypto {
 namespace tink {
 namespace internal {
+namespace {
 
-// Just a stub.
-// Internally we have great control over core dump collection and use this
-// function to redact execution state (e.g. CPU register values) of sensitive
-// crypto operations.
-// If you are interested in implementing it for your platform, open a GitHub
-// issue.
-template <typename Func>
-auto CallWithCoreDumpProtection(Func&& func) -> decltype(func()) {
-  return func();
+TEST(CallWithCoreDumpProtectionTest, Basic) {
+  EXPECT_EQ(CallWithCoreDumpProtection([]() {
+    return 1 + 2;
+  }), 3);
 }
 
+TEST(CallWithCoreDumpProtectionTest, WithCapture) {
+  int a = 10;
+  int b = 20;
+  EXPECT_EQ(CallWithCoreDumpProtection([&]() {
+    return a + b;
+  }), a + b);
+}
+
+}  // namespace
 }  // namespace internal
 }  // namespace tink
 }  // namespace crypto
-
-#endif  // TINK_INTERNAL_CALL_WITH_CORE_DUMP_PROTECTION_H_
