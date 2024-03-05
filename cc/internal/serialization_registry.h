100d3c5f34cb650d879c1b55cad7927d4c919f7b
Tink Team <tink-dev@google.com>
2024-03-05 01:19:17 -0800

    Automated Code Change
    PiperOrigin-RevId: 612745098

--- cc/internal/serialization_registry.h
+++ cc/internal/serialization_registry.h
@@ -82,12 +82,12 @@ class SerializationRegistry {
                 parameters_parsers,
             const absl::flat_hash_map<SerializerIndex, ParametersSerializer*>&
                 parameters_serializers,
-            const absl::flat_hash_map<ParserIndex, KeyParser*> key_parsers,
+            absl::flat_hash_map<ParserIndex, KeyParser*> key_parsers,
             const absl::flat_hash_map<SerializerIndex, KeySerializer*>
                 key_serializers)
         : parameters_parsers_(parameters_parsers),
           parameters_serializers_(parameters_serializers),
-          key_parsers_(key_parsers),
+          key_parsers_(std::move(key_parsers)),
           key_serializers_(key_serializers) {}
 
     absl::flat_hash_map<ParserIndex, ParametersParser*> parameters_parsers_;
