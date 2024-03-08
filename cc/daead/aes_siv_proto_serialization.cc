b6c2f2c970acdbcaec92caf145383b20cf5ed0a7
Tink Team <tink-dev@google.com>
2024-03-08 00:17:27 -0800

    Automated Code Change
    PiperOrigin-RevId: 613838468

--- cc/daead/aes_siv_proto_serialization.cc
+++ cc/daead/aes_siv_proto_serialization.cc
@@ -144,8 +144,7 @@ util::StatusOr<AesSivKey> ParseKey(
   google::crypto::tink::AesSivKey proto_key;
   RestrictedData restricted_data = serialization.SerializedKeyProto();
   // OSS proto library complains if input is not converted to a string.
-  if (!proto_key.ParseFromString(
-          std::string(restricted_data.GetSecret(*token)))) {
+  if (!proto_key.ParseFromString(restricted_data.GetSecret(*token))) {
     return util::Status(absl::StatusCode::kInvalidArgument,
                         "Failed to parse AesSivKey proto");
   }
--- cc/daead/aes_siv_proto_serialization_test.cc
+++ cc/daead/aes_siv_proto_serialization_test.cc
@@ -372,9 +372,9 @@ TEST_P(AesSivProtoSerializationTest, SerializeKey) {
 
   google::crypto::tink::AesSivKey proto_key;
   // OSS proto library complains if input is not converted to a string.
-  ASSERT_THAT(proto_key.ParseFromString(std::string(
+  ASSERT_THAT(proto_key.ParseFromString(
                   proto_serialization->SerializedKeyProto().GetSecret(
-                      InsecureSecretKeyAccess::Get()))),
+                      InsecureSecretKeyAccess::Get())),
               IsTrue());
   EXPECT_THAT(proto_key.key_value().size(), Eq(test_case.key_size));
 }
