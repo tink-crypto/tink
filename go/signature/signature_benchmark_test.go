64ea42026570d3aa33c97f3e379b53ae345084e7
juerg <juerg@google.com>
2024-03-11 02:27:11 -0700

    Add Benchmark tests for signatures in Golang.
    Because the performance of signing and verifying can differ a lot, we keep
    the separate.
    
    Output on a Xeon CPU:
    BenchmarkSign/RSA_SSA_PKCS1_3072-8                   484           2423003 ns/op             980 B/op          5 allocs/op
    BenchmarkSign/RSA_SSA_PSS_3072-8                     500           2758965 ns/op             984 B/op          5 allocs/op
    BenchmarkSign/RSA_SSA_PKCS1_4096-8                   223           5363075 ns/op            1444 B/op          5 allocs/op
    BenchmarkSign/RSA_SSA_PSS_4096-8                     224           5276641 ns/op            1448 B/op          5 allocs/op
    BenchmarkSign/ECDSA_P256-8                         13022             92094 ns/op            1131 B/op         30 allocs/op
    BenchmarkSign/ECDSA_P384-8                          1501            812957 ns/op            1456 B/op         30 allocs/op
    BenchmarkSign/ECDSA_P521-8                           579           1953952 ns/op            1657 B/op         29 allocs/op
    BenchmarkSign/ED25519-8                            10000            114443 ns/op             944 B/op          8 allocs/op
    BenchmarkVerify/RSA_SSA_PKCS1_3072-8               10000            100497 ns/op             176 B/op          2 allocs/op
    BenchmarkVerify/RSA_SSA_PSS_3072-8                 10000            105632 ns/op             176 B/op          2 allocs/op
    BenchmarkVerify/RSA_SSA_PKCS1_4096-8               10000            117100 ns/op             352 B/op          2 allocs/op
    BenchmarkVerify/RSA_SSA_PSS_4096-8                  8594            117292 ns/op             353 B/op          2 allocs/op
    BenchmarkVerify/ECDSA_P256-8                        8449            145441 ns/op            1606 B/op         47 allocs/op
    BenchmarkVerify/ECDSA_P384-8                        1322            832999 ns/op            1876 B/op         45 allocs/op
    BenchmarkVerify/ECDSA_P521-8                         594           2046539 ns/op            2150 B/op         46 allocs/op
    BenchmarkVerify/ED25519-8                           9547            127274 ns/op             352 B/op          2 allocs/op
    
    PiperOrigin-RevId: 614587834

---
+++ go/signature/signature_benchmark_test.go
@@ -0,0 +1,123 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// //////////////////////////////////////////////////////////////////////////////
+
+package signature_test
+
+import (
+	"testing"
+
+	"github.com/google/tink/go/keyset"
+	"github.com/google/tink/go/signature"
+	"github.com/google/tink/go/subtle/random"
+	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
+)
+
+// Benchmarks for Signature algorithms.
+
+const benchmarkDataSize = 16 * 1024
+
+var benchmarkTestCases = []struct {
+	name     string
+	template *tinkpb.KeyTemplate
+}{
+	{
+		name:     "RSA_SSA_PKCS1_3072",
+		template: signature.RSA_SSA_PKCS1_3072_SHA256_F4_Key_Template(),
+	}, {
+		name:     "RSA_SSA_PSS_3072",
+		template: signature.RSA_SSA_PSS_3072_SHA256_32_F4_Key_Template(),
+	}, {
+		name:     "RSA_SSA_PKCS1_4096",
+		template: signature.RSA_SSA_PKCS1_4096_SHA512_F4_Key_Template(),
+	}, {
+		name:     "RSA_SSA_PSS_4096",
+		template: signature.RSA_SSA_PSS_4096_SHA512_64_F4_Key_Template(),
+	}, {
+		name:     "ECDSA_P256",
+		template: signature.ECDSAP256KeyTemplate(),
+	}, {
+		name:     "ECDSA_P384",
+		template: signature.ECDSAP384SHA384KeyTemplate(),
+	}, {
+		name:     "ECDSA_P521",
+		template: signature.ECDSAP521KeyTemplate(),
+	}, {
+		name:     "ED25519",
+		template: signature.ED25519KeyTemplate(),
+	},
+}
+
+func BenchmarkSign(b *testing.B) {
+	for _, tc := range benchmarkTestCases {
+		b.Run(tc.name, func(b *testing.B) {
+			b.ReportAllocs()
+
+			handle, err := keyset.NewHandle(tc.template)
+			if err != nil {
+				b.Fatal(err)
+			}
+			primitive, err := signature.NewSigner(handle)
+			if err != nil {
+				b.Fatal(err)
+			}
+			data := random.GetRandomBytes(benchmarkDataSize)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_, err = primitive.Sign(data)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkVerify(b *testing.B) {
+	for _, tc := range benchmarkTestCases {
+		b.Run(tc.name, func(b *testing.B) {
+			b.ReportAllocs()
+
+			handle, err := keyset.NewHandle(tc.template)
+			if err != nil {
+				b.Fatal(err)
+			}
+			signer, err := signature.NewSigner(handle)
+			if err != nil {
+				b.Fatal(err)
+			}
+			data := random.GetRandomBytes(benchmarkDataSize)
+			sig, err := signer.Sign(data)
+			if err != nil {
+				b.Fatal(err)
+			}
+			publicHandle, err := handle.Public()
+			if err != nil {
+				b.Fatal(err)
+			}
+			primitive, err := signature.NewVerifier(publicHandle)
+			if err != nil {
+				b.Fatal(err)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				err = primitive.Verify(sig, data)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
